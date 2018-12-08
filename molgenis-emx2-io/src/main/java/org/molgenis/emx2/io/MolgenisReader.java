024479288223bb01a6e234ea5669ccc74dc790aa
mswertz <m.a.swertz@rug.nl>
2018-12-08 21:50:28 +0100

    removed sonar warnings

--- molgenis-emx2-io/src/main/java/org/molgenis/emx2/io/MolgenisReader.java
+++ molgenis-emx2-io/src/main/java/org/molgenis/emx2/io/MolgenisReader.java
@@ -7,7 +7,6 @@ import org.molgenis.emx2.io.format.MolgenisFileRow;
 import org.molgenis.emx2.io.parsers.ColumnDefinition;
 import org.molgenis.emx2.EmxModel;
 import org.molgenis.emx2.EmxType;
-import org.molgenis.emx2.beans.EmxColumnBean;
 import org.molgenis.emx2.beans.EmxModelBean;
 import org.molgenis.emx2.beans.EmxTableBean;
 import org.molgenis.emx2.io.parsers.ColumnDefinitionParser;
@@ -57,43 +56,40 @@ public class MolgenisReader {
             line++;
 
             String tableName = row.getTable();
+            String columnName = row.getColumn();
 
             if(tableName != null)
             {
+                tables.computeIfAbsent(tableName, k -> new EmxTableBean(tableName));
                 EmxTableBean table = tables.get(tableName);
-                if( table == null) {
-                    table = new EmxTableBean(tableName);
-                    tables.put(tableName, table);
-                }
 
-                String columnName = row.getColumn();
                 //if table != null and column== null, this definition is for a table
-                if(columnName == null) {
-
-                }
                 //if table!= null and column != null, this definition if for a column
-                else {
+                if(columnName != null) {
                     if(table.getColumn(columnName) != null) {
-                        throw new RuntimeException("error on line "+line+": duplicate column definition table='"+tableName+"', column='"+columnName+"'");
+                        throw new MolgenisReaderException("error on line "+line+": duplicate column definition table='"+tableName+"', column='"+columnName+"'");
                     }
-
                     List<ColumnDefinition> terms = new ColumnDefinitionParser().parse(line, messages, row.getDefinition());
-                    //get type
-                    EmxType type = EmxType.STRING;
-                    for(ColumnDefinition term: terms) {
-                        try {
-                            type = EmxType.valueOf(term.name());
-                        } catch(Exception e) {
-                            //no problem,irrelevant term.
-                        }
-                    }
-                    EmxColumnBean column = table.addColumn(columnName, type);
 
+                    table.addColumn(columnName, getType(terms));
                 }
             }
         }
 
         return new EmxModelBean(tables);
+    }
+
+    private EmxType getType(List<ColumnDefinition> terms) {
+        //get type
+        EmxType type = EmxType.STRING;
+        for(ColumnDefinition term: terms) {
+            try {
+                type = EmxType.valueOf(term.name());
+            } catch(Exception e) {
+                //no problem,irrelevant term.
+            }
+        }
+        return type;
     }
 
 }
