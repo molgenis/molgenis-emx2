d70412f87bf82583f4c70b9089179462a2d4cbdd
mswertz <m.a.swertz@rug.nl>
2019-09-28 22:52:26 +0200

    simplifications

--- molgenis-emx2-io/src/main/java/org/molgenis/emx2/io/stores/RowStoreForXlsxFile.java
+++ molgenis-emx2-io/src/main/java/org/molgenis/emx2/io/stores/RowStoreForXlsxFile.java
@@ -1,6 +1,5 @@
 package org.molgenis.emx2.io.stores;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.molgenis.emx2.Row;
@@ -26,7 +25,7 @@ public class RowStoreForXlsxFile implements RowStore {
 
   @Override
   public void write(String name, List<Row> rows) throws IOException {
-    if (rows.size() == 0) return;
+    if (rows.isEmpty()) return;
 
     if (name.length() > 30)
       throw new IOException("Excel sheet name '" + name + "' is too long. Maximum 30 characters");
@@ -53,38 +52,38 @@ public class RowStoreForXlsxFile implements RowStore {
 
   private void writeRowsToSheet(String name, List<Row> rows, Workbook wb) {
 
-      Sheet sheet = wb.createSheet(name);
-      Map<String, Integer> columNames = new LinkedHashMap<>();
-      int rowNum = 0;
-      for (Row row : rows) {
-        // create header row
-        if (rowNum == 0) {
-          int columnIndex = 0;
-          // define the column indexes
-          for (String columnName : row.getColumnNames()) {
-            columNames.put(columnName, columnIndex++);
-          }
-          // write a header row
-          org.apache.poi.ss.usermodel.Row excelRow = sheet.createRow(rowNum);
-          for (Map.Entry<String, Integer> entry : columNames.entrySet()) {
-            excelRow.createCell(entry.getValue()).setCellValue(entry.getKey());
-          }
-          rowNum++;
+    Sheet sheet = wb.createSheet(name);
+    Map<String, Integer> columNames = new LinkedHashMap<>();
+    int rowNum = 0;
+    for (Row row : rows) {
+      // create header row
+      if (rowNum == 0) {
+        int columnIndex = 0;
+        // define the column indexes
+        for (String columnName : row.getColumnNames()) {
+          columNames.put(columnName, columnIndex++);
         }
-        // write a contents row
+        // write a header row
         org.apache.poi.ss.usermodel.Row excelRow = sheet.createRow(rowNum);
-        for (String columnName : columNames.keySet()) {
-          excelRow.createCell(columNames.get(columnName)).setCellValue(row.getString(columnName));
+        for (Map.Entry<String, Integer> entry : columNames.entrySet()) {
+          excelRow.createCell(entry.getValue()).setCellValue(entry.getKey());
         }
         rowNum++;
       }
+      // write a contents row
+      org.apache.poi.ss.usermodel.Row excelRow = sheet.createRow(rowNum);
+      for (Map.Entry<String, Integer> entry : columNames.entrySet()) {
+        excelRow.createCell(entry.getValue()).setCellValue(row.getString(entry.getKey()));
+      }
+      rowNum++;
+    }
   }
 
   @Override
   public List<Row> read(String name) throws IOException {
     List<Row> result = new ArrayList<>();
-    try (InputStream inp = new FileInputStream(excelFilePath.toFile())) {
-      Workbook wb = WorkbookFactory.create(inp);
+    try (InputStream inp = new FileInputStream(excelFilePath.toFile());
+        Workbook wb = WorkbookFactory.create(inp)) {
       Sheet sheet = wb.getSheet(name);
       if (sheet == null) throw new IOException("Sheet '" + name + "' not found in Excel file");
 
@@ -102,43 +101,7 @@ public class RowStoreForXlsxFile implements RowStore {
         }
         // otherwise it is a normal row to be added to result
         else {
-          Row row = new Row();
-          for (Cell cell : excelRow) {
-            String colName = columnNames.get(cell.getColumnIndex());
-            if (colName == null)
-              throw new IOException(
-                  "Read of "
-                      + name
-                      + " failed: column index "
-                      + cell.getColumnIndex()
-                      + " has no column name");
-
-            switch (cell.getCellType()) {
-              case STRING:
-                row.set(colName, cell.getRichStringCellValue().getString());
-                break;
-              case NUMERIC:
-                if (DateUtil.isCellDateFormatted(cell)) {
-                  row.setDate(
-                      colName,
-                      cell.getDateCellValue()
-                          .toInstant()
-                          .atZone(ZoneId.systemDefault())
-                          .toLocalDate());
-                } else {
-                  row.setDecimal(colName, cell.getNumericCellValue());
-                }
-                break;
-              case BOOLEAN:
-                row.setBool(colName, cell.getBooleanCellValue());
-                break;
-              case FORMULA:
-                throw new IOException("formula's not supported");
-              case BLANK:
-                break;
-              default:
-            }
-          }
+          Row row = convertRow(name, columnNames, excelRow);
           result.add(row);
         }
       }
@@ -146,10 +109,50 @@ public class RowStoreForXlsxFile implements RowStore {
     }
   }
 
+  private Row convertRow(
+      String name, Map<Integer, String> columnNames, org.apache.poi.ss.usermodel.Row excelRow)
+      throws IOException {
+    Row row = new Row();
+    for (Cell cell : excelRow) {
+      String colName = columnNames.get(cell.getColumnIndex());
+      if (colName == null)
+        throw new IOException(
+            "Read of "
+                + name
+                + " failed: column index "
+                + cell.getColumnIndex()
+                + " has no column name");
+
+      switch (cell.getCellType()) {
+        case STRING:
+          row.set(colName, cell.getRichStringCellValue().getString());
+          break;
+        case NUMERIC:
+          if (DateUtil.isCellDateFormatted(cell)) {
+            row.setDate(
+                colName,
+                cell.getDateCellValue().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+          } else {
+            row.setDecimal(colName, cell.getNumericCellValue());
+          }
+          break;
+        case BOOLEAN:
+          row.setBool(colName, cell.getBooleanCellValue());
+          break;
+        case FORMULA:
+          throw new IOException("formula's not supported");
+        case BLANK:
+          break;
+        default:
+      }
+    }
+    return row;
+  }
+
   @Override
   public boolean containsTable(String name) throws IOException {
-    try (InputStream inp = new FileInputStream(excelFilePath.toFile())) {
-      Workbook wb = WorkbookFactory.create(inp);
+    try (InputStream inp = new FileInputStream(excelFilePath.toFile());
+        Workbook wb = WorkbookFactory.create(inp)) {
       if (wb.getSheet(name) != null) return true;
     }
     return false;
