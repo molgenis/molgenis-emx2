e72d151c18282d77b76b28239ff977c6257fbb48
mswertz <m.a.swertz@rug.nl>
2019-12-09 23:48:01 +0100

    remove vulnerabilities by making fields final

--- molgenis-emx2/src/main/java/org/molgenis/emx2/Constants.java
+++ molgenis-emx2/src/main/java/org/molgenis/emx2/Constants.java
@@ -3,9 +3,9 @@ package org.molgenis.emx2;
 import static org.molgenis.emx2.Operator.*;
 
 public class Constants {
-  public static Operator[] ORDINAL_OPERATORS = {IS, IS_NOT, BETWEEN, NOT_BETWEEN};
-  public static Operator[] STRING_OPERATORS = {
+  protected static final Operator[] ORDINAL_OPERATORS = {IS, IS_NOT, BETWEEN, NOT_BETWEEN};
+  protected static final Operator[] STRING_OPERATORS = {
     IS, IS_NOT, CONTAINS, DOES_NOT_CONTAIN, TRIGRAM_MATCH, LEXICAL_MATCH
   };
-  public static Operator[] EQUALITY_OPERATORS = {IS, IS_NOT};
+  protected static final Operator[] EQUALITY_OPERATORS = {IS, IS_NOT};
 }
