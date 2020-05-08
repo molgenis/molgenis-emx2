3af750e07430b2080eed0bfdc3f820f66255e2f0
mswertz <m.a.swertz@rug.nl>
2020-05-09 00:04:11 +0200

    work in progress on tables

--- apps/tables/src/App.vue
+++ apps/tables/src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <Molgenis :menuItems="menuItems" :title="title" id="__top">
+    <ListTables />
+  </Molgenis>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { Molgenis } from "@mswertz/emx2-styleguide";
+import ListTables from "./components/ListTables";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Molgenis,
+    ListTables
+  },
+  data() {
+    return {
+      menuItems: [{ label: "Home", href: "." }]
+    };
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
