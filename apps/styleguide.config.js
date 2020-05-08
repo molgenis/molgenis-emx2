f389f24eda531e0ffa165e5c56c1de8d1dc01a27
mswertz <m.a.swertz@rug.nl>
2020-05-09 00:06:00 +0200

    fix: proxy setting for developer

--- apps/styleguide.config.js
+++ apps/styleguide.config.js
@@ -1,76 +1,73 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    // set your styleguidist configuration here
-    title: 'MOLGENIS EMX2 Style Guide',
-    ribbon: {
-        // Link to open on the ribbon click (required)
-        url: 'https://github.com/mswertz/molgenis-emx2-ui/',
-        // Text to show on the ribbon (optional)
-        text: 'Fork me on GitHub'
-    },
-    theme: {
-        color: {
-            ribbonBackground: 'black'
-        }
-    },
-    assetsDir: 'styleguide/public',
-    // here proxy
-    webpackConfig: {
-        devServer: {
-            proxy: {
-                '/api': 'http://localhost:8080'
-            },
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            }
-        }
-    },
-    exampleMode: 'collapse',
-    template: {
-        head: {
-            links: [
-                {
-                    rel: 'stylesheet',
-                    href:
-                        'https://fonts.googleapis.com/css?family=Oswald:500|Roboto|Roboto+Mono&display=swap'
-                },
-                {
-                    rel: 'stylesheet',
-                    href:
-                    // 'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css'
-                        'assets/css/bootstrap-molgenis-blue.css'
-                },
-                {
-                    rel: 'stylesheet',
-                    href:
-                        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
-                }
-            ]
-        }
-    },
-    styleguideDir: '../docs/styleguide',
-    sections: [
+  // set your styleguidist configuration here
+  title: "MOLGENIS EMX2 Style Guide",
+  ribbon: {
+    // Link to open on the ribbon click (required)
+    url: "https://github.com/mswertz/molgenis-emx2-ui/",
+    // Text to show on the ribbon (optional)
+    text: "Fork me on GitHub"
+  },
+  theme: {
+    color: {
+      ribbonBackground: "black"
+    }
+  },
+  assetsDir: "styleguide/public",
+  // here proxy
+  webpackConfig: {
+    devServer: {
+      proxy: {
+        "/graphql": "http://localhost:8080/pet%20store"
+      }
+    }
+  },
+  exampleMode: "collapse",
+  template: {
+    head: {
+      links: [
         {
-            name: 'Introduction',
-            content: 'styleguide/src/styleguide/introduction.md'
+          rel: "stylesheet",
+          href:
+            "https://fonts.googleapis.com/css?family=Oswald:500|Roboto|Roboto+Mono&display=swap"
         },
         {
-            name: 'Components',
-            components: 'styleguide/src/components/[A-Z]*.vue',
-            content: 'styleguide/src/styleguide/components.md'
+          rel: "stylesheet",
+          href:
+            // 'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css'
+            "assets/css/bootstrap-molgenis-blue.css"
         },
         {
-            name: 'Molecules',
-            components: 'styleguide/src/molecules/[A-Z]*.vue'
-        },
-        {
-            name: 'Organisms',
-            components: 'styleguide/src/organisms/[A-Z]*.vue'
+          rel: "stylesheet",
+          href:
+            "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         }
-        // {
-        //   name: 'Pages',
-        //   components: 'src/components/pages/[A-Z]*.vue'
-        // }
-    ]
-}
+      ]
+    }
+  },
+  styleguideDir: "../docs/styleguide",
+  sections: [
+    {
+      name: "Introduction",
+      content: "styleguide/src/styleguide/introduction.md"
+    },
+    {
+      name: "Components",
+      components: "styleguide/src/components/[A-Z]*.vue",
+      content: "styleguide/src/styleguide/components.md"
+    },
+    {
+      name: "Molecules",
+      components: "styleguide/src/molecules/[A-Z]*.vue"
+    },
+    {
+      name: "Organisms",
+      components: "styleguide/src/organisms/[A-Z]*.vue"
+    }
+    // {
+    //   name: 'Pages',
+    //   components: 'src/components/pages/[A-Z]*.vue'
+    // }
+  ]
+};
