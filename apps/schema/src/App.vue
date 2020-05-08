2a0ba94227bbe98832edcc2fcd98a2f0a08f2329
mswertz <m.a.swertz@rug.nl>
2020-05-09 00:03:49 +0200

    fix: changed schema to work with local graphql

--- apps/schema/src/App.vue
+++ apps/schema/src/App.vue
@@ -1,37 +1,42 @@
 <template>
-    <div id="app">
-        <Molgenis :menuItems="menuItems" :title="title" id="__top">
-            <router-view/>
-        </Molgenis>
-    </div>
+  <div id="app">
+    <Molgenis :menuItems="menuItems" :title="title" id="__top">
+      <Schema />
+    </Molgenis>
+  </div>
 </template>
 
 <script>
-    import {
-        Molgenis
-    } from '@mswertz/emx2-styleguide'
+import { Molgenis } from '@mswertz/emx2-styleguide'
+import Schema from './components/Schema'
 
-    export default {
-        components: {
-            Molgenis
-        },
-        data: function () {
-            return {
-                schema: null
-            }
-        },
-        computed: {
-            title() {
-                return this.$route.params.schema + ': ' + this.$route.name;
-            },
-            menuItems() {
-                return [
-                    {label: 'Home', href: '#/' + this.$route.params.schema},
-                    {label: 'Design', href: '#/' + this.$route.params.schema + "/design/"},
-                    {label: 'Members', href: '#/' + this.$route.params.schema + "/members/"}
-
-                ]
-            }
-        }
+export default {
+  components: {
+    Schema,
+    Molgenis
+  },
+  data: function() {
+    return {
+      schema: null
     }
+  },
+  computed: {
+    title() {
+      return 'Schema' //todo get schema name from metadata
+    },
+    menuItems() {
+      return [
+        { label: 'Home', href: '../home' },
+        {
+          label: 'Design',
+          href: '../design'
+        },
+        {
+          label: 'Members',
+          href: '../members'
+        }
+      ]
+    }
+  }
+}
 </script>
--- apps/schema/src/main.js
+++ apps/schema/src/main.js
@@ -1,25 +1,8 @@
 import Vue from 'vue'
-import SchemaDesign from './components/SchemaDesign'
-import SchemaView from './components/SchemaView'
-import NotFound from './components/NotFound'
-import VueRouter from 'vue-router'
-import App from "./App";
+import App from './App'
 
 Vue.config.productionTip = false
 
-const routes = [
-    {path: '/:schema/design', name: 'Design', component: SchemaDesign, props: true},
-    {path: '/:schema', name: 'Home', component: SchemaView, props: true},
-    {path: '*', name: 'NotFound', component: NotFound}]
-
-const router = new VueRouter({
-    routes
-})
-
-Vue.use(VueRouter)
-
 new Vue({
-    router,
-    render: h => h(App)
-}).$mount("#app");
-
+  render: h => h(App)
+}).$mount('#app')
