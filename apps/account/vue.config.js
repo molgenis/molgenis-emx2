a39de4b2787b832a77beb997dcb704ea70d14f66
mswertz <m.a.swertz@rug.nl>
2020-09-27 12:24:18 +0200

    chore: removed 'account' module

--- apps/account/vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  publicPath: '', // to ensure relative paths are used
-  devServer: {
-    port: '9090',
-    proxy: {
-      '^/api': { target: 'http://localhost:8080' }
-    }
-  }
-}
