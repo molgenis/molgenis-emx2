fe902cb7d6171c7442bf03a1e8a56cb5254455fd
mswertz <m.a.swertz@rug.nl>
2020-11-08 22:53:35 +0100

    fix: remove smells

--- backend/molgenis-emx2-linkeddata/src/main/java/org/molgenis/emx2/linkeddata/LinkedDataService.java
+++ backend/molgenis-emx2-linkeddata/src/main/java/org/molgenis/emx2/linkeddata/LinkedDataService.java
@@ -1,7 +1,6 @@
 package org.molgenis.emx2.linkeddata;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.jooq.JSON;
 import org.molgenis.emx2.*;
 import org.molgenis.emx2.utils.JavaScriptUtils;
 
@@ -14,61 +13,74 @@ import static org.molgenis.emx2.SelectColumn.s;
 
 public class LinkedDataService {
 
+  private LinkedDataService() {
+    // hidden
+  }
+
   // naive implementation, need some kind of paging in the future, and column selection
   public static void dump(Schema schema, PrintWriter writer) {
 
     // iterate through meta model
     for (Table table : schema.getTablesSorted()) {
-
-      // check if has any semantic stuff
-      if (table.getMetadata().getColumns().stream().anyMatch(c -> c.getRdfTemplate() != null)) {
-        // need two layer select
-        Query q = table.query();
-        for (Column c : table.getMetadata().getColumns()) {
-          SelectColumn s = s(c.getName());
-          if (c.isReference()) {
-            for (Column sub : c.getRefTable().getColumns()) {
-              s.select(sub.getName());
-            }
-          }
-          q.select(s);
-        }
-
-        try {
-          for (Map<String, Object> row :
-              (List<Map<String, Object>>)
-                  new ObjectMapper().readValue(q.retrieveJSON(), Map.class).get(table.getName())) {
-            StringBuffer buffer = new StringBuffer();
-            for (Column c : table.getMetadata().getColumns()) {
-              if (c.getRdfTemplate() != null && row.get(c.getName()) != null) {
-                if (c.isArray()) {
-                  for (Object sub : (List<Object>) row.get(c.getName())) {
-                    Map nest = new LinkedHashMap();
-                    nest.put(c.getName(), sub);
-                    buffer.append(
-                        JavaScriptUtils.executeJavascriptOnMap("`" + c.getRdfTemplate() + "`", nest)
-                            + "\n");
-                  }
-                } else {
-                  buffer.append(
-                      JavaScriptUtils.executeJavascriptOnMap("`" + c.getRdfTemplate() + "`", row)
-                          + "\n");
-                  // }
-                }
-              }
-            }
-            String subject = buffer.toString().trim();
-            if (subject.endsWith(";")) subject = subject.substring(0, subject.length() - 1) + ".";
-            writer.println(subject);
-          }
-        } catch (Exception e) {
-          throw new MolgenisException("rdf generation failed", e);
-        }
-      }
+      applySemanticDump(writer, table);
     }
     // query each table
 
     // write each column value that has the rdfTemplate property
 
+  }
+
+  private static void applySemanticDump(PrintWriter writer, Table table) {
+    // check if has any semantic stuff
+    if (table.getMetadata().getColumns().stream().anyMatch(c -> c.getRdfTemplate() != null)) {
+      // need two layer select
+      Query q = table.query();
+      for (Column c : table.getMetadata().getColumns()) {
+        SelectColumn s = s(c.getName());
+        if (c.isReference()) {
+          for (Column sub : c.getRefTable().getColumns()) {
+            s.select(sub.getName());
+          }
+        }
+        q.select(s);
+      }
+
+      try {
+        for (Map<String, Object> row :
+            (List<Map<String, Object>>)
+                new ObjectMapper().readValue(q.retrieveJSON(), Map.class).get(table.getName())) {
+          applyTemplateOnRow(writer, table, row);
+        }
+      } catch (Exception e) {
+        throw new MolgenisException("rdf generation failed", e);
+      }
+    }
+  }
+
+  private static void applyTemplateOnRow(PrintWriter writer, Table table, Map<String, Object> row) {
+    StringBuilder builder = new StringBuilder();
+    for (Column c : table.getMetadata().getColumns()) {
+      applyTemplateOnColumn(row, builder, c);
+    }
+    String subject = builder.toString().trim();
+    if (subject.endsWith(";")) subject = subject.substring(0, subject.length() - 1) + ".";
+    writer.println(subject);
+  }
+
+  private static void applyTemplateOnColumn(
+      Map<String, Object> row, StringBuilder builder, Column c) {
+    if (c.getRdfTemplate() != null && row.get(c.getName()) != null) {
+      if (c.isArray()) {
+        for (Object sub : (List<Object>) row.get(c.getName())) {
+          Map<String, Object> nest = new LinkedHashMap<>();
+          nest.put(c.getName(), sub);
+          builder.append(
+              JavaScriptUtils.executeJavascriptOnMap("`" + c.getRdfTemplate() + "`", nest) + "\n");
+        }
+      } else {
+        builder.append(
+            JavaScriptUtils.executeJavascriptOnMap("`" + c.getRdfTemplate() + "`", row) + "\n");
+      }
+    }
   }
 }
