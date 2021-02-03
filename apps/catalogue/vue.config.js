2e272745378bb4bc5d576020e71849861d4e79d0
connoratrug <c.stroomberg@rug.nl>
2021-02-03 16:15:38 +0100

    chore(apps/catalogue): setup dev proxy for use with remove test backend server

--- apps/catalogue/vue.config.js
+++ apps/catalogue/vue.config.js
@@ -2,12 +2,21 @@ module.exports = {
   publicPath: "", // to ensure relative paths are used
   devServer: {
     port: "9090",
+    // proxy: {
+    //   "^/graphql": {
+    //     target: "http://localhost:8080/CohortsCentral",
+    //   },
+    //   "/api": { target: "http://localhost:8080" },
+    //   "/apps": { target: "http://localhost:8080" },
+    // },
     proxy: {
-      "^/graphql": {
-        target: "http://localhost:8080/CohortsCentral",
+      '/graphql$': {
+        target: 'https://emx2-catalogue.test.molgenis.org/CohortsCentral',
+        ws: true,
+        changeOrigin: true
       },
-      "/api": { target: "http://localhost:8080" },
-      "/apps": { target: "http://localhost:8080" },
+      "/api": { target: "https://emx2.test.molgenis.org" },
+      "/apps": { target: "https://emx2.test.molgenis.org" },
     },
   },
 };
