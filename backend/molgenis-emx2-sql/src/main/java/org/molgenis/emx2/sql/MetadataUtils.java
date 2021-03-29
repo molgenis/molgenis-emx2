b55476411b6dc0517fd6d7d689938d32e56bc4fb
Morris Swertz <m.a.swertz@rug.nl>
2021-03-29 23:38:28 +0200

    chore(build): move lock to inside transaction

--- backend/molgenis-emx2-sql/src/main/java/org/molgenis/emx2/sql/MetadataUtils.java
+++ backend/molgenis-emx2-sql/src/main/java/org/molgenis/emx2/sql/MetadataUtils.java
@@ -97,7 +97,7 @@ public class MetadataUtils {
         config -> {
           DSLContext jooq = config.dsl();
           if (jooq.meta().getSchemas(MOLGENIS).size() == 0) {
-            j.execute("LOCK TABLE pg_catalog.pg_namespace");
+            jooq.execute("LOCK TABLE pg_catalog.pg_namespace");
             try (CreateSchemaFinalStep step = jooq.createSchemaIfNotExists(MOLGENIS)) {
               step.execute();
             }
