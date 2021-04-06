170df519ae0f773d25069df27e46dddc1f9518c6
Morris Swertz <m.a.swertz@rug.nl>
2021-04-06 23:29:56 +0200

    fix: show error in table explorer if no tables are in schema

--- apps/tables/src/components/ListTables.vue
+++ apps/tables/src/components/ListTables.vue
@@ -4,47 +4,52 @@
     <MessageError v-if="!schema">
       No tables found. Might you need to login?
     </MessageError>
-    Download all tables:
-    <a href="../api/zip">zip</a> | <a href="../api/excel">excel</a> |
-    <a href="../api/jsonld">jsonld</a> | <a href="../api/ttl">ttl</a><br />
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">
-            Table
-            <div class="form-check form-check-inline">
-              <InputCheckbox
-                class="ml-2"
-                v-model="tableFilter"
-                :defaultValue="tableFilter"
-                :options="['external']"
-                :clear="false"
-              />
-            </div>
-          </th>
-          <th scope="col" v-if="tableFilter.includes('external')">
-            externalSchema
-          </th>
-          <th scope="col">Description</th>
+    <MessageError v-if="!schema.tables">
+      No tables found. Might you need to create them?
+    </MessageError>
+    <div v-else>
+      Download all tables:
+      <a href="../api/zip">zip</a> | <a href="../api/excel">excel</a> |
+      <a href="../api/jsonld">jsonld</a> | <a href="../api/ttl">ttl</a><br />
+      <table class="table" v-if="schema.tables">
+        <thead>
+          <tr>
+            <th scope="col">
+              Table
+              <div class="form-check form-check-inline">
+                <InputCheckbox
+                  class="ml-2"
+                  v-model="tableFilter"
+                  :defaultValue="tableFilter"
+                  :options="['external']"
+                  :clear="false"
+                />
+              </div>
+            </th>
+            <th scope="col" v-if="tableFilter.includes('external')">
+              externalSchema
+            </th>
+            <th scope="col">Description</th>
+          </tr>
+        </thead>
+        <tr
+          v-for="table in schema.tables.filter(
+            (table) =>
+              table.externalSchema == undefined ||
+              tableFilter.includes('external')
+          )"
+          :key="table.name"
+        >
+          <td>
+            <router-link :to="table.name">{{ table.name }}</router-link>
+          </td>
+          <td v-if="tableFilter.includes('external')">
+            {{ table.externalSchema }}
+          </td>
+          <td>{{ table.description }}</td>
         </tr>
-      </thead>
-      <tr
-        v-for="table in schema.tables.filter(
-          (table) =>
-            table.externalSchema == undefined ||
-            tableFilter.includes('external')
-        )"
-        :key="table.name"
-      >
-        <td>
-          <router-link :to="table.name">{{ table.name }}</router-link>
-        </td>
-        <td v-if="tableFilter.includes('external')">
-          {{ table.externalSchema }}
-        </td>
-        <td>{{ table.description }}</td>
-      </tr>
-    </table>
+      </table>
+    </div>
   </div>
 </template>
 
