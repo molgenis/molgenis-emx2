965900579b255ec66ea743a7989e2882515f06d4
mswertz <m.a.swertz@rug.nl>
2021-06-07 16:59:35 +0200

    fix[usermanager]: add equals and hashcode to User.java

--- backend/molgenis-emx2/src/main/java/org/molgenis/emx2/User.java
+++ backend/molgenis-emx2/src/main/java/org/molgenis/emx2/User.java
@@ -1,5 +1,7 @@
 package org.molgenis.emx2;
 
+import java.util.Objects;
+
 public class User {
   String username;
 
@@ -9,5 +11,18 @@ public class User {
 
   public void setUsername(String username) {
     this.username = username;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    User user = (User) o;
+    return Objects.equals(username, user.username);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(username);
   }
 }
