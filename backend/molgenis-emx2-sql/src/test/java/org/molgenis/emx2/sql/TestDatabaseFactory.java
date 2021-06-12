733c6e2c6421e0ae42fd470ed804d95c5d4b52fb
mswertz <m.a.swertz@rug.nl>
2021-06-12 08:30:33 +0200

    build: move sql/test to sql-it/tests to ensure SqlDatabase(init) happens only once

--- backend/molgenis-emx2-sql/src/test/java/org/molgenis/emx2/sql/TestDatabaseFactory.java
+++ backend/molgenis-emx2-sql/src/test/java/org/molgenis/emx2/sql/TestDatabaseFactory.java
@@ -1,14 +1,15 @@
 package org.molgenis.emx2.sql;
 
-import org.molgenis.emx2.Database;
+import org.junit.Test;
 
 public class TestDatabaseFactory {
-  private static Database db;
 
-  public static synchronized Database getTestDatabase() {
-    if (db == null) {
-      db = new SqlDatabase(true);
-    }
-    return db;
+  @Test
+  public void createDatabase() {
+    // we want this run only once and NOT parallel for total test suite
+    // AND we want run all other tests in parallel
+    // so tests are in molgenis-emx2-sql-it ('integration test')
+    // and 'init' only happence once, here
+    new SqlDatabase(true);
   }
 }
