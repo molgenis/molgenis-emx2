ef2273ef27eea38ac7d618d47dc8597242f9486c
mswertz <m.a.swertz@rug.nl>
2021-07-24 10:36:04 +0200

    refactor: remove token feature

--- backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/MolgenisSessionManager.java
+++ backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/MolgenisSessionManager.java
@@ -12,23 +12,31 @@ import spark.Request;
 import spark.embeddedserver.EmbeddedServers;
 
 public class MolgenisSessionManager {
-  public static final String MOLGENIS_TOKEN = "x-molgenis-token";
   private static final Logger logger = LoggerFactory.getLogger(MolgenisSessionManager.class);
 
-  // map so we can track the sessions
+  // map so we can track the sessions, necessary for 'clearCache' in case of schema changes
   // session id is the key
   private Map<String, MolgenisSession> sessions = new ConcurrentHashMap<>();
 
   public MolgenisSessionManager() {
-    // Register custom server to sparkjava that listens to the session changes
+    MolgenisSessionManager _this = this;
+    // Register custom server to sparkjava so we can listen to the session changes
     EmbeddedServers.add(
         EmbeddedServers.Identifiers.JETTY,
         new SessionListeningJettyFactory(
             new HttpSessionListener() {
 
               public void sessionCreated(HttpSessionEvent httpSessionEvent) {
-                // add session into session pool
-                createSession(httpSessionEvent.getSession().getId());
+                logger.info("Initializing session");
+                // create private database wrapper to session
+                Database database = new SqlDatabase(false);
+                database.setActiveUser("anonymous"); // set default use to "anonymous"
+                database.setListener(new MolgenisSessionManagerDatabaseListener(_this, database));
+
+                // create session and add to sessions lists so we can also access all active
+                // sessions
+                MolgenisSession molgenisSession = new MolgenisSession(database);
+                sessions.put(httpSessionEvent.getSession().getId(), molgenisSession);
                 logger.info("session created: " + httpSessionEvent.getSession().getId());
               }
 
@@ -40,21 +48,13 @@ public class MolgenisSessionManager {
             }));
   }
 
+  /** Retrieve MolgenisSession for current request */
   public MolgenisSession getSession(Request request) {
-
-    // if valid token return session from token
-    // todo: this is only for testing where we inject into this.sessions; we will implement JWT
-    // tokens properly later
-    if (request.headers(MOLGENIS_TOKEN) != null) {
-      String token = request.headers(MOLGENIS_TOKEN);
-      if (sessions.containsKey(token)) {
-        return sessions.get(token);
-      }
-    }
 
     // if new session create a MolgenisSession object
     if (request.session().isNew()) {
-      request.session(true); // will create session stuff, see handler above
+      // jetty session will manage session create/destroy, see handler in constructor
+      request.session(true);
     }
 
     // get the session
@@ -63,22 +63,9 @@ public class MolgenisSessionManager {
     return session;
   }
 
-  MolgenisSession createSession(String token) {
-    // default user
-    String user = "anonymous";
-    // create new session
-    Database database = new SqlDatabase(false);
-    database.setActiveUser(user);
-    database.setListener(new MolgenisSessionManagerDatabaseListener(this, database));
-    logger.info("Initializing session for user: {}", database.getActiveUser());
-    MolgenisSession session = new MolgenisSession(database, token);
-    logger.info("Initializing session complete for user: {}", database.getActiveUser());
-
-    // put in session lists so we can easily access all session
-    sessions.put(session.getToken(), session);
-    return session;
-  }
-
+  /**
+   * this method is used to reset cache of all sessions, necessary when for example metadata changes
+   */
   void clearAllCaches() {
     for (MolgenisSession session : sessions.values()) {
       session.clearCache();
