e59808a87837efd88792127e1e5cd87eb4d11ac9
connoratrug <c.stroomberg@rug.nl>
2021-09-03 14:53:47 +0200

    switch to using settings ( instead of table)
    - add option to save settings

--- apps/catalogue/src/views/HomeView.vue
+++ apps/catalogue/src/views/HomeView.vue
@@ -1,33 +1,63 @@
 <template>
   <div class="row">
-    <div v-show="loaded" :class="editMode ? 'col-6' : 'col-12'">
-      <button class="btn float-right" v-on:click="editMode = !editMode">
+    <div v-if="loaded" :class="editMode ? 'col-6' : 'col-12'">
+      <button
+        v-show="canEdit && !editMode"
+        class="btn btn-outline-secondary float-right"
+        v-on:click="editMode = !editMode"
+      >
         Edit
       </button>
-      <h1 class="text-center">{{ title }}</h1>
-      <h3 class="text-center text-secondary" v-show="blurb">{{ blurb }}</h3>
+      <h1 class="text-center">{{ settings.title }}</h1>
+      <h3 class="text-center text-secondary" v-show="settings.subTitle">
+        {{ settings.subTitle }}
+      </h3>
       <div v-for="wiget in activeWigets" :key="wiget.name">
         <keep-alive>
           <component
-            v-bind:is="wiget.component"
+            v-bind:is="componentStore[wiget.name]"
             v-bind="wiget.props"
           ></component>
         </keep-alive>
       </div>
     </div>
     <div v-if="loaded && editMode" class="col-6 mg-editor">
+      <button
+        class="btn btn-outline-secondary"
+        v-on:click="editMode = !editMode"
+      >
+        Close
+      </button>
+      <button class="btn btn-outline-secondary" v-on:click="saveSetting">
+        Save
+      </button>
+      <p v-show="saving">Saving...</p>
       <form action="">
         <div class="form-group">
           <label for="title">Title</label>
-          <input id="title" type="text" class="form-control" v-model="title" />
+          <input
+            id="title"
+            type="text"
+            class="form-control"
+            v-model="settings.title"
+          />
         </div>
 
         <div class="form-group">
-          <label for="title">Blurb</label>
-          <input id="blurb" type="text" class="form-control" v-model="blurb" />
+          <label for="title">Sub title</label>
+          <input
+            id="subTitle"
+            type="text"
+            class="form-control"
+            v-model="settings.subTitle"
+          />
         </div>
 
-        <div class="form-check" v-for="wiget in wigets" :key="wiget.name">
+        <div
+          class="form-check"
+          v-for="wiget in settings.wigets"
+          :key="wiget.name"
+        >
           <input
             class="form-check-input"
             type="checkbox"
@@ -58,45 +88,80 @@ export default {
   data() {
     return {
       loaded: false,
+      saving: false,
       editMode: false,
-      title: "My title",
-      blurb: "this is your home page",
-      wigets: [],
+      componentStore: {},
+      settings: {
+        title: "My title",
+        subTitle: "this is your home page",
+        wigets: [],
+      },
     };
   },
   computed: {
     activeWigets() {
-      return this.wigets.filter((w) => w.active);
+      return this.settings.wigets.filter((w) => w.active);
+    },
+    canEdit() {
+      return !!(
+        // todo: wireup session via prop or store
+        (this.$parent.session && this.$parent.session.roles.includes("Editor"))
+      );
     },
   },
   methods: {
     async toggleWiget(name) {
-      const wiget = this.wigets.find((w) => w.name === name);
-      if (wiget.active && !wiget.component) {
+      const wiget = this.settings.wigets.find((w) => w.name === name);
+      if (wiget.active && !this.componentStore[name]) {
         const res = await loadWidget(wiget.name);
-        wiget.component = res.default;
+        this.componentStore[wiget.name] = res.default;
       }
     },
-    async querySettings() {
+    async loadSettings() {
       const fetchHomeSettings = gql`
         query HomePage {
-          HomePage(limit: 1) {
-            name
-            title
-            subTitle
-            component
+          _settings {
+            key
+            value
           }
         }
       `;
       const resp = await request("graphql", fetchHomeSettings).catch((e) =>
         console.error(e)
       );
-      return resp.HomePage[0];
+      return resp._settings && resp._settings.find((s) => s.key === "home-page")
+        ? resp._settings.find((s) => s.key === "home-page").value
+        : null;
     },
-    parseSettings(settings) {
-      this.title = settings.title;
-      this.blurb = settings.subTitle;
-      this.wigets = JSON.parse(settings.component);
+    async saveSetting() {
+      if (this.saving) {
+        return;
+      }
+      this.saving = true;
+      const settingsMutation = gql`
+        mutation change($settings: [MolgenisSettingsInput]) {
+          change(settings: $settings) {
+            message
+          }
+        }
+      `;
+
+      const resp = await request("graphql", settingsMutation, {
+        settings: {
+          key: "home-page",
+          value: JSON.stringify(this.settings),
+        },
+      }).catch((e) => {
+        console.error(e);
+      });
+
+      console.log(resp.change.message);
+      this.saving = false;
+    },
+    parseSettings(settingsString) {
+      if (settingsString) {
+        this.settings = JSON.parse(settingsString);
+      }
 
       this.activeWigets.forEach((aw) => {
         this.toggleWiget(aw.name);
@@ -104,8 +169,8 @@ export default {
     },
   },
   async created() {
-    const settings = await this.querySettings();
-    this.parseSettings(settings);
+    const settingsString = await this.loadSettings();
+    this.parseSettings(settingsString);
     this.loaded = true;
   },
 };
