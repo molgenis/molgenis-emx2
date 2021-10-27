06dfc8e426313f07b180743ec31dbb1764f61a74
connoratrug <c.stroomberg@rug.nl>
2021-10-27 17:27:53 +0200

    fix object display merge

--- apps/styleguide/src/tables/cellTypes/ObjectDisplay.vue
+++ apps/styleguide/src/tables/cellTypes/ObjectDisplay.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="hasTemplate">asTemplate</div>
+  <div v-if="hasTemplate">{{ asTemplate }}</div>
   <div v-else>{{ asDotSeparatedString }}</div>
 </template>
 
@@ -21,27 +21,28 @@ export default {
       return !!this.metaData.refLabel;
     },
     asTemplate() {
-      const names = Object.keys(this.object);
-      const vals = Object.values(this.object);
+      const names = Object.keys(this.data);
+      const vals = Object.values(this.data);
       try {
-        return new Function(...names, "return `" + this.template + "`;")(
-          ...vals
-        );
+        return new Function(
+          ...names,
+          "return `" + this.metaData.refLabel + "`;"
+        )(...vals);
       } catch (err) {
         const namesString = JSON.stringify(names);
         const valsString = JSON.stringify(vals);
-        return `${err.message} we got keys: ${namesString} vals: ${valsString} and template: ${this.template}`;
+        return `${err.message} we got keys: ${namesString} vals: ${valsString} and template: ${this.metaData.refLabel}`;
       }
     },
     asDotSeparatedString() {
       let result = "";
-      Object.keys(this.object).forEach((key) => {
-        if (this.object[key] === null) {
+      Object.keys(this.data).forEach((key) => {
+        if (this.data[key] === null) {
           //nothing
-        } else if (typeof this.object[key] === "object") {
-          result += this.flattenObject(this.object[key]);
+        } else if (typeof this.data[key] === "object") {
+          result += this.flattenObject(this.data[key]);
         } else {
-          result += "." + this.object[key];
+          result += "." + this.data[key];
         }
       });
       return result.replace(/^\./, "");
