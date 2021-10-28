0ca1a08c16452bab51bd06073e015a51368e29f3
Sido Haakma <sido@haakma.org>
2021-10-28 13:29:00 +0200

    chore: run tests always as non-super user

--- backend/molgenis-emx2-sql/src/main/java/org/molgenis/emx2/sql/TestDatabaseFactory.java
+++ backend/molgenis-emx2-sql/src/main/java/org/molgenis/emx2/sql/TestDatabaseFactory.java
@@ -1,11 +1,16 @@
 package org.molgenis.emx2.sql;
 
+import org.molgenis.emx2.Constants;
 import org.molgenis.emx2.Database;
 
 public class TestDatabaseFactory {
   private static Database db;
 
   public static Database getTestDatabase() {
+    System.setProperty(Constants.MOLGENIS_POSTGRES_URI, "jdbc:postgresql://localhost/azure");
+    System.setProperty(Constants.MOLGENIS_POSTGRES_PASS, "azure");
+    System.setProperty(Constants.MOLGENIS_POSTGRES_USER, "azure");
+
     if (db == null) {
       db = new SqlDatabase(false);
     }
