6db471c140a974f337a19d25884ccd9e55dcaf68
Sido Haakma <sido@haakma.org>
2021-10-28 23:23:15 +0200

    fix: also create schema for tests

--- .docker/initdb.sql
+++ .docker/initdb.sql
@@ -1,4 +1,8 @@
 /* use this init script if you want to test EMX2 with non-molgenis user */
-CREATE USER molgenis_cloud WITH LOGIN NOSUPERUSER INHERIT CREATEROLE ENCRYPTED PASSWORD 'molgenis_cloud';
 CREATE DATABASE molgenis_cloud;
+CREATE USER molgenis_cloud WITH LOGIN NOSUPERUSER INHERIT CREATEROLE ENCRYPTED PASSWORD 'molgenis_cloud';
 GRANT ALL PRIVILEGES ON DATABASE molgenis_cloud TO molgenis_cloud;
+\connect molgenis_cloud;
+CREATE SCHEMA MOLGENIS AUTHORIZATION molgenis_cloud;
+GRANT ALL PRIVILEGES ON SCHEMA MOLGENIS TO molgenis_cloud;
+
