18855d52f6ab2549d6088323c94ff0a151a8468e
mswertz <m.a.swertz@rug.nl>
2021-10-31 10:33:33 +0100

    chore: undo formatting

--- .docker/initdb.sql
+++ .docker/initdb.sql
@@ -1,8 +1,5 @@
 /* use this init script if you want to test EMX2 with non-molgenis user */
-CREATE
-DATABASE molgenisdb;
-CREATE
-USER molgenis_admin WITH LOGIN NOSUPERUSER INHERIT CREATEROLE ENCRYPTED PASSWORD 'molgenis_admin';
-GRANT ALL PRIVILEGES ON DATABASE
-molgenisdb TO molgenis_admin;
+CREATE DATABASE molgenisdb;
+CREATE USER molgenis_admin WITH LOGIN NOSUPERUSER INHERIT CREATEROLE ENCRYPTED PASSWORD 'molgenis_admin';
+GRANT ALL PRIVILEGES ON DATABASE molgenisdb TO molgenis_admin;
 
