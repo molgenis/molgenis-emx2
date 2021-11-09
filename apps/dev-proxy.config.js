93e48ad63eb464ff65e3f7ed36e010b10ac04454
mswertz <m.a.swertz@rug.nl>
2021-11-09 12:39:10 +0100

    chore: remove unwanted commit

--- apps/dev-proxy.config.js
+++ apps/dev-proxy.config.js
@@ -1,5 +1,5 @@
-const HOST = process.env.MOLGENIS_APPS_HOST || "http://localhost:8080";
-const SCHEMA = process.env.MOLGENIS_APPS_SCHEMA || "testNamesWithSpaces";
+const HOST = process.env.MOLGENIS_APPS_HOST || "https://emx2.dev.molgenis.org";
+const SCHEMA = process.env.MOLGENIS_APPS_SCHEMA || "Catalogue_test";
 
 module.exports = {
   "^/graphql": {
