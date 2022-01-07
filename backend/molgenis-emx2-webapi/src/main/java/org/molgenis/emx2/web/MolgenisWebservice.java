e85f0eac55874104acbedec2835b3bd1f9f1ca68
connoratrug <c.stroomberg@rug.nl>
2022-01-07 14:12:36 +0100

    remove verivication poc code from PR

--- backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/MolgenisWebservice.java
+++ backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/MolgenisWebservice.java
@@ -13,11 +13,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import org.molgenis.emx2.MolgenisException;
 import org.molgenis.emx2.Schema;
-import org.molgenis.emx2.Setting;
 import org.molgenis.emx2.Table;
 import org.molgenis.emx2.Version;
 import org.molgenis.emx2.web.controllers.OIDCController;
@@ -98,12 +96,9 @@ public class MolgenisWebservice {
     before(
         "/:schema",
         (req, res) -> {
-          String path = req.pathInfo();
-          if (path.startsWith("/google") && path.endsWith(".html")) {
-            MolgenisWebservice.handleGoogleVerification(req, res);
-          } else if (!("/" + OIDC_LOGIN_PATH).equals(path)
-              && !("/" + OIDC_CALLBACK_PATH).equals(path)
-              && !("/" + ROBOTS_TXT).equals(path)) {
+          if (!("/" + OIDC_LOGIN_PATH).equals(req.pathInfo())
+              && !("/" + OIDC_CALLBACK_PATH).equals(req.pathInfo())
+              && !("/" + ROBOTS_TXT).equals(req.pathInfo())) {
             res.redirect("/" + req.params("schema") + "/");
           }
         });
@@ -134,19 +129,6 @@ public class MolgenisWebservice {
   private static String robotsDotTxt(Request request, Response response) {
     response.type("text/plain;charset=UTF-8");
     return USER_AGENT_ALLOW;
-  }
-
-  private static void handleGoogleVerification(Request request, Response response) {
-    Optional<Setting> verificationKey =
-        sessionManager.getSession(request).getDatabase().getSettings().stream()
-            .filter(s -> "google-site-verification".equals(s.key()))
-            .findFirst();
-    if (verificationKey.isPresent()) {
-      response.body(
-          String.format("google-site-verification: google%s.html", verificationKey.get().value()));
-    } else {
-      halt(401, "Google site verification is not set ");
-    }
   }
 
   private static void redirectSchemaToFirstMenuItem(Request request, Response response) {
