77716c58e2e92da55e62b7f1ff3d45da03c08c19
mswertz <m.a.swertz@rug.nl>
2022-01-15 10:38:29 +0100

    chore: remove unreleated bugfix

--- apps/styleguide/src/tables/cellTypes/FileDisplay.vue
+++ apps/styleguide/src/tables/cellTypes/FileDisplay.vue
@@ -16,28 +16,28 @@ export default {
       type: Object,
       required: true,
     },
-  },
-  computed: {
-    fileSize() {
-      const SI_SYMBOL = ["", "k", "M", "G", "T", "P", "E"];
+    computend: {
+      fileSize() {
+        const SI_SYMBOL = ["", "k", "M", "G", "T", "P", "E"];
 
-      // what tier? (determines SI symbol)
-      const tier = (Math.log10(this.data.size) / 3) | 0;
+        // what tier? (determines SI symbol)
+        const tier = (Math.log10(this.data.size) / 3) | 0;
 
-      // if zero, we don't need a suffix
-      if (tier == 0) {
-        return this.data.size;
-      }
+        // if zero, we don't need a suffix
+        if (tier == 0) {
+          return this.data.size;
+        }
 
-      // get suffix and determine scale
-      var suffix = SI_SYMBOL[tier];
-      var scale = Math.pow(10, tier * 3);
+        // get suffix and determine scale
+        var suffix = SI_SYMBOL[tier];
+        var scale = Math.pow(10, tier * 3);
 
-      // scale the number
-      var scaled = this.data.size / scale;
+        // scale the number
+        var scaled = this.number / scale;
 
-      // format number and add suffix
-      return scaled.toFixed(1) + suffix;
+        // format number and add suffix
+        return scaled.toFixed(1) + suffix;
+      },
     },
   },
 };
