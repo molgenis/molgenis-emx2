d827e6842c93aea16614402caea750612caaebf8
mswertz <m.a.swertz@rug.nl>
2022-01-15 10:55:41 +0100

    chore: remove unrelated change

--- backend/molgenis-emx2-webapi/src/test/java/org.molgenis.emx2.web/WebApiSmokeTests.java
+++ backend/molgenis-emx2-webapi/src/test/java/org.molgenis.emx2.web/WebApiSmokeTests.java
@@ -1,6 +1,5 @@
 package org.molgenis.emx2.web;
 
-import static io.restassured.RestAssured.expect;
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.equalTo;
