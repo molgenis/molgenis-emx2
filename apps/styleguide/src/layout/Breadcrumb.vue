0dc95f507cb8df0bc3ee78dfb7f976ed5ca62458
Morris Swertz <m.a.swertz@rug.nl>
2022-01-23 13:50:16 +0100

    Update Breadcrumb.vue

--- apps/styleguide/src/layout/Breadcrumb.vue
+++ apps/styleguide/src/layout/Breadcrumb.vue
@@ -16,7 +16,6 @@
               @click="toggleDropdown"
             ></i>
             <div class="dropdown-menu" :class="{ show: showDropdown }">
-              <p>Switch to:</p>
               <a
                 v-for="(url, label, index) in dropdown"
                 class="dropdown-item text-primary"
