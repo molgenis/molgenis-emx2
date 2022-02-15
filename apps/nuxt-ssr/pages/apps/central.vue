916360926fe6f51d2dd093674f7d4ec6f1cd57ec
connoratrug <c.stroomberg@rug.nl>
2022-02-15 10:46:55 +0100

    fix gql route + cleanup code

--- apps/nuxt-ssr/pages/apps/central.vue
+++ apps/nuxt-ssr/pages/apps/central.vue
@@ -1,30 +1,34 @@
 <template>
-<div>
-  <div class="container">
-    <h1>Schemas</h1>
-    <div class="list-group">
-      <nuxt-link v-for="schema in schemas" :key="schema.name" :to="'/' + schema.name + '/ssr-catalogue'" class="list-group-item list-group-item-action">
-        {{schema.name}}
-      </nuxt-link>
+  <div>
+    <div class="container">
+      <h1>Schemas</h1>
+      <div class="list-group">
+        <nuxt-link
+          v-for="schema in schemas"
+          :key="schema.name"
+          :to="'/' + schema.name + '/ssr-catalogue'"
+          class="list-group-item list-group-item-action"
+        >
+          {{ schema.name }}
+        </nuxt-link>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 
 <script>
-  export default {
-    async asyncData({ params, $axios, store }) {
-      store.dispatch('fetchSession')
-      const query = '{Schemas{name description}}'
-      const resp = await $axios({
-      url: "apps/central/graphql",
-      method: "post",
-      data: { query }
-    }).catch(e => {
-      console.error(e)
+const query = "{ Schemas { name description } }";
+export default {
+  async asyncData({ $axios, store }) {
+    store.dispatch("fetchSession");
+
+    const resp = await $axios.post("/apps/graphql", { query }).catch((e) => {
+      console.log("failed to fetch schemas");
+      console.log(e);
     });
-    return { schemas: resp.data.data.Schemas }
-    }
-  }
+
+    return resp ? { schemas: resp.data.data.Schemas } : null;
+  },
+};
 </script>
