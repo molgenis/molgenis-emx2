f884b22bc502be22a59a2e083b24048a089d0df4
connoratrug <c.stroomberg@rug.nl>
2022-02-16 13:28:33 +0100

    add pagination to cohorts list

--- apps/nuxt-ssr/pages/_schema/ssr-catalogue/cohorts/index.vue
+++ apps/nuxt-ssr/pages/_schema/ssr-catalogue/cohorts/index.vue
@@ -1,35 +1,64 @@
 <template>
-<div class="container mt-3">
-  <h2>Cohorts</h2>
-  <ul class="mt-3 list-group">
-    <li v-for="row in rows" :key="row.pid" class="list-group-item">
-      <nuxt-link :to="'cohorts/' + row.pid" >
-         {{ row.name }}
-      </nuxt-link>
-    </li>
-  </ul>
-</div>
-
+  <div class="container mt-3">
+    <h1>Cohorts</h1>
+    <div class="pb-1">
+      <pagination v-if="count > limit" v-model="page" :count="count"></pagination>
+    </div>
+    <ul class="mt-3 list-group">
+      <li v-for="row in rows" :key="row.pid" class="list-group-item">
+        <nuxt-link :to="'cohorts/' + row.pid">
+          {{ row.name }}
+        </nuxt-link>
+      </li>
+    </ul>
+    <div class="p-3">
+      <pagination v-if="count > limit" v-model="page" :count="count" :limit="limit" :defaultValue="page"></pagination>
+    </div>
+  </div>
 </template>
 
 <script>
-import query from "../../../../store/gql/cohorts.gql";
-  export default {
-    name: "Cohorts",
-    async asyncData({ params, $axios, store }) {
-      const resp = await $axios({
-        url: store.state.schema + "/graphql",
-        method: "post",
-        data: {query} ,
-      }).catch((e) => console.error(e));
+import cohortsQuery from "../../../../store/gql/cohorts.gql";
+import { Pagination } from "molgenis-components";
+export default {
+  name: "Cohorts",
+  components: { Pagination },
+  data() {
+    return {
+      page: 1,
+      limit: 20
+    };
+  },
+  async asyncData({ $axios, store, params, query }) {
+    const offset = query.offset === undefined ? 0 : parseInt(query.offset)
+    console.log('offset: ' + offset)
+    const resp = await $axios({
+      url: store.state.schema + "/graphql",
+      method: "post",
+      data: { query: cohortsQuery, variables: { offset, limit: 20 } },
+    }).catch((e) => console.error(e));
 
-      return { cohorts: resp.data.data.Cohorts }
+    if (!resp) return;
+
+    return {
+      cohorts: resp.data.data.Cohorts,
+      count: resp.data.data.Cohorts_agg.count,
+    };
+  },
+  computed: {
+    rows() {
+      return this.cohorts ? this.cohorts : [];
     },
-    computed: {
-      rows () {
-        return this.cohorts ? this.cohorts : []
+  },
+  watch: {
+    page (newVal, oldVal) {
+      if(newVal !== oldVal) {
+        this.$router.push({path: this.$route.path, query: { offset: (newVal - 1) * this.limit }})
       }
+    },
+    $route () {
+      this.$nuxt.refresh()
     }
- 
   }
+};
 </script>
