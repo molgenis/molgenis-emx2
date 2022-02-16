38c3f60a7dff4232ac08265355ea461f6522da2c
Joris de Keijser <chinook25@gmail.com>
2022-02-16 14:03:37 +0100

    chore: forget to add file

---
+++ apps/styleguide/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2020
+  }
+};
---
+++ apps/styleguide/.prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: false,
+  arrowParens: 'always'
+};
