5d32cf4a3ddc42c8be029e5d37b43a9e6ec4cf14
mswertz <m.a.swertz@rug.nl>
2022-02-20 19:56:08 +0100

    build: add asw init script

--- .aws/aws_start_preview_from_build.sh
+++ .aws/aws_start_preview_from_build.sh
@@ -2,7 +2,7 @@
 aws --version
 
 # create an instance, that will shutdown after 60mins
-INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0ba724c59d3c2b346 --count 1 --instance-type t2.micro --key-name mswertz --security-group-ids sg-030fc72bb47b17f64 --subnet-id subnet-093e36ba6d1b5f420 --instance-initiated-shutdown-behavior terminate --user-data "sudo shutdown -P +10" --query 'Instances[0].InstanceId' --output text)
+INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0ba724c59d3c2b346 --count 1 --instance-type t2.micro --key-name mswertz --security-group-ids sg-030fc72bb47b17f64 --subnet-id subnet-093e36ba6d1b5f420 --instance-initiated-shutdown-behavior terminate --user-data "nohup sudo shutdown -P +10 1>/dev/null 2>/dev/null &" --query 'Instances[0].InstanceId' --output text)
 echo "starting AWS instance with id=${INSTANCE_ID}"
 
 #wait
@@ -22,7 +22,5 @@ echo "uploaded molgenis binary, starting emx2"
 ssh -o StrictHostKeyChecking=no ubuntu@$PUBLIC_DNS 'nohup sudo java -DMOLGENIS_HTTP_PORT=80 -jar molgenis-emx2.jar 1>/dev/null 2>/dev/null &'
 echo "emx2 started"
 
-ssh -o StrictHostKeyChecking=no ubuntu@$PUBLIC_DNS 'nohup sudo shutdown -P +60 1>/dev/null 2>/dev/null &'
-echo "sent shutdown timer set for 60 minutes"
-
-#todo, post a message to github stating preview available until +60mins
+ssh -o StrictHostKeyChecking=no ubuntu@$PUBLIC_DNS 'nohup sudo shutdown -P +10 1>/dev/null 2>/dev/null &'
+echo "sent shutdown timer set for 10 minutes"
