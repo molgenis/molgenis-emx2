48f8a48867253b2ac8d858e10fbf06b925b9423f
Postema Max <max@elynor.com>
2022-02-23 14:51:28 +0100

    test: testing the tests tester

---
+++ apps/styleguide/tests/unit/tabels/RowEditModal.spec.js
@@ -0,0 +1,18 @@
+import {shallowMount} from '@vue/test-utils';
+import RowEditModal from '@src/tables/RowEditModal';
+
+describe('RowEditModal', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(RowEditModal, {});
+  });
+  describe('getRefBackType', () => {
+    it("Should return undefined if column type is not 'REFBACK'", () => {
+      const column = {
+        columnType: 'NOT_REFBACK'
+      };
+      const result = wrapper.vm.getRefBackType(column);
+      expect(result).toBeUndefined();
+    });
+  });
+});
