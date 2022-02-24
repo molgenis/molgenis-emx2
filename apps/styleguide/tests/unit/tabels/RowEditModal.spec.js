01cdcffb19c2ad5f52181840ecc5f2959708cfb4
Postema Max <max@elynor.com>
2022-02-24 14:29:00 +0100

    test: have the first test working

--- apps/styleguide/tests/unit/tabels/RowEditModal.spec.js
+++ apps/styleguide/tests/unit/tabels/RowEditModal.spec.js
@@ -1,11 +1,19 @@
 import {shallowMount} from '@vue/test-utils';
 import RowEditModal from '../../../src/tables/RowEditModal.vue';
+import GraphqlRequest from 'graphql-request';
+
+jest.mock('graphql-request');
 
 describe('RowEditModal', () => {
   let wrapper;
+
   beforeEach(() => {
+    const requestResult = {_session: undefined, _schema: undefined};
+    GraphqlRequest.mockResolvedValue(requestResult);
     wrapper = shallowMount(RowEditModal, {});
+    expect(GraphqlRequest).toBeCalled();
   });
+
   describe('getRefBackType', () => {
     it("Should return undefined if column type is not 'REFBACK'", () => {
       const column = {
