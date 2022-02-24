43e2bff20a00e4a3b3b5a94bce84c86525eaffef
Postema Max <max@elynor.com>
2022-02-24 14:57:34 +0100

    test: have complete getRefBackType tests

--- apps/styleguide/tests/unit/tabels/RowEditModal.spec.js
+++ apps/styleguide/tests/unit/tabels/RowEditModal.spec.js
@@ -1,26 +1,50 @@
 import {shallowMount} from '@vue/test-utils';
 import RowEditModal from '../../../src/tables/RowEditModal.vue';
 import GraphqlRequest from 'graphql-request';
+import Vue from 'vue';
 
 jest.mock('graphql-request');
 
 describe('RowEditModal', () => {
-  let wrapper;
-
-  beforeEach(() => {
-    const requestResult = {_session: undefined, _schema: undefined};
-    GraphqlRequest.mockResolvedValue(requestResult);
-    wrapper = shallowMount(RowEditModal, {});
-    expect(GraphqlRequest).toBeCalled();
-  });
-
   describe('getRefBackType', () => {
     it("Should return undefined if column type is not 'REFBACK'", () => {
+      const requestResult = {_session: undefined, _schema: undefined};
+      GraphqlRequest.mockResolvedValue(requestResult);
+      const wrapper = shallowMount(RowEditModal, {});
+      expect(GraphqlRequest).toBeCalled();
+
       const column = {
         columnType: 'NOT_REFBACK'
       };
       const result = wrapper.vm.getRefBackType(column);
       expect(result).toBeUndefined();
+    });
+
+    it('Should return the type (string) of the referenced column', async (done) => {
+      const tableName = 'TableName';
+      const schema = {
+        tables: [
+          {
+            name: tableName,
+            columns: [{name: 'ColumnName', columnType: 'Succes'}]
+          }
+        ]
+      };
+      const requestResult = {_session: undefined, _schema: schema};
+      GraphqlRequest.mockResolvedValue(requestResult);
+      const wrapper = shallowMount(RowEditModal, {});
+      expect(GraphqlRequest).toBeCalled();
+
+      const column = {
+        columnType: 'REFBACK',
+        refBack: 'ColumnName',
+        refTable: tableName
+      };
+
+      await Vue.nextTick();
+      const result = wrapper.vm.getRefBackType(column);
+      expect(result).toBe('Succes');
+      done();
     });
   });
 });
