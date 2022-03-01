4ba5652ade2f8a23092b69a633686c124a7293bf
connoratrug <c.stroomberg@rug.nl>
2022-03-01 09:47:30 +0100

    feat: setup default columns for ssr resource list view

---
+++ apps/nuxt-ssr/pages/_schema/ssr-catalogue/_resource/_resourceDetail.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <h1>Resource detail</h1>
+  <h3>Type: {{$route.params.resource}}</h3>
+  <h3>Item: {{$route.params.resourceDetail}}</h3>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
