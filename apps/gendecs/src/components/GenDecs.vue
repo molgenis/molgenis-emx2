3f2d4701adf747e510fe55a57109afca0dbfc420
JonathanKlimp <78917640+JonathanKlimp@users.noreply.github.com>
2022-03-01 13:37:07 +0100

    added Match found message if the selected HPO term is connected to the selected patient

--- apps/gendecs/src/components/GenDecs.vue
+++ apps/gendecs/src/components/GenDecs.vue
@@ -8,23 +8,22 @@
       found terms will be checked if they match with the given root term/phenotype. This information
       is then reported back.
     </p>
-    <div id="app">
-      <h2>Select HPO root term</h2>
-      <SearchAutoComplete :items= "allHpoTerms"
-      @selectedHpoTerm="apiCall"
-      >
-      </SearchAutoComplete>
+    <MessageSuccess v-if="foundMatch">Match found!</MessageSuccess>
 
-      <br/>
-      <PatientSearch @geneOfPatient="geneToHpo">
-      </PatientSearch>
+    <SearchAutoComplete :items= "allHpoTerms"
+      @selectedHpoTerm="apiCall" class="inputForm"
+    >
+    </SearchAutoComplete>
 
-    </div>
-    <br/> <br/>
+    <br/>
+    <PatientSearch @geneOfPatient="geneToHpo" class="inputForm">
+    </PatientSearch>
+
+    <br/>
     <h3 v-if="loadingHpo">here are results from the HPO api call</h3>
     {{ hpoResults }}
     <br/> <br/>
-    <h3 v-if="loadingHpo">here are results from the Gene api call</h3>
+    <h3 v-if="loadingGeneAssociates">here are results from the Gene api call</h3>
     {{ geneAssociates }}
 
   </div>
@@ -39,7 +38,7 @@ import {
 } from "@mswertz/emx2-styleguide";
 import SearchAutoComplete from "./SearchAutoComplete";
 import PatientSearch from "./PatientSearch";
-import hpoData from '../js/autoSearchData.js';
+import hpoData from "../js/autoSearchData.js";
 
 export default {
   components: {
@@ -52,29 +51,26 @@ export default {
   },
   data() {
     return {
-      count: 0,
-      selected: 1234,
       hpoResults: null,
       geneEntered: '',
       geneAssociates: null,
       loadingHpo: false,
-      allHpoTerms: hpoData
+      loadingGeneAssociates: false,
+      allHpoTerms: hpoData,
+      selectedHpoTerm: null,
+      foundMatch: false
     };
   },
-  methods: {
-    plusOne() {
-      this.count++;
-    },
-    async apiCall(hpoTerm) {
-      let resultData = await fetch("https://hpo.jax.org/api/hpo/search/?q=" + hpoTerm)
+  methods: { //2 abnormality of the radius
+    async apiCall(selectedHpoTerm) {
+      this.selectedHpoTerm = selectedHpoTerm;
+      let resultData = await fetch("https://hpo.jax.org/api/hpo/search/?q=" + selectedHpoTerm)
         .then(response => response.json());
       this.loadingHpo = true;
       this.hpoResults = resultData['terms'];
       this.getHpoId();
     },
     async geneToHpo(geneOfPatient) {
-      // HMGCL
-      console.log("ik be nu hier")
       let resultData = await fetch("https://hpo.jax.org/api/hpo/search/?q=" + geneOfPatient)
         .then(response => response.json());
       let entrezId = resultData['genes'][0].entrezGeneId;
@@ -86,6 +82,8 @@ export default {
         geneTermAssoc.push(resultData2['termAssoc'][i].name);
       }
       this.geneAssociates = geneTermAssoc;
+      this.loadingGeneAssociates = true;
+      this.checkIfMatch();
     },
     getHpoId() {
       let id = this.hpoResults[0].id;
@@ -101,10 +99,20 @@ export default {
       for (let i = 0; i < hpoChildren.length; i++) {
         hpoChildrenName.push(hpoChildren[i].label);
       }
-
       console.log("Children of " + this.hpoResults[0].name + ": " + hpoChildrenName);
-
+    },
+    checkIfMatch() {
+      if(this.geneAssociates.includes(this.selectedHpoTerm)) {
+        this.foundMatch = true;
+      }
     }
   },
 };
 </script>
+
+<style scoped>
+/*.inputForm {*/
+/*  float: left;*/
+/*  padding: 50px;*/
+/*}*/
+</style>
