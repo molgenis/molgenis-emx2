c531a5e0fcbccd9374a3d3a512987f7f509d0a56
Postema Max <max@elynor.com>
2022-03-04 16:40:15 +0100

    fix: remove error on close without selection, fix: improved styling

--- apps/styleguide/src/forms/_appendClear.vue
+++ apps/styleguide/src/forms/_appendClear.vue
@@ -5,7 +5,6 @@
     type="button"
     title="Clear"
     @click="$emit('clear')"
-    style="margin-left: -38px; z-index: 100"
     :class="{
       'text-primary': !error,
       'text-danger': error,
@@ -18,6 +17,14 @@
     </i>
   </button>
 </template>
+
+<style scoped>
+.btn {
+  margin-left: -38px;
+  z-index: 100;
+}
+</style>
+
 <script>
 export default {
   props: {
