b5ffca78b12768465af3e0c777b440be6585040a
K. Joeri van der Velde <joeriv@gmail.com>
2022-03-14 16:38:08 +0100

    Beacon API now on root level

--- backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/beaconv2/responses/datasets/DatasetsResponse.java
+++ backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/beaconv2/responses/datasets/DatasetsResponse.java
@@ -3,21 +3,29 @@ package org.molgenis.emx2.beaconv2.responses.datasets;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import java.util.ArrayList;
 import java.util.List;
-import org.molgenis.emx2.Schema;
-import org.molgenis.emx2.Table;
+import spark.Request;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class DatasetsResponse {
   Collection[] collections;
 
-  public DatasetsResponse(Schema schema) {
+  public DatasetsResponse(Request request, java.util.Collection<String> schemaNames) {
     List<Collection> cList = new ArrayList<>();
-    for (Table t : schema.getTablesSorted()) {
+
+    //    for (Table t : schema.getTablesSorted()) {
+    //      Collection c =
+    //          new Collection(
+    //              t.getName(), t.getName(), "2022-01-01T00:00:00+00:00",
+    // "2022-01-01T00:00:00+00:00");
+    //      cList.add(c);
+    //    }
+
+    for (String schema : schemaNames) {
       Collection c =
-          new Collection(
-              t.getName(), t.getName(), "2022-01-01T00:00:00+00:00", "2022-01-01T00:00:00+00:00");
+          new Collection(schema, schema, "2022-01-01T00:00:00+00:00", "2022-01-01T00:00:00+00:00");
       cList.add(c);
     }
+
     this.collections = cList.toArray(new Collection[cList.size()]);
   }
 }
