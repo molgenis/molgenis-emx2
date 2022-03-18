23dd096fb4035cf6788df36faa268f5a3a40647e
connoratrug <c.stroomberg@rug.nl>
2022-03-18 15:33:22 +0100

    add workaround for non existent resource in schema

--- backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/controllers/SiteMapController.java
+++ backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/controllers/SiteMapController.java
@@ -6,12 +6,13 @@ import static org.molgenis.emx2.web.MolgenisWebservice.getSchema;
 import com.redfin.sitemapgenerator.ChangeFreq;
 import com.redfin.sitemapgenerator.WebSitemapGenerator;
 import com.redfin.sitemapgenerator.WebSitemapUrl;
-
 import java.net.MalformedURLException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
+import kotlin.collections.EmptyList;
 import org.molgenis.emx2.MolgenisException;
 import org.molgenis.emx2.Row;
 import org.molgenis.emx2.Schema;
@@ -22,65 +23,68 @@ import spark.Request;
 import spark.Response;
 
 public class SiteMapController {
-    private static final Logger logger = LoggerFactory.getLogger(SiteMapController.class);
-    private static final List<String> ResourceList = Arrays.asList("Cohorts", "Studies", "Networks", "Institutions", "Databanks", "Datasources");
+  private static final Logger logger = LoggerFactory.getLogger(SiteMapController.class);
+  private static final List<String> ResourceList =
+      Arrays.asList("Cohorts", "Studies", "Networks", "Institutions", "Databanks", "Datasources");
 
-    private SiteMapController() {
+  private SiteMapController() {}
+
+  public static String getSiteMapForSchema(Request request, Response response) {
+    response.type("text/xml, application/xml");
+    Schema schema = getSchema(request);
+
+    final String baseUrl = request.scheme() + "://" + request.host() + "/" + schema.getName();
+    final String appName = "ssr-catalogue";
+    final String resourceBasePath = baseUrl + "/" + appName;
+
+    try {
+      WebSitemapGenerator wsg = new WebSitemapGenerator(baseUrl);
+
+      ResourceList.forEach(
+          resource -> {
+            List<String> pids = SiteMapController.getResourcePids(schema, resource);
+            pids.forEach(
+                pid -> {
+                  try {
+                    wsg.addUrl(SiteMapController.urlForResource(resourceBasePath, resource, pid));
+                  } catch (MalformedURLException e) {
+                    logger.error(
+                        "Failed to generate sitemap url (schema: ("
+                            + schema.getName()
+                            + " , resource: "
+                            + resource
+                            + " , pid: "
+                            + pid,
+                        e);
+                  }
+                });
+          });
+
+      return String.join(System.lineSeparator(), wsg.writeAsStrings());
+    } catch (MalformedURLException e) {
+      String errorDescription = "Error initializing WebSitemapGenerator";
+      logger.error(errorDescription, e);
+      throw new MolgenisException(errorDescription);
     }
+  }
 
-    public static String getSiteMapForSchema(Request request, Response response) {
-        response.type("text/xml, application/xml");
-        Schema schema = getSchema(request);
-
-        final String baseUrl = request.scheme() + "://" + request.host() + "/" + schema.getName();
-        final String appName = "ssr-catalogue";
-        final String resourceBasePath = baseUrl + "/" + appName;
-
-        try {
-            WebSitemapGenerator wsg = new WebSitemapGenerator(baseUrl);
-
-            ResourceList.forEach(
-                    resource -> {
-                        List<String> pids = SiteMapController.getResourcePids(schema, resource);
-                        pids.forEach(
-                                pid -> {
-                                    try {
-                                        wsg.addUrl(SiteMapController.urlForResource(resourceBasePath, resource, pid));
-                                    } catch (MalformedURLException e) {
-                                        logger.error(
-                                                "Failed to generate sitemap url (schema: ("
-                                                        + schema.getName()
-                                                        + " , resource: "
-                                                        + resource
-                                                        + " , pid: "
-                                                        + pid,
-                                                e);
-                                    }
-                                });
-                    });
-
-            return String.join(System.lineSeparator(), wsg.writeAsStrings());
-        } catch (MalformedURLException e) {
-            String errorDescription = "Error initializing WebSitemapGenerator";
-            logger.error(errorDescription, e);
-            throw new MolgenisException(errorDescription);
-        }
+  private static List<String> getResourcePids(Schema schema, String resourceName) {
+    Table table = schema.getTable(resourceName);
+    if(table == null) {
+      return Collections.emptyList();
     }
+    List<Row> rows = table.select(s("pid")).retrieveRows();
+    return rows.stream().map(row -> row.getString("pid")).toList();
+  }
 
-    private static List<String> getResourcePids(Schema schema, String resourceName) {
-        Table table = schema.getTable(resourceName);
-        List<Row> rows = table.select(s("pid")).retrieveRows();
-        return rows.stream().map(row -> row.getString("pid")).toList();
-    }
-
-    private static WebSitemapUrl urlForResource(
-            String resourceBasePath, String resourceName, String resourceId)
-            throws MalformedURLException {
-        return new WebSitemapUrl.Options(
-                resourceBasePath + "/" + resourceName.toLowerCase() + "/" + resourceId)
-                .lastMod(new Date())
-                .priority(1.0)
-                .changeFreq(ChangeFreq.DAILY)
-                .build();
-    }
+  private static WebSitemapUrl urlForResource(
+      String resourceBasePath, String resourceName, String resourceId)
+      throws MalformedURLException {
+    return new WebSitemapUrl.Options(
+            resourceBasePath + "/" + resourceName.toLowerCase() + "/" + resourceId)
+        .lastMod(new Date())
+        .priority(1.0)
+        .changeFreq(ChangeFreq.DAILY)
+        .build();
+  }
 }
