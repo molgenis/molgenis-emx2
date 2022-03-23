213af288c4c767000fff6478d7be54320cb43493
connoratrug <c.stroomberg@rug.nl>
2022-03-23 15:18:47 +0100

    rearange ssr catalogue vue

--- apps/nuxt-ssr/pages/_schema/ssr-catalogue/index.vue
+++ apps/nuxt-ssr/pages/_schema/ssr-catalogue/index.vue
@@ -28,12 +28,12 @@
         </count-card>
 
         <count-card
-          class="bg-primary text-white"
-          :count="counts.datasources"
-          label="Data sources"
-          :to="{ path: this.routePath + 'Datasources' }"
+          class="bg-danger text-white"
+          :count="counts.networks"
+          label="Networks"
+          :to="{ path: this.routePath + 'Networks' }"
         >
-          Collections of data banks covering the same population
+          Collaborations of multiple institutions
         </count-card>
 
         <count-card
@@ -55,12 +55,12 @@
         </count-card>
 
         <count-card
-          class="bg-danger text-white"
-          :count="counts.networks"
-          label="Networks"
-          :to="{ path: this.routePath + 'Networks' }"
+          class="bg-primary text-white"
+          :count="counts.datasources"
+          label="Data sources"
+          :to="{ path: this.routePath + 'Datasources' }"
         >
-          Collaborations of multiple institutions
+          Collections of data banks covering the same population
         </count-card>
 
         <count-card
