526115fe7d46def4f71aa67ed547ef1f34d8ada6
JonathanKlimp <78917640+JonathanKlimp@users.noreply.github.com>
2022-03-30 17:08:17 +0200

    Added class Constants with file constants

---
+++ backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/semantics/gendecs/Constants.java
@@ -0,0 +1,11 @@
+package org.molgenis.emx2.semantics.gendecs;
+
+public class Constants {
+
+  private Constants() {
+    // hide constructor
+  }
+
+  public static final String FILENAMECLINVAR = "data/gendecs/clinvar_20220205.vcf";
+  public static final String FILENAMEVCFDATA = "data/gendecs/vcfdata.vcf";
+}
