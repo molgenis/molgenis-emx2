2a696be2511dff6b7040fb444d0f953c9a485e5d
JonathanKlimp <78917640+JonathanKlimp@users.noreply.github.com>
2022-03-31 10:40:09 +0200

    Removed Arraylist genes from Variants

--- backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/semantics/gendecs/Variants.java
+++ backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/semantics/gendecs/Variants.java
@@ -5,37 +5,12 @@ import java.util.HashMap;
 
 public class Variants {
   private ArrayList<String> variants = new ArrayList<>();
-  private ArrayList<String> genes = new ArrayList<>();
   private HashMap<String, String> genesHpo = new HashMap<>();
 
   public void addVariant(String variants) {
     this.variants.add(variants);
   }
 
-  /**
-   * Method that filters the genes from the variants if the arraylist genes is empty.
-   *
-   * @return ArrayList with gene symbols.
-   */
-  public ArrayList<String> getGenes() {
-    if (this.genes.size() == 0) {
-      for (String variant : variants) {
-        String[] splittedLine = variant.split("\t");
-        String[] infoString = splittedLine[7].split(";");
-        for (String i : infoString) {
-          if (i.contains("GENEINFO")) {
-            this.addGene(i.split("=")[1]);
-          }
-        }
-      }
-    }
-    return genes;
-  }
-
-  private void addGene(String genes) {
-    this.genes.add(genes);
-  }
-  
   public HashMap<String, String> getGeneHpo() {
     return genesHpo;
   }
