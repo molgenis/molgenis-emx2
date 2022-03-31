38d80ebcbf93a6e62e5a658f5df029ec2a528495
connoratrug <c.stroomberg@rug.nl>
2022-03-31 09:24:42 +0200

    and back again

--- apps/molgenis-components/vite.config.js
+++ apps/molgenis-components/vite.config.js
@@ -1,63 +1,63 @@
-import {defineConfig} from 'vite';
-import {createVuePlugin} from 'vite-plugin-vue2';
-import path from 'path';
-import docTagPlugin from './docs-plugin.js';
+import { defineConfig } from "vite";
+import { createVuePlugin } from "vite-plugin-vue2";
+import path from "path";
+import docTagPlugin from "./docs-plugin.js";
 
-const BACKEND_LOCATION = process.env.PROXY_API || 'http://localhost:8080/';
+const BACKEND_LOCATION = process.env.PROXY_API || "http://localhost:8080/";
 
 // basic build conf fo both library and showCase builds
 let conf = {
   plugins: [docTagPlugin(), createVuePlugin()],
   server: {
     proxy: {
-      '^/.*/graphql': {
+      "^/.*/graphql": {
         target: `${BACKEND_LOCATION}`,
         changeOrigin: true,
-        secure: false
+        secure: false,
       },
-      '/apps/central/theme.css': {
+      "/apps/central/theme.css": {
         target: `${BACKEND_LOCATION}`,
         changeOrigin: true,
-        secure: false
+        secure: false,
       },
-      '^/apps/resources/webfonts/.*': {
+      "^/apps/resources/webfonts/.*": {
         target: `${BACKEND_LOCATION}`,
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+        secure: false,
+      },
+    },
+  },
 };
 
 // In case the SHOW_CASE flag is not set to 'on' build in library mode ( i.e. lib mode is the default)
-if (process.env.SHOW_CASE !== 'on') {
-  console.log('prod build in library mode');
+if (process.env.SHOW_CASE !== "on") {
+  console.log("prod build in library mode");
   conf.build = {
     lib: {
-      entry: path.resolve(__dirname, 'lib/main.js'),
-      name: 'MolgenisComponents',
-      fileName: (format) => `molgenis-components.${format}.js`
+      entry: path.resolve(__dirname, "lib/main.js"),
+      name: "MolgenisComponents",
+      fileName: (format) => `molgenis-components.${format}.js`,
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue', 'axios'],
+      external: ["vue", "axios"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: 'Vue'
-        }
-      }
-    }
+          vue: "Vue",
+        },
+      },
+    },
   };
 } else {
-  console.log('prod build in show case mode');
+  console.log("prod build in show case mode");
   conf.build = {
-    outDir: './showCase'
+    outDir: "./showCase",
   };
 
-  conf.base = ''; // use relative base path for use in public_html/app folder
+  conf.base = ""; // use relative base path for use in public_html/app folder
 }
 
 // https://vitejs.dev/config/
