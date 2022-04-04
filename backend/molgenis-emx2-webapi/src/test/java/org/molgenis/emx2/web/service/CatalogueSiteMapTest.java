77b983df3ce07432a29cf9a3ea0de0776f919c6b
connoratrug <47183404+connoratrug@users.noreply.github.com>
2022-04-04 09:59:15 +0200

    fix: Resource paths use capitalised names (#1127)

--- backend/molgenis-emx2-webapi/src/test/java/org/molgenis/emx2/web/service/CatalogueSiteMapTest.java
+++ backend/molgenis-emx2-webapi/src/test/java/org/molgenis/emx2/web/service/CatalogueSiteMapTest.java
@@ -28,7 +28,7 @@ public class CatalogueSiteMapTest {
         "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
             + "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" >\n"
             + "  <url>\n"
-            + "    <loc>https://my/base/url/ssr-catalogue/cohorts/my-cohort-pid</loc>\n"
+            + "    <loc>https://my/base/url/ssr-catalogue/Cohorts/my-cohort-pid</loc>\n"
             + "  </url>\n"
             + "</urlset>";
     assertEquals(expected, catalogueSiteMap.buildSiteMap());
