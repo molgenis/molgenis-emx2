f1bfb2f5c66ad6f1125ddd1c71116222a142998b
connoratrug <c.stroomberg@rug.nl>
2022-04-15 16:53:41 +0200

    Dynamic form input

---
+++ apps/molgenis-components/src/components/forms/ArrayInput.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div v-for="(value, index) in values" :key="index">
+      <component
+        :is="inputType"
+        :id="id + index"
+        v-model="values[index]"
+        @updateInput="$emit('input', values)"
+        :showAddButton="index === values.length"
+      >
+        <template v-slot:append>
+          <button
+            v-if="values.length > 1"
+            @click="clearInput(index)"
+            class="btn btn-outline-primary"
+            type="button"
+          >
+            <i class="fas fa-fw fa-times"></i>
+          </button>
+          <button
+            @click="addItem(index)"
+            class="btn btn-outline-primary"
+            type="button"
+          >
+            <i class="fas fa-fw fa-plus"></i>
+          </button>
+        </template>
+      </component>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseInput from "./BaseInput.vue";
+import InputString from "./InputString.vue";
+
+export default {
+  name: "ArrayInput",
+  extends: BaseInput,
+  data() {
+    return { values: this.value || [null] };
+  },
+  computed: {
+    inputType() {
+      return {
+        STRING_ARRAY: InputString,
+      }[this.type];
+    },
+  },
+  methods: {
+    addItem(index) {
+      this.values.splice(index + 1, 0, null)
+      this.$emit("input", this.values);
+    },
+    clearInput(index) {
+      if (this.values.length > 1) {
+        this.values.splice(index , 1);
+      }
+      this.$emit("input", this.values);
+    },
+  },
+};
+</script>
