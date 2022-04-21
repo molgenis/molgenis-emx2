e911c7179f642c71b290c5d9e8a6d024ad7a01c8
JonathanKlimp <78917640+JonathanKlimp@users.noreply.github.com>
2022-04-21 12:43:42 +0200

    removed matching and filtering classes from the backend. filtering is pre-processing. Matching happens client side

--- backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/semantics/gendecs/StarRating.java
+++
@@ -1,8 +0,0 @@
-package org.molgenis.emx2.semantics.gendecs;
-
-public enum StarRating {
-  ZEROSTAR,
-  ONESTAR,
-  TWOSTAR,
-  THREESTAR
-}
--- backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/GendecsApi.java
+++ backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/GendecsApi.java
@@ -7,7 +7,6 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import java.util.ArrayList;
 import org.molgenis.emx2.semantics.gendecs.*;
-import org.molgenis.emx2.semantics.gendecs.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Request;
@@ -15,12 +14,9 @@ import spark.Response;
 
 public class GendecsApi {
   private static final Logger logger = LoggerFactory.getLogger(GendecsApi.class);
-  private static final ArrayList<HpoTerm> hpoTermsObjects = new ArrayList<>();
-  private static ArrayList<Variant> variantsList = new ArrayList<>();
 
   public static void create() {
     post("/:schema/api/gendecs/queryHpo", GendecsApi::queryHpo);
-    post("/:scheme/api/gendecs/vcffile", GendecsApi::matchVcfWithHpo);
   }
 
   private static String queryHpo(Request request, Response response) {
@@ -41,36 +37,6 @@ public class GendecsApi {
       ArrayList<String> hpoTermChildren = owlQuerier.getSubClasses();
       hpoTerm.setChildren(hpoTermChildren);
     }
-    hpoTermsObjects.add(hpoTerm);
     return Serialize.serializeHpo(hpoTerm);
-  }
-
-  private static String matchVcfWithHpo(Request request, Response response) {
-    JsonObject jsonObject = JsonParser.parseString(request.body()).getAsJsonObject();
-    JsonArray hpoTermsIn = jsonObject.get("hpoTerms").getAsJsonArray();
-
-    if (hpoTermsObjects.size() == 0) {
-      for (int i = 0; i < hpoTermsIn.size(); i++) {
-        HpoTerm hpoTerm = new HpoTerm(hpoTermsIn.get(i).getAsString());
-        hpoTermsObjects.add(hpoTerm);
-      }
-    }
-    String variants = getVariants();
-
-    hpoTermsObjects.clear();
-    return variants;
-  }
-
-  private static String getVariants() {
-    StarRating starRating = StarRating.ONESTAR;
-    ClinvarFilter clinvarFilter = new ClinvarFilter(starRating);
-    String filteredClinvar = clinvarFilter.removeStatus();
-
-    logger.info("Removed " + starRating + " and lower from " + Constants.FILENAMECLINVAR);
-    logger.info("Matching variants with the entered HPO terms");
-    logger.debug("Matching variants with the following HPO terms: " + hpoTermsObjects);
-    HpoMatcher hpoMatcher = new HpoMatcher(hpoTermsObjects, filteredClinvar);
-    variantsList = hpoMatcher.getHpoMatches();
-    return Serialize.serialzeVariants(variantsList);
   }
 }
