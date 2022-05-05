84f3c5e147fd2084bfe7ff4000290084e9cd48ca
mswertz <m.a.swertz@rug.nl>
2022-05-05 12:32:57 +0200

    undo changes

--- apps/molgenis-components/src/client/client.js
+++ apps/molgenis-components/src/client/client.js
@@ -147,14 +147,6 @@ const fetchTableData = async (
   return resp.data.data;
 };
 
-const request = async (url, graqphl) => {
-  const result = await axios.post(url, { query: graqphl }).catch((error) => {
-    console.log(JSON.stringify(error));
-    return error;
-  });
-  return result.data.data;
-};
-
 export default {
   newClient: (graphqlURL, externalAxios) => {
     const myAxios = externalAxios || axios;
