d66bc7c7bf381481d5646eb990e48781c08a995f
connoratrug <47183404+connoratrug@users.noreply.github.com>
2022-05-09 11:58:38 +0200

    fix: Add missing components to export (#1236)

--- apps/molgenis-components/lib/main.js
+++ apps/molgenis-components/lib/main.js
@@ -50,9 +50,13 @@ import Breadcrumb from "../src/components/layout/Breadcrumb.vue";
 import LayoutCard from "../src/components/layout/LayoutCard.vue";
 import LayoutForm from "../src/components/layout/LayoutForm.vue";
 import LayoutModal from "../src/components/layout/LayoutModal.vue";
+import Molgenis from "../src/components/layout/Molgenis.vue";
+import MolgenisAccount from "../src/components/layout/MolgenisAccount.vue";
 import MolgenisFooter from "../src/components/layout/MolgenisFooter.vue";
 import MolgenisMenu from "../src/components/layout/MolgenisMenu.vue";
+import MolgenisSession from "../src/components/layout/MolgenisSession.vue";
 import MolgenisSignin from "../src/components/layout/MolgenisSignin.vue";
+import MolgenisSignup from "../src/components/layout/MolgenisSignup.vue";
 import ReadMore from "../src/components/layout/ReadMore.vue";
 import ShowMore from "../src/components/layout/ShowMore.vue";
 import Spinner from "../src/components/layout/Spinner.vue";
@@ -125,7 +129,12 @@ export {
   DataDisplayCell,
   ExplorerTable,
   TableMolgenis,
+  Molgenis,
+  MolgenisAccount,
   MolgenisFooter,
+  MolgenisSession,
+  MolgenisSignin,
+  MolgenisSignup,
   Pagination,
   FileDisplay,
   ListDisplay,
@@ -133,7 +142,6 @@ export {
   StringDisplay,
   TextDisplay,
   ShowMore,
-  MolgenisSignin,
   Client,
   LayoutCard,
 };
