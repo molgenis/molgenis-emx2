cfead57e2ff7212b0e9d80c1eb97a83c6481a424
Postema Max <max@elynor.com>
2022-05-10 13:37:50 +0200

    feat: add InputFile component

---
+++ apps/molgenis-components/src/components/forms/InputFile.vue
@@ -0,0 +1,117 @@
+<template>
+  <FormGroup :id="id" :label="label" :description="description">
+    <div class="input-group">
+      <input
+        :id="id"
+        ref="file"
+        type="file"
+        style="display: none"
+        @change="handleFileUpload"
+      />
+      <input
+        class="form-control active"
+        :placeholder="filename"
+        @click="$refs.file.click()"
+        @keydown.prevent
+      />
+      <div class="input-group-append">
+        <button
+          class="btn bg-transparent text-primary"
+          type="button"
+          title="Toggle"
+          data-toggle
+          @click="clearInput"
+          style="margin-left: -40px; z-index: 100"
+        >
+          <i class="fa fa-times">
+            <span aria-hidden="true" class="sr-only">Clear</span>
+          </i>
+        </button>
+      </div>
+      <div class="input-group-append">
+        <button
+          class="btn"
+          :class="{
+            'btn-outline-primary': !errorMessage,
+            'btn-outline-danger': errorMessage,
+          }"
+          type="button"
+          title="Toggle"
+          data-toggle
+          @click="$refs.file.click()"
+        >
+          Browse
+        </button>
+      </div>
+    </div>
+    <div v-for="val in valueArray" :key="JSON.stringify(val)">
+      <a v-if="val && val.url" :href="val.url">
+        Previous value: {{ name }}.{{ val.extension }}
+      </a>
+    </div>
+  </form-group>
+</template>
+
+<style scoped>
+.form-control.is-invalid {
+  background-image: none;
+}
+</style>
+
+<script>
+import _baseInput from "./_baseInput.vue";
+import FormGroup from "./_formGroup";
+
+export default {
+  extends: _baseInput,
+  components: { FormGroup },
+  computed: {
+    filename() {
+      if (this.value) return this.value.name;
+      return null;
+    },
+  },
+  methods: {
+    handleFileUpload() {
+      this.value = this.$refs.file.files[0];
+      this.$emit("input", this.value);
+    },
+    clearInput() {
+      this.$refs.file.value = "";
+      this.value = null;
+      this.$emit("input", this.value);
+    },
+  },
+};
+</script>
+
+<docs>
+Example
+```
+<template>
+  <div>
+    <InputFile label="My file input" v-model="check"/>
+    Selected: {{ check }}
+  </div>
+</template>
+<script>
+  export default {
+    data: function () {
+      return {
+        check: null
+      };
+    },
+    methods: {
+      clear() {
+        this.check = null;
+      }
+    }
+  };
+</script>
+```
+
+Example with error
+```
+<InputFile label="My file input" errorMessage="Some error with files"/>
+```
+</docs>
