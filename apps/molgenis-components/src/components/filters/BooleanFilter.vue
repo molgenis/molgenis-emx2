e6596157d90182fdb036c0643740251f5b9c4140
connoratrug <47183404+connoratrug@users.noreply.github.com>
2022-05-17 20:24:46 +0200

    feat: Components boolean filter (#1276)

---
+++ apps/molgenis-components/src/components/filters/BooleanFilter.vue
@@ -0,0 +1,43 @@
+<template>
+  <InputBoolean
+    :id="id"
+    :value="condition"
+    @input="onUpdateCondition"
+    @clearCondition="$emit('clearCondition')"
+  />
+</template>
+
+<script>
+import InputBoolean from "../forms/InputBoolean.vue";
+
+export default {
+  name: "BooleanFilter",
+  components: { InputBoolean },
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: false,
+      default: function () {
+        return this.id;
+      },
+    },
+    condition: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  methods: {
+    onUpdateCondition(newValue) {
+      if (newValue === null) {
+        this.$emit("clearCondition", newValue);
+      } else {
+        this.$emit("updateCondition", newValue);
+      }
+    },
+  },
+};
+</script>
