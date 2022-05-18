7a6477686a614d51f4d6408e212fefb88b9fe219
Joris de Keijser <chinook25@gmail.com>
2022-05-18 12:08:10 +0200

    chore: revert type change

--- apps/molgenis-components/src/components/forms/baseInputs/BaseInput.vue
+++ apps/molgenis-components/src/components/forms/baseInputs/BaseInput.vue
@@ -48,9 +48,10 @@ export default {
      * A string specifying the type of control to render.
      * (note: no direct relation to emx2 type, emx2 types are implemented as vue components)
      */
-    columnType: {
+    type: {
       type: String,
       required: false,
+      default: () => "text",
     },
     required: {
       type: Boolean,
