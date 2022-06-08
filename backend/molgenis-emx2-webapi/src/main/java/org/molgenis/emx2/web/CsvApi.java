4e5a360f9ef6b49784bda310b84ad6ebe1f9d6cf
K. Joeri van der Velde <joeriv@gmail.com>
2022-06-08 15:10:55 +0200

    Fix for #894

--- backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/CsvApi.java
+++ backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/CsvApi.java
@@ -6,8 +6,10 @@ import static spark.Spark.*;
 
 import java.io.*;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 import org.molgenis.emx2.*;
 import org.molgenis.emx2.io.emx2.Emx2;
 import org.molgenis.emx2.io.readers.CsvTableReader;
@@ -50,10 +52,52 @@ public class CsvApi {
   }
 
   static String mergeMetadata(Request request, Response response) {
-    SchemaMetadata schema = Emx2.fromRowList(getRowList(request));
-    getSchema(request).migrate(schema);
-    response.status(200);
-    return "add/update metadata success";
+    try {
+      SchemaMetadata schema = Emx2.fromRowList(getRowList(request));
+      getSchema(request).migrate(schema);
+      response.status(200);
+      return "add/update metadata success";
+    } catch (MolgenisException me) {
+      // the upload might actual table data instead of metadata
+      // but, we don't know which table because there is no filename (no multipart form)
+      // therefore, use findTableByColumns() to figure out where the data must go
+      Iterable<Row> rows = getRowList(request);
+      Row firstRow = rows.iterator().next();
+      Table t = findTableByColumns(firstRow.getColumnNames(), getSchema(request));
+      int count = t.save(getRowList(request));
+      response.status(200);
+      response.type(ACCEPT_CSV);
+      return "" + count;
+    }
+  }
+
+  /**
+   * Intersect a list of column names with the column names of tables of a specified schema. If we
+   * find exactly one table that contains all of these columns, that table is returned.
+   *
+   * @param checkColNames
+   * @param schema
+   * @return
+   */
+  static Table findTableByColumns(Set<String> checkColNames, Schema schema) {
+    List<Table> candidateTables = new ArrayList<>();
+    for (String tableName : schema.getTableNames()) {
+      Table t = schema.getTable(tableName);
+      List<String> intersect = new ArrayList<>();
+      for (String colName : t.getMetadata().getColumnNames()) {
+        if (checkColNames.contains(colName)) {
+          intersect.add(colName);
+        }
+      }
+      if (checkColNames.size() == intersect.size()) {
+        candidateTables.add(t);
+      }
+    }
+    if (candidateTables.size() == 1) {
+      return candidateTables.get(0);
+    } else {
+      return null;
+    }
   }
 
   static String getMetadata(Request request, Response response) throws IOException {
