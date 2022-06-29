1735068bcd9f681c885975da1578fcafcc670da4
connoratrug <47183404+connoratrug@users.noreply.github.com>
2022-06-29 10:37:50 +0200

    fix: ssr build
    cannot pass attribute via nuxt config

--- apps/nuxt-ssr/nuxt.config.js
+++ apps/nuxt-ssr/nuxt.config.js
@@ -18,7 +18,7 @@ export default {
       { rel: "stylesheet", href: "theme.css" },
     ],
     script: [
-      {  src: "https://unpkg.com/@molgenis/expressions", crossorigin="anonymous" },
+      {  src: "https://unpkg.com/@molgenis/expressions" },
     ],
   },
 
