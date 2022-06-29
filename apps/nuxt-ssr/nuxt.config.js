804710fde139e57e7e41cb7afee4c917ee3bf537
connoratrug <47183404+connoratrug@users.noreply.github.com>
2022-06-29 13:06:01 +0200

    fix: test cors attr for scripts

--- apps/nuxt-ssr/nuxt.config.js
+++ apps/nuxt-ssr/nuxt.config.js
@@ -24,6 +24,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ["assets/css/style.css"],
+  
+  crossorigin: "anonymous",
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
