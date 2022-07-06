a736f19f5d9b25d0c1d32f642cf94159e6539218
K. Joeri van der Velde <joeriv@gmail.com>
2022-07-06 16:37:07 +0200

    Added JUnit tests for various FDP endpoints

--- backend/molgenis-emx2-semantics/src/test/java/org/molgenis/emx2/semantics/FAIRDataPointTest.java
+++ backend/molgenis-emx2-semantics/src/test/java/org/molgenis/emx2/semantics/FAIRDataPointTest.java
@@ -13,6 +13,9 @@ import org.molgenis.emx2.Database;
 import org.molgenis.emx2.Schema;
 import org.molgenis.emx2.datamodels.FAIRDataHubLoader;
 import org.molgenis.emx2.fairdatapoint.FAIRDataPoint;
+import org.molgenis.emx2.fairdatapoint.FAIRDataPointCatalog;
+import org.molgenis.emx2.fairdatapoint.FAIRDataPointDataset;
+import org.molgenis.emx2.fairdatapoint.FAIRDataPointDistribution;
 import org.molgenis.emx2.sql.TestDatabaseFactory;
 import spark.Request;
 
@@ -40,7 +43,6 @@ public class FAIRDataPointTest {
     when(request.url()).thenReturn("http://localhost:8080/api/fdp");
     FAIRDataPoint fairDataPoint = new FAIRDataPoint(request, fairDataHubSchemas);
     String result = fairDataPoint.getResult();
-    System.out.println(result);
     assertTrue(
         result.contains(
             """
@@ -59,5 +61,79 @@ public class FAIRDataPointTest {
                   <http://localhost:8080/api/fdp/catalog/fairDataHub_nr2/minCatId03> ."""
                 .indent(2)));
     assertEquals(4082, result.length());
+  }
+
+  @Test
+  public void FDPCatalog() throws Exception {
+    Request request = mock(Request.class);
+    when(request.url())
+        .thenReturn("http://localhost:8080/api/fdp/catalog/fairDataHub_nr1/catalogId01");
+    when(request.params("id")).thenReturn("catalogId01");
+    FAIRDataPointCatalog fairDataPointCatalog =
+        new FAIRDataPointCatalog(request, fairDataHubSchemas.get(0).getTable("FDP_Catalog"));
+    String result = fairDataPointCatalog.getResult();
+    assertTrue(
+        result.contains(
+            "dcterms:hasPart <http://localhost:8080/api/fdp/dataset/fairDataHub_nr1/datasetId01>,\n"
+                + "    <http://localhost:8080/api/fdp/dataset/fairDataHub_nr1/datasetId02>;"));
+    assertTrue(
+        result.contains(
+            """
+            dcterms:rights [ a dcterms:RightsStatement;
+                  dcterms:description "Rights are provided on a per-dataset basis."
+                ];"""));
+    assertEquals(1812, result.length());
+  }
+
+  @Test
+  public void FDPDataset() throws Exception {
+    Request request = mock(Request.class);
+    when(request.url())
+        .thenReturn("http://localhost:8080/api/fdp/dataset/fairDataHub_nr1/datasetId01");
+    when(request.params("id")).thenReturn("datasetId01");
+    FAIRDataPointDataset fairDataPointDataset =
+        new FAIRDataPointDataset(request, fairDataHubSchemas.get(0).getTable("FDP_Dataset"));
+    String result = fairDataPointDataset.getResult();
+    assertTrue(
+        result.contains(
+            """
+                        dcat:Distribution <http://localhost:8080/api/fdp/distribution/fairDataHub_nr1/Analyses/csv>,
+                            <http://localhost:8080/api/fdp/distribution/fairDataHub_nr1/Analyses/excel>, <http://localhost:8080/api/fdp/distribution/fairDataHub_nr1/Analyses/jsonld>,
+                            <http://localhost:8080/api/fdp/distribution/fairDataHub_nr1/Analyses/ttl>, <http://localhost:8080/api/fdp/distribution/fairDataHub_nr1/Analyses/zip>;"""));
+    assertTrue(
+        result.contains(
+            """
+                    dcterms:accrualPeriodicity "datasetAccrualPeriodicity01";
+                      dcterms:spatial <https://www.iso.org/obp/ui/#iso:code:3166:FR>, <https://www.iso.org/obp/ui/#iso:code:3166:ES>;
+                      dcat:spatialResolutionInMeters 1.0E1;"""));
+    assertTrue(
+        result.contains(
+            "dcterms:language <http://www.lexvo.org/data/iso639-3/dut>, <http://www.lexvo.org/data/iso639-3/eng>;"));
+
+    assertEquals(2211, result.length());
+  }
+
+  @Test
+  public void FDPDistribution() throws Exception {
+    Request request = mock(Request.class);
+    when(request.url())
+        .thenReturn("http://localhost:8080/api/fdp/distribution/fairDataHub_nr1/Analyses/ttl");
+    when(request.params("schema")).thenReturn("fairDataHub_nr1");
+    when(request.params("table")).thenReturn("Analyses");
+    when(request.params("format")).thenReturn("ttl");
+    FAIRDataPointDistribution fairDataPointDistribution =
+        new FAIRDataPointDistribution(request, database);
+    String result = fairDataPointDistribution.getResult();
+    assertTrue(
+        result.contains(
+            """
+            <http://localhost:8080/api/fdp/distribution/fairDataHub_nr1/Analyses/ttl> a dcat:Distribution;
+              dcterms:title "Data distribution for http://localhost:8080/api/fdp/distribution/fairDataHub_nr1/Analyses/ttl";
+              dcterms:description "MOLGENIS EMX2 data distribution at http://localhost:8080/ for table Analyses in schema fairDataHub_nr1, formatted as ttl.";
+              dcat:downloadURL <http://localhost:8080/fairDataHub_nr1/api/ttl/Analyses>;
+              dcat:mediaType <https://www.iana.org/assignments/media-types/text/turtle>;
+              dcterms:format "ttl";
+              dcterms:conformsTo <https://www.w3.org/TR/vocab-dcat-2/#Class:Distribution> ."""));
+    assertEquals(813, result.length());
   }
 }
