4338d9f84b289485e133988ea83f57ad05a4998a
connoratrug <c.stroomberg@rug.nl>
2022-07-11 17:47:37 +0200

    show changes in settings app

---
+++ apps/settings/src/components/LogViewer.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <h1>Changes</h1>
+    <table-display :columns="columns" :rows="changes" />
+  </div>
+</template>
+
+<script>
+import { request } from "graphql-request";
+import { TableDisplay } from "@mswertz/emx2-styleguide";
+
+export default {
+  name: "LogViewer",
+  components: { TableDisplay },
+  data() {
+    return {
+      columns: [
+        { name: "operation", label: "Action" },
+        { name: "userId", label: "Who" },
+        { name: "stamp", label: "When" },
+        { name: "oldRowData", label: "Old" },
+        { name: "newRowData", label: "New" },
+      ],
+      changes: [],
+    };
+  },
+  methods: {
+    async fetchChanges() {
+      const resp = await request(
+        "/graphql",
+        `{_changes {operation, stamp, userId, oldRowData, newRowData}}`
+      );
+      this.changes = resp._changes
+        .map(this.formatOperation)
+        .map(this.formatDateTime)
+        .map(this.formatRowData);
+    },
+    formatOperation(change) {
+      if (change.operation === "I") {
+        change.operation = "create";
+      } else if (change.operation === "U") {
+        change.operation = "update";
+      } else if (change.operation === "D") {
+        change.operation = "delete";
+      } else {
+        change.operation = "unknown action";
+      }
+
+      return change;
+    },
+    formatDateTime(change) {
+      change.timeStamp = new Date(change.timeStamp).toLocaleString();
+      return change;
+    },
+    formatRowData(change) {
+      change.oldRowData = change.oldRowData ? JSON.stringify(change.oldRowData, null, 2) : '-';
+      change.newRowData = change.newRowData ? JSON.stringify(change.newRowData, null, 2) : '-';
+      return change;
+    },
+  },
+  mounted: function () {
+    this.fetchChanges();
+  },
+};
+</script>
+
+<style>
+</style>
