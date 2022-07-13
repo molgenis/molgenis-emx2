b3fce1aa6baaecfd0fe48ae0800ab402dfb5625c
mswertz <m.a.swertz@rug.nl>
2022-07-13 11:50:06 +0200

    deduplicate code

--- backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/DataCatalogueCohortStagingLoader.java
+++ backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/DataCatalogueCohortStagingLoader.java
@@ -5,7 +5,6 @@ import static org.molgenis.emx2.datamodels.DataCatalogueNetworkStagingLoader.SHA
 
 import org.molgenis.emx2.Database;
 import org.molgenis.emx2.Schema;
-import org.molgenis.emx2.io.MolgenisIO;
 
 public class DataCatalogueCohortStagingLoader implements AvailableDataModels.DataModelLoader {
 
@@ -13,18 +12,17 @@ public class DataCatalogueCohortStagingLoader implements AvailableDataModels.Dat
 
   @Override
   public void load(Schema schema, boolean includeDemoData) {
-    // create ontology schema
-    Database db = schema.getDatabase();
+    // create shared schemas
+    createSharedSchema(schema.getDatabase());
+    // create the schema
+    createSchema(schema, "datacatalogue/stagingCohorts/molgenis.csv");
+  }
 
-    Schema ontologySchema = db.getSchema(CATALOGUE_ONTOLOGIES);
-    if (ontologySchema == null) {
-      ontologySchema = db.createSchema(CATALOGUE_ONTOLOGIES);
-    }
-
+  static void createSharedSchema(Database db) {
+    // create DataCatalogue and CatalogueOntologies
     Schema dataCatalogueSchema = db.getSchema(DATA_CATALOGHUE);
     if (dataCatalogueSchema == null) {
-      dataCatalogueSchema = db.createSchema(DATA_CATALOGHUE);
-      createSchema(dataCatalogueSchema, "datacatalogue/molgenis.csv");
+      new DataCatalogueLoader().load(db.createSchema(DATA_CATALOGHUE), false);
     }
 
     Schema sharedSchema = db.getSchema(SHARED_STAGING);
@@ -33,11 +31,5 @@ public class DataCatalogueCohortStagingLoader implements AvailableDataModels.Dat
       // create the shared schema
       createSchema(sharedSchema, "datacatalogue/stagingShared/molgenis.csv");
     }
-
-    // create the schema
-    createSchema(schema, "datacatalogue/stagingCohorts/molgenis.csv");
-
-    // load data into ontology schema
-    MolgenisIO.fromClasspathDirectory("datacatalogue/CatalogueOntologies", ontologySchema, false);
   }
 }
--- backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/DataCatalogueNetworkStagingLoader.java
+++ backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/DataCatalogueNetworkStagingLoader.java
@@ -1,12 +1,9 @@
 package org.molgenis.emx2.datamodels;
 
-import static org.molgenis.emx2.datamodels.DataCatalogueCohortStagingLoader.DATA_CATALOGHUE;
-import static org.molgenis.emx2.datamodels.DataCatalogueLoader.CATALOGUE_ONTOLOGIES;
+import static org.molgenis.emx2.datamodels.DataCatalogueCohortStagingLoader.createSharedSchema;
 import static org.molgenis.emx2.datamodels.DataCatalogueLoader.createSchema;
 
-import org.molgenis.emx2.Database;
 import org.molgenis.emx2.Schema;
-import org.molgenis.emx2.io.MolgenisIO;
 
 public class DataCatalogueNetworkStagingLoader implements AvailableDataModels.DataModelLoader {
 
@@ -14,30 +11,10 @@ public class DataCatalogueNetworkStagingLoader implements AvailableDataModels.Da
 
   @Override
   public void load(Schema schema, boolean includeDemoData) {
-    // create ontology schema
-    Database db = schema.getDatabase();
-    Schema ontologySchema = db.getSchema(CATALOGUE_ONTOLOGIES);
-    if (ontologySchema == null) {
-      ontologySchema = db.createSchema(CATALOGUE_ONTOLOGIES);
-    }
-
-    Schema dataCatalogueSchema = db.getSchema(DATA_CATALOGHUE);
-    if (dataCatalogueSchema == null) {
-      dataCatalogueSchema = db.createSchema(DATA_CATALOGHUE);
-      createSchema(dataCatalogueSchema, "datacatalogue/molgenis.csv");
-    }
-
-    Schema sharedSchema = db.getSchema(SHARED_STAGING);
-    if (sharedSchema == null) {
-      sharedSchema = db.createSchema(SHARED_STAGING);
-      // create the shared schema
-      createSchema(sharedSchema, "datacatalogue/stagingShared/molgenis.csv");
-    }
+    // create shared schemas
+    createSharedSchema(schema.getDatabase());
 
     // create the schema
     createSchema(schema, "datacatalogue/stagingNetworks/molgenis.csv");
-
-    // load data into ontology schema
-    MolgenisIO.fromClasspathDirectory("datacatalogue/CatalogueOntologies", ontologySchema, false);
   }
 }
