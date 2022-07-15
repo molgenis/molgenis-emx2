b01479e3bfbd23354c16b094e8d98cf8292d32c3
connoratrug <47183404+connoratrug@users.noreply.github.com>
2022-07-15 15:19:52 +0200

    feat: Log insert, update and delete of row data (#1382)
    By adding the setting 'CHANGELOG_SCHEMAS' at the system level the creation of a change-log for a schema can be set.
    The value of the 'CHANGELOG_SCHEMAS' should be a comma separated list of schema names
    When a schema is created with a name that is part of the list set in CHANGELOG_SCHEMAS a change-log is created and maintained
    
    
    Co-authored-by: Morris Swertz <m.a.swertz@rug.nl>

--- backend/molgenis-emx2-graphql/src/main/java/org/molgenis/emx2/graphql/GraphqlApiFactory.java
+++ backend/molgenis-emx2-graphql/src/main/java/org/molgenis/emx2/graphql/GraphqlApiFactory.java
@@ -231,6 +231,12 @@ public class GraphqlApiFactory {
     queryBuilder.field(schemaFields.settingsQuery(schema));
     mutationBuilder.field(schemaFields.changeMutation(schema));
     mutationBuilder.field(schemaFields.dropMutation(schema));
+    if ((schema.getRoleForActiveUser() != null
+            && schema.getRoleForActiveUser().equals(Privileges.MANAGER.toString()))
+        || schema.getDatabase().isAdmin()) {
+      queryBuilder.field(schemaFields.changeLogQuery(schema));
+      queryBuilder.field(schemaFields.changeLogCountQuery(schema));
+    }
 
     // table
     GraphqlTableFieldFactory tableField = new GraphqlTableFieldFactory();
