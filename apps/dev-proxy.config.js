abc191efab3ca2e2cb0937263e06ec4f1c61081f
mswertz <m.a.swertz@rug.nl>
2022-07-26 22:54:01 +0200

    undo commit of dev-proxy

--- apps/dev-proxy.config.js
+++ apps/dev-proxy.config.js
@@ -1,7 +1,7 @@
-const HOST = process.env.MOLGENIS_APPS_HOST || "http://127.0.0.1:8080";
+const HOST = process.env.MOLGENIS_APPS_HOST || "https://emx2.dev.molgenis.org";
 const SCHEMA = process.env.MOLGENIS_APPS_SCHEMA || "pet%20store";
 
-const opts = { changeOrigin: true, secure: false, logLevel: 'debug' };
+const opts = { changeOrigin: true, secure: false };
 
 module.exports = {
   "/graphql": {
@@ -11,5 +11,5 @@ module.exports = {
   "**/graphql": { target: `${HOST}`, ...opts },
   "/api": { target: `${HOST}`, ...opts },
   "/apps": { target: `${HOST}`, ...opts },
-  "/theme.css": { target: `${HOST}/apps/central/theme.css`, ...opts },
+  "/theme.css": { target: `${HOST}/${SCHEMA}`, ...opts },
 };
