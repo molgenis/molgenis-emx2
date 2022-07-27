ff71f5a5883df7dfe0f69e90d1d780f09236ca2e
mswertz <m.a.swertz@rug.nl>
2022-07-27 15:56:50 +0200

    wip

--- backend/molgenis-emx2-datamodels/src/test/java/org/molgenis/emx2/datamodels/TestLoaders.java
+++ backend/molgenis-emx2-datamodels/src/test/java/org/molgenis/emx2/datamodels/TestLoaders.java
@@ -42,7 +42,7 @@ public class TestLoaders {
   @Test
   public void testDataCatalogueLoader() {
     AvailableDataModels.DATA_CATALOGUE.install(dataCatalogue, true);
-    assertEquals(43, dataCatalogue.getTableNames().size());
+    assertEquals(48, dataCatalogue.getTableNames().size());
   }
 
   @Test
