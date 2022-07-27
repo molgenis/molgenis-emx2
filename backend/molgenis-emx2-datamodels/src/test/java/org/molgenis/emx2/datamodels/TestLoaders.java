ab288c9f6db8c44e5b8255d990758f7d5fcc822e
mswertz <m.a.swertz@rug.nl>
2022-07-27 12:19:40 +0200

    wip

--- backend/molgenis-emx2-datamodels/src/test/java/org/molgenis/emx2/datamodels/TestLoaders.java
+++ backend/molgenis-emx2-datamodels/src/test/java/org/molgenis/emx2/datamodels/TestLoaders.java
@@ -42,7 +42,7 @@ public class TestLoaders {
   @Test
   public void testDataCatalogueLoader() {
     AvailableDataModels.DATA_CATALOGUE.install(dataCatalogue, true);
-    assertEquals(39, dataCatalogue.getTableNames().size());
+    assertEquals(43, dataCatalogue.getTableNames().size());
   }
 
   @Test
