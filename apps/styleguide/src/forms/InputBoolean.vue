c78aa6cfa217944dcd629c405b86a828229760f7
Morris Swertz <m.a.swertz@rug.nl>
2022-08-18 22:22:06 +0200

    fix: in styleguide boolean input did not return typeof boolean but typeof string (#1526)
    * build: try fix of missing cross-env and/or vue-cli by upgrading npm/yarn/node
    
    * fix: couldn't set boolean to null or false
    
    * keep listeners
    
    * Update _baseInput.vue

--- apps/styleguide/src/forms/InputBoolean.vue
+++ apps/styleguide/src/forms/InputBoolean.vue
@@ -2,7 +2,7 @@
   <div v-if="inplace">
     <div
       class="form-check form-check-inline"
-      :class="{'is-invalid': errorMessage}"
+      :class="{ 'is-invalid': errorMessage }"
     >
       <input
         :id="id"
@@ -25,12 +25,21 @@
 </template>
 
 <script>
-import BaseInput from './_baseInput.vue';
-import InputRadio from './InputRadio';
+import BaseInput from "./_baseInput.vue";
+import InputRadio from "./InputRadio";
 
 export default {
-  components: {InputRadio},
-  extends: BaseInput
+  components: { InputRadio },
+  extends: BaseInput,
+  methods: {
+    emitValue() {
+      console.log("input " + this.valueArray);
+      this.$emit(
+        "input",
+        this.valueArray[0] ? this.valueArray[0] === "true" : null
+      );
+    },
+  },
 };
 </script>
 
@@ -40,14 +49,15 @@ Example
 <template>
   <div>
     <InputBoolean v-model="value" label="My first boolean" description="do you need some boolean help?"/>
-    value: {{ value }}
+    value: {{ value }} type: {{ typeof value }}
+
   </div>
 </template>
 <script>
   export default {
     data() {
       return {
-        value: null
+        value: false
       }
     }
   }
