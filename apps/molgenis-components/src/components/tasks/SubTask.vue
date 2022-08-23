796e9429ab55fb3394e006f06eb241ed7b4669b7
mswertz <m.a.swertz@rug.nl>
2022-08-23 23:39:32 +0200

    feat[molgenis-components]: added task progress monitoring component

---
+++ apps/molgenis-components/src/components/tasks/SubTask.vue
@@ -0,0 +1,54 @@
+<template>
+  <ul class="fa-ul">
+    <li :class="color" v-if="task">
+      <i class="fa-li fa" :class="icon"></i>
+      {{ task.description }}
+      <SubTask
+        :task="subtask"
+        v-for="subtask in task.subTasks"
+        :key="subtask.id"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "SubTask",
+  props: {
+    task: Object,
+  },
+  computed: {
+    color() {
+      switch (this.task.status) {
+        case "COMPLETED":
+          return "text-success";
+        case "ERROR":
+          return "text-danger";
+        case "WARNING":
+          return "text-warning";
+        case "SKIPPED":
+          return "text-muted";
+        default:
+          return "text-primary";
+      }
+    },
+    icon() {
+      switch (this.task.status) {
+        case "COMPLETED":
+          return "fa-check";
+        case "ERROR":
+          return "fa-times";
+        case "WARNING":
+          return "fa-exclamation-circle";
+        case "SKIPPED":
+          return "fa-check";
+        case "RUNNING":
+          return "fa-spinner fa-spin";
+        default:
+          return "fa-question";
+      }
+    },
+  },
+};
+</script>
---
+++ apps/molgenis-components/src/components/tasks/Task.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <Spinner v-if="loading" />
+    <SubTask v-else-if="task" :task="task" />
+    <span v-else>Task with id = '{{ taskId }}' not found</span>
+  </div>
+</template>
+
+<script>
+import { request } from "graphql-request";
+import SubTask from "./SubTask.vue";
+import Spinner from "../layout/Spinner.vue";
+
+export default {
+  name: "Task",
+  components: {
+    SubTask,
+    Spinner,
+  },
+  props: {
+    taskId: String,
+  },
+  data() {
+    return {
+      task: null,
+      loading: true,
+      error: null,
+      success: null,
+    };
+  },
+  methods: {
+    startMonitorTask() {
+      if (
+        (!this.error && !this.task) ||
+        !["COMPLETED", "ERROR"].includes(this.task.status)
+      ) {
+        setTimeout(this.monitorTask, 500);
+      } else {
+        console.log("timed out");
+      }
+    },
+    monitorTask() {
+      request(
+        "graphql",
+        `{_tasks(id:"${this.taskId}"){id,description,status,subTasks{id,description,status,subTasks{id,description,status,subTasks{id,description,status}}}}}`
+      )
+        .then((data) => {
+          this.task = data._tasks[0];
+          this.loading = false;
+        })
+        .catch((error) => {
+          console.log(JSON.stringify(error));
+          if (Array.isArray(error.response.errors)) {
+            this.error = error.response.errors[0].message;
+          } else {
+            this.error = error;
+          }
+        });
+      this.startMonitorTask();
+    },
+  },
+  created() {
+    this.startMonitorTask();
+  },
+};
+</script>
+
+<docs>
+<template>
+  <demo-item>
+    <Task taskId="6956bf6d-3f78-4798-873f-dd5382ac0e24"/>
+  </demo-item>
+</template>
+</docs>
