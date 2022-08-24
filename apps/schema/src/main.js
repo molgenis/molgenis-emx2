fcd7a9429070f9a988847567e12bcba61fd8be4a
mswertz <m.a.swertz@rug.nl>
2022-08-25 01:01:49 +0200

    rename

--- apps/schema/src/main.js
+++ apps/schema/src/main.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
-import SchemaBeta from "./components/SchemaBeta.vue";
+import SchemaApp from "./components/SchemaBeta.vue";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes: [{ name: "simple", path: "/", component: SchemaBeta }],
+  routes: [{ name: "simple", path: "/", component: SchemaApp }],
 });
 
 new Vue({
