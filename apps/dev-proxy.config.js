3db69a23104b956baf37229f8d807ee9e3772823
mswertz <m.a.swertz@rug.nl>
2022-08-27 10:12:00 +0200

    fix dev-proxy to work with graphql in other schema

--- apps/dev-proxy.config.js
+++ apps/dev-proxy.config.js
@@ -1,15 +1,18 @@
-const HOST = process.env.MOLGENIS_APPS_HOST || "https://emx2.dev.molgenis.org";
-const SCHEMA = process.env.MOLGENIS_APPS_SCHEMA || "pet%20store";
+const HOST = process.env.MOLGENIS_APPS_HOST || "http://localhost:8080";
+const SCHEMA = process.env.MOLGENIS_APPS_SCHEMA || "fairgenomes";
 
-const opts = { changeOrigin: true, secure: false };
+const opts = { changeOrigin: true, secure: false, logLevel: "debug" };
 
 module.exports = {
   "/graphql": {
     target: `${HOST}/${SCHEMA}`,
     ...opts,
   },
-  "**/graphql": { target: `${HOST}`, ...opts },
+  "/theme.css": { target: `${HOST}/${SCHEMA}`, ...opts },
   "/api": { target: `${HOST}`, ...opts },
   "/apps": { target: `${HOST}`, ...opts },
-  "/theme.css": { target: `${HOST}/${SCHEMA}`, ...opts },
+  "^/[a-zA-Z0-9_.-]+/graphql": {
+    target: `${HOST}`,
+    ...opts,
+  },
 };
