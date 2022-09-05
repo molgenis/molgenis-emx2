f47aa91c2ac5150a3f30d7fb92543ff1bf5eedce
connoratrug <c.stroomberg@rug.nl>
2022-09-05 15:00:03 +0200

    fix: add missing id

--- apps/tables/src/components/ListTables.vue
+++ apps/tables/src/components/ListTables.vue
@@ -10,7 +10,7 @@
       Download all tables:
       <a href="../api/zip">zip</a> | <a href="../api/excel">excel</a> |
       <a href="../api/jsonld">jsonld</a> | <a href="../api/ttl">ttl</a><br />
-      <InputSearch placeholder="search by name" v-model="search" />
+      <InputSearch id="tables-list-search-input" placeholder="search by name" v-model="search" />
       <h2>Data tables</h2>
       <TablesTable v-if="tables.length > 0" :tables="tables" />
       <p v-else>No tables found</p>
