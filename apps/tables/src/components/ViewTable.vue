73b348380b66b434b86290574fd4bef26e54dbc9
Joris de Keijser <chinook25@gmail.com>
2022-09-07 17:03:04 +0200

    wip

--- apps/tables/src/components/ViewTable.vue
+++ apps/tables/src/components/ViewTable.vue
@@ -5,23 +5,9 @@
     </router-link>
     <TableExplorer
       :tableName="table"
-      :showColumns="showColumns"
-      :showFilters="showFilters"
-      :conditions="conditions"
-      :showPage="showPage"
-      :showLimit="showLimit"
-      :showOrderBy="showOrderBy"
-      :showOrder="showOrder"
       :key="timestamp"
       :canEdit="canEdit"
       :canManage="canManage"
-      @update:showColumns="updateColumns"
-      @update:showFilters="updateFilters"
-      @update:conditions="updateConditions"
-      @update:showPage="updatePage"
-      @update:showLimit="updateLimit"
-      @update:showOrderBy="updateOrderBy"
-      @update:showOrder="updateOrder"
     />
   </div>
 </template>
@@ -33,7 +19,7 @@ export default {
   props: {
     table: { type: String, required: true },
     schema: { type: Object, default: null },
-    session: { session: Object }
+    session: { session: Object },
   },
   data() {
     return { timestamp: Date.now(), query: {} };
@@ -41,171 +27,25 @@ export default {
   components: {
     TableExplorer,
   },
-  methods: {
-    updateOrderBy(showOrderBy) {
-      this.query = Object.assign({}, this.$route.query);
-      this.query._orderBy = showOrderBy;
-      if (JSON.stringify(this.query) != JSON.stringify(this.$route.query)) {
-        this.$router.push({ query: this.query });
-      }
-    },
-    updateOrder(showOrder) {
-      this.query = Object.assign({}, this.$route.query);
-      if (showOrder) {
-        this.query._order = showOrder;
-      }
-      if (JSON.stringify(this.query) != JSON.stringify(this.$route.query)) {
-        this.$router.push({ query: this.query });
-      }
-    },
-    updatePage(showPage) {
-      this.query = Object.assign({}, this.$route.query);
-      this.query._page = showPage;
-      if (JSON.stringify(this.query) != JSON.stringify(this.$route.query)) {
-        this.$router.push({ query: this.query });
-      }
-    },
-    updateLimit(showLimit) {
-      this.query = Object.assign({}, this.$route.query);
-      this.query._limit = showLimit;
-      if (JSON.stringify(this.query) != JSON.stringify(this.$route.query)) {
-        this.$router.push({ query: this.query });
-      }
-    },
-    updateColumns(showColumns) {
-      this.query = Object.assign({}, this.$route.query);
-      if (showColumns.length > 0) this.query._col = showColumns.join(",");
-      else delete this.query._col;
-      if (JSON.stringify(this.query) != JSON.stringify(this.$route.query)) {
-        this.$router.push({ query: this.query });
-      }
-    },
-    updateFilters(showFilters) {
-      this.query = Object.assign({}, this.$route.query);
-      if (showFilters.length > 0) this.query._filter = showFilters.join(",");
-      else delete this.query._filter;
-      if (JSON.stringify(this.query) != JSON.stringify(this.$route.query)) {
-        this.$router.push({ query: this.query });
-      }
-    },
-    updateConditions(conditions) {
-      this.query = Object.assign({}, this.$route.query);
-      this.activeTable.columns.forEach((c) => {
-        if (conditions[c.name]) {
-          if (["REF", "REF_ARRAY", "REFBACK"].includes(c.columnType)) {
-            //todo try to make this human readible too
-            this.query[c.name] = JSON.stringify(conditions[c.name]);
-          } else if (
-            ["DATE", "DATETIME", "INT", "DECIMAL"].includes(c.columnType)
-          ) {
-            this.query[c.name] = conditions[c.name]
-              .map((v) => v.join(".."))
-              .join(",");
-          } else {
-            this.query[c.name] = conditions[c.name].join(",");
-          }
-        } else {
-          delete this.query[c.name];
-        }
-      });
-      if (JSON.stringify(this.query) != JSON.stringify(this.$route.query)) {
-        this.$router.push({ query: this.query });
-      }
-    },
-  },
   computed: {
     canEdit() {
       return (
-        this.session &&
-        (this.session.email == 'admin' ||
-          (this.session.roles &&
-            (this.session.roles.includes('Editor') ||
-              this.session.roles.includes('Manager'))))
+        this.session?.email == "admin" ||
+        this.session?.roles?.includes("Editor") ||
+        this.session?.roles?.includes("Manager")
       );
     },
     canManage() {
       return (
-        this.session &&
-        (this.session.email == 'admin' ||
-          this.session.roles.includes('Manager'))
+        this.session?.email == "admin" ||
+        this.session?.roles?.includes("Manager")
       );
     },
     activeTable() {
       if (this.schema) {
         return this.schema.tables.filter((t) => t.name == this.table)[0];
-      }
-      return null;
-    },
-    showOrderBy() {
-      if (this.$route.query._orderBy) {
-        return this.$route.query._orderBy;
       } else {
         return null;
-      }
-    },
-    showOrder() {
-      if (this.$route.query._order) {
-        return this.$route.query._order;
-      } else {
-        return null;
-      }
-    },
-    showColumns() {
-      if (this.$route.query._col) {
-        if (Array.isArray(this.$route.query._col)) {
-          return this.$route.query._col;
-        } else {
-          return this.$route.query._col.split(",");
-        }
-      }
-      return null;
-    },
-    showFilters() {
-      if (this.$route.query._filter) {
-        if (Array.isArray(this.$route.query._filter)) {
-          return this.$route.query._filter;
-        } else {
-          return this.$route.query._filter.split(",");
-        }
-      }
-      return null;
-    },
-    showPage() {
-      if (this.$route.query._page) return parseInt(this.$route.query._page);
-      else return 1;
-    },
-    showLimit() {
-      if (this.$route.query._limit) return parseInt(this.$route.query._limit);
-      else return 20;
-    },
-    conditions() {
-      if (this.schema) {
-        let result = {};
-        //find the table and then iterate the colums
-        this.activeTable.columns.forEach((c) => {
-          if (this.$route.query[c.name]) {
-            if (["DATE", "DATETIME", "INT", "DECIMAL"].includes(c.columnType)) {
-              result[c.name] = this.$route.query[c.name]
-                .split(",")
-                .map((v) => v.split(".."));
-            } else if (["REF", "REF_ARRAY", "REFBACK"].includes(c.columnType)) {
-              result[c.name] = JSON.parse(this.$route.query[c.name]);
-            } else {
-              result[c.name] = this.$route.query[c.name].split(",");
-            }
-          }
-        });
-        return result;
-      }
-      return [];
-    },
-  },
-  watch: {
-    $route(to) {
-      //this is to prevent updates if changes come from outside vs inside
-      if (JSON.stringify(to.query) != JSON.stringify(this.query)) {
-        this.timestamp = Date.now();
-        this.query = to.query;
       }
     },
   },
