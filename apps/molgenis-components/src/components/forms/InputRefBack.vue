7cc7753087b8ae76884849b8b9ca0642a6bc8bb6
connoratrug <47183404+connoratrug@users.noreply.github.com>
2022-09-15 14:06:22 +0200

    fix: inputRefback (#1596)
    fix: inputRefback
    
    - Add actions handlers to table buttons
    - fix passing visible columns

--- apps/molgenis-components/src/components/forms/InputRefBack.vue
+++ apps/molgenis-components/src/components/forms/InputRefBack.vue
@@ -19,11 +19,7 @@
         <RowButton
           v-if="canEdit"
           type="add"
-          :table="tableName"
-          :graphqlURL="graphqlURL"
-          :visible-columns="visibleColumnNames"
-          :default-value="defaultValue"
-          @close="reload"
+          @add="handleRowAction('add')"
           class="d-inline p-0"
         />
       </template>
@@ -40,8 +36,13 @@
           :table="tableName"
           :graphqlURL="graphqlURL"
           :visible-columns="visibleColumnNames"
-          :refTablePrimaryKeyObject="getPrimaryKey(slotProps.row, tableMetadata)"
+          :refTablePrimaryKeyObject="
+            getPrimaryKey(slotProps.row, tableMetadata)
+          "
           @close="reload"
+          @edit="
+            handleRowAction('edit', getPrimaryKey(slotProps.row, tableMetadata))
+          "
         />
         <RowButton
           v-if="canEdit"
@@ -49,23 +50,51 @@
           :table="tableName"
           :graphqlURL="graphqlURL"
           :pkey="getPrimaryKey(slotProps.row, tableMetadata)"
-          @close="reload"
           :visible-columns="visibleColumnNames"
           :default-value="defaultValue"
+          @clone="
+            handleRowAction(
+              'clone',
+              getPrimaryKey(slotProps.row, tableMetadata)
+            )
+          "
         />
         <RowButton
           v-if="canEdit"
           type="delete"
-          :table="tableName"
-          :graphqlURL="graphqlURL"
-          :pkey="getPrimaryKey(slotProps.row, tableMetadata)"
-          @close="reload"
+          @delete="
+            handleDeleteRowRequest(getPrimaryKey(slotProps.row, tableMetadata))
+          "
         />
       </template>
     </TableMolgenis>
     <MessageWarning v-else>
       This can only be filled in after you have saved (or saved draft).
     </MessageWarning>
+    <MessageError v-if="graphqlError">{{ graphqlError }}</MessageError>
+
+    <EditModal
+      v-if="isEditModalShown"
+      :isModalShown="true"
+      :id="tableName + '-edit-modal'"
+      :tableName="tableName"
+      :pkey="editRowPrimaryKey"
+      :visibleColumns="visibleColumns"
+      :clone="editMode === 'clone'"
+      :graphqlURL="graphqlURL"
+      @close="handleModalClose"
+    />
+
+    <ConfirmModal
+      v-if="isDeleteModalShown"
+      :title="'Delete from ' + tableName"
+      actionLabel="Delete"
+      actionType="danger"
+      :tableName="tableName"
+      :pkey="editRowPrimaryKey"
+      @close="isDeleteModalShown = false"
+      @confirmed="handleExecuteDelete"
+    />
   </FormGroup>
 </template>
 
@@ -77,6 +106,7 @@ import TableMolgenis from "../tables/TableMolgenis.vue";
 import RowButton from "../tables/RowButton.vue";
 import MessageWarning from "./MessageWarning.vue";
 import Spinner from "../layout/Spinner.vue";
+import ConfirmModal from "./ConfirmModal.vue";
 import { getPrimaryKey } from "../utils";
 
 export default {
@@ -88,19 +118,20 @@ export default {
     RowButton,
     Spinner,
     MessageWarning,
+    ConfirmModal,
   },
   props: {
     /** name of the table from which is referred back to this field */
     tableName: {
       type: String,
-      required: true
+      required: true,
     },
     /** name of the column in the other table */
     refBack: {
       type: String,
       required: true,
     },
-    /** 
+    /**
      * primary key of the current table that refback should point to
      * when empty ( in case of draft , add message is shown instead of the table)
      *  */
@@ -112,7 +143,7 @@ export default {
       type: String,
       default: "graphql",
     },
-    /** 
+    /**
      * if table (that has a column that is referred to by this table) can be edited
      *  */
     canEdit: {
@@ -126,8 +157,13 @@ export default {
       client: null,
       tableMetadata: null,
       data: null,
-      isLoading: false
-    }
+      isLoading: false,
+      isEditModalShown: false,
+      isDeleteModalShown: false,
+      editMode: "add", // add, edit, clone
+      editRowPrimaryKey: null,
+      graphqlError: null,
+    };
   },
   computed: {
     defaultValue() {
@@ -153,19 +189,51 @@ export default {
         );
       }
       return [];
-    }
+    },
   },
   methods: {
     getPrimaryKey,
-    async reload () {
-      this.isLoading = true
-      this.data = await this.client.fetchTableDataValues(this.tableName, { filter: this.graphqlFilter });
-      this.isLoading = false
-    }
+    async reload() {
+      this.isLoading = true;
+      this.data = await this.client.fetchTableDataValues(this.tableName, {
+        filter: this.graphqlFilter,
+      });
+      this.isLoading = false;
+    },
+    handleRowAction(type, key) {
+      this.editMode = type;
+      this.editRowPrimaryKey = key;
+      this.isEditModalShown = true;
+    },
+    handleModalClose() {
+      this.isEditModalShown = false;
+      this.reload();
+    },
+    handleDeleteRowRequest(key) {
+      this.editRowPrimaryKey = key;
+      this.isDeleteModalShown = true;
+    },
+    async handleExecuteDelete() {
+      this.isDeleteModalShown = false;
+      const resp = await this.client
+        .deleteRow(this.editRowPrimaryKey, this.tableName)
+        .catch(this.handleError);
+      if (resp) {
+        this.reload();
+      }
+    },
+    handleError(error) {
+      if (Array.isArray(error?.response?.data?.errors)) {
+        this.graphqlError = error.response.data.errors[0].message;
+      } else {
+        this.graphqlError = error;
+      }
+      this.loading = false;
+    },
   },
   mounted: async function () {
     this.client = Client.newClient(this.graphqlURL);
-    this.isLoading = true
+    this.isLoading = true;
     this.tableMetadata = await this.client.fetchTableMetaData(this.tableName);
     await this.reload();
   },
