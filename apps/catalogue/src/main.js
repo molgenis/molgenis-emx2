2f696535b286ab092ef1fed3fe3a8125a174233c
connoratrug <c.stroomberg@rug.nl>
2022-09-15 09:50:34 +0200

    add work around for recursive inputs in EditModal (via InputRefBack)

--- apps/catalogue/src/main.js
+++ apps/catalogue/src/main.js
@@ -22,11 +22,15 @@ import NetworkVariables from "./network/NetworkVariables";
 import NetworkCohorts from "./network/NetworkCohorts";
 import NetworkDetails from "./network/NetworkDetails";
 import HomeView from "./views/HomeView"
+import { EditModal } from "molgenis-components";
 Vue.config.productionTip = false;
 
 import "molgenis-components/dist/style.css";
 
 Vue.use(VueRouter);
+
+// workaround for not importing recursive component
+Vue.component("EditModal", EditModal);
 
 const router = new VueRouter({
   routes: [
