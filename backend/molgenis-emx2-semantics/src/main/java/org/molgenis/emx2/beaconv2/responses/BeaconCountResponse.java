c74b334a29c8f48cbfb21a16f7f4a217d48eda66
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2022-09-29 15:32:15 +0200

    Refactor big Beacon class into smaller ones

--- backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/beaconv2/responses/BeaconCountResponse.java
+++ backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/beaconv2/responses/BeaconCountResponse.java
@@ -1,11 +1,55 @@
 package org.molgenis.emx2.beaconv2.responses;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import java.util.Map;
 import org.molgenis.emx2.beaconv2.common.misc.Handover;
 
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class BeaconCountResponse {
+
+  private String $schema;
   private BeaconResponseMeta meta;
   private BeaconSummaryResponseSection responseSummary;
   private Map<String, Object> info;
   private Handover[] beaconHandovers;
+
+  public String get$schema() {
+    return $schema;
+  }
+
+  public void set$schema(String $schema) {
+    this.$schema = $schema;
+  }
+
+  public BeaconResponseMeta getMeta() {
+    return meta;
+  }
+
+  public void setMeta(BeaconResponseMeta meta) {
+    this.meta = meta;
+  }
+
+  public BeaconSummaryResponseSection getResponseSummary() {
+    return responseSummary;
+  }
+
+  public void setResponseSummary(BeaconSummaryResponseSection responseSummary) {
+    this.responseSummary = responseSummary;
+  }
+
+  public Map<String, Object> getInfo() {
+    return info;
+  }
+
+  public void setInfo(Map<String, Object> info) {
+    this.info = info;
+  }
+
+  public Handover[] getBeaconHandovers() {
+    return beaconHandovers;
+  }
+
+  public void setBeaconHandovers(Handover[] beaconHandovers) {
+    this.beaconHandovers = beaconHandovers;
+  }
 }
