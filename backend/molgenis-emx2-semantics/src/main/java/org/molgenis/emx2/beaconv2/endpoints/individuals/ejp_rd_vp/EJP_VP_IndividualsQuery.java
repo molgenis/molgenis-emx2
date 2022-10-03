4fdb44a764dd0d205f975c4dcfea3e99f496df11
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2022-10-03 11:41:44 +0200

    Add mapping descriptions and todo

--- backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/beaconv2/endpoints/individuals/ejp_rd_vp/EJP_VP_IndividualsQuery.java
+++ backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/beaconv2/endpoints/individuals/ejp_rd_vp/EJP_VP_IndividualsQuery.java
@@ -101,14 +101,30 @@ public class EJP_VP_IndividualsQuery {
         HashMap<String, String> mapping = new HashMap<>();
         // full links? e.g. http://purl.obolibrary.org/obo/NCIT_C16576 ->
         // http://purl.obolibrary.org/obo/GSSO_000123
-        mapping.put("NCIT_C16576", "GSSO_000123");
-        mapping.put("NCIT_C20197", "GSSO_000124");
-        mapping.put("NCIT_C124294", "GSSO_009509");
-        mapping.put("NCIT_C17998", "GSSO_009515");
+
+        // NCIT "Female". A person who belongs to the sex that normally produces ova. The term is
+        // used to indicate biological sex distinctions, or cultural gender role distinctions, or
+        // both.
         mapping.put("http://purl.obolibrary.org/obo/NCIT_C16576", "GSSO_000123");
+        mapping.put("NCIT_C16576", "GSSO_000123");
+
+        // NCIT "Male". A person who belongs to the sex that normally produces sperm. The term is
+        // used to indicate biological sex distinctions, cultural gender role distinctions, or both.
         mapping.put("http://purl.obolibrary.org/obo/NCIT_C20197", "GSSO_000124");
+        mapping.put("NCIT_C20197", "GSSO_000124");
+
+        // NCIT "Undetermined". A term referring to the lack of definitive criteria for
+        // classification of a finding.
         mapping.put("http://purl.obolibrary.org/obo/NCIT_C124294", "GSSO_009509");
+        mapping.put("NCIT_C124294", "GSSO_009509");
+
+        // NCIT "Unknown". Not known, observed, recorded; or reported as unknown by the data
+        // contributor.
         mapping.put("http://purl.obolibrary.org/obo/NCIT_C17998", "GSSO_009515");
+        mapping.put("NCIT_C17998", "GSSO_009515");
+
+        // todo also map Undetermined/Unknown to "assigned no gender at birth" ?
+
         String filterTerm = id;
         if (mapping.containsKey(id)) {
           filterTerm = mapping.get(id);
