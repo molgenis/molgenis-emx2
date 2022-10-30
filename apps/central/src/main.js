57206c8ce1779da282e17a1bcbefe72cd491ef42
mswertz <m.a.swertz@rug.nl>
2022-10-30 23:10:44 +0100

    moved central to vue3

--- apps/central/src/main.js
+++ apps/central/src/main.js
@@ -1,6 +1,6 @@
-import Vue from "vue";
+import {createApp} from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 import App from "./App.vue";
-import VueRouter from "vue-router";
 import Groups from "./components/Groups.vue";
 import Admin from "./components/admin/Admin.vue";
 import ManageSettings from "./components/admin/ManageSettings.vue";
@@ -8,11 +8,7 @@ import ManageUsers from "./components/admin/ManageUsers.vue";
 
 import "molgenis-components/dist/style.css";
 
-Vue.config.productionTip = false;
-
-Vue.use(VueRouter);
-
-const router = new VueRouter({
+const router = createRouter({  history: createWebHistory(),
   routes: [
     { name: "central", path: "/", component: Groups },
     {
@@ -25,10 +21,9 @@ const router = new VueRouter({
         { name: "settings", path: "settings", component: ManageSettings },
       ],
     },
-  ],
+  ]
 });
 
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount("#app");
+const app = createApp(App);
+app.use(router);
+app.mount("#app")
--- apps/central/vite.config.js
+++ apps/central/vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite'
-import { createVuePlugin as vue } from "vite-plugin-vue2";
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+const opts = { changeOrigin: true, secure: false, logLevel: "debug" };
+
 export default defineConfig({
     plugins: [vue()],
     base: "",
     server: {
         proxy: {
-            "/graphql": { target: "http://localhost:8080/api" },
-            "/theme.css": { target: "http://localhost:8080/apps/central" },
-            "/apps": { target: "http://localhost:8080" },
+            "/graphql": { target: "http://localhost:8080/api", ...opts},
+            "/theme.css": { target: "http://localhost:8080/apps/central", ...opts },
+            "/apps": { target: "http://localhost:8080" , ...opts},
             "^/theme.css": {
-                target: "http://localhost:8080/apps/central",
+                target: "http://localhost:8080/apps/central", ...opts
             },
         },
     }
