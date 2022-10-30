f4d334dc2a7ce6c49f643304754e9c189fa68847
mswertz <m.a.swertz@rug.nl>
2022-10-30 23:52:10 +0100

    fixes to central

--- apps/central/vite.config.js
+++ apps/central/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 const opts = { changeOrigin: true, secure: false, logLevel: "debug" };
 
 export default defineConfig({
     plugins: [vue()],
-    base: "",
     server: {
         proxy: {
             "/graphql": { target: "http://localhost:8080/api", ...opts},
