73e5c47d4170314dcbca76235a34fa0c250d16e9
mswertz <m.a.swertz@rug.nl>
2022-10-30 23:15:04 +0100

    moved helloworld to vue3

--- apps/helloworld/src/main.js
+++ apps/helloworld/src/main.js
@@ -1,16 +1,12 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-
+import {createApp} from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 import App from "./App.vue";
 import HelloWorld from "./components/HelloWorld.vue";
 import SomeQuery from "./components/SomeQuery.vue";
 
 import "molgenis-components/dist/style.css";
 
-Vue.use(VueRouter);
-
-/** use vue router only to react to change url attributes */
-const router = new VueRouter({
+const router = createRouter({  history: createWebHistory(),
   routes: [
     {
       path: "/",
@@ -25,7 +21,6 @@ const router = new VueRouter({
   ],
 });
 
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount("#app");
+const app = createApp(App);
+app.use(router);
+app.mount("#app")
--- apps/helloworld/vite.config.js
+++ apps/helloworld/vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from "vite";
-import { createVuePlugin as vue } from "vite-plugin-vue2";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      vue: require.resolve("vue/dist/vue.js"),
-    },
-  },
   base: "",
   server: {
     proxy: require("../dev-proxy.config"),
