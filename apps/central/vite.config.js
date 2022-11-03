71d899384fda3ea0b3d3b8496272dc1f4e169ca7
mswertz <m.a.swertz@rug.nl>
2022-11-03 23:59:02 +0100

    fix catalogue app not starting

--- apps/central/vite.config.js
+++ apps/central/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/graphql": { target: "http://localhost:8080/api", ...opts},
+            "/api": { target: "http://localhost:8080/", ...opts},
             "/theme.css": { target: "http://localhost:8080/apps/central", ...opts },
             "/apps": { target: "http://localhost:8080" , ...opts},
             "^/theme.css": {
