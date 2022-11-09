2714218be03592128cc3b58b417a85b491cccd70
connoratrug <c.stroomberg@rug.nl>
2022-11-09 15:43:09 +0100

    chore: use backend data

--- apps/nuxt3-ssr/components/CohortCard.vue
+++ apps/nuxt3-ssr/components/CohortCard.vue
@@ -1,64 +1,49 @@
-<script setup>
+<script setup >
 import IconButton from "./IconButton.vue";
+defineProps({
+  cohort: {
+    type: Object,
+    required: true,
+  },
+});
 </script>
 
 <template>
   <article class="p-12.5">
     <header class="flex items-center">
-      <h2
-        class="text-heading-base font-extrabold text-blue-500 min-w-[160px] inline-block mr-4"
-      >
-        LIFELINES NEXT
-      </h2>
+      <NuxtLink :to="`/${cohort.pid}`">
+        <h2 class="text-heading-base font-extrabold text-blue-500 min-w-[160px] inline-block mr-4">
+          {{ cohort.name }}
+        </h2>
+      </NuxtLink>
 
-      <span class="text-heading-base grow">Lifelines NEXT</span>
+      <span class="text-heading-base grow"> {{ cohort?.institution?.acronym }}</span>
 
       <div class="flex">
-        <IconButton icon="star" class="text-blue-500" />
-        <IconButton icon="arrow-right" class="text-blue-500" />
+        <!-- <IconButton icon="star" class="text-blue-500" /> -->
+        <NuxtLink :to="`/${cohort.pid}`">
+          <IconButton icon="arrow-right" class="text-blue-500" />
+        </NuxtLink>
       </div>
     </header>
 
     <p class="text-body-base my-5">
-      The European Human Exposome Network (EHEN) is the world’s largest network
-      of projects studying the impact of environmental exposures across a
-      lifetime - the exposome - on human health. Collectively, the EHEN projects
-      are working in 24 countries across Europe and the USA. There are over 100
-      data sets being used including cohorts, cross-sectional studies, biobanks,
-      randomised controlled trials (RCTs), case control studies and health or
-      occupational registries.
+      {{ cohort?.description }}
     </p>
 
     <dl class="flex flex-col md:flex-row gap-5 md:gap-14 text-body-base">
       <div>
         <dt class="flex-auto block text-gray-600">Keywords</dt>
-        <dd>Primary care data, general practice, morbidity</dd>
+        <dd>{{ cohort?.keywords }}</dd>
       </div>
       <div>
         <dt class="flex-auto block text-gray-600">Type</dt>
-        <dd>Clinical cohort, Population cohort</dd>
+        <dd>{{ cohort?.type?.name }}</dd>
       </div>
       <div>
         <dt class="flex-auto block text-gray-600">Design</dt>
-        <dd>Longitudinal</dd>
+        <dd>{{ cohort?.design?.name }}</dd>
       </div>
     </dl>
   </article>
 </template>
-
-<!-- 
-      <dl class="flex flex-col md:flex-row gap-5 md:gap-14 text-body-base">
-      <dt>
-        <span class="flex-auto block text-gray-400">Keywords</span>
-        Primary care data, general practice, morbidity
-      </dt>
-      <dt>
-        <span class="flex-auto block text-gray-400">Type</span>
-        Clinical cohort, Population cohort
-      </dt>
-      <dt>
-        <span class="flex-auto block text-gray-400">Design</span>
-        Longitudinal
-      </dt>
-    </dl>
--->
--- apps/nuxt3-ssr/pages/index.vue
+++ apps/nuxt3-ssr/pages/index.vue
@@ -1,66 +1,78 @@
 <script setup>
-const query = `{ Schemas { name description } }`;
-const { data, error } = await fetchGql("apps/graphql", query);
-console.log(data);
-console.log(error);
-const catalogueQuery = `{_schema{name}, Cohorts_agg{count}}`;
+// const query = `{ Schemas { name description } }`;
+// const { data, error } = await fetchGql("apps/graphql", query);
+// console.log(data);
+// console.log(error);
+const catalogueQuery = `
+  {
+    Cohorts {
+      pid
+      name
+      description
+      keywords
+      type {
+        name
+      }
+      design {
+        name
+      }
+      institution {
+        name
+        acronym
+      }
+    }
+    Cohorts_agg {
+      count
+    }
+  }
+`;
+console.log(catalogueQuery)
 const catalogueResp = await fetchGql(
   "catalogue/catalogue/graphql",
   catalogueQuery
 );
 console.log(catalogueResp);
 const cohortCount = catalogueResp.data.Cohorts_agg.count;
+const cohorts = catalogueResp.data.Cohorts;
 </script>
 
 <template>
   <LayoutsSearchPage>
-      <template #side>
-        <SearchFilter title="Filters">
-          <SearchFilterGroup title="Search in networks" />
-          <SearchFilterGroup title="Countries" />
-          <SearchFilterGroup title="Institutions" />
-        </SearchFilter>
-      </template>
-      <template #main>
-        <SearchResults>
-          <template #header>
-            <PageHeader
-              title="Cohorts"
-              description="Group of individuals sharing a defining demographic characteristic."
-              icon="image-link"
-            >
-              <template #suffix>
-                <SearchResultsViewTabs
-                  buttonLeftLabel="Detailed"
-                  buttonLeftName="detailed"
-                  buttonLeftIcon="view-normal"
-                  buttonRightLabel="Compact"
-                  buttonRightName="compact"
-                  buttonRightIcon="view-compact"
-                  activeName="compact"
-                />
-              </template>
-            </PageHeader>
-          </template>
+    <template #side>
+      <SearchFilter title="Filters">
+        <SearchFilterGroup title="Search in networks" />
+        <SearchFilterGroup title="Countries" />
+        <SearchFilterGroup title="Institutions" />
+      </SearchFilter>
+    </template>
+    <template #main>
+      <SearchResults>
+        <template #header>
+          <PageHeader title="Cohorts" description="Group of individuals sharing a defining demographic characteristic."
+            icon="image-link">
+            <template #suffix>
+              <SearchResultsViewTabs buttonLeftLabel="Detailed" buttonLeftName="detailed" buttonLeftIcon="view-normal"
+                buttonRightLabel="Compact" buttonRightName="compact" buttonRightIcon="view-compact"
+                activeName="compact" />
+            </template>
+          </PageHeader>
+        </template>
 
-          <template #search-results>
-            <SearchResultsList>
-              <CardList>
-                <CardListItem><CohortCard /></CardListItem>
-                <CardListItem><CohortCard /></CardListItem>
-                <CardListItem><CohortCard /></CardListItem>
-                <CardListItem><CohortCard /></CardListItem>
-                <CardListItem><CohortCard /></CardListItem>
-                <CardListItem><CohortCard /></CardListItem>
-              </CardList>
-            </SearchResultsList>
-          </template>
+        <template #search-results>
+          <span>{{ cohortCount }}</span>
+          <SearchResultsList>
+            <CardList>
+              <CardListItem v-for="cohort in cohorts" :key="cohort.name">
+                <CohortCard :cohort="cohort" />
+              </CardListItem>
+            </CardList>
+          </SearchResultsList>
+        </template>
 
-          <template #pagination>
-            <Pagination />
-          </template>
-        </SearchResults>
-      </template>
-    </LayoutsSearchPage>
-
+        <template #pagination>
+          <Pagination />
+        </template>
+      </SearchResults>
+    </template>
+  </LayoutsSearchPage>
 </template>
