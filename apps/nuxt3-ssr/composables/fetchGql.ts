2e906fd6926a8c3eee659a409400d0732ee34024
connoratrug <c.stroomberg@rug.nl>
2022-11-09 10:27:34 +0100

    route test

--- apps/nuxt3-ssr/composables/fetchGql.ts
+++ apps/nuxt3-ssr/composables/fetchGql.ts
@@ -1,8 +1,11 @@
-export const fetchGql = async (url: string, query: string) => {
+export const fetchGql = async (url: string, query: string, variables?: object) => {
+    const body: { query: string, variables?: object } = { query }
+    if (variables) {
+        body.variables = variables
+    }
+
     return await $fetch(`http://localhost:3000/${url}`, {
         method: "POST",
-        body: {
-            query,
-        },
+        body
     });
 }
---
+++ apps/nuxt3-ssr/pages/[cohort].vue
@@ -0,0 +1,72 @@
+<script setup>
+const query = `query Cohorts ($pid: String){
+    Cohorts(filter: { pid: { equals: [$pid] } }){
+        name
+        description
+    }}`;
+const route = useRoute()
+console.log(route.params.cohort);
+const variables = { pid: route.params.cohort };
+const resp = await fetchGql(
+  "catalogue/catalogue/graphql",
+  query,
+  variables
+).catch((error) => console.log(error));
+console.log(resp);
+const cohort = resp?.data?.Cohorts[0];
+</script>
+<template>
+  <LayoutsDetailPage>
+    <template #header>
+      <PageHeader
+        :title="cohort.name"
+        description="Short description of the cohort could be placed here."
+      >
+        <template #prefix>
+          <BreadCrumbs />
+        </template>
+        <template #title-suffix>
+          <IconButton icon="star" label="Favorite" />
+        </template>
+      </PageHeader>
+    </template>
+    <template #side> <SideNavigation /> </template>
+    <template #main>
+      <ContentBlocks>
+        <ContentBlockIntro />
+        <ContentBlockDescription
+          title="Description"
+          description="Lifelines NEXT is a prospective birth cohort aiming to include 1.500 pregnant women and their children residing in the northern provinces of The Netherlands. The women are followed from the third month of their pregnancy with the aim of investigating in pregnants/mothers and their child the effect of early life or pre-conceptional transgenerational events on healthy ageing and chronic disease in (early) childhood. As of xxxx, partners were also invited to enroll in Lifelines NEXT. Standardized protocols and guidelines are available upon request.<br><br><strong>Keywords</strong><br>Children of the 90s, birth,  Genetics, DNA, RNA, Genetic disease, heritable disease "
+        />
+        <ContentBlockGeneralDesign title="General Design" />
+        <ContentBlockAttachedFiles title="Attached Files Generic Example" />
+        <ContentBlockContact title="Contact and Contributers" />
+        <ContentBlockVariables
+          title="Variables & Topics"
+          description="Explantation about variables and the functionality seen here. similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga."
+        />
+        <ContentBlockData
+          title="Available Data & Samples"
+          description="Explantation about variables and the functionality seen here. similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga."
+        />
+        <ContentBlockSubpopulations
+          title="Subpopulations"
+          description="Explanation about subpopulations and the functionality seen here. similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga."
+        />
+        <ContentBlockCollectionEvents
+          title="Collection Events"
+          description="Explanation about collection events and the functionality seen here. similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga."
+        />
+        <ContentBlockNetwork
+          title="Networks"
+          description="Networks Explanation about networks from this cohort and the functionality seen here. similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga."
+        />
+        <ContentBlockPartners
+          title="Partners"
+          description="Partners Explanation about networks from this cohort and the functionality seen here. similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga."
+        />
+      </ContentBlocks>
+    </template>
+  </LayoutsDetailPage>
+</template>
+
