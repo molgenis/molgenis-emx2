ef4a9bc9a394e436ffdfb6163dddbd0aa686066f
connoratrug <47183404+connoratrug@users.noreply.github.com>
2022-11-10 13:49:30 +0100

    chore: update nuxt ssr docker build (#1806)
    The ssr app can now be build via gradle
    After de build only .nuxt and .output are copied into docker image
    The files build by gradle are used to run de nuxt node app inside the container

--- apps/.nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = string
-declare module "/Users/umcg-mswertz/git/molgenis-emx2-nuxt3/molgenis-emx2/apps/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
