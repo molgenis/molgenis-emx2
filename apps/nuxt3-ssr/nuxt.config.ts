84f52dd1ff531a2a1af14958c4162b28490aa457
connoratrug <c.stroomberg@rug.nl>
2022-11-11 15:19:47 +0100

    try to fix proxy

--- apps/nuxt3-ssr/nuxt.config.ts
+++ apps/nuxt3-ssr/nuxt.config.ts
@@ -1,18 +1,26 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 import { defineNuxtConfig } from 'nuxt/config'
 
+const opts = { changeOrigin: true, secure: false, logLevel: "debug" };
+
 export default defineNuxtConfig({
-    css: ["assets/css/main.css"],
-    modules: ['nuxt-proxy', '@nuxt/image-edge', '@nuxtjs/tailwindcss'],
-    // See options here https://github.com/chimurai/http-proxy-middleware#options
-    proxy: {
-        options: {
-            target: 'http://localhost:8080/',
-            pathFilter: [
-                '**/*/graphql'
-            ],
-        }
+  css: ["assets/css/main.css"],
+  modules: ['nuxt-proxy', '@nuxt/image-edge', '@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    // Keys within public, will be also exposed to the client-side
+    public: {
+      apiBase: 'https://emx2.molgeniscloud.org/'
     }
+  },
+  // proxy: {
+  //   options: {
+  //     target: process.env.PROXY_TARGET || 'http://localhost:8080/',
+  //     pathFilter: [
+  //       '**/*/graphql'
+  //     ],
+  //     changeOrigin: true, secure: false
+  //   }
+  // },
+
 })
 
-    
--- apps/nuxt3-ssr/pages/index.vue
+++ apps/nuxt3-ssr/pages/index.vue
@@ -1,30 +1,42 @@
-<script setup>
-const query = `{ Schemas { name description } }`;
-const { data, error } = await fetchGql("apps/graphql", query);
+<script setup lang="ts">
+
+const config = useRuntimeConfig()
+
+const { data, pending, error, refresh } = await useFetch(
+  "/catalogue/catalogue/graphql",
+  {
+    baseURL: config.public.apiBase,
+    method: "POST",
+    body: {
+      query: '{Cohorts {name, pid}}'
+    }
+   }
+);
 console.log(data);
 console.log(error);
-const catalogueQuery = `{_schema{name}, Institutions_agg{count}, Studies_agg{count}, Cohorts_agg{count}}`;
-const catalogueResp = await fetchGql(
-  "catalogue/catalogue/graphql",
-  catalogueQuery
-);
-console.log(catalogueResp);
-const cohortCount = catalogueResp.data.Cohorts_agg.count;
+
+async function log() {
+  const { data, pending, error, refresh } = await useFetch(
+    "/catalogue/catalogue/graphql",
+    {
+      baseURL: config.public.apiBase,
+      method: "POST",
+      body: {
+        query: '{Cohorts {name, pid}}'
+      }
+    }
+  );
+}
+
 </script>
 
 <template>
   <div>
     <h1 class="text-center text-5xl">
-      European Networks Health Data & Cohort Catalogue.
+      European Networks Health Data & Cohort Catalogue. test
     </h1>
-    <ul v-if="data.Schemas" class="pl-6">
-      <li v-for="schema in data.Schemas" :key="schema.name">
-        {{ schema.name }}
-      </li>
-    </ul>
-    <div v-if="error">{{ error }}</div>
-    <!-- <div>{{ catalogueResp.data }}</div> -->
-    <HomePageCard title="Cohorts" :count="cohortCount"></HomePageCard>
-   
+    <button @click="log">Click mij</button>
+  {{data}}
+ 
   </div>
 </template>
