84528df13971ed70168f371a5ed3e957887128b5
connoratrug <c.stroomberg@rug.nl>
2022-11-14 09:35:18 +0100

    setup proxy en prod deploy

--- apps/nuxt3-ssr/nuxt.config.ts
+++ apps/nuxt3-ssr/nuxt.config.ts
@@ -1,26 +1,30 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 import { defineNuxtConfig } from 'nuxt/config'
 
-const opts = { changeOrigin: true, secure: false, logLevel: "debug" };
+const devProxy = {
+  options: {
+    target: process.env.PROXY_TARGET || 'http://localhost:8080/',
+    pathFilter: [
+      '**/*/graphql'
+    ],
+    changeOrigin: true, secure: false, logLevel: "debug"
+  }
+}
 
-export default defineNuxtConfig({
+const config = {
   css: ["assets/css/main.css"],
   modules: ['nuxt-proxy', '@nuxt/image-edge', '@nuxtjs/tailwindcss'],
   runtimeConfig: {
     // Keys within public, will be also exposed to the client-side
     public: {
-      apiBase: 'https://emx2.molgeniscloud.org/'
+      apiBase: 'http://localhost:3000/' //'https://emx2.molgeniscloud.org/'
     }
-  },
-  // proxy: {
-  //   options: {
-  //     target: process.env.PROXY_TARGET || 'http://localhost:8080/',
-  //     pathFilter: [
-  //       '**/*/graphql'
-  //     ],
-  //     changeOrigin: true, secure: false
-  //   }
-  // },
+  }
+}
 
-})
+if (process.env.NODE_ENV === 'development') {
+  config.proxy = devProxy
+}
+
+export default defineNuxtConfig(config)
 
---
+++ apps/nuxt3-ssr/pages/[schema]/ssr-catalogue/index.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+const config = useRuntimeConfig()
+
+const { data, pending, error, refresh } = await useFetch(
+    "/catalogue/catalogue/graphql",
+    {
+        baseURL: config.public.apiBase,
+        method: "POST",
+        body: {
+            query: '{Cohorts {name, pid}}'
+        }
+    }
+);
+
+async function log() {
+    const { data, pending, error, refresh } = await useFetch(
+        "/catalogue/catalogue/graphql",
+        {
+            baseURL: config.public.apiBase,
+            method: "POST",
+            body: {
+                query: '{Cohorts {name, pid}}'
+            }
+        }
+    );
+}
+
+</script>
+
+<template>
+    <div>
+        <h1 class="text-center text-5xl">
+            European Networks Health Data & Cohort Catalogue. test
+        </h1>
+        <button @click="log">Click mij</button>
+        {{ data }}
+
+    </div>
+</template>
