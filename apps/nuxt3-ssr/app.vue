1760162c1af1a55899b4d15abec2e85eb3b13535
connoratrug <c.stroomberg@rug.nl>
2022-11-16 13:55:05 +0100

    fix: implement AppWrapper as app.vue

--- apps/nuxt3-ssr/app.vue
+++ apps/nuxt3-ssr/app.vue
@@ -1,18 +1,21 @@
 <template>
-  <!-- Markup shared across all pages, ex: NavBar -->
-  <div class="min-h-screen bg-base-gradient">
-    <slot name="header">
-      <AppHeader />
-    </slot>
-    <main>
-      <slot>
-        <NuxtPage />
+  <div class="min-h-screen bg-base-gradient relative overflow-x-hidden">
+    <BackgroundGradient class="z-10" />
+    <div class="z-20 relative">
+      <slot name="header">
+        <AppHeader />
       </slot>
-    </main>
-    <slot name="footer"></slot>
+      <main>
+        <slot>
+          <NuxtPage />
+        </slot>
+      </main>
+      <slot name="footer"></slot>
+    </div>
   </div>
 </template>
 
 <script>
 import '@/assets/css/main.css'
+import BackgroundGradient from "./components/BackgroundGradient.vue";
 </script>
