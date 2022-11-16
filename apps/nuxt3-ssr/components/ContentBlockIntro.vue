f82629adf331b7dcc917f6ca0c48831648b38006
Postema Max <max@elynor.com>
2022-11-16 16:34:48 +0100

    fix: limit ContentBlockIntro image size

--- apps/nuxt3-ssr/components/ContentBlockIntro.vue
+++ apps/nuxt3-ssr/components/ContentBlockIntro.vue
@@ -27,14 +27,32 @@ defineProps({
 <template>
   <section class="bg-white py-9 px-12.5 text-gray-900">
     <div class="flex gap-11 flex-wrap items-center">
-      <img class="" :src="image" />
+      <img class="max-h-11" :src="image" />
       <div class="flex-grow align-middle">
-        <a href="#" target="_blank" class="text-blue-500 underline hover:bg-blue-50">
+        <a
+          href="#"
+          target="_blank"
+          class="text-blue-500 underline hover:bg-blue-50"
+        >
           <BaseIcon name="external-link" class="inline mr-2" />link
         </a>
       </div>
       <button
-        class="transition-colors rounded-full tracking-widest bg-yellow-500 text-gray-900 uppercase text-heading-xl font-display py-2.5 px-7.5 hover:bg-blue-200 mr-5">
+        class="
+          transition-colors
+          rounded-full
+          tracking-widest
+          bg-yellow-500
+          text-gray-900
+          uppercase
+          text-heading-xl
+          font-display
+          py-2.5
+          px-7.5
+          hover:bg-blue-200
+          mr-5
+        "
+      >
         Contact
       </button>
     </div>
