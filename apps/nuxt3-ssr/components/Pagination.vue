55683ab0dc7affadd79681513169fcd4f14fbc1e
connoratrug <c.stroomberg@rug.nl>
2022-11-17 13:32:42 +0100

    feat: refresh from client side
    - use paging as refresh example

--- apps/nuxt3-ssr/components/Pagination.vue
+++ apps/nuxt3-ssr/components/Pagination.vue
@@ -12,11 +12,13 @@ defineProps({
     required: true,
   },
 });
+
+const emit = defineEmits(['update'])
 </script>
 
 <template>
   <nav class="py-12.5 flex items-center justify-center font-display text-heading-xl">
-    <a href="#" class="rounded-full bg-blue-800 text-white h-15 w-15 flex justify-center">
+    <a href="#" @click="$emit('update', currentPage - 1)" class="rounded-full bg-blue-800 text-white h-15 w-15 flex justify-center">
       <BaseIcon name="caret-left" width="24" />
     </a>
     <div class="px-5 text-gray-400">Page</div>
@@ -24,7 +26,7 @@ defineProps({
       {{ currentPage }}
     </div>
     <div class="px-5 text-gray-400 whitespace-nowrap">OF {{ totalPages }}</div>
-    <a href="#" class="rounded-full bg-blue-800 text-white h-15 w-15 flex justify-center">
+    <a href="#" @click="$emit('update', currentPage + 1)" class="rounded-full bg-blue-800 text-white h-15 w-15 flex justify-center">
       <BaseIcon name="caret-right" width="24" />
     </a>
   </nav>
--- apps/nuxt3-ssr/pages/[schema]/ssr-catalogue/index.vue
+++ apps/nuxt3-ssr/pages/[schema]/ssr-catalogue/index.vue
@@ -2,81 +2,91 @@
 const route = useRoute()
 const config = useRuntimeConfig()
 const pageSize = 5
-const catalogueQuery = `
+const currentPage = ref(1)
+let offset = computed(() => currentPage.value * pageSize)
+
+const query = computed(() => {
+  return `
   {
-    Cohorts(limit: ${pageSize}) {
+      Cohorts(limit: ${pageSize} offset: ${offset.value}) {
       pid
       name
       description
       keywords
       type {
-        name
+          name
       }
       design {
-        name
+          name
       }
       institution {
-        name
-        acronym
+          name
+          acronym
       }
-    }
-    Cohorts_agg {
+      }
+      Cohorts_agg {
       count
-    }
+      }
   }
-`;
+  `
+})
 
 const { data, pending, error, refresh } = await useFetch(
-    `/${route.params.schema}/catalogue/graphql`,
-    {
-        baseURL: config.public.apiBase,
-        method: "POST",
-        body: {
-            query: catalogueQuery
-        }
+  `/${route.params.schema}/catalogue/graphql`,
+  {
+    key: `cohorts-${offset.value}`,
+    baseURL: config.public.apiBase,
+    method: "POST",
+    body: {
+      query
     }
+  }
 );
+
+async function handlePagination(pageNumber: number) {
+  currentPage.value = pageNumber
+}
 
 </script>
 
 <template>
-    <LayoutsSearchPage>
-        <template #side>
-            <SearchFilter title="Filters">
-                <SearchFilterGroup title="Search in networks" />
-                <SearchFilterGroup title="Countries" />
-                <SearchFilterGroup title="Institutions" />
-            </SearchFilter>
+  <LayoutsSearchPage>
+    <template #side>
+      <SearchFilter title="Filters">
+        <SearchFilterGroup title="Search in networks" />
+        <SearchFilterGroup title="Countries" />
+        <SearchFilterGroup title="Institutions" />
+      </SearchFilter>
+    </template>
+    <template #main>
+      <SearchResults>
+        <template #header>
+          <PageHeader title="Cohorts" description="Group of individuals sharing a defining demographic characteristic."
+            icon="image-link">
+            <template #suffix>
+              <SearchResultsViewTabs buttonLeftLabel="Detailed" buttonLeftName="detailed" buttonLeftIcon="view-normal"
+                buttonRightLabel="Compact" buttonRightName="compact" buttonRightIcon="view-compact"
+                activeName="compact" />
+            </template>
+          </PageHeader>
         </template>
-        <template #main>
-            <SearchResults>
-                <template #header>
-                    <PageHeader title="Cohorts"
-                        description="Group of individuals sharing a defining demographic characteristic."
-                        icon="image-link">
-                        <template #suffix>
-                            <SearchResultsViewTabs buttonLeftLabel="Detailed" buttonLeftName="detailed"
-                                buttonLeftIcon="view-normal" buttonRightLabel="Compact" buttonRightName="compact"
-                                buttonRightIcon="view-compact" activeName="compact" />
-                        </template>
-                    </PageHeader>
-                </template>
 
-                <template #search-results>
-        
-                    <SearchResultsList>
-                        <CardList>
-                            <CardListItem v-for="cohort in data?.data?.Cohorts" :key="cohort.name">
-                                <CohortCard :cohort="cohort" :schema="route.params.schema" />
-                            </CardListItem>
-                        </CardList>
-                    </SearchResultsList>
-                </template>
+        <template #search-results>
 
-                <template #pagination>
-                    <Pagination :current-page="1" :totalPages="Math.ceil(data?.data?.Cohorts_agg.count / pageSize)" />
-                </template>
-            </SearchResults>
+          <SearchResultsList>
+            <CardList>
+              <CardListItem v-for="cohort in data?.data?.Cohorts" :key="cohort.name">
+                <CohortCard :cohort="cohort" :schema="route.params.schema" />
+              </CardListItem>
+            </CardList>
+          </SearchResultsList>
         </template>
-    </LayoutsSearchPage>
+
+        <template #pagination>
+          <Pagination :current-page="currentPage" :totalPages="Math.ceil(data?.data?.Cohorts_agg.count / pageSize)"
+            @update="handlePagination($event)" />
+        </template>
+      </SearchResults>
+    </template>
+  </LayoutsSearchPage>
 </template>
