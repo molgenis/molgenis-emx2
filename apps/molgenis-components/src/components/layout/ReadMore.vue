1fba7c2b08fc74b66ad2cc528622584ff2c8af01
Morris Swertz <m.a.swertz@rug.nl>
2022-11-29 19:34:09 +0100

    fix: Clicking 'show more' in a table takes you back to tables overview. closes #1842 (#1845)

--- apps/molgenis-components/src/components/layout/ReadMore.vue
+++ apps/molgenis-components/src/components/layout/ReadMore.vue
@@ -4,11 +4,11 @@
     <span v-else-if="text.length <= length">{{ text }}</span>
     <span v-else-if="expand">
       {{ text }}
-      <a @click.stop="expand = false" href="#">...show less </a>
+      <a @click.stop.prevent="expand = false" href="#">...show less </a>
     </span>
     <span v-else>
       {{ text.slice(0, length) }}
-      <a @click.stop="expand = true" href="#" title="Expand text">
+      <a @click.stop.prevent="expand = true" href="#" title="Expand text">
         ...show more
       </a>
     </span>
