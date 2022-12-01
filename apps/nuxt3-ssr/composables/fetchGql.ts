5c9a53988310145d9ac0220b402a29eea1f154d4
connoratrug <c.stroomberg@rug.nl>
2022-12-01 12:59:57 +0100

    feat: allow gql fetch with optional GqlObject and variables

--- apps/nuxt3-ssr/composables/fetchGql.ts
+++ apps/nuxt3-ssr/composables/fetchGql.ts
@@ -1,11 +1,30 @@
-export const fetchGql = (query: string) => {
+import { DocumentNode } from "graphql";
+
+export const fetchGql = (query: string | DocumentNode, variables?: object) => {
+
+    let queryValue
+    if (typeof query !== 'string') {
+        if (query.loc?.source.body === undefined) {
+            throw 'unable to load query: ' + query.toString()
+        }
+        queryValue = query.loc?.source.body
+    } else {
+        queryValue = query
+    }
+
+    let body: { query: string, variables?: object } = {
+        query: queryValue
+    }
+
+    if (variables) {
+        body.variables = variables
+    }
+
     const route = useRoute();
     const config = useRuntimeConfig();
     return $fetch(`/${route.params.schema}/catalogue/graphql`, {
         method: "POST",
         baseURL: config.public.apiBase,
-        body: {
-            query,
-        },
+        body
     });
 }
