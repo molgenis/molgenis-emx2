47ee6930ac9d0ef6de789fa08e56b32f91051e94
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2022-12-09 15:08:25 +0100

    Change naming in filters

--- backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/beaconv2/requests/Filter.java
+++ backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/beaconv2/requests/Filter.java
@@ -5,18 +5,19 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class Filter {
   String id;
-  String type;
   String operator; // todo: use BeaconFilterOperator but serialization is tricky
+  String value;
 
   public String getId() {
     return id;
   }
 
-  public String getType() {
-    return type;
-  }
-
   public String getOperator() {
     return operator;
   }
+
+  public String getValue() {
+    return value;
+  }
+
 }
