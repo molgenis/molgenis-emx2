dfc991f6abaafe09c2e5b6c781dbca44c6db4c53
Postema Max <max@elynor.com>
2022-12-14 11:24:32 +0100

    fix filterwell issues

--- apps/nuxt3-ssr/components/Button.vue
+++ apps/nuxt3-ssr/components/Button.vue
@@ -54,6 +54,7 @@ const COLOR_MAPPING = {
 };
 
 const SIZE_MAPPING = {
+  tiny: "h-8 px-5 text-heading-sm gap-3",
   small: "h-10.5 px-5 text-heading-lg gap-3",
   medium: "h-14 px-7.5 text-heading-xl gap-4",
   large: "h-18 px-8.75 text-heading-xl gap-5",
