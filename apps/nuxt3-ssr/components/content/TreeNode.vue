7add7684db0da4ed4fd3a05b1cfbdc0e34ca3ca9
Postema Max <max@elynor.com>
2022-12-15 15:29:29 +0100

    fix: bump collapsible-list-item icon up a little bit

--- apps/nuxt3-ssr/components/content/TreeNode.vue
+++ apps/nuxt3-ssr/components/content/TreeNode.vue
@@ -1,39 +1,52 @@
 <script setup lang="ts">
-
-const { node, collapseAll } = withDefaults(defineProps<{
-  node: IOntologyNode;
-  collapseAll?: boolean
-}>(), {
-  collapseAll: true
-})
+const { node, collapseAll } = withDefaults(
+  defineProps<{
+    node: IOntologyNode;
+    collapseAll?: boolean;
+  }>(),
+  {
+    collapseAll: true,
+  }
+);
 
 let collapsed = ref(collapseAll);
 const toggleCollapse = () => {
   collapsed.value = !collapsed.value;
 };
-
 </script>
 
 <template>
   <li class="my-[5px]">
-
     <div class="flex gap-1 items-start">
-      <span v-if="node.children?.length" @click="toggleCollapse()"
-        class="text-blue-500 mr-1 mt-0.5 rounded-full hover:bg-blue-50 hover:cursor-pointer p-0.5 "
-        :class="{ 'rotate-180': collapsed }">
+      <span
+        v-if="node.children?.length"
+        @click="toggleCollapse()"
+        class="text-blue-500 mr-1 mt-0.5 rounded-full hover:bg-blue-50 hover:cursor-pointer p-0.5"
+        :class="{ 'rotate-180': collapsed }"
+      >
         <BaseIcon name="caret-up" :width="20" />
-
       </span>
-      <span v-else>
-        <BaseIcon name="collapsible-list-item" :width="20" class="text-gray-400" />
+      <span v-else class="relative" style="top: -0.35rem;">
+        <BaseIcon
+          name="collapsible-list-item"
+          :width="20"
+          class="text-gray-400"
+        />
       </span>
 
       <div>
-        <span @click="toggleCollapse()" :class="{ 'cursor-pointer': node.children?.length }">
+        <span
+          @click="toggleCollapse()"
+          :class="{ 'cursor-pointer': node.children?.length }"
+        >
           {{ node.name }}
         </span>
         <div class="whitespace-nowrap inline-flex items-center">
-          <span v-if="node.children?.length" class="text-gray-400 inline-block ml-1">- {{ node.children.length }}</span>
+          <span
+            v-if="node.children?.length"
+            class="text-gray-400 inline-block ml-1"
+            >- {{ node.children.length }}</span
+          >
           <div v-if="node.definition" class="inline-block ml-1">
             <CustomTooltip label="Read more" :content="node.definition" />
           </div>
@@ -41,9 +54,16 @@ const toggleCollapse = () => {
       </div>
     </div>
 
-    <ul v-if="node.children?.length" class="break-inside-avoid" :class="{ hidden: collapsed }">
-      <TreeNode class="pt-1 pl-8" v-for="child in node.children" :node="child"></TreeNode>
+    <ul
+      v-if="node.children?.length"
+      class="break-inside-avoid"
+      :class="{ hidden: collapsed }"
+    >
+      <TreeNode
+        class="pt-1 pl-8"
+        v-for="child in node.children"
+        :node="child"
+      ></TreeNode>
     </ul>
   </li>
-
 </template>
