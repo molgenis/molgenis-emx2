0298b35344f549f44dfc0c972950d9872ae9f972
connoratrug <c.stroomberg@rug.nl>
2023-01-25 15:36:59 +0100

    add private constructor for util class

--- backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/DownloadApiUtils.java
+++ backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/DownloadApiUtils.java
@@ -5,6 +5,10 @@ import static org.molgenis.emx2.web.Constants.INCLUDE_SYSTEM_COLUMNS;
 import spark.Request;
 
 public class DownloadApiUtils {
+
+  private DownloadApiUtils() {
+    throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+  }
   static boolean includeSystemColumns(Request request) {
     return String.valueOf(request.queryParams(INCLUDE_SYSTEM_COLUMNS)).equalsIgnoreCase("true");
   }
