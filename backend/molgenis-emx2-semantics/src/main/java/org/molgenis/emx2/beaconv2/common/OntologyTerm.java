91ffb8759dc286478f15735354b9e68b855c11cd
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2023-02-06 17:30:27 +0100

    Add URI to Beacon ontology term for reuse elsewhere

--- backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/beaconv2/common/OntologyTerm.java
+++ backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/beaconv2/common/OntologyTerm.java
@@ -1,16 +1,19 @@
 package org.molgenis.emx2.beaconv2.common;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /** Use for any id-label combinations to express ontologies */
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class OntologyTerm {
   private String id;
   private String label;
+  @JsonIgnore private String URI;
 
-  public OntologyTerm(String id, String label) {
+  public OntologyTerm(String id, String label, String URI) {
     this.id = id;
     this.label = label;
+    this.URI = URI;
   }
 
   public OntologyTerm() {}
@@ -29,5 +32,13 @@ public class OntologyTerm {
 
   public String getLabel() {
     return label;
+  }
+
+  public String getURI() {
+    return URI;
+  }
+
+  public void setURI(String URI) {
+    this.URI = URI;
   }
 }
