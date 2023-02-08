68794215d0eca1542cc6708307b66cc649bd72e4
connoratrug <47183404+connoratrug@users.noreply.github.com>
2023-02-08 12:11:04 +0100

    chore: format code and update lockfile (#2038)

--- apps/catalogue/src/components/VariableListItem.vue
+++ apps/catalogue/src/components/VariableListItem.vue
@@ -110,7 +110,9 @@
           <dd class="col-10">
             <span v-if="variable.variableDetails.mappings">
               <span
-                v-for="mapping in Object.values(variable.variableDetails.mappings).filter(m => ['complete','partial'].includes(m.match.name))"
+                v-for="mapping in Object.values(
+                  variable.variableDetails.mappings
+                ).filter((m) => ['complete', 'partial'].includes(m.match.name))"
                 :key="mapping.fromTable.dataDictionary.resource.pid"
               >
                 {{ mapping.fromTable.dataDictionary.resource.pid }},
