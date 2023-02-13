a7f041605324cb13b10e993e00ca6ae12528ab17
BrendaH <brenda.hijmans@gmail.com>
2023-02-13 11:34:47 +0100

    add run for 2.8 to 2.9

---
+++ data/scripts/molgenis-model-update/run_UMCG_2.8to2.9.py
@@ -0,0 +1,162 @@
+from decouple import config
+from client import Session
+from update import TransformGeneral
+from update import TransformDataCatalogue
+from update import TransformDataStaging
+from spaces import Spaces
+from zip_handling import Zip
+
+# Data model details
+DATA_MODEL_VERSION = config('MG_DATA_MODEL_VERSION')
+
+# Server details
+SERVER_URL = config('MG_SERVER_URL')
+SERVER_USERNAME = config('MG_SERVER_USERNAME')
+SERVER_PASSWORD = config('MG_SERVER_PASSWORD')
+CATALOGUE_SCHEMA_NAME = config('MG_CATALOGUE_SCHEMA_NAME')
+ONTOLOGIES_SCHEMA_NAME = config('MG_ONTOLOGIES_SCHEMA_NAME')
+SHARED_STAGING_NAME = config('MG_SHARED_STAGING_NAME')
+
+COHORTS = config('MG_COHORTS', cast=lambda v: [s.strip() for s in v.split(',')])
+
+print('-----  Config variables loaded ----')
+
+print('SERVER_URL: ' + SERVER_URL)
+print('SERVER_USERNAME: ' + SERVER_USERNAME)
+print('SERVER_PASSWORD: ******')
+print('CATALOGUE_SCHEMA_NAME: ' + CATALOGUE_SCHEMA_NAME)
+print('ONTOLOGIES_SCHEMA_NAME: ' + ONTOLOGIES_SCHEMA_NAME)
+print('SHARED_STAGING_NAME: ' + SHARED_STAGING_NAME)
+
+print('-----   ----')
+
+print('Updating catalogue data model to version ' + DATA_MODEL_VERSION)
+
+# sign in to server
+print('Sign in to server: ' + SERVER_URL)
+session = Session(
+    url=SERVER_URL,
+    email=SERVER_USERNAME,
+    password=SERVER_PASSWORD
+)
+# extract data from SharedStaging
+print('Extract data from ' + SHARED_STAGING_NAME + ': ' + SHARED_STAGING_NAME + '_data.zip')
+session.download_zip(database_name=SHARED_STAGING_NAME)
+
+# extract data from UMCG schema
+print('Extract data from ' + CATALOGUE_SCHEMA_NAME + ': ' + CATALOGUE_SCHEMA_NAME + '_data.zip')
+session.download_zip(database_name=CATALOGUE_SCHEMA_NAME)
+
+# transform data from catalogue
+print('Transform data from ' + CATALOGUE_SCHEMA_NAME)
+# get instances of classes
+zip_handling = Zip(CATALOGUE_SCHEMA_NAME)
+zip_handling_shared_staging = Zip(SHARED_STAGING_NAME)
+update_general = TransformGeneral(CATALOGUE_SCHEMA_NAME, 'catalogue')
+spaces = Spaces(CATALOGUE_SCHEMA_NAME)
+
+# run download and transform functions
+zip_handling.unzip_data()
+zip_handling_shared_staging.unzip_data()
+update_general.delete_data_model_file()  # delete molgenis.csv from data folder
+spaces.get_spaces()
+zip_handling.zip_data()
+
+print('----------------')
+
+# extract data from CatalogueOntologies
+print('Extract data from ' + ONTOLOGIES_SCHEMA_NAME + ': ' + ONTOLOGIES_SCHEMA_NAME + '_data.zip')
+session.download_zip(database_name=ONTOLOGIES_SCHEMA_NAME)
+
+# transform data from CatalogueOntologies
+print('Transform data from ' + ONTOLOGIES_SCHEMA_NAME)
+# call instances
+zip_handling = Zip(ONTOLOGIES_SCHEMA_NAME)
+update_general = TransformGeneral(ONTOLOGIES_SCHEMA_NAME, 'ontologies')
+spaces = Spaces(ONTOLOGIES_SCHEMA_NAME)
+
+# run transform functions
+zip_handling.remove_unzipped_data()
+zip_handling.unzip_data()
+update_general.delete_data_model_file()
+spaces.get_spaces()
+zip_handling.zip_data()
+zip_handling.remove_unzipped_data()
+
+# Cohorts update
+print('-----------------------')
+print('Cohort data update to data model ' + DATA_MODEL_VERSION)
+# sign in to server
+print('Sign in to server: ' + SERVER_URL)
+session = Session(
+    url=SERVER_URL,
+    email=SERVER_USERNAME,
+    password=SERVER_PASSWORD
+)
+for cohort in COHORTS:
+    # extract data
+    print('Extract data for ' + cohort + ': ' + cohort + '_data.zip')
+    session.download_zip(database_name=cohort)
+
+    # transform data from cohorts
+    print('Transform data from ' + cohort)
+    zip_handling = Zip(cohort)
+    update_general = TransformGeneral(cohort, 'cohort_UMCG')
+    spaces = Spaces(cohort)
+
+    zip_handling.remove_unzipped_data()
+    zip_handling.unzip_data()
+    update_general.delete_data_model_file()
+    spaces.get_spaces()
+    update_general.update_data_model_file()
+    zip_handling.zip_data()
+    zip_handling.remove_unzipped_data()
+    # delete and create new cohort schema
+    schema_description = session.get_database_description(database_name=cohort)
+    session.drop_database(database_name=cohort)
+    session.create_database(database_name=cohort, database_description=schema_description)
+
+# delete schemas UMCG and CatalogueOntologies
+print('------------------------')
+print('Updating catalogue and CatalogueOntologies schemas')
+# delete and create new UMCG schema
+schema_description = session.get_database_description(database_name=CATALOGUE_SCHEMA_NAME)
+session.drop_database(database_name=CATALOGUE_SCHEMA_NAME)
+session.create_database(database_name=CATALOGUE_SCHEMA_NAME, database_description=schema_description)
+
+# delete SharedStaging schema
+session.drop_database(database_name=SHARED_STAGING_NAME)
+
+# delete and create new CatalogueOntologies schema
+schema_description = session.get_database_description(database_name=ONTOLOGIES_SCHEMA_NAME)
+session.drop_database(database_name=ONTOLOGIES_SCHEMA_NAME)
+session.create_database(database_name=ONTOLOGIES_SCHEMA_NAME, database_description=schema_description)
+
+# upload molgenis.csv to UMCG schema
+update_general = TransformGeneral(CATALOGUE_SCHEMA_NAME, 'catalogue')
+data_model_file = update_general.update_data_model_file()
+session.upload_zip(database_name=CATALOGUE_SCHEMA_NAME, data_to_upload='catalogue_data_model')
+
+# upload transformed CatalogueOntologies data to CatalogueOntologies schema
+session.upload_zip(database_name=ONTOLOGIES_SCHEMA_NAME, data_to_upload=ONTOLOGIES_SCHEMA_NAME)
+
+# upload transformed data to UMCG schema
+session.upload_zip(database_name=CATALOGUE_SCHEMA_NAME, data_to_upload=CATALOGUE_SCHEMA_NAME)
+
+
+# per cohort:
+# Cohorts update
+print('-----------------------')
+
+print('Updating data for cohorts')
+# sign in to server
+print('Sign in to server: ' + SERVER_URL)
+session = Session(
+    url=SERVER_URL,
+    email=SERVER_USERNAME,
+    password=SERVER_PASSWORD
+)
+
+for cohort in COHORTS:
+    print('Upload transformed data for: ' + cohort)
+    session.upload_zip(database_name=cohort, data_to_upload=cohort)
