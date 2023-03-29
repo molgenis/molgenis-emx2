ccc9165b9a109729cc849052869ec04fefda0556
mswertz <m.a.swertz@rug.nl>
2023-03-29 23:38:30 +0200

    rename schema format file

--- backend/molgenis-emx2-io/src/main/java/org/molgenis/emx2/io/yaml/ModelRepository.java
+++ backend/molgenis-emx2-io/src/main/java/org/molgenis/emx2/io/yaml/ModelRepository.java
@@ -2,75 +2,77 @@ package org.molgenis.emx2.io.yaml;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
 import org.molgenis.emx2.MolgenisException;
 
 public class ModelRepository {
-  private static final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+    private static final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
 
-  Map<String, YamlTable> archetypes = new LinkedHashMap<>();
-  Map<String, YamlSchema> standards = new LinkedHashMap<>();
-  Map<String, YamlSchema> instances = new LinkedHashMap<>();
+    Map<String, YamlTable> archetypes = new LinkedHashMap<>();
+    Map<String, YamlSchema> standards = new LinkedHashMap<>();
+    Map<String, YamlSchema> instances = new LinkedHashMap<>();
 
-  public ModelRepository(File archetypesDir, File instancesDir) {
+    public ModelRepository(File archetypesDir, File instancesDir) {
 
-    Arrays.stream(archetypesDir.listFiles())
-        .forEach(
-            archetypeFile -> {
-              archetypes.put(
-                  archetypeFile.getName().replace(".yaml", ""), readYamlTable(archetypeFile));
-            });
-    Arrays.stream(instancesDir.listFiles())
-        .forEach(
-            instanceFile -> {
-              instances.put(
-                  instanceFile.getName().replace(".yaml", ""), readYamlSchema(instanceFile));
-            });
-  }
-
-  public static YamlTable readYamlTable(File file) {
-    try {
-      YamlValidator.validate(mapper.readTree(file));
-      return mapper.readValue(file, YamlTable.class);
-    } catch (IOException e) {
-      throw new MolgenisException("Parsing " + file.getName() + " failed: \n" + e.getMessage());
+        Arrays.stream(archetypesDir.listFiles())
+                .forEach(
+                        archetypeFile -> {
+                            archetypes.put(
+                                    archetypeFile.getName().replace(".yaml", ""), readYamlTable(archetypeFile));
+                        });
+        Arrays.stream(instancesDir.listFiles())
+                .forEach(
+                        instanceFile -> {
+                            instances.put(
+                                    instanceFile.getName().replace(".yaml", ""), readYamlSchema(instanceFile));
+                        });
     }
-  }
 
-  public static YamlSchema readYamlSchema(File file) {
-    try {
-      YamlValidator.validate(mapper.readTree(file));
-      return mapper.readValue(file, YamlSchema.class);
-    } catch (IOException e) {
-      throw new MolgenisException("Parsing " + file.getName() + " failed: \n" + e.getMessage());
+    public static YamlTable readYamlTable(File file) {
+        try {
+            YamlSchemaValidator.validate(mapper.readTree(file));
+            return mapper.readValue(file, YamlTable.class);
+        } catch (IOException e) {
+            throw new MolgenisException("Parsing " + file.getName() + " failed: \n" + e.getMessage());
+        }
     }
-  }
 
-  public Map<String, YamlTable> getArchetypes() {
-    return archetypes;
-  }
+    public static YamlSchema readYamlSchema(File file) {
+        try {
+            YamlSchemaValidator.validate(mapper.readTree(file));
+            return mapper.readValue(file, YamlSchema.class);
+        } catch (IOException e) {
+            throw new MolgenisException("Parsing " + file.getName() + " failed: \n" + e.getMessage());
+        }
+    }
 
-  public void setArchetypes(Map<String, YamlTable> archetypes) {
-    this.archetypes = archetypes;
-  }
+    public Map<String, YamlTable> getArchetypes() {
+        return archetypes;
+    }
 
-  public Map<String, YamlSchema> getStandards() {
-    return standards;
-  }
+    public void setArchetypes(Map<String, YamlTable> archetypes) {
+        this.archetypes = archetypes;
+    }
 
-  public void setStandards(Map<String, YamlSchema> standards) {
-    this.standards = standards;
-  }
+    public Map<String, YamlSchema> getStandards() {
+        return standards;
+    }
 
-  public Map<String, YamlSchema> getInstances() {
-    return instances;
-  }
+    public void setStandards(Map<String, YamlSchema> standards) {
+        this.standards = standards;
+    }
 
-  public void setInstances(Map<String, YamlSchema> instances) {
-    this.instances = instances;
-  }
+    public Map<String, YamlSchema> getInstances() {
+        return instances;
+    }
+
+    public void setInstances(Map<String, YamlSchema> instances) {
+        this.instances = instances;
+    }
 }
---
+++ backend/molgenis-emx2-io/src/main/java/org/molgenis/emx2/io/yaml/YamlSchemaValidator.java
@@ -0,0 +1,24 @@
+package org.molgenis.emx2.io.yaml;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+import com.networknt.schema.ValidationMessage;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class YamlSchemaValidator {
+    public static void validate(JsonNode input) throws IOException {
+        JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V202012);
+        JsonSchema jsonSchema =
+                factory.getSchema(YamlSchemaValidator.class.getResourceAsStream("emx2_yaml_format_schema.json"));
+        Set<ValidationMessage> errors = jsonSchema.validate(input);
+        if (errors.size() > 0) {
+            throw new IOException(
+                    errors.stream().map(message -> message.getMessage()).collect(Collectors.joining(".\n")));
+        }
+    }
+}
