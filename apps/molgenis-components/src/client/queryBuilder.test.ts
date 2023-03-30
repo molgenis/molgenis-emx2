2ea8a5264f1e5369ecdd907079f5f1e368f495f4
mswertz <m.a.swertz@rug.nl>
2023-03-30 17:02:19 +0200

    should not expand _array beyond rootlevel

--- apps/molgenis-components/src/client/queryBuilder.test.ts
+++ apps/molgenis-components/src/client/queryBuilder.test.ts
@@ -1,241 +1,241 @@
-import { describe, assert, test } from "vitest";
-import { columnNames } from "./queryBuilder";
+import {describe, assert, test} from "vitest";
+import {columnNames} from "./queryBuilder";
 
 describe("columnNames", () => {
-  const expand = 2;
-  test("it should return the column names", () => {
-    const expectedResult =
-      " name category { name } tags { order name label parent { name } children { name } } weight orders { orderId pet { name } }";
-    const result = columnNames("pet store", "Pet", metaData, expand);
-    assert.equal(result, expectedResult);
-  });
+    const expand = 2;
+    test("it should return the column names", () => {
+        const expectedResult =
+            " name category { name } tags { order name label parent { name } } weight orders { orderId pet { name } }";
+        const result = columnNames("pet store", "Pet", metaData, expand);
+        assert.equal(result, expectedResult);
+    });
 });
 
 // test meta data with mg_columns removed
 const metaData = {
-  name: "pet store",
-  tables: [
-    {
-      name: "Category",
-      tableType: "DATA",
-      id: "Category",
-      externalSchema: "pet store",
-      columns: [
+    name: "pet store",
+    tables: [
         {
-          name: "name",
-          id: "name",
-          columnType: "STRING",
-          key: 1,
-          required: true,
-        },
-      ],
-    },
-    {
-      name: "Order",
-      tableType: "DATA",
-      id: "Order",
-      externalSchema: "pet store",
-      columns: [
-        {
-          name: "orderId",
-          id: "orderId",
-          columnType: "STRING",
-          key: 1,
-          required: true,
+            name: "Category",
+            tableType: "DATA",
+            id: "Category",
+            externalSchema: "pet store",
+            columns: [
+                {
+                    name: "name",
+                    id: "name",
+                    columnType: "STRING",
+                    key: 1,
+                    required: true,
+                },
+            ],
         },
         {
-          name: "pet",
-          id: "pet",
-          columnType: "REF",
-          refTable: "Pet",
-          refLabelDefault: "${name}",
-          position: 1,
-        },
-      ],
-    },
-    {
-      name: "Pet",
-      tableType: "DATA",
-      id: "Pet",
-      descriptions: [
-        {
-          locale: "en",
-          value: "My pet store example table",
-        },
-      ],
-      externalSchema: "pet store",
-      columns: [
-        {
-          name: "name",
-          id: "name",
-          columnType: "STRING",
-          key: 1,
-          required: true,
-          descriptions: [
-            {
-              locale: "en",
-              value: "the name",
-            },
-          ],
+            name: "Order",
+            tableType: "DATA",
+            id: "Order",
+            externalSchema: "pet store",
+            columns: [
+                {
+                    name: "orderId",
+                    id: "orderId",
+                    columnType: "STRING",
+                    key: 1,
+                    required: true,
+                },
+                {
+                    name: "pet",
+                    id: "pet",
+                    columnType: "REF",
+                    refTable: "Pet",
+                    refLabelDefault: "${name}",
+                    position: 1,
+                },
+            ],
         },
         {
-          name: "category",
-          id: "category",
-          columnType: "REF",
-          refTable: "Category",
-          refLabelDefault: "${name}",
-          required: true,
-          position: 1,
-        },
+            name: "Pet",
+            tableType: "DATA",
+            id: "Pet",
+            descriptions: [
+                {
+                    locale: "en",
+                    value: "My pet store example table",
+                },
+            ],
+            externalSchema: "pet store",
+            columns: [
+                {
+                    name: "name",
+                    id: "name",
+                    columnType: "STRING",
+                    key: 1,
+                    required: true,
+                    descriptions: [
+                        {
+                            locale: "en",
+                            value: "the name",
+                        },
+                    ],
+                },
+                {
+                    name: "category",
+                    id: "category",
+                    columnType: "REF",
+                    refTable: "Category",
+                    refLabelDefault: "${name}",
+                    required: true,
+                    position: 1,
+                },
 
-        {
-          name: "tags",
-          id: "tags",
-          columnType: "ONTOLOGY_ARRAY",
-          refTable: "Tag",
-          refLabelDefault: "${name}",
-          position: 5,
+                {
+                    name: "tags",
+                    id: "tags",
+                    columnType: "ONTOLOGY_ARRAY",
+                    refTable: "Tag",
+                    refLabelDefault: "${name}",
+                    position: 5,
+                },
+                {
+                    name: "weight",
+                    id: "weight",
+                    columnType: "DECIMAL",
+                    required: true,
+                    position: 6,
+                },
+                {
+                    name: "orders",
+                    id: "orders",
+                    columnType: "REFBACK",
+                    refTable: "Order",
+                    refLabelDefault: "${orderId}",
+                    refBack: "pet",
+                    position: 7,
+                },
+            ],
         },
         {
-          name: "weight",
-          id: "weight",
-          columnType: "DECIMAL",
-          required: true,
-          position: 6,
-        },
-        {
-          name: "orders",
-          id: "orders",
-          columnType: "REFBACK",
-          refTable: "Order",
-          refLabelDefault: "${orderId}",
-          refBack: "pet",
-          position: 7,
-        },
-      ],
-    },
-    {
-      name: "Tag",
-      tableType: "ONTOLOGIES",
-      id: "Tag",
-      externalSchema: "pet store",
-      columns: [
-        {
-          name: "order",
-          id: "order",
-          columnType: "INT",
-          semantics: ["http://purl.obolibrary.org/obo/NCIT_C42680"],
-          descriptions: [
-            {
-              locale: "en",
-              value: "Order of this term within the code system",
-            },
-          ],
-        },
-        {
-          name: "name",
-          id: "name",
-          columnType: "STRING",
-          key: 1,
-          required: true,
-          semantics: ["http://purl.obolibrary.org/obo/NCIT_C42614"],
-          descriptions: [
-            {
-              locale: "en",
-              value: "Unique name of the term within this table",
-            },
-          ],
-          position: 1,
-        },
-        {
-          name: "label",
-          id: "label",
-          columnType: "STRING",
-          semantics: ["http://purl.obolibrary.org/obo/NCIT_C45561"],
-          descriptions: [
-            {
-              locale: "en",
-              value:
-                "User-friendly label for this term. Should be unique in parent",
-            },
-          ],
-          position: 2,
-        },
-        {
-          name: "parent",
-          id: "parent",
-          columnType: "REF",
-          refTable: "Tag",
-          refLabelDefault: "${name}",
-          semantics: ["http://purl.obolibrary.org/obo/NCIT_C80013"],
-          descriptions: [
-            {
-              locale: "en",
-              value: "The parent term, in case this code exists in a hierarchy",
-            },
-          ],
-          position: 3,
-        },
+            name: "Tag",
+            tableType: "ONTOLOGIES",
+            id: "Tag",
+            externalSchema: "pet store",
+            columns: [
+                {
+                    name: "order",
+                    id: "order",
+                    columnType: "INT",
+                    semantics: ["http://purl.obolibrary.org/obo/NCIT_C42680"],
+                    descriptions: [
+                        {
+                            locale: "en",
+                            value: "Order of this term within the code system",
+                        },
+                    ],
+                },
+                {
+                    name: "name",
+                    id: "name",
+                    columnType: "STRING",
+                    key: 1,
+                    required: true,
+                    semantics: ["http://purl.obolibrary.org/obo/NCIT_C42614"],
+                    descriptions: [
+                        {
+                            locale: "en",
+                            value: "Unique name of the term within this table",
+                        },
+                    ],
+                    position: 1,
+                },
+                {
+                    name: "label",
+                    id: "label",
+                    columnType: "STRING",
+                    semantics: ["http://purl.obolibrary.org/obo/NCIT_C45561"],
+                    descriptions: [
+                        {
+                            locale: "en",
+                            value:
+                                "User-friendly label for this term. Should be unique in parent",
+                        },
+                    ],
+                    position: 2,
+                },
+                {
+                    name: "parent",
+                    id: "parent",
+                    columnType: "REF",
+                    refTable: "Tag",
+                    refLabelDefault: "${name}",
+                    semantics: ["http://purl.obolibrary.org/obo/NCIT_C80013"],
+                    descriptions: [
+                        {
+                            locale: "en",
+                            value: "The parent term, in case this code exists in a hierarchy",
+                        },
+                    ],
+                    position: 3,
+                },
 
-        {
-          name: "children",
-          id: "children",
-          columnType: "REFBACK",
-          refTable: "Tag",
-          refLabelDefault: "${name}",
-          refBack: "parent",
-          semantics: ["http://purl.obolibrary.org/obo/NCIT_C90504"],
-          descriptions: [
-            {
-              locale: "en",
-              value:
-                "Child terms, in case this term is the parent of other terms",
-            },
-          ],
-          position: 8,
-        },
-      ],
-    },
-    {
-      name: "User",
-      tableType: "DATA",
-      id: "User",
-      externalSchema: "pet store",
-      columns: [
-        {
-          name: "username",
-          id: "username",
-          columnType: "STRING",
-          key: 1,
-          required: true,
+                {
+                    name: "children",
+                    id: "children",
+                    columnType: "REFBACK",
+                    refTable: "Tag",
+                    refLabelDefault: "${name}",
+                    refBack: "parent",
+                    semantics: ["http://purl.obolibrary.org/obo/NCIT_C90504"],
+                    descriptions: [
+                        {
+                            locale: "en",
+                            value:
+                                "Child terms, in case this term is the parent of other terms",
+                        },
+                    ],
+                    position: 8,
+                },
+            ],
         },
         {
-          name: "firstName",
-          id: "firstName",
-          columnType: "STRING",
-          position: 1,
+            name: "User",
+            tableType: "DATA",
+            id: "User",
+            externalSchema: "pet store",
+            columns: [
+                {
+                    name: "username",
+                    id: "username",
+                    columnType: "STRING",
+                    key: 1,
+                    required: true,
+                },
+                {
+                    name: "firstName",
+                    id: "firstName",
+                    columnType: "STRING",
+                    position: 1,
+                },
+                {
+                    name: "lastName",
+                    id: "lastName",
+                    columnType: "STRING",
+                    position: 2,
+                },
+                {
+                    name: "picture",
+                    id: "picture",
+                    columnType: "FILE",
+                    position: 3,
+                },
+                {
+                    name: "pets",
+                    id: "pets",
+                    columnType: "REF_ARRAY",
+                    refTable: "Pet",
+                    refLabelDefault: "${name}",
+                    position: 8,
+                },
+            ],
         },
-        {
-          name: "lastName",
-          id: "lastName",
-          columnType: "STRING",
-          position: 2,
-        },
-        {
-          name: "picture",
-          id: "picture",
-          columnType: "FILE",
-          position: 3,
-        },
-        {
-          name: "pets",
-          id: "pets",
-          columnType: "REF_ARRAY",
-          refTable: "Pet",
-          refLabelDefault: "${name}",
-          position: 8,
-        },
-      ],
-    },
-  ],
+    ],
 };
--- apps/molgenis-components/src/client/queryBuilder.ts
+++ apps/molgenis-components/src/client/queryBuilder.ts
@@ -1,6 +1,6 @@
-import { convertToPascalCase } from "../components/utils";
-import { ISchemaMetaData } from "../Interfaces/IMetaData";
-import { ITableMetaData } from "../Interfaces/ITableMetaData";
+import {convertToPascalCase} from "../components/utils";
+import {ISchemaMetaData} from "../Interfaces/IMetaData";
+import {ITableMetaData} from "../Interfaces/ITableMetaData";
 
 /**
  * @param {String} schemaName - schema where initial table is in
@@ -12,77 +12,54 @@ import { ITableMetaData } from "../Interfaces/ITableMetaData";
  * Other fields until level is reached
  */
 export const columnNames = (
-  schemaName: string,
-  tableName: string,
-  metaData: ISchemaMetaData,
-  //allows expansion of ref fields to add their next layer of details.
-  expandLevel: number,
-  //default is rootLevel listing of columnNames
-  rootLevel: boolean = true
+    schemaName: string,
+    tableName: string,
+    metaData: ISchemaMetaData,
+    //allows expansion of ref fields to add their next layer of details.
+    expandLevel: number,
+    //rootLevel
+    rootLevel = true
 ) => {
-  let result = "";
-  getTable(schemaName, tableName, metaData.tables)?.columns?.forEach((col) => {
-    //we always expand the subfields of key=1, but other 'ref' fields only if they do not break server
-    if (expandLevel > 0 || col.key == 1) {
-      if (
-        //we always can expand singular refs
-        ["REF", "ONTOLOGY"].includes(col.columnType) ||
-        //but we only expand *_array refs if we are on root level to not break server (keys shouldn't contain these)
-        (rootLevel &&
-          ["REF_ARRAY", "REFBACK", "ONTOLOGY_ARRAY"].includes(col.columnType))
-      ) {
-        result =
-          result +
-          " " +
-          col.id +
-          " {" +
-          columnNames(
-            col.refSchema ? col.refSchema : schemaName,
-            col.refTable,
-            metaData,
-            expandLevel - 1,
-            //indicate that sub queries should not be expanded on ref_array, refback, ontology_array
-            false
-          ) +
-          " }";
-      } else if (
-        ["REF_ARRAY", "REFBACK", "ONTOLOGY_ARRAY"].includes(col.columnType)
-      ) {
-        // only list the keys but down not expand further
-        result =
-          result +
-          " " +
-          col.id +
-          " {" +
-          columnNames(
-            col.refSchema ? col.refSchema : schemaName,
-            col.refTable,
-            metaData,
-            //should only include the keys by setting level to 0
-            0,
-            //indicate that sub queries should not be expanded on ref_array, refback, ontology_array
-            false
-          ) +
-          " }";
-      } else if (col.columnType === "FILE") {
-        result += ` ${col.id} { id, size, extension, url }`;
-      } else if (col.columnType !== "HEADING") {
-        result += ` ${col.id}`;
-      }
-    }
-  });
-  return result;
+    let result = "";
+    getTable(schemaName, tableName, metaData.tables)?.columns?.forEach((col) => {
+        //we always expand the subfields of key=1, but other 'ref' fields only if they do not break server
+        if (expandLevel > 0 || col.key == 1) {
+            if (!rootLevel && ["REF_ARRAY", "REFBACK", "ONTOLOGY_ARRAY"].includes(col.columnType)) {
+                //skip
+            } else if (["REF", "ONTOLOGY", "REF_ARRAY", "REFBACK", "ONTOLOGY_ARRAY"].includes(col.columnType)) {
+                result =
+                    result +
+                    " " +
+                    col.id +
+                    " {" +
+                    columnNames(
+                        col.refSchema ? col.refSchema : schemaName,
+                        col.refTable,
+                        metaData,
+                        //indicate that sub queries should not be expanded on ref_array, refback, ontology_array
+                        expandLevel - 1,
+                        false
+                    ) +
+                    " }";
+            } else if (col.columnType === "FILE") {
+                result += ` ${col.id} { id, size, extension, url }`;
+            } else if (col.columnType !== "HEADING") {
+                result += ` ${col.id}`;
+            }
+        }
+    });
+    return result;
 };
 
 const getTable = (
-  schemaName: string,
-  tableName: string,
-  tableStore: ITableMetaData[]
+    schemaName: string,
+    tableName: string,
+    tableStore: ITableMetaData[]
 ) => {
-  const result = tableStore.find(
-    (table) =>
-      table.id === convertToPascalCase(tableName) &&
-      table.externalSchema === schemaName
-  );
-  return result;
+    const result = tableStore.find(
+        (table) =>
+            table.id === convertToPascalCase(tableName) &&
+            table.externalSchema === schemaName
+    );
+    return result;
 };
