aaa43145c43bb67a8518654a8c79ddf0ea5c4935
mswertz <m.a.swertz@rug.nl>
2023-03-31 00:03:22 +0200

    undo ignore

--- backend/molgenis-emx2-sql/src/test/java/org/molgenis/emx2/sql/InitTestDatabaseAndRunNonParallelTests.java
+++ backend/molgenis-emx2-sql/src/test/java/org/molgenis/emx2/sql/InitTestDatabaseAndRunNonParallelTests.java
@@ -11,96 +11,95 @@ import static org.molgenis.emx2.sql.Migrations.migration5addMgTableclassUpdateTr
 
 import java.util.Collections;
 import java.util.List;
+
 import org.jooq.DSLContext;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.molgenis.emx2.Database;
 import org.molgenis.emx2.Schema;
 
-@Ignore
 public class InitTestDatabaseAndRunNonParallelTests {
 
-  @BeforeClass
-  public static void createDatabase() {
-    // we want this run only once and NOT parallel for total test suite
-    // AND we want run all other tests in parallel
-    // so tests are in molgenis-emx2-sql-it ('integration test')
-    // and 'init' only happens once, here
-    Database db = new SqlDatabase(true);
-    assertTrue(db.getDatabaseVersion() > 0);
-  }
-
-  @Test
-  public void testMigration2() {
-    SqlDatabase database = (SqlDatabase) TestDatabaseFactory.getTestDatabase();
-    database.dropCreateSchema("TestMigrations");
-
-    DSLContext jooq = database.getJooq();
-
-    // ensure no legacy roles exist from a previous test
-    List<String> roles =
-        jooq.selectFrom(name("pg_catalog", "pg_roles"))
-            .where(field("rolname").like("MG_ROLE_TESTMIGRATIONS%"))
-            .fetch()
-            .getValues("rolname", String.class);
-    Collections.reverse(roles);
-    for (String role : roles) {
-      jooq.execute("DROP ROLE {0}", name(role));
+    @BeforeClass
+    public static void createDatabase() {
+        // we want this run only once and NOT parallel for total test suite
+        // AND we want run all other tests in parallel
+        // so tests are in molgenis-emx2-sql-it ('integration test')
+        // and 'init' only happens once, here
+        Database db = new SqlDatabase(true);
+        assertTrue(db.getDatabaseVersion() > 0);
     }
 
-    // in current MOLGENIS role would be MG_ROLE_TestMigrations
-    assertEquals(
-        1,
-        jooq.selectFrom(name("pg_catalog", "pg_roles"))
-            .where(field("rolname").eq("MG_ROLE_TestMigrations/Viewer"))
-            .fetch()
-            .size());
+    @Test
+    public void testMigration2() {
+        SqlDatabase database = (SqlDatabase) TestDatabaseFactory.getTestDatabase();
+        database.dropCreateSchema("TestMigrations");
 
-    // in previous MOLGENIS roles where uppercase, i.e. MG_ROLE_TEST_MIGRATIONS
-    jooq.execute(
-        "ALTER ROLE \"MG_ROLE_TestMigrations/Viewer\" RENAME TO \"MG_ROLE_TESTMIGRATIONS/Viewer\"");
+        DSLContext jooq = database.getJooq();
 
-    // verify
-    assertEquals(
-        0,
-        jooq.selectFrom(name("pg_catalog", "pg_roles"))
-            .where(field("rolname").eq("MG_ROLE_TestMigrations/Viewer"))
-            .fetch()
-            .size());
-    assertEquals(
-        1,
-        jooq.selectFrom(name("pg_catalog", "pg_roles"))
-            .where(field("rolname").eq("MG_ROLE_TESTMIGRATIONS/Viewer"))
-            .fetch()
-            .size());
+        // ensure no legacy roles exist from a previous test
+        List<String> roles =
+                jooq.selectFrom(name("pg_catalog", "pg_roles"))
+                        .where(field("rolname").like("MG_ROLE_TESTMIGRATIONS%"))
+                        .fetch()
+                        .getValues("rolname", String.class);
+        Collections.reverse(roles);
+        for (String role : roles) {
+            jooq.execute("DROP ROLE {0}", name(role));
+        }
 
-    // run the migration
-    executeMigrationFile(
-        database,
-        "migration2.sql",
-        "database migration: role names are made case-sensitive matching schema names, to fix issue where roles where conflicting between schemas with same uppercase(name)");
+        // in current MOLGENIS role would be MG_ROLE_TestMigrations
+        assertEquals(
+                1,
+                jooq.selectFrom(name("pg_catalog", "pg_roles"))
+                        .where(field("rolname").eq("MG_ROLE_TestMigrations/Viewer"))
+                        .fetch()
+                        .size());
 
-    // should now be MG_ROLE_TestMigrations
-    assertEquals(
-        1,
-        jooq.selectFrom(name("pg_catalog", "pg_roles"))
-            .where(field("rolname").eq("MG_ROLE_TestMigrations/Viewer"))
-            .fetch()
-            .size());
-    assertEquals(
-        0,
-        jooq.selectFrom(name("pg_catalog", "pg_roles"))
-            .where(field("rolname").eq("MG_ROLE_TESTMIGRATIONS/Viewer"))
-            .fetch()
-            .size());
+        // in previous MOLGENIS roles where uppercase, i.e. MG_ROLE_TEST_MIGRATIONS
+        jooq.execute(
+                "ALTER ROLE \"MG_ROLE_TestMigrations/Viewer\" RENAME TO \"MG_ROLE_TESTMIGRATIONS/Viewer\"");
 
-    // create test schema and run migration 5
-    Schema testSchemm =
-        database.dropCreateSchema(InitTestDatabaseAndRunNonParallelTests.class.getSimpleName());
-    testSchemm.getMetadata().create(table("pet", column("name").setPkey()));
-    testSchemm.getMetadata().create(table("cat").setInherit("pet"));
+        // verify
+        assertEquals(
+                0,
+                jooq.selectFrom(name("pg_catalog", "pg_roles"))
+                        .where(field("rolname").eq("MG_ROLE_TestMigrations/Viewer"))
+                        .fetch()
+                        .size());
+        assertEquals(
+                1,
+                jooq.selectFrom(name("pg_catalog", "pg_roles"))
+                        .where(field("rolname").eq("MG_ROLE_TESTMIGRATIONS/Viewer"))
+                        .fetch()
+                        .size());
 
-    migration5addMgTableclassUpdateTrigger(database);
-  }
+        // run the migration
+        executeMigrationFile(
+                database,
+                "migration2.sql",
+                "database migration: role names are made case-sensitive matching schema names, to fix issue where roles where conflicting between schemas with same uppercase(name)");
+
+        // should now be MG_ROLE_TestMigrations
+        assertEquals(
+                1,
+                jooq.selectFrom(name("pg_catalog", "pg_roles"))
+                        .where(field("rolname").eq("MG_ROLE_TestMigrations/Viewer"))
+                        .fetch()
+                        .size());
+        assertEquals(
+                0,
+                jooq.selectFrom(name("pg_catalog", "pg_roles"))
+                        .where(field("rolname").eq("MG_ROLE_TESTMIGRATIONS/Viewer"))
+                        .fetch()
+                        .size());
+
+        // create test schema and run migration 5
+        Schema testSchemm =
+                database.dropCreateSchema(InitTestDatabaseAndRunNonParallelTests.class.getSimpleName());
+        testSchemm.getMetadata().create(table("pet", column("name").setPkey()));
+        testSchemm.getMetadata().create(table("cat").setInherit("pet"));
+
+        migration5addMgTableclassUpdateTrigger(database);
+    }
 }
