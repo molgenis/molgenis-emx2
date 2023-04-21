f5f9dbb2b60972956e4be5bb500016b1241db41a
connoratrug <c.stroomberg@rug.nl>
2023-04-21 16:06:46 +0200

    feat: rename catalogue3 to catalogue

---
+++ apps/catalogue/src/components/DocumentationList.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul v-if="documentation">
+    <li v-for="d in documentation">
+      <a v-if="d.url" :href="d.url">{{ d.name }}</a>
+      <a v-else-if="d.file" :href="d.file.url">{{ d.name }}</a>
+    </li>
+  </ul>
+  <p v-else>N/A</p>
+</template>
+
+<script>
+export default {
+  props: {
+    documentation: Array,
+  },
+};
+</script>
