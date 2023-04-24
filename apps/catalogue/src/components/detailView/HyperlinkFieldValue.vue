290361d3384a98587b760189302bb31355bcfcae
connoratrug <47183404+connoratrug@users.noreply.github.com>
2023-04-24 10:37:52 +0200

    feat: rename catalogue3 to catalogue (#2254)
    feat: rename catalogue3 to catalogue
    
    - includes rename of data folder files and Java class names

---
+++ apps/catalogue/src/components/detailView/HyperlinkFieldValue.vue
@@ -0,0 +1,19 @@
+<template>
+  <a :href="data">{{ data }}</a>
+</template>
+
+<script>
+export default {
+  name: "HyperlinkFieldValue",
+  props: {
+    data: {
+      type: [String, Object],
+      required: true,
+    },
+    metaData: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ apps/catalogue/src/store/repository/cohortRepository.js
@@ -0,0 +1,19 @@
+import { request } from "graphql-request";
+import cohortQuery from "../query/cohortDetails.js";
+
+let cache = {};
+
+const fetchById = async (id) => {
+  if (!cache[id]) {
+    cache[id] = (
+      await request("graphql", cohortQuery, { id }).catch((e) => {
+        console.error(`Error fetching data for cohort (id: ${id})`);
+        console.error(e);
+      })
+    ).Cohorts[0];
+  }
+
+  return cache[id];
+};
+
+export { fetchById };
