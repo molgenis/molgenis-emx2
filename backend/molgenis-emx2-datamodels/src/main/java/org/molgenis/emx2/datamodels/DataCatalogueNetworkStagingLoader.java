1065849031658e1029463b0b4f7c6da6d94357c9
erikzwart <e.zwart@umcg.nl>
2023-05-01 10:48:46 +0200

    update DataCatalogueNetworkStagingLoader.java so test will run correctly

--- backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/DataCatalogueNetworkStagingLoader.java
+++ backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/DataCatalogueNetworkStagingLoader.java
@@ -1,17 +1,33 @@
 package org.molgenis.emx2.datamodels;
 
-import static org.molgenis.emx2.datamodels.DataCatalogueCohortStagingLoader.createSharedStaging;
+//import static org.molgenis.emx2.datamodels.DataCatalogueCohortStagingLoader.createSharedStaging;
 
+import org.molgenis.emx2.Database;
 import org.molgenis.emx2.Schema;
 
 public class DataCatalogueNetworkStagingLoader extends AbstractDataLoader {
+
+  static String DATA_CATALOGUE = "catalogue";
+  static final String SHARED_STAGING = "SharedStaging";
 
   @Override
   public void loadInternalImplementation(Schema schema, boolean includeDemoData) {
     // create shared schemas
     createSharedStaging(schema.getDatabase());
-
     // create the schema
     createSchema(schema, "datacatalogue/stagingNetworks/molgenis.csv");
+  }
+
+  static void createSharedStaging(Database db) {
+    // create DataCatalogue and CatalogueOntologies
+    Schema dataCatalogueSchema = db.getSchema(DATA_CATALOGUE);
+    if (dataCatalogueSchema == null) {
+      new DataCatalogueLoader().loadInternalImplementation(db.createSchema(DATA_CATALOGUE), false);
+    }
+
+    Schema sharedSchema = db.getSchema(SHARED_STAGING);
+    if (sharedSchema == null) {
+      createSchema(db.createSchema(SHARED_STAGING), "datacatalogue/stagingShared/molgenis.csv");
+    }
   }
 }
