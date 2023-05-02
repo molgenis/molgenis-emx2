147fc68b5939a5db4c6464a58b65bf0fedb0628c
Postema Max <max@elynor.com>
2023-05-02 16:00:08 +0200

    fix: broken-styling-for-mobile-filters

--- apps/nuxt3-ssr/components/filter/Container.vue
+++ apps/nuxt3-ssr/components/filter/Container.vue
@@ -13,6 +13,10 @@ const props = defineProps({
     type: Boolean,
     default: true,
   },
+  mobileDisplay: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 let isCollapsed = ref(props.initialCollapsed);
@@ -46,13 +50,19 @@ function clearSearch() {
   <div class="flex items-center gap-1 p-5">
     <div class="inline-flex gap-1 group" @click="toggleCollapseTitle()">
       <h3
-        class="text-search-filter-group-title font-sans text-body-base font-bold mr-[5px] group-hover:underline group-hover:cursor-pointer"
+        class="font-sans text-body-base font-bold mr-[5px] group-hover:underline group-hover:cursor-pointer"
+        :class="`text-search-filter-group-title${
+          mobileDisplay ? '-mobile' : ''
+        }`"
       >
         {{ title }}
       </h3>
       <span
-        :class="{ 'rotate-180': isCollapsed }"
-        class="flex items-center justify-center w-8 h-8 rounded-full text-search-filter-group-toggle group-hover:bg-search-filter-group-toggle group-hover:cursor-pointer"
+        :class="{
+          'rotate-180': isCollapsed,
+          'text-search-filter-group-toggle': !mobileDisplay,
+        }"
+        class="flex items-center justify-center w-8 h-8 rounded-full group-hover:bg-search-filter-group-toggle group-hover:cursor-pointer"
       >
         <BaseIcon name="caret-up" :width="26" />
       </span>
@@ -60,13 +70,15 @@ function clearSearch() {
     <div class="text-right grow">
       <span
         v-if="selected.length"
-        class="text-body-sm text-search-filter-expand hover:underline hover:cursor-pointer"
+        class="text-body-sm hover:underline hover:cursor-pointer"
+        :class="`text-search-filter-expand${mobileDisplay ? '-mobile' : ''}`"
         @click="clearSelection()"
       >
         Remove {{ selected?.length }} selected
       </span>
       <span
-        class="text-body-sm text-search-filter-expand hover:underline hover:cursor-pointer"
+        class="text-body-sm hover:underline hover:cursor-pointer"
+        :class="`text-search-filter-expand${mobileDisplay ? '-mobile' : ''}`"
         @click="clearSearch()"
         v-else-if="search"
         >Clear</span
@@ -76,7 +88,8 @@ function clearSearch() {
 
   <div
     v-if="!isCollapsed"
-    class="mb-5 ml-5 mr-5 text-search-filter-group-title"
+    class="mb-5 ml-5 mr-5"
+    :class="`text-search-filter-group-title${mobileDisplay ? '-mobile' : ''}`"
   >
     <slot />
   </div>
--- apps/nuxt3-ssr/tailwind.config.cjs
+++ apps/nuxt3-ssr/tailwind.config.cjs
@@ -176,7 +176,9 @@ module.exports = {
 
         "search-filter-title": theme("colors.white"),
         "search-filter-expand": theme("colors.yellow.500"),
+        "search-filter-expand-mobile": theme("colors.blue.800"),
         "search-filter-group-title": theme("colors.white"),
+        "search-filter-group-title-mobile": theme("colors.black"),
         "search-filter-group-checkbox": theme("colors.yellow.500"),
         "search-filter-group-toggle": theme("colors.white"),
 
