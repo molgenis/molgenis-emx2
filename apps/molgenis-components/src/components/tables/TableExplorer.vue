5ec1c62abbef1fa7f357e56b52f19326568609d7
Morris Swertz <m.a.swertz@rug.nl>
2023-05-11 12:09:55 +0200

    undo changes

--- apps/molgenis-components/src/components/tables/TableExplorer.vue
+++ apps/molgenis-components/src/components/tables/TableExplorer.vue
@@ -36,14 +36,14 @@
               <div>
                 <span class="fixed-width">zip</span>
                 <ButtonAlt :href="'/' + schemaName + '/api/zip/' + tableId"
-                  >all rows
-                </ButtonAlt>
+                  >all rows</ButtonAlt
+                >
               </div>
               <div>
                 <span class="fixed-width">csv</span>
                 <ButtonAlt :href="'/' + schemaName + '/api/csv/' + tableId"
-                  >all rows
-                </ButtonAlt>
+                  >all rows</ButtonAlt
+                >
                 <span v-if="Object.keys(graphqlFilter).length > 0">
                   |
                   <ButtonAlt
@@ -63,8 +63,8 @@
               <div>
                 <span class="fixed-width">excel</span>
                 <ButtonAlt :href="'/' + schemaName + '/api/excel/' + tableId"
-                  >all rows
-                </ButtonAlt>
+                  >all rows</ButtonAlt
+                >
                 <span v-if="Object.keys(graphqlFilter).length > 0">
                   |
                   <ButtonAlt
@@ -90,8 +90,8 @@
               <div>
                 <span class="fixed-width">ttl</span>
                 <ButtonAlt :href="'/' + schemaName + '/api/ttl/' + tableId"
-                  >all rows
-                </ButtonAlt>
+                  >all rows</ButtonAlt
+                >
               </div>
             </div>
           </form>
@@ -829,13 +829,11 @@ function graphqlFilter(defaultFilter, columns, errorCallback) {
   border-bottom-left-radius: 0;
   border-left: 0;
 }
-
 .btn-group >>> span:not(:last-child) .btn {
   margin-left: 0;
   border-top-right-radius: 0;
   border-bottom-right-radius: 0;
 }
-
 .inline-form-group {
   margin-bottom: 0;
 }
@@ -847,19 +845,19 @@ function graphqlFilter(defaultFilter, columns, errorCallback) {
     <div class="border p-1 my-1">
       <label>Read only example</label>
       <table-explorer
-          id="my-table-explorer"
-          tableName="Pet"
-          schemaName="pet store"
-          :showColumns="showColumns"
-          :showFilters="showFilters"
-          :urlConditions="urlConditions"
-          :showPage="page"
-          :showLimit="limit"
-          :showOrderBy="showOrderBy"
-          :showOrder="showOrder"
-          :canEdit="canEdit"
-          :canManage="canManage"
-          :locale="locale"
+        id="my-table-explorer"
+        tableName="Pet"
+        schemaName="pet store"
+        :showColumns="showColumns"
+        :showFilters="showFilters"
+        :urlConditions="urlConditions"
+        :showPage="page"
+        :showLimit="limit"
+        :showOrderBy="showOrderBy"
+        :showOrder="showOrder"
+        :canEdit="canEdit"
+        :canManage="canManage"
+        :locale="locale"
       />
       <div class="border mt-3 p-2">
         <h5>synced props: </h5>
