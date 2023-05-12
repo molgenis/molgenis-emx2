7193c13f2ea8300b21665a00f0ee133292a3c728
davidruvolo51 <davidruvolo51@gmail.com>
2023-05-12 12:57:36 +0200

    added missing parameter

--- backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/Rd3Loader.java
+++ backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/Rd3Loader.java
@@ -6,7 +6,7 @@ import org.molgenis.emx2.io.MolgenisIO;
 public class Rd3Loader extends AbstractDataLoader {
 
   @Override
-  void loadInternalImplementation(Schema schema) {
+  void loadInternalImplementation(Schema schema, boolean includeDemoData) {
     createSchema(schema, "rd3/rd3/molgenis.csv");
     MolgenisIO.fromClasspathDirectory("rd3/ontologies", schema, false);
   }
