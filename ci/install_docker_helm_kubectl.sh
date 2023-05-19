b53b1213a30bc015d2b9d406306e61e93643e01f
mswertz <m.a.swertz@rug.nl>
2023-05-19 22:13:50 +0200

    build: refactoring build preparing to move functionality to circleci

---
+++ ci/install_docker_helm_kubectl.sh
@@ -0,0 +1,18 @@
+# install software needed for our build env
+apt-get update
+apt-get -y install ca-certificates gnupg lsb-release curl
+
+# kctl repo
+curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+
+#helm repo
+https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
+
+#docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
+
+apt-get update
+apt-get install -y kubectl helm gnupg lsb-release curl docker-ce docker-ce-cli containerd.io
