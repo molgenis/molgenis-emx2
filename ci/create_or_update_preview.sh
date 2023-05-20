f6ef16c3ab583d22acac3529f8accaa0f504550a
mswertz <m.a.swertz@rug.nl>
2023-05-20 22:08:55 +0200

    retry

--- ci/create_or_update_preview.sh
+++ ci/create_or_update_preview.sh
@@ -5,7 +5,7 @@ KUBE_CLUSTER=$3
 KUBE_TOKEN=$4
 
 echo "Using namespace $NAME"
-echo "Using docker tagname $TAG_NAME"
+echo "Using docker tag_name $TAG_NAME"
 echo "Using kube_cluster $KUBE_CLUSTER"
 echo "Using kube_token $KUBE_TOKEN"
 
@@ -13,9 +13,9 @@ echo "Using kube_token $KUBE_TOKEN"
 
 #create config
 kubectl config set-cluster molgenis-dev --server=$KUBE_CLUSTER
-kubectl config set-credentials molgenis-dev --token=$KUBE_TOKEN
 kubectl config set-context molgenis-dev --cluster=molgenis-dev --user=molgenis-dev
 kubectl config use-context molgenis-dev
+kubectl config set-credentials molgenis-dev --token=$KUBE_TOKEN
 kubectl config view
 
 # delete if exists
