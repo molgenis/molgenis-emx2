c69d2e77dfc520e2f13677da3e1dd675c06a39ac
mswertz <m.a.swertz@rug.nl>
2023-05-20 11:41:18 +0200

    retry install kubectl

--- ci/install_docker_helm_kubectl.sh
+++ ci/install_docker_helm_kubectl.sh
@@ -2,7 +2,7 @@
 apt-get install -y gnupg lsb-release ca-certificates curl
 
 ##docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io -y
