04f8051a1d1fc3481de6e43ae6b115908997790d
mswertz <m.a.swertz@rug.nl>
2023-05-20 13:15:14 +0200

    retry install helm

--- ci/install_docker_helm_kubectl.sh
+++ ci/install_docker_helm_kubectl.sh
@@ -14,15 +14,15 @@ apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io -y
 
 # kubectl repo
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install kubectl -y
 
 #
 ##helm repo
-curl -fsSL https://baltocdn.com/helm/signing.asc | apt-key add -
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | tee /usr/share/keyrings/helm.gpg > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 apt-get update
 apt-get install helm -y
 #
