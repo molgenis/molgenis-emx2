449c66735ef9a2a98b43ea749801d747c0d92141
mswertz <m.a.swertz@rug.nl>
2023-05-20 12:22:19 +0200

    retry install docker

--- ci/install_docker_helm_kubectl.sh
+++ ci/install_docker_helm_kubectl.sh
@@ -5,7 +5,11 @@ install -m 0755 -d /etc/apt/keyrings
 
 ##docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io -y
 
