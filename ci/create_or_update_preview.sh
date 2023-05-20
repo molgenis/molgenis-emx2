6d1258fe215804d1a9757e033e71e706a2906216
mswertz <m.a.swertz@rug.nl>
2023-05-20 09:06:08 +0200

    add kubecluster and kube token parameters

--- ci/create_or_update_preview.sh
+++ ci/create_or_update_preview.sh
@@ -1,29 +1,32 @@
-#this scripts two arguments
-# $1 = ${NAME} meaning name of the namespace on the server
-# $2 = and ${TAG_NAME} meaning the docker image tag name
-
+#this scripts takes arguments
 NAME=$1
 TAG_NAME=$2
+KUBE_CLUSTER=$3
+KUBE_TOKEN=$4
 
 echo "Using namespace $NAME"
 echo "Using docker tagname $TAG_NAME"
+echo "Using kube_cluster <hidden>"
+echo "Using kube_token <hidden>"
 
 #assuming ubuntu, install kubcetl and helm
 
-##delete if exists
-#kubectl delete namespace $NAME || true
-## wait for deletion to complete
-#sleep 15s
-#kubectl create namespace $NAME
-#kubectl annotate --overwrite ns $NAME field.cattle.io/projectId=\"c-l4svj:p-tl227\"
-#helm install $NAME ./helm-chart --namespace $NAME \
-#--set ingress.hosts[0].host=$NAME.dev.molgenis.org \
-#--set adminPassword=admin \
-#--set image.tag=${TAG_NAME} \
-#--set image.repository=molgenis/molgenis-emx2-snapshot \
-#--set image.pullPolicy=Always \
-#--set ingress.hosts[0].host=$NAME.dev.molgenis.org \
-#--set ssrCatalogue.image.tag=$TAG_NAME \
-#--set ssrCatalogue.environment.siteTitle=\"Preview Catalogue\" \
-#--set ssrCatalogue.environment.apiBase=https://$NAME.dev.molgenis.org/ \
-#--set catalogue.includeCatalogueDemo=true
+#delete if exists
+kubectl config set-cluster molgenis-dev --server=$KUBE_CLUSTER
+kubectl config set-credentials molgenis-dev --token=$KUBE_TOKEN
+kubectl delete namespace $NAME || true
+# wait for deletion to complete
+sleep 15s
+kubectl create namespace $NAME
+kubectl annotate --overwrite ns $NAME field.cattle.io/projectId=\"c-l4svj:p-tl227\"
+helm install $NAME ./helm-chart --namespace $NAME \
+--set ingress.hosts[0].host=$NAME.dev.molgenis.org \
+--set adminPassword=admin \
+--set image.tag=${TAG_NAME} \
+--set image.repository=molgenis/molgenis-emx2-snapshot \
+--set image.pullPolicy=Always \
+--set ingress.hosts[0].host=$NAME.dev.molgenis.org \
+--set ssrCatalogue.image.tag=$TAG_NAME \
+--set ssrCatalogue.environment.siteTitle=\"Preview Catalogue\" \
+--set ssrCatalogue.environment.apiBase=https://$NAME.dev.molgenis.org/ \
+--set catalogue.includeCatalogueDemo=true
