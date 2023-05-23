9dae001d595874a8f7fa020d11ee5212e3111de5
mswertz <m.a.swertz@rug.nl>
2023-05-23 22:40:44 +0200

    change print width to standard 80

--- apps/.prettierrc.js
+++ apps/.prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   tabWidth: 2,
   singleQuote: false,
   trailingComma: "es5",
-  printWidth: 120,
+  printWidth: 80,
   bracketSpacing: true,
   arrowParens: "always",
   semi: true,
