20bbaa55e0108d1aeedacb3c70ba1f6e03ec7614
mswertz <m.a.swertz@rug.nl>
2023-05-23 12:02:04 +0200

    updated

--- backend/molgenis-emx2-cafe-variome/src/main/java/org/molgenis/emx2/cafevariome/CafeVariomeApi.java
+++ backend/molgenis-emx2-cafe-variome/src/main/java/org/molgenis/emx2/cafevariome/CafeVariomeApi.java
@@ -1,3 +1,9 @@
 package org.molgenis.emx2.cafevariome;
 
-public class CafeVariomeApi {}
+public class CafeVariomeApi {
+
+  public static String query(String input) {
+    // neo4j magie
+    return "{'result':'blaat'}";
+  }
+}
