12028c094142658360fa4256d4c4379e47fdb609
Morris Swertz <m.a.swertz@rug.nl>
2023-05-24 22:58:25 +0200

    build: fix regexp (#2347)
    * making repo match tag
    
    * also distighuish snapshot for catalogue
    
    * replace sh with bash
    
    * retry
    
    * fix the 'delete' option

--- ci/create_or_update_k8s.sh
+++ ci/create_or_update_k8s.sh
@@ -5,18 +5,18 @@ DELETE=$3
 
 echo "Using namespace $NAME"
 echo "Using docker tag_name $TAG_NAME"
-echo "Delete $DELETE"
+echo "Delete=$DELETE"
 
 REPO=molgenis/molgenis-emx2
 REPO2=molgenis/ssr-catalogue
-if [[ $TAG_NAME =~"SNAPSHOT" ]]; then
+if [[ $TAG_NAME == *"SNAPSHOT"* ]]; then
   REPO=molgenis/molgenis-emx2-snapshot
   REPO2=molgenis/ssr-catalogue-snapshot
 fi
 echo "Using repositories $REPO and $REPO2"
 
 # delete if exists
-if [ -z "$DELETE" ]
+if [ ! -z "$DELETE" ]
 then
   kubectl delete namespace $NAME || true
 fi
