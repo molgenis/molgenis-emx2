fe8a6dffc09ccbba8b7cf322cdb5b2f16a7ca255
Ype Zijlstra <info@ypezijlstra.frl>
2023-05-25 12:10:12 +0200

    Improved imports

--- tools/ontomanager/src/ontomanager/manager.py
+++ tools/ontomanager/src/ontomanager/manager.py
@@ -6,24 +6,27 @@ class Manager:
     def __init__(self):
         pass
 
-    def perform(self, action: str, **kwargs):
+    def perform(self, action: str, table: str, **kwargs):
         """Select the method to perform and pass any keyword arguments"""
         match action:
             case 'add':
-                self.add(**kwargs)
+                self.add(table, **kwargs)
             case 'delete':
-                self.delete(**kwargs)
+                self.delete(table, **kwargs)
             case 'rename':
-                self.rename(**kwargs)
+                self.rename(table, **kwargs)
 
-    def add(self, **kwargs):
+    def add(self, table: str, **kwargs):
         """Add a term to an ontology."""
+        print(f"Adding to table {table}.")
         pass
 
-    def delete(self, **kwargs):
+    def delete(self, table: str, **kwargs):
         """Delete a term from an ontology."""
+        print(f"Deleting from table {table}.")
         pass
 
-    def rename(self, **kwargs):
+    def rename(self, table: str, **kwargs):
         """Rename a term in an ontology."""
+        print(f"Renaming in table {table}.")
         pass
