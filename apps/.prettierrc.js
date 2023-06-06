4979d197076616538c2d9faa7646f680ed283425
mswertz <m.a.swertz@rug.nl>
2023-06-07 00:34:26 +0200

    update prettier

--- apps/.prettierrc.js
+++ apps/.prettierrc.js
@@ -1,7 +1,11 @@
 module.exports = {
+  tabWidth: 2,
   singleQuote: false,
   trailingComma: "es5",
+  printWidth: 80,
   bracketSpacing: true,
-  arrowParens: "always",
+  bracketSameLine: true,
+  arrowParens: "avoid",
   semi: true,
+  endOfLine: "lf"
 };
