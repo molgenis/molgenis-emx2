2f437bb7c55449382240a31ab3f6b095a665438a
mswertz <m.a.swertz@rug.nl>
2023-06-13 00:39:39 +0200

    also reroute proxy for pet%20store

--- apps/dev-proxy.config.js
+++ apps/dev-proxy.config.js
@@ -9,7 +9,7 @@ module.exports = {
     ...opts,
   },
   /* should match only '/schema_name/graphql', previous ** was to eager also matching if graphql was /graphql or /a/b/graphql */
-  "^/[a-zA-Z0-9_.-]+/graphql": {
+  "^/[a-zA-Z0-9_.%-]+/graphql": {
     target: `${HOST}`,
     ...opts,
   },
