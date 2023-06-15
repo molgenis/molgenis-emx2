287bacda3208ce2911961e204fcec051cd25ba15
mswertz <m.a.swertz@rug.nl>
2023-06-15 12:32:58 +0200

    feat(gportal): add dataset view

---
+++ apps/gportal/src/components/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Welcome to genome data portal</h1>
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </div>
+    <RouterLink to="/datasets" class="btn btn-primary mr-2">
+      Navigate datasets
+    </RouterLink>
+    <RouterLink to="/beacon" class="btn btn-primary">Go to beacon</RouterLink>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
---
+++ apps/gportal/src/main.js
@@ -0,0 +1,33 @@
+import { createApp } from "vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import App from "./App.vue";
+import Datasets from "./components/Datasets.vue";
+import Beacon from "./components/Beacon.vue";
+import Welcome from "./components/Welcome.vue";
+
+import "molgenis-components/dist/style.css";
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: "/beacon",
+      component: Beacon,
+      props: true,
+    },
+    {
+      path: "/datasets",
+      component: Datasets,
+      props: true,
+    },
+    {
+      path: "/",
+      component: Welcome,
+      props: true,
+    },
+  ],
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
