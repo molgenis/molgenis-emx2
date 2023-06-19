bfc0694a59424696fb4d62d5251900318edbe167
mswertz <m.a.swertz@rug.nl>
2023-06-19 13:11:25 +0200

    fix test

--- backend/molgenis-emx2-sql-it/src/test/java/org/molgenis/emx2/sql/TestAggregatePermission.java
+++ backend/molgenis-emx2-sql-it/src/test/java/org/molgenis/emx2/sql/TestAggregatePermission.java
@@ -1,12 +1,14 @@
 package org.molgenis.emx2.sql;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.molgenis.emx2.Privileges.AGGREGATOR;
 
 import java.sql.SQLException;
+import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.molgenis.emx2.Database;
-import org.molgenis.emx2.Privileges;
 import org.molgenis.emx2.Schema;
 import org.molgenis.emx2.datamodels.PetStoreLoader;
 import org.slf4j.Logger;
@@ -14,6 +16,7 @@ import org.slf4j.LoggerFactory;
 
 public class TestAggregatePermission {
 
+  public static final String AGGREGATE_PERMISSION_TESTER = "aggregatePermissionTester";
   private static Database db;
   static Schema schema;
   private static Logger logger =
@@ -29,9 +32,18 @@ public class TestAggregatePermission {
 
   @Test
   public void testAggregatePermission() {
-    schema.addMember("aggregatePermissionTester", Privileges.AGGREGATOR.toString());
-    db.setActiveUser("aggregatePermissionTester");
     schema = db.getSchema(TestAggregatePermission.class.getSimpleName());
+    // ensure remove anonymous viewer from this pet store, otherwise we still be a viewer
+    schema.removeMember("anonymous");
+    schema.addMember(AGGREGATE_PERMISSION_TESTER, AGGREGATOR.toString());
+
+    // switch to this test user
+    db.setActiveUser(AGGREGATE_PERMISSION_TESTER);
+
+    // should be aggregator
+    List<String> roles = schema.getInheritedRolesForActiveUser();
+    assertEquals(1, roles.size());
+    assertEquals(AGGREGATOR.toString(), roles.get(0));
 
     // should not be able to query rows
     try {
