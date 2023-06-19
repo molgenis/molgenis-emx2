d9f5033bad086a4a3f3d62af563cf8d4c0f5f404
Joris de Keijser <chinook25@gmail.com>
2023-06-19 11:21:52 +0200

    chore: add util file

---
+++ apps/molgenis-components/src/components/forms/formUtils/formUtils.ts
@@ -0,0 +1,140 @@
+import { IColumn } from "../../../Interfaces/IColumn";
+import { ITableMetaData } from "../../../Interfaces/ITableMetaData";
+import constants from "../../constants.js";
+import { deepClone, convertToCamelCase } from "../../utils";
+
+const { EMAIL_REGEX, HYPERLINK_REGEX, AUTO_ID, HEADING } = constants;
+
+export function getColumnError(
+  column: IColumn,
+  values: Record<string, any>,
+  tableMetaData: ITableMetaData
+) {
+  const value = values[column.id];
+  const type = column.columnType;
+  const isInvalidNumber = isInValidNumericValue(type, value);
+  // FIXME: this function should also check all array types
+  // FIXME: longs are not checked
+  const missesValue = value === undefined || value === null || value === "";
+
+  if (column.columnType === AUTO_ID || column.columnType === HEADING) {
+    return undefined;
+  }
+  if (column.required && (missesValue || isInvalidNumber)) {
+    return column.name + " is required ";
+  }
+  if (missesValue) {
+    return undefined;
+  }
+  if (type === "EMAIL" && !isValidEmail(value)) {
+    return "Invalid email address";
+  }
+  if (type === "EMAIL_ARRAY" && containsInvalidEmail(value)) {
+    return "Invalid email address";
+  }
+  if (type === "HYPERLINK" && !isValidHyperlink(value)) {
+    return "Invalid hyperlink";
+  }
+  if (type === "HYPERLINK_ARRAY" && containsInvalidHyperlink(value)) {
+    return "Invalid hyperlink";
+  }
+  if (column.validation) {
+    return getColumnValidationError(column.validation, values, tableMetaData);
+  }
+  if (isRefLinkWithoutOverlap(column, values)) {
+    return `value should match your selection in column '${column.refLink}' `;
+  }
+
+  return undefined;
+}
+
+function isInValidNumericValue(columnType: string, value: number) {
+  if (["DECIMAL", "INT"].includes(columnType)) {
+    return isNaN(value);
+  } else {
+    return false;
+  }
+}
+
+function getColumnValidationError(
+  validation: string,
+  values: Record<string, any>,
+  tableMetaData: ITableMetaData
+) {
+  try {
+    const result = executeExpression(validation, values, tableMetaData);
+    if (result === false) {
+      return `Applying validation rule returned error: ${validation}`;
+    } else if (result === true || result === undefined) {
+      return undefined;
+    } else {
+      return `Applying validation rule returned error: ${result}`;
+    }
+  } catch (error) {
+    return `Invalid validation expression '${validation}', reason: ${error}`;
+  }
+}
+
+function executeExpression(
+  expression: string,
+  values: Record<string, any>,
+  tableMetaData: ITableMetaData
+) {
+  //make sure all columns have keys to prevent reference errors
+  const copy: Record<string, any> = deepClone(values);
+  tableMetaData.columns.forEach((column) => {
+    if (!copy.hasOwnProperty(column.id)) {
+      copy[column.id] = null;
+    }
+  });
+
+  // FIXME: according to the new Function definition the input here is incorrectly typed
+  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function
+  // FIXME: use es2021 instead of es2020 as you need it for replaceAll
+  const func = new Function(
+    Object.keys(copy),
+    `return eval('${expression.replaceAll("'", '"')}');`
+  );
+  return func(...Object.values(copy));
+}
+
+function isRefLinkWithoutOverlap(column: IColumn, values: Record<string, any>) {
+  if (!column.refLink) {
+    return false;
+  }
+  const columnRefLink = column.refLink;
+  const refLinkId = convertToCamelCase(columnRefLink);
+
+  const value = values[column.id];
+  const refValue = values[refLinkId];
+
+  if (typeof value === "string" && typeof refValue === "string") {
+    return value && refValue && value !== refValue;
+  } else {
+    //FIXME: empty ref_array => should give 'required' error instead if applicable
+    if (Array.isArray(value) && value.length === 0) {
+      return false;
+    }
+    return (
+      value &&
+      refValue &&
+      !JSON.stringify(value).includes(JSON.stringify(refValue))
+    );
+  }
+}
+
+function isValidHyperlink(value: any) {
+  return HYPERLINK_REGEX.test(String(value).toLowerCase());
+}
+
+function containsInvalidHyperlink(hyperlinks: any) {
+  return hyperlinks.find((hyperlink: any) => !isValidHyperlink(hyperlink));
+}
+
+function isValidEmail(value: any) {
+  return EMAIL_REGEX.test(String(value).toLowerCase());
+}
+
+function containsInvalidEmail(emails: any) {
+  return emails.find((email: any) => !isValidEmail(email));
+}
