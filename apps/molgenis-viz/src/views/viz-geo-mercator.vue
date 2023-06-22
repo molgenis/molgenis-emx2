6955a415a8dacb43b0609170c17f85154ceb0a23
davidruvolo51 <davidruvolo51@gmail.com>
2023-06-22 15:39:53 +0200

    finised geomercator view

--- apps/molgenis-viz/src/views/viz-geo-mercator.vue
+++ apps/molgenis-viz/src/views/viz-geo-mercator.vue
@@ -35,21 +35,22 @@
         rowId="name"
         :geojson="geojson"
         :chartData="data"
-        latitude="lat"
-        longitude="lng"
-        groupingVariable="type"
+        latitude="latitude"
+        longitude="longitude"
+        groupingVariable="hasSubmittedData"
         :groupColorMappings="mapColorGroups"
         :legendData="mapColorGroups"
-        :chartScale="2"
-        :mapCenter="{ latitude: 4.515, longitude: 49.205 }"
+        :chartScale="6"
+        :mapCenter="{ latitude: 4.515, longitude: 51.55 }"
         :zoomLimits="[0.2, 25]"
         :tooltipTemplate="
           (row) => {
             return `
             <p class='title'>${row.name}</p>
             <p class='location'>
+              ${row.city}, ${row.country}
               <span class='location-city'>${row.city}</span>
-              <span class='location-type'>${row.type}</span>
+              <span class='location-country'>${row.country}</span>
             </p>
           `;
           }
@@ -67,65 +68,115 @@
   </Page>
 </template>
 
-<script>
+<script setup>
+import { ref, onMounted } from "vue"
+
+import { fetchData } from "@/utils/utils.js";
+import geojson from "@/data/world.geo.json";
+
+
 import Page from "@/components/layouts/Page.vue";
 import PageHeader from "@/components/layouts/PageHeader.vue";
 import PageSection from "@/components/layouts/PageSection.vue";
 import MessageBox from "@/components/display/MessageBox.vue";
 import GeoMercator from "@/components/viz/GeoMercator.vue";
 import Breadcrumbs from "@/app-components/breadcrumbs.vue";
-
-import { fetchData } from "@/utils/utils.js";
-import geojson from "@/data/world.geo.json";
-
 import headerImage from "@/assets/t-h-chia-unsplash.jpg";
 
-export default {
-  components: {
-    Page,
-    PageHeader,
-    PageSection,
-    MessageBox,
-    GeoMercator,
-    Breadcrumbs,
-  },
-  data() {
-    return {
-      headerImage: headerImage,
-      loading: true,
-      hasError: false,
-      error: null,
-      data: {},
-      geojson: geojson,
-      mapColorGroups: {
-        Education: "#F4D58D",
-        Facility: "#FC7573",
-        Healthcare: "#B6C2D9",
-      },
-      location: null,
-    };
-  },
-  methods: {
-    updateSelection(data) {
-      this.location = data;
-    },
-  },
-  mounted() {
-    Promise.resolve(
-      fetchData('/api/v2/rdcomponents_institutions?num=5000&q=lat!=""')
-    )
-      .then((response) => {
-        const data = response.items;
-        data.forEach((row) => delete row["_href"]);
-        this.data = data;
-        this.loading = false;
-      })
-      .catch((error) => {
-        this.loading = false;
-        this.hasError = true;
-        this.error = error;
-        throw new Error(error);
-      });
-  },
+let loading = ref(true);
+let hasError = ref(false);
+let error = ref(null);
+let data = ref([]);
+let location = ref(null);
+const mapColorGroups = {
+  Submitted: "#F4D58D",
+  'No Data': "#B6C2D9"
 };
+
+function updateSelection(value) {
+  location.value = value
+}
+
+
+const query = `{
+  Organisations(
+    filter: { latitude: { between: [0, 100] } }
+  ) {
+    name
+    code
+    ontologyTermURI
+    country
+    city
+    latitude
+    longitude
+    providerInformation {
+      hasSubmittedData
+    }
+  }
+}`
+
+onMounted(() => {
+  Promise.resolve(fetchData(query))
+  .then(response => {
+    const orgs = response.data.Organisations.map(row => {
+      return {...row, hasSubmittedData: row.providerInformation[0].hasSubmittedData ? 'Submitted' : 'No Data' }
+    })
+    data.value = orgs
+    loading.value = false
+  }).catch(error => {
+    loading.value = false
+    hasError.value = false
+    error.value = error
+  })
+})
+
+
+// export default {
+//   components: {
+//     Page,
+//     PageHeader,
+//     PageSection,
+//     MessageBox,
+//     GeoMercator,
+//     Breadcrumbs,
+//   },
+//   data() {
+//     return {
+//       headerImage: headerImage,
+//       loading: true,
+//       hasError: false,
+//       error: null,
+//       data: {},
+//       geojson: geojson,
+//       mapColorGroups: {
+//         Education: "#F4D58D",
+//         Facility: "#FC7573",
+//         Healthcare: "#B6C2D9",
+//       },
+//       location: null,
+//     };
+//   },
+//   methods: {
+//     updateSelection(data) {
+//       this.location = data;
+//     },
+//   },
+//   mounted() {
+//     Promise.resolve(
+//       fetchData('/api/v2/rdcomponents_institutions?num=5000&q=lat!=""')
+//     )
+//       .then((response) => {
+//         const data = response.items;
+//         data.forEach((row) => delete row["_href"]);
+//         this.data = data;
+//         this.loading = false;
+//       })
+//       .catch((error) => {
+//         this.loading = false;
+//         this.hasError = true;
+//         this.error = error;
+//         throw new Error(error);
+//       });
+//   },
+// };
 </script>
