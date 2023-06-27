a8c4581a0ade795ff23dc22102285294575f62ed
mswertz <m.a.swertz@rug.nl>
2023-06-27 11:05:11 +0200

    wip

--- playwright.config.ts
+++ playwright.config.ts
@@ -68,10 +68,10 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  /* When CI, start our local dev server before starting the tests */
+  webServer: {
+    command: 'gradle run',
+    url: 'http://localhost:8080',
+    reuseExistingServer: !process.env.CI,
+  },
 });
