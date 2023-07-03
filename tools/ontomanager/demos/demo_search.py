afcd13fbfcae2feefacaffd6e1eefd327540e98d
ypezijlstra <info@ypezijlstra.frl>
2023-07-03 16:54:12 +0200

    Added demonstrations of the search and the add/delete from pandas functionality

---
+++ tools/ontomanager/demos/demo_search.py
@@ -0,0 +1,48 @@
+"""
+Script to demonstrate the search functionality of the Molgenis EMX2 Ontomanager package
+Create an OntologyManager object by supplying a server's url and login credentials as arguments in the
+function below or create a .env file with the following structure
+
+MG_URL = https://myserver.molgeniscloud.org
+MG_USERNAME = username
+MG_PASSWORD = password
+
+Ensure the CatalogueOntologies database is present on the server before running the script.
+"""
+import os
+
+from dotenv import load_dotenv
+
+from tools.ontomanager.src.molgenis_emx2_ontomanager import OntologyManager
+
+
+def demo_search(search_terms: list | str):
+    """Function to demo search.
+    :param search_terms: a string or list of strings
+    """
+    load_dotenv()
+
+    url = os.environ['MG_URL']
+    username = os.environ['MG_USERNAME']
+    password = os.environ['MG_PASSWORD']
+
+    manager = OntologyManager(url, username, password)
+
+    if isinstance(search_terms, str):
+        search_terms = list(search_terms)
+    if not isinstance(search_terms, list):
+        raise ValueError("Supply the search terms as a string or a list.")
+
+    results = dict()
+    for st in search_terms:
+        _table = manager.search(st)
+        results[st] = _table
+
+    return results
+
+
+if __name__ == '__main__':
+    terms = ['Sibling', 'Blabla', 'morbidity', 'Hospital data', 'medication']
+    demo_results = demo_search(terms)
+    for (term, table) in demo_results.items():
+        print(f"{term}: {table}")
