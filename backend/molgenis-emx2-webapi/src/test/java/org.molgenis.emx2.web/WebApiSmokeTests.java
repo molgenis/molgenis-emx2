49a1b7f8720e85cd5105d77d84b422a99816fd53
Morris Swertz <m.a.swertz@rug.nl>
2023-07-05 17:15:11 +0200

    build: remove 'v' from version number, disable flakey tests (#2503)
    * build: remove 'v' from version number, disable flakey tests
    
    * fix so python also still works
    
    * explain @Disabled tests

--- backend/molgenis-emx2-webapi/src/test/java/org.molgenis.emx2.web/WebApiSmokeTests.java
+++ backend/molgenis-emx2-webapi/src/test/java/org.molgenis.emx2.web/WebApiSmokeTests.java
@@ -340,6 +340,7 @@ public class WebApiSmokeTests {
   }
 
   @Test
+  @Disabled("unstable")
   public void testJsonYamlApi() {
     String schemaJson = given().sessionId(SESSION_ID).when().get("/pet store/api/json").asString();
 
@@ -914,7 +915,7 @@ public class WebApiSmokeTests {
   }
 
   @Test
-  @Disabled
+  @Disabled("unstable")
   public void testScriptExecution() throws JsonProcessingException, InterruptedException {
     // get token for admin
     String result =
@@ -1129,6 +1130,7 @@ public class WebApiSmokeTests {
   }
 
   @Test
+  @Disabled("unstable")
   public void testBeaconApiSmokeTests() {
     // todo: ideally we would here validate the responses against json schemas, are those schemas
     // easily available?
