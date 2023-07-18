3681cabb8fc81868c3796fcf7ca2e4888241db32
davidruvolo51 <davidruvolo51@gmail.com>
2023-07-18 12:35:05 +0200

    fixed loader

--- backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/DashboardLoader.java
+++ backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/DashboardLoader.java
@@ -8,7 +8,7 @@ public class DashboardLoader extends AbstractDataLoader {
   @Override
   void loadInternalImplementation(Schema schema, boolean includeDemoData) {
     createSchema(schema, "dashboard/molgenis.csv");
-    createSchema(schema, "dashboard/molgenis_settings.csv");
+    MolgenisIO.fromClasspathDirectory("dashboard/molgenis_settings.csv", schema, false);
 
     if (includeDemoData) {
       MolgenisIO.fromClasspathDirectory("dashboard/demodata/ontologies", schema, false);
