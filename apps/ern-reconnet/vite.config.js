714bd335e81ae7d037c81866ce5ac5082f917028
davidruvolo51 <davidruvolo51@gmail.com>
2023-07-20 14:50:01 +0200

    dropped rollup options

--- apps/ern-reconnet/vite.config.js
+++ apps/ern-reconnet/vite.config.js
@@ -17,20 +17,5 @@ export default defineConfig({
         `
       }
     }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          const extension = assetInfo.name.split('.').pop()
-          if (/png|jpg|svg/.test(extension)) {
-            return `img/[name].[hash][extname]`
-          }
-          return `${extension}/[name].[hash][extname]`
-        },
-        chunkFileNames: 'js/[name].[hash].js',
-        entryFileNames: 'js/[name].[hash].js'
-      }
-    }
   }
 });
