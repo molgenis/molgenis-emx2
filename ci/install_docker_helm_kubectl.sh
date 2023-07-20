c7a0c0ed25332349e6eb12063a3145fa386c4309
Morris Swertz <m.a.swertz@rug.nl>
2023-07-20 09:53:41 +0200

    Update install_docker_helm_kubectl.sh

--- ci/install_docker_helm_kubectl.sh
+++ ci/install_docker_helm_kubectl.sh
@@ -4,17 +4,7 @@ apt-get install -y gnupg lsb-release ca-certificates curl apt-transport-https
 install -m 0755 -d /etc/apt/keyrings
 
 ##docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose
-systemctl start docker
-systemctl enable docker
-docker version
+curl -sSL https://get.docker.com/ | sh
 
 # kubectl repo
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
