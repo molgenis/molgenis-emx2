62da0d939bd8d78ea78d6978ca20a84765e80773
davidruvolo51 <davidruvolo51@gmail.com>
2023-07-21 11:43:12 +0200

    fixed import path

--- apps/ern-reconnet/src/main.js
+++ apps/ern-reconnet/src/main.js
@@ -2,7 +2,8 @@ import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
 
-import "../../molgenis-viz/dist/molgenis-viz.css"
+// import "../../molgenis-viz/dist/molgenis-viz.css"
+import "molgenis-viz/dist/molgenis-viz.css"
 
 const app = createApp(App);
 app.use(router);
