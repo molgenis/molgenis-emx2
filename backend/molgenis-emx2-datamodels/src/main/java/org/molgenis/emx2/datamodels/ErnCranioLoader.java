75cbf75445bd697fd7af709fb59f8b08a6dad913
dcruvolo <davidruvolo51@gmail.com>
2023-08-15 13:33:06 +0200

    fix: removed unused imports

--- backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/ErnCranioLoader.java
+++ backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/ErnCranioLoader.java
@@ -1,23 +1,13 @@
 package org.molgenis.emx2.datamodels;
 
-import org.molgenis.emx2.Privileges;
 import org.molgenis.emx2.Schema;
 import org.molgenis.emx2.io.MolgenisIO;
-import org.molgenis.emx2.sql.SqlDatabase;
 
 public class ErnCranioLoader extends AbstractDataLoader {
-  
+
   @Override
   void loadInternalImplementation(Schema schema, boolean includeDemoData) {
-    createSchema(schema, 'ern-cranio/model/molgenis.csv');
-    
-    // load ontologies
+    createSchema(schema, "ern-cranio/model/molgenis.csv");
     MolgenisIO.fromClasspathDirectory("ern-cranio/ontologies", schema, false);
-    
-    // load demo data
-    // if (includeDemoData) {
-    //   MolgenisIO.fromClasspathDirectory('ern-cranio/demoddata/...', schema, false);
-    // }
-    
   }
 }
