3cb4f435be90cdad42479f03c5c5f3f5214026e1
Postema Max <max@elynor.com>
2023-08-15 14:21:54 +0200

    fix: have working title/text entry

--- apps/dashboard/src/components/Modules/AggregateDisplay.vue
+++ apps/dashboard/src/components/Modules/AggregateDisplay.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
     <h1>Aggregate</h1>
-    <p>Wow cool aggregate bro!</p>
+    <AggregateTable
+      tableName="Pet"
+      schemaName="pet store"
+      :minimumValue="1"
+      :allColumns="allColumns"
+    />
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import { AggregateTable } from "molgenis-components";
+let allColumns = [
+  {
+    name: "name",
+    columnType: "STRING",
+  },
+  {
+    name: "category",
+    columnType: "REF",
+  },
+  {
+    name: "tags",
+    columnType: "ONTOLOGY_ARRAY",
+  },
+  {
+    name: "orders",
+    columnType: "REFBACK",
+  },
+];
 </script>
 
 <style></style>
--- apps/dashboard/src/components/Modules/TextDisplay.vue
+++ apps/dashboard/src/components/Modules/TextDisplay.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <div v-if="admin">
-      <input
-        :value="settings.title"
+      <InputString
+        id="TextDisplayTitle"
+        :modelValue="title"
         placeholder="title"
-        @input="change($event, 'title')"
-      /><br />
-      <input
-        :value="settings.text"
+        @update:modelValue="change($event, 'title')"
+        class="mb-1"
+      />
+      <InputString
+        id="TextDisplayText"
+        :modelValue="text"
         placeholder="text"
-        @input="change($event, 'text')"
+        @update:modelValue="change($event, 'text')"
       />
     </div>
     <h1>{{ settings.title }}</h1>
@@ -18,17 +21,28 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { InputString } from "molgenis-components";
 
 const props = defineProps({
   settings: Object,
   admin: Boolean,
 });
 
+let title = props.setting?.title;
+let text = props.setting?.text;
+
+watch(props, (newProps) => {
+  if (title === "" && newProps.settings.title !== "")
+    title = newProps.settings.title;
+  if (text === "" && newProps.settings.text !== "")
+    text = newProps.settings.text;
+});
+
 const emit = defineEmits(["update"]);
 
-function change(event, id) {
-  emit("update", { ...props.settings, [id]: event.target.value });
+function change(value, id) {
+  emit("update", { ...props.settings, [id]: value });
 }
 </script>
 
