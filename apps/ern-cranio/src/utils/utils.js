98ee214e1a53960c9d4b576efb644e4feabfd775
dcruvolo <davidruvolo51@gmail.com>
2023-09-05 16:00:13 +0200

    added styles and utils; added example header image

---
+++ apps/ern-cranio/src/utils/utils.js
@@ -0,0 +1,23 @@
+
+export async function getSessionInfo () {
+  const query = `{
+    _session {
+      email
+      roles
+      schemas
+      token
+    }
+  }`
+  
+  const response = await fetch('/api/graphql', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ query: query }),
+  });
+  
+  return response.json();
+  
+}
