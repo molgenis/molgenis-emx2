965eb96656164cad23f9b107242da996bee01164
dcruvolo <davidruvolo51@gmail.com>
2023-09-07 16:12:16 +0200

    init additional provider pages

---
+++ apps/ern-cranio/src/views/provider-genetic-deafness.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="providerGeneticDeafness" class="dashboard-content">
+    <h2>Genetic Deafness Dashboard</h2>
+  </div>
+</template>
---
+++ apps/ern-cranio/src/views/provider-larnyxcleft.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="providerLarnyxcleft" class="dashboard-content">
+    <h2>Larynxcleft Dashboard</h2>
+  </div>
+</template>
