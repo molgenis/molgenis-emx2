cffca318d665bed87f6a354bc9386f116b8c26a9
dcruvolo <davidruvolo51@gmail.com>
2023-09-07 16:14:27 +0200

    added additional components; redesigned layout

--- apps/ern-cranio/src/views/provider-overview.vue
+++ apps/ern-cranio/src/views/provider-overview.vue
@@ -1,81 +1,100 @@
 <template>
-  <div id="providerOverview" class="dashboard-content">
-    <div id="welcomeMessage" class="dashboard-section">
-      <h2>Welcome, [user]!</h2>
-      <p>Welcome to {{ center }}'s dashboard. These pages provide an overview of the patients you have submitted to relavent subregistries. On the current page, you will find a snapshot of your center as of today.</p>
-    </div>
-    <div id="totalPatientsSubmitted" class="dashboard-section value-highlight">
+  <div id="provider-overview" class="provider-visualizations">
+    <DashboardBox id="provider-overview-welcome">
+      <h2>Welcome, {{ user }}!</h2>
+      <p>
+        Welcome to {{ organization.name }}'s dashboard. These pages provide an
+        overview of the patients you have submitted to relavent subregistries.
+        On the current page, you will find a snapshot of your center as of
+        today.
+      </p>
+    </DashboardBox>
+    <DashboardBox
+      id="provider-overview-patients-submitted"
+      class="value-highlight"
+    >
       <h2>
         <UserGroupIcon />
         <span>{{ totalPatientsSubmitted }} patients submitted</span>
       </h2>
-      <p>Your center submitted on average of {{ avergagePatientsSubmitted }} patients per month.</p>
-    </div>
-    <div id="currentCenterRanking" class="dashboard-section value-highlight">
+      <p>
+        Your center submitted on average of
+        {{ avergagePatientsSubmitted }} patients per month.
+      </p>
+    </DashboardBox>
+    <DashboardBox id="provider-overview-current-rank" class="value-highlight">
       <h2>
         <TrophyIcon />
         <span>{{ currentCenterRanking }} in overall submitted patients</span>
       </h2>
-      <p>Patient's from your center account for {{ percentOfRegistry }}% of the registry.</p>
-    </div>
-    <div id="patientsBySubregistry" class="dashboard-section visualisation">
+      <p>
+        Patient's from your center account for {{ percentOfRegistry }}% of the
+        registry.
+      </p>
+    </DashboardBox>
+    <DashboardBox id="provider-overview-patients-by-workstream">
       <PieChart
         chartId="patientsByWorkstream"
         title="Patients submitted by workstream"
         :chartData="patientsBySubregistry"
-        :chartHeight="225"
+        :chartHeight="pieChartHeight"
         :asDonutChart="true"
-        :chartScale="0.9"
-        :enableHoverEvents="false"
-        :enableClicks="false"
+        :chartScale="0.95"
       />
-    </div>
-    <div id="patientsBySexAtBirth" class="dashboard-section visualisation">
+    </DashboardBox>
+    <DashboardBox id="provider-overview-patients-by-sex-at-birth">
       <PieChart
         chartId="sexAtBirth"
         title="Sex at birth"
         :chartData="sexAtBirth"
-        :chartHeight="225"
+        :chartHeight="pieChartHeight"
         :asDonutChart="true"
-        :chartScale="0.9"
-        :enableHoverEvents="false"
-        :enableClicks="false"
+        :chartScale="0.95"
       />
-    </div>
+    </DashboardBox>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { useRoute } from "vue-router";
 import { UserGroupIcon, TrophyIcon } from "@heroicons/vue/24/outline";
-import { PieChart } from "molgenis-viz";
+import { DashboardBox, PieChart } from "molgenis-viz";
 
-const route = useRoute();
-const center = ref(route.params.provider);
+const props = defineProps({
+  user: String,
+  organization: Object,
+});
+
+let pieChartHeight = ref(250);
+
+import { randomInt } from "d3";
 
 // generate random data for display purposes
-import { randomInt } from "d3";
-let totalPatientsSubmitted = ref(randomInt(25,100)());
-let avergagePatientsSubmitted = ref(Math.floor(totalPatientsSubmitted.value / 9));
+let totalPatientsSubmitted = ref(randomInt(25, 100)());
+let avergagePatientsSubmitted = ref(
+  Math.floor(totalPatientsSubmitted.value / 9)
+);
 
 const rank = randomInt(1, 25)();
-let currentCenterRanking = ref(rank === 1 ? '1st' : (rank === 2 ? '2nd' : `${rank}th`));
-let percentOfRegistry = ref(Math.round((totalPatientsSubmitted.value / 500) * 100));
+let currentCenterRanking = ref(
+  rank === 1 ? "1st" : rank === 2 ? "2nd" : `${rank}th`
+);
+let percentOfRegistry = ref(
+  Math.round((totalPatientsSubmitted.value / 500) * 100)
+);
 
 let patientsBySubregistry = ref({
-  'Cleft lip and palate': 0.4,
-  Craniosynostosis: 0.3,
-  Larynxcleft:  0.25,
-  'Genetic Deafness': 0.05,
-})
+  Larynxcleft: 56,
+  "Cleft lip and palate": 21,
+  Craniosynostosis: 14,
+  "Genetic Deafness": 9,
+});
 
 let sexAtBirth = ref({
-  Female: 0.5,
-  Male: 0.35,
-  Undetermined: 0.15
-})
-
+  Female: 50,
+  Male: 35,
+  Undetermined: 15,
+});
 </script>
 
 <style lang="scss">
@@ -90,31 +109,30 @@ let sexAtBirth = ref({
   }
 }
 
-#providerOverview {
-  grid-template-areas: 
+#provider-overview {
+  grid-template-areas:
     "header header"
     "totalPatients providerRank"
     "pieSubregistry pieSexAtBirth";
 }
 
-#welcomeMessage {
+#provider-overview-welcome {
   grid-area: header;
 }
 
-#totalPatientsSubmitted {
+#provider-overview-patients-submitted {
   grid-area: totalPatients;
 }
 
-#currentCenterRanking {
+#provider-overview-current-rank {
   grid-area: providerRank;
 }
 
-#patientsBySubregistry {
+#provider-overview-patients-by-workstream {
   grid-area: pieSubregistry;
 }
 
-#patientsBySexAtBirth {
+#provider-overview-patients-by-sex-at-birth {
   grid-area: pieSexAtBirth;
 }
-
 </style>
