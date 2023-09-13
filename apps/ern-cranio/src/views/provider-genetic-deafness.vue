32b6f0506fe2e6f988a6bf305e45bf78392769bf
dcruvolo <davidruvolo51@gmail.com>
2023-09-13 09:12:23 +0200

    added temporary message for pages not yet designed

--- apps/ern-cranio/src/views/provider-genetic-deafness.vue
+++ apps/ern-cranio/src/views/provider-genetic-deafness.vue
@@ -1,5 +1,12 @@
 <template>
-  <div id="providerGeneticDeafness" class="dashboard-content">
+  <div id="providerGeneticDeafness">
     <h2>Genetic Deafness Dashboard</h2>
+    <MessageBox type="warning">
+      <p>This page is under construction.</p>
+    </MessageBox>
   </div>
 </template>
+
+<script setup>
+import { MessageBox } from "molgenis-viz";
+</script>
--- apps/ern-cranio/src/views/provider-larnyxcleft.vue
+++ apps/ern-cranio/src/views/provider-larnyxcleft.vue
@@ -1,5 +1,12 @@
 <template>
-  <div id="providerLarnyxcleft" class="dashboard-content">
+  <div id="providerLarnyxcleft">
     <h2>Larynxcleft Dashboard</h2>
+    <MessageBox type="warning">
+      <p>This page is under construction.</p>
+    </MessageBox>
   </div>
 </template>
+
+<script setup>
+import { MessageBox } from "molgenis-viz";
+</script>
