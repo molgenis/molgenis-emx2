881ffb0937135ad176a50ef272aeeb326f1a1352
dcruvolo <davidruvolo51@gmail.com>
2023-09-25 14:48:49 +0200

    fix: adjusted proxy config

--- apps/cranio-provider/vite.config.js
+++ apps/cranio-provider/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const HOST = process.env.MOLGENIS_APPS_HOST || "https://emx2.dev.molgenis.org";
+const SCHEMA = process.env.MOLGENIS_APPS_SCHEMA || "NL1";
+const opts = { changeOrigin: true, secure: false, logLevel: "debug" };
+
 export default defineConfig(() => {
-  require('dotenv').config({ path: `./.env` });
+  // require('dotenv').config({ path: `./.env` });
  
   return {
     css: {
@@ -20,7 +24,31 @@ export default defineConfig(() => {
     plugins: [vue()],
     base: "",
     server: {
-      proxy: require("../dev-proxy.config"),
+      // proxy: require("../dev-proxy.config"),
+      proxy: {
+        "/graphql": {
+          target: `${HOST}/${SCHEMA}`,
+          ...opts,
+        },
+        // "^/.*/graphql$": {
+        //   target: `${HOST}/${SCHEMA}`,
+        //   changeOrigin: true,
+        //   secure: false,
+        // },
+        "/apps/central/theme.css": {
+          target: `${HOST}/${SCHEMA}`,
+          changeOrigin: true,
+          secure: false,
+        },
+        /* should match only '/schema_name/graphql', previous ** was to eager also matching if graphql was /graphql or /a/b/graphql */
+        "^/[a-zA-Z0-9_.%-]+/graphql": {
+          target: `${HOST}`,
+          ...opts,
+        },
+        "/api": { target: `${HOST}`, ...opts },
+        "/apps": { target: `${HOST}`, ...opts },
+        "/theme.css": { target: `${HOST}/${SCHEMA}`, ...opts },
+      }
     },
   }
 });
