5fd6e92a0b74d48c2f239d9179e8fd2d401fb59e
connoratrug <c.stroomberg@rug.nl>
2023-09-28 17:09:07 +0200

    feat: add generic hyperlink field
    - Add render component for generic field of type hyperlink
    - Field is rendered using Hyperlink component
    - Replace similar pieces of html with component

---
+++ apps/nuxt3-ssr/components/HyperLink.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    href: string;
+    target?: "_self" | "_blank" | "_parent" | "_top";
+  }>(),
+  {
+    target: "_self",
+  }
+);
+</script>
+
+<template>
+  <a
+    :href="href"
+    :target="target"
+    class="text-blue-500 underline hover:bg-blue-50"
+  >
+    <BaseIcon
+      v-if="target === '_blank'"
+      name="external-link"
+      class="inline mr-2"
+    />{{ href }}
+  </a>
+</template>
