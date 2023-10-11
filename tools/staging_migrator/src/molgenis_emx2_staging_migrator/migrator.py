5909dbabe0b4087c4981a37c61b80e76d9f771a9
ypezijlstra <info@ypezijlstra.frl>
2023-10-11 16:13:15 +0200

    Removed redundant methods

--- tools/staging_migrator/src/molgenis_emx2_staging_migrator/migrator.py
+++ tools/staging_migrator/src/molgenis_emx2_staging_migrator/migrator.py
@@ -6,12 +6,12 @@ import zipfile
 from io import BytesIO
 from typing import TypeAlias, Literal
 
-from molgenis_emx2_pyclient.exceptions import NoSuchSchemaException, NoSuchTableException, PyclientException
+from molgenis_emx2_pyclient.exceptions import NoSuchSchemaException
 
 from tools.pyclient.src.molgenis_emx2_pyclient import Client
 from tools.staging_migrator.src.molgenis_emx2_staging_migrator.constants import BASE_DIR
 from tools.staging_migrator.src.molgenis_emx2_staging_migrator.utils import get_cohort_ids, \
-    find_cohort_references, construct_delete_query, construct_delete_variables, prepare_pkey, has_statement_of_consent, \
+    find_cohort_references, construct_delete_query, construct_delete_variables, has_statement_of_consent, \
     process_statement
 
 log = logging.getLogger(__name__)
@@ -145,55 +145,6 @@ class StagingMigrator(Client):
             raise NoSuchSchemaException(f"Schema '{self.catalogue}' not found on server."
                                         f" Available schemas: {', '.join(self.schemas)}.")
 
-    def _synchronize_table(self, table_name: str, ref_col: str, cohort_ids: list):
-        """Synchronizes the table from the staging area to the catalogue by performing three tasks:
-        1. Delete rows from the catalogue relating to the cohort that are not in the staging area table
-        2. Update rows in the catalogue that are also in the staging area
-        3. Insert rows from the staging area into the catalogue that were not yet present
-        """
-        # Query the related rows in the staging area
-        staging_rows = self._get_staging_rows(table_name, ref_col, cohort_ids)
-
-        # Query the related rows in the catalogue
-        catalogue_rows = self._get_catalogue_rows(table_name, ref_col, cohort_ids)
-
-        # Filter records that are in catalogue table but not in staging area table
-        missing_rows = [row for row in catalogue_rows if row not in staging_rows]
-        self._delete_rows(table_name, missing_rows)
-
-        # Update rows to the catalogue table
-        present_rows = [row for row in staging_rows if row in catalogue_rows]
-        self._update_rows(table_name, present_rows, ref_col, cohort_ids)
-
-        # Insert rows to the catalogue table
-        new_rows = [row for row in staging_rows if row not in catalogue_rows]
-        self._insert_rows(table_name, new_rows)
-
-    def _get_staging_rows(self, table_name: str, ref_col: str, cohort_ids: list) -> list:
-        """Retrieves the rows corresponding to the Cohorts in the staging area table."""
-        return self._get_database_rows(self.staging_area, table_name, ref_col, cohort_ids)
-
-    def _get_catalogue_rows(self, table_name: str, ref_col: str, cohort_ids: list) -> list:
-        """Retrieves the rows corresponding to the Cohorts in the catalogue table."""
-        return self._get_database_rows(self.catalogue, table_name, ref_col, cohort_ids)
-
-    def _get_database_rows(self, schema, table_name: str, ref_col: str, cohort_ids: list) -> list:
-        """Retrieves the rows corresponding to the Cohorts in a schema's table."""
-        schema_schema = self._schema_schema(schema)
-        _schema_query = construct_delete_query(db_schema=schema_schema, table=table_name)
-        _schema_variables = construct_delete_variables(db_schema=schema_schema, cohort_ids=cohort_ids,
-                                                       t_name=table_name, t_type=ref_col)
-        _schema_response = self.session.post(url=f"{self.url}/{schema}/graphql",
-                                             json={"query": _schema_query, "variables": _schema_variables})
-        table_id = schema_schema[table_name].get('id')
-        if _schema_response.status_code == 400:
-            if 'Unknown type' in _schema_response.json().get('errors')[0].get('message'):
-                raise NoSuchTableException(f"Table '{table_id}' not in schema tables.")
-            else:
-                raise PyclientException
-        schema_rows = _schema_response.json().get('data').get(table_id, [])
-        return schema_rows
-
     def _create_upload_zip(self) -> BytesIO:
         """Combines the relevant tables of the staging area into a zipfile."""
         tables_to_sync = find_cohort_references(self._schema_schema(self.staging_area))
@@ -234,51 +185,3 @@ class StagingMigrator(Client):
         response_status = response.status_code
         print(response_status)
         print(str(response.text))
-
-    def _delete_rows(self, table_name: str, rows: list):
-        """Deletes rows from the table within a schema."""
-        self._delete_table_entries(table_name, rows)
-
-    def _update_rows(self, table_name: str, rows: list, ref_col: str, cohort_ids: list):
-        """Updates the already present rows in the catalogue table."""
-
-        # Do not update if there are no rows to update
-        if len(rows) < 1:
-            return
-
-        # Generate query that fetches all the table's columns for these rows
-        staging_schema = self._schema_schema(self.staging_area)
-        table_id = staging_schema[table_name].get('id')
-
-        filter_query = construct_delete_query(staging_schema, table_name, all_columns=True)
-
-        filter_variables = construct_delete_variables(staging_schema, cohort_ids, table_name, ref_col)
-
-        filter_response = self.session.post(url=f"{self.url}/{self.staging_area}/graphql",
-                                            json={"query": filter_query, "variables": filter_variables})
-        # TODO: remove following debugging line
-        if table_name == 'Contacts':
-            query = construct_delete_query(staging_schema, table_name, all_columns=True)
-            print("Does not work due to missing column.")
-            return
-
-        data_rows = filter_response.json().get('data').get(table_id)
-
-        # Use the result as variables for the mutation query
-
-        # Generate update query
-        update_query = f"mutation update($value:[{table_id}Input]){{update({table_id}:$value){{message}}}}"
-        update_variables = {"value": data_rows}
-
-        # Perform update query on catalogue database
-        update_response = self.session.post(url=f"{self.url}/{self.catalogue}/graphql",
-                                            json={"query": update_query, "variable": update_variables})
-
-        if update_response.status_code == 200:
-            log.info(update_response.json().get('message')[0])
-        else:
-            log.error(f"Error {update_response.status_code}: {update_response.json().get('errors')}")
-
-    def _insert_rows(self, table_name: str, rows: list):
-        """Inserts the new rows from the staging area into the catalogue."""
-        log.warning(f"Method '_insert_rows' not implemented yet.")
