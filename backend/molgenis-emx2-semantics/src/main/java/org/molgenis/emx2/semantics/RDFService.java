098d2eca65e889c0688b935465cd4ed3a2cfb5bf
mswertz <m.a.swertz@rug.nl>
2023-10-15 22:03:53 +0200

    undo change

--- backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/semantics/RDFService.java
+++ backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/semantics/RDFService.java
@@ -1,7 +1,6 @@
 package org.molgenis.emx2.semantics;
 
-import static org.eclipse.rdf4j.model.util.Values.iri;
-import static org.molgenis.emx2.semantics.rdf.ColumnTypeToXSDDataType.columnTypeToXSD;
+import static org.molgenis.emx2.semantics.rdf.ColumnToRDF.describeColumns;
 import static org.molgenis.emx2.semantics.rdf.RootToRDF.describeRoot;
 import static org.molgenis.emx2.semantics.rdf.SchemaToRDF.describeSchema;
 import static org.molgenis.emx2.semantics.rdf.SupportedRDFFileFormats.RDF_FILE_FORMATS;
@@ -150,47 +149,5 @@ public class RDFService {
 
   public RDFFormat getRdfFormat() {
     return rdfFormat;
-  }
-
-  public static void describeColumns(
-      ModelBuilder builder, String columnName, Table table, String schemaContext) {
-    String tableContext = schemaContext + "/" + table.getName();
-    for (Column column : table.getMetadata().getColumns()) {
-      if (columnName == null || column.getName().equals(columnName)) {
-        describeColumn(builder, schemaContext, column, tableContext);
-      }
-    }
-  }
-
-  private static void describeColumn(
-      ModelBuilder builder, String schemaContext, Column column, String tableContext) {
-    String columnContext = tableContext + "/column/" + column.getName();
-    // SIO:000757 = database column
-    builder.add(columnContext, RDF.TYPE, iri("http://semanticscience.org/resource/SIO_000757"));
-    builder.add(columnContext, RDF.TYPE, iri("http://purl.org/linked-data/cube#MeasureProperty"));
-    if (column.isReference()) {
-      builder.add(columnContext, RDF.TYPE, OWL.OBJECTPROPERTY);
-      builder.add(
-          columnContext,
-          RDFS.RANGE,
-          IRIParsingEncoding.encodedIRI(schemaContext + "/" + column.getRefTableName()));
-    } else {
-      builder.add(columnContext, RDF.TYPE, OWL.DATATYPEPROPERTY);
-      builder.add(columnContext, RDFS.RANGE, columnTypeToXSD(column.getColumnType()));
-    }
-    builder.add(columnContext, RDFS.LABEL, column.getName());
-    builder.add(columnContext, RDFS.DOMAIN, IRIParsingEncoding.encodedIRI(tableContext));
-    if (column.getSemantics() != null) {
-      for (String columnSemantics : column.getSemantics()) {
-        if (columnSemantics.equals("id")) {
-          // todo: need to figure out how to better handle 'id' tagging
-          columnSemantics = "http://semanticscience.org/resource/SIO_000115";
-        }
-        builder.add(columnContext, RDFS.ISDEFINEDBY, iri(columnSemantics));
-      }
-    }
-    if (column.getDescriptions() != null) {
-      builder.add(columnContext, DC.DESCRIPTION, column.getDescriptions());
-    }
   }
 }
--- backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/semantics/rdf/ColumnToRDF.java
+++ backend/molgenis-emx2-semantics/src/main/java/org/molgenis/emx2/semantics/rdf/ColumnToRDF.java
@@ -2,7 +2,6 @@ package org.molgenis.emx2.semantics.rdf;
 
 import static org.eclipse.rdf4j.model.util.Values.iri;
 import static org.molgenis.emx2.semantics.rdf.ColumnTypeToXSDDataType.columnTypeToXSD;
-import static org.molgenis.emx2.semantics.rdf.IRIParsingEncoding.encodedIRI;
 
 import org.eclipse.rdf4j.model.util.ModelBuilder;
 import org.eclipse.rdf4j.model.vocabulary.DC;
@@ -17,39 +16,44 @@ public class ColumnToRDF {
   // todo: unit is missing (which would also be a sdmx-attribute:unitMeasure, typed as an
   // qb:AttributeProperty)
   public static void describeColumns(
-      ModelBuilder builder, String columnName, Table table, String schemaContext) throws Exception {
+      ModelBuilder builder, String columnName, Table table, String schemaContext) {
     String tableContext = schemaContext + "/" + table.getName();
-    for (Column c : table.getMetadata().getColumns()) {
-      // allow selecting one particular column by ignoring the rest
-      if (columnName != null && !c.getName().equals(columnName)) {
-        continue;
+    for (Column column : table.getMetadata().getColumns()) {
+      if (columnName == null || column.getName().equals(columnName)) {
+        describeColumn(builder, schemaContext, column, tableContext);
       }
-      String columnContext = tableContext + "/column/" + c.getName();
-      // SIO:000757 = database column
-      builder.add(columnContext, RDF.TYPE, iri("http://semanticscience.org/resource/SIO_000757"));
-      builder.add(columnContext, RDF.TYPE, iri("http://purl.org/linked-data/cube#MeasureProperty"));
-      if (c.isReference() || c.isOntology()) {
-        builder.add(columnContext, RDF.TYPE, OWL.OBJECTPROPERTY);
-        builder.add(
-            columnContext, RDFS.RANGE, encodedIRI(schemaContext + "/" + c.getRefTableName()));
-      } else {
-        builder.add(columnContext, RDF.TYPE, OWL.DATATYPEPROPERTY);
-        builder.add(columnContext, RDFS.RANGE, columnTypeToXSD(c.getColumnType()));
-      }
-      builder.add(columnContext, RDFS.LABEL, c.getName());
-      builder.add(columnContext, RDFS.DOMAIN, encodedIRI(tableContext));
-      if (c.getSemantics() != null) {
-        for (String columnSemantics : c.getSemantics()) {
-          if (columnSemantics.equals("id")) {
-            // fixme: need to figure out how to better handle 'id' tagging
-            columnSemantics = "http://semanticscience.org/resource/SIO_000115";
-          }
-          builder.add(columnContext, RDFS.ISDEFINEDBY, iri(columnSemantics));
+    }
+  }
+
+  private static void describeColumn(
+      ModelBuilder builder, String schemaContext, Column column, String tableContext) {
+    String columnContext = tableContext + "/column/" + column.getName();
+    // SIO:000757 = database column
+    builder.add(columnContext, RDF.TYPE, iri("http://semanticscience.org/resource/SIO_000757"));
+    builder.add(columnContext, RDF.TYPE, iri("http://purl.org/linked-data/cube#MeasureProperty"));
+    if (column.isReference()) {
+      builder.add(columnContext, RDF.TYPE, OWL.OBJECTPROPERTY);
+      builder.add(
+          columnContext,
+          RDFS.RANGE,
+          IRIParsingEncoding.encodedIRI(schemaContext + "/" + column.getRefTableName()));
+    } else {
+      builder.add(columnContext, RDF.TYPE, OWL.DATATYPEPROPERTY);
+      builder.add(columnContext, RDFS.RANGE, columnTypeToXSD(column.getColumnType()));
+    }
+    builder.add(columnContext, RDFS.LABEL, column.getName());
+    builder.add(columnContext, RDFS.DOMAIN, IRIParsingEncoding.encodedIRI(tableContext));
+    if (column.getSemantics() != null) {
+      for (String columnSemantics : column.getSemantics()) {
+        if (columnSemantics.equals("id")) {
+          // todo: need to figure out how to better handle 'id' tagging
+          columnSemantics = "http://semanticscience.org/resource/SIO_000115";
         }
+        builder.add(columnContext, RDFS.ISDEFINEDBY, iri(columnSemantics));
       }
-      if (c.getDescriptions() != null) {
-        builder.add(columnContext, DC.DESCRIPTION, c.getDescriptions());
-      }
+    }
+    if (column.getDescriptions() != null) {
+      builder.add(columnContext, DC.DESCRIPTION, column.getDescriptions());
     }
   }
 }
