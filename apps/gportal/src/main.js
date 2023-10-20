6e861412f334c88009b6a8c06d0f163fa3a80a5b
dcruvolo <davidruvolo51@gmail.com>
2023-10-20 17:02:26 +0200

    feat: initial rewrite of gdi portal UI

--- apps/gportal/src/main.js
+++ apps/gportal/src/main.js
@@ -3,11 +3,15 @@ import { createRouter, createWebHashHistory } from "vue-router";
 
 import App from "./App.vue";
 import Beacon from "./components/Beacon.vue";
-import Datasets from "./components/Datasets.vue";
-import Welcome from "./components/Welcome.vue";
+// import Datasets from "./components/Datasets.vue";
+
+import HomePage from "./views/view-home.vue";
+import DatasetSearch from "./views/search-dataset.vue";
 
 import "molgenis-components/dist/style.css";
-import "./styles/index.scss";
+import "molgenis-viz/dist/style.css";
+
+const project = "Local GDI Portal";
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -19,24 +23,27 @@ const router = createRouter({
       props: true,
     },
     {
-      path: "/datasets",
-      component: Datasets,
-      props: true,
-    },
-    {
       name: "datasets",
       path: "/datasets",
-      component: Datasets,
+      component: DatasetSearch,
       props: true,
-    },
+    }, 
     {
       name: "home",
       path: "/",
-      component: Welcome,
+      component: HomePage,
       props: true,
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    return savedPosition || { top: 0, left: 0 };
+  },
 });
+
+router.afterEach((to) => {
+  document.title = to.meta.title ? `${to.meta.title} | ${project}` : project;
+});
+
 
 const app = createApp(App);
 app.use(router);
---
+++ apps/gportal/src/views/view-home.vue
@@ -0,0 +1,30 @@
+<template>
+  <Page>
+    <PageHeader
+      title="Local GDI Portal"
+      subtitle="Search for data and request access"
+      imageSrc="gdi-portal.jpg"
+      height="large"
+      titlePositionX="center"
+    />
+    <PageSection aria-labelledby="get-started-title" :verticalPadding="2">
+      <h2 id="get-started-title">Get Started</h2>
+      <p>Welcome to the <strong>GDI Local Portal</strong>. Here you can search for datasets and records, and apply for access to one or more datasets. To get started, use the dataset search for find collections of use the beacon search to find individual records.</p>
+    </PageSection>
+    <PageSection class="bg-gray-050" :verticalPadding="2" aria-labelledby="search-for-data-title">
+      <h2 id="search-for-data-title" class="visually-hidden">Search for data</h2>
+      <div class="link-card-container">
+        <LinkCard>
+          <router-link :to="{ name: 'datasets' }">Find Datasets</router-link>
+        </LinkCard>
+        <LinkCard>
+          <router-link :to="{ name: 'beacon' }">Search with Beacon</router-link>
+        </LinkCard>
+      </div>
+    </PageSection>
+  </Page>
+</template>
+
+<script setup>
+import { Page, PageHeader, PageSection, LinkCard } from "molgenis-viz";
+</script>
