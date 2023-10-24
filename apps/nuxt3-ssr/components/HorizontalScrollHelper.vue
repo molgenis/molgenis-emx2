6eb1eeca0195bf975829e289d7bf452b13e68043
Postema Max <max@elynor.com>
2023-10-24 17:24:03 +0200

    fix(nuxt3-ssr): layout brakes with incompressible components

---
+++ apps/nuxt3-ssr/components/HorizontalScrollHelper.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  addFade: {
+    type: Boolean,
+  },
+  addScrollButton: {
+    type: Boolean,
+  },
+});
+</script>
+
+<template>
+  <div class="relative">
+    <div
+      class="absolute z-10 right-0 inset-y-0 w-10 bg-gradient-to-r from-transparent to-white pointer-events-none"
+    ></div>
+    <div class="overflow-x-auto">
+      <slot></slot>
+    </div>
+  </div>
+</template>
