84ec9395271cbd5526e61fdfde57d50c5f2fc30f
mswertz <m.a.swertz@rug.nl>
2023-11-05 22:12:56 +0100

    first commits

---
+++ apps/molgenis-design-system/histoire.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "histoire";
+import { HstVue } from "@histoire/plugin-vue";
+
+export default defineConfig({
+  plugins: [HstVue()],
+  setupFile: "histoire.setup.ts",
+  vite: {
+    server: {
+      fs: {
+        allow: [
+          "../node_modules/histoire/dist/node/builtin-plugins/vanilla-support",
+          "../node_modules/@histoire/plugin-vue/dist/bundled/client/app",
+        ],
+      },
+    },
+  },
+});
---
+++ apps/molgenis-design-system/src/components/inputs/Button.story.vue
@@ -0,0 +1,8 @@
+<template>
+  <Story title="Inputs/Buttons">
+    <Button label="Call to action" type="primary" size="medium" />
+  </Story>
+</template>
+<script setup>
+import Button from "./Button.vue";
+</script>
