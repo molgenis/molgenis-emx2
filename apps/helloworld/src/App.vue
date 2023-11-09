0e6df4dc349ec6d5722e2c2d0593837b8f4ee94f
connoratrug <c.stroomberg@rug.nl>
2023-11-09 16:01:01 +0100

    chore: add lib functions
    and extend sample use with function and type import

--- apps/helloworld/src/App.vue
+++ apps/helloworld/src/App.vue
@@ -8,10 +8,18 @@
 
 <script setup lang="ts">
 import { Molgenis } from "molgenis-components";
-import { sayHello } from "meta-data-utils";
+import { sayHello, isEmpty } from "meta-data-utils";
+import type { ISetting } from "meta-data-utils";
 import { ref } from "vue";
 
 const msg = sayHello("World");
+
+const setting: ISetting = {
+  key: "test",
+  value: "test",
+};
+
+console.log("is setting empty : ", isEmpty(setting));
 
 const session = ref(null);
 const page = ref(null);
