1befd52150222c676e2a7873aa687d027f7eb2e2
connoratrug <c.stroomberg@rug.nl>
2023-11-09 15:10:57 +0100

    chore: setup utils module with type exports and usage example

--- apps/helloworld/src/App.vue
+++ apps/helloworld/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <Molgenis id="__top" v-model="session">
     <router-view :session="session" :page="page" />
+
+    <h3>{{ msg }}</h3>
   </Molgenis>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { Molgenis } from "molgenis-components";
+import { sayHello } from "meta-data-utils";
 import { ref } from "vue";
+
+const msg = sayHello("World");
 
 const session = ref(null);
 const page = ref(null);
