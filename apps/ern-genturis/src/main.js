fcf7682ff593efca3806b0ecc5968cb83a58846e
dcruvolo <davidruvolo51@gmail.com>
2023-11-10 13:01:32 +0100

    init router

--- apps/ern-genturis/src/main.js
+++ apps/ern-genturis/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./router/router";
 
 import "molgenis-components/dist/style.css";
 import "molgenis-viz/dist/style.css";
