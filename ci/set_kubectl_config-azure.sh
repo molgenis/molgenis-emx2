d74b5f45d28dc8439bffa62f929c99215ba99974
DickPostma <f.d.postma@umcg.nl>
2023-11-10 14:07:37 +0100

    Variablen goed zetten

--- ci/set_kubectl_config-azure.sh
+++ ci/set_kubectl_config-azure.sh
@@ -2,7 +2,7 @@ KUBE_CLUSTER=$1
 KUBE_TOKEN=$2
 
 echo " logging in to azure with service principal and get kube config"
-az login --service-principal --tenant ${AZURE_SP_TENANT} -u ${AZURE_SP} -p ${AZURE_SP_PASSWORD}
+az login --service-principal --tenant ${AZURE_SP_TENANT} -u ${AZURE_CLIENT_ID} -p ${AZURE_SECRET}
 az aks get-credentials -g ${RESOURCE_GROUP} -n ${RESOURCE_GROUP}
 
 kubectl config set-cluster ${RESOURCE_GROUP}
