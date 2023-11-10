e955f1efd2c9f64b8a0d020485b7fb1b398cc4d0
DickPostma <f.d.postma@umcg.nl>
2023-11-10 12:34:37 +0100

    feat: deploy also to azure

--- ci/set_kubectl_config-azure.sh
+++ ci/set_kubectl_config-azure.sh
@@ -5,5 +5,5 @@ echo " logging in to azure with service principal and get kube config"
 az login --service-principal --tenant ${AZURE_SP_TENANT} -u ${AZURE_SP} -p ${AZURE_SP_PASSWORD}
 az aks get-credentials -g ${RESOURCE_GROUP} -n ${RESOURCE_GROUP}
 
-kubectl config set-cluster kb-medgen-dev
-kubectl config use-context kb-medgen-dev
+kubectl config set-cluster ${RESOURCE_GROUP}
+kubectl config use-context ${RESOURCE_GROUP}
