8b63f9e1c0e4131e56a00d681fc7ce0b994f0e59
dcruvolo <davidruvolo51@gmail.com>
2023-11-13 16:24:48 +0100

    added padding options and updated styling

--- apps/molgenis-viz/src/components/layouts/DashboardChart.vue
+++ apps/molgenis-viz/src/components/layouts/DashboardChart.vue
@@ -1,9 +1,40 @@
 <template>
-  <div class="dashboard-box">
+  <div :class="classNames">
     <!-- Layout component for visualization -->
     <slot></slot>
   </div>
 </template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  // specify the left and right padding of the default slot
+  horizontalPadding: {
+    // `0:5`
+    type: Number,
+    // `1`
+    default: 1,
+    validator: (value) => {
+      return value >= 0 && value <= 5;
+    },
+  },
+  // specify the top and bottom padding of the default slote
+  verticalPadding: {
+    // `0:5`
+    type: Number,
+    // `1`
+    default: 1,
+    validator: (value) => {
+      return value >= 0 && value <= 5;
+    },
+  },
+});
+
+const classNames = computed(() => {
+  return `dashboard-box padding-h-${props.horizontalPadding} padding-v-${props.verticalPadding}`;
+});
+</script>
 
 <style lang="scss">
 .dashboard-box {
