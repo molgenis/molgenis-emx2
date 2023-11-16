afa2efe51978a2784829d8309e0262b80a8dd9ab
connoratrug <47183404+connoratrug@users.noreply.github.com>
2023-11-16 11:15:00 +0100

    feat: catalogue variable  harmonisation (#2976)
    feat: catalogue variable harmonization
    
    Only show cohorts that have a mapping on the variable hamonization details view
    Handle case where var has no mappings
    Hamonization name icon size adjustable

--- apps/nuxt3-ssr/components/harmonization/HarmonizationLegend.vue
+++ apps/nuxt3-ssr/components/harmonization/HarmonizationLegend.vue
@@ -1,11 +1,29 @@
+<script setup lang="ts">
+import type { HarmonizationIconSize } from "../../interfaces/types";
+
+const props = withDefaults(
+  defineProps<{
+    size: HarmonizationIconSize;
+  }>(),
+  { size: "large" }
+);
+</script>
 <template>
   <div class="flex">
     <div class="flex items-center h-16">
-      <HarmonizationStatusIcon size="5" status="complete" class="mr-1 ml-6" />
+      <HarmonizationStatusIcon :size="size" status="complete" class="mr-1" />
       Completed
-      <HarmonizationStatusIcon size="5" status="partial" class="mr-1 ml-6" />
+      <HarmonizationStatusIcon
+        :size="size"
+        status="partial"
+        class="mr-1 ml-6"
+      />
       Partial
-      <HarmonizationStatusIcon size="5" status="unmapped" class="mr-1 ml-6" />
+      <HarmonizationStatusIcon
+        :size="size"
+        status="unmapped"
+        class="mr-1 ml-6"
+      />
       No Data
       <a class="ml-6 mr-8 hover:underline text-blue-500 cursor-help"
         >About statuses</a
