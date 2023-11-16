23efa8bee5dc330d6daa6e0f51f4a0fe94eaa8ed
dcruvolo <davidruvolo51@gmail.com>
2023-11-16 10:40:23 +0100

    integrated new dashboard layout components

--- apps/cranio-provider/vite.config.js
+++ apps/cranio-provider/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 const host = "https://beta-erncranio.molgeniscloud.org";
-const schema = "NL1";
+const schema = "NL2";
 const opts = { changeOrigin: true, secure: false, logLevel: "debug" };
 
 export default defineConfig(() => {
