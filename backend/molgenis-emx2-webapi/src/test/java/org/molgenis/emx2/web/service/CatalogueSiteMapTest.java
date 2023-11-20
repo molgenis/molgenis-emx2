a6cf771d64dbe33c80ba2f035f16ef763879921f
mswertz <m.a.swertz@rug.nl>
2023-11-20 15:39:43 +0100

    fix test

--- backend/molgenis-emx2-webapi/src/test/java/org/molgenis/emx2/web/service/CatalogueSiteMapTest.java
+++ backend/molgenis-emx2-webapi/src/test/java/org/molgenis/emx2/web/service/CatalogueSiteMapTest.java
@@ -30,7 +30,7 @@ public class CatalogueSiteMapTest {
         "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
             + "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" >\n"
             + "  <url>\n"
-            + "    <loc>https://my/base/url/ssr-catalogue/all/cohorts/my-cohort-id</loc>\n"
+            + "    <loc>https://my/base/url/ssr-catalogue/all/Cohorts/my-cohort-id</loc>\n"
             + "  </url>\n"
             + "</urlset>";
     assertEquals(expected, catalogueSiteMap.buildSiteMap());
