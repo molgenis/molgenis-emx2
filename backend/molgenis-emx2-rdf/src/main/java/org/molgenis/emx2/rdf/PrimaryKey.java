971faf98680ae722801742838bdc5f9d2a9597f6
Esther van Enckevort <esther@vanenckevort.net>
2023-11-20 16:55:52 +0100

    fix #3007 urls including commas give issues with rdf
    - fixed #3007 urls including commas give issues with RDF by encoding the key components
    - fixed #2987 Download all links don't work in case of a table name with a space by using getTableById in MolgenisWebservice.getTable and removed duplicated code for the two variants of getTable
    - Separated out some primary key resolving to a separate class to reduce complexity of RDFService

---
+++ backend/molgenis-emx2-rdf/src/main/java/org/molgenis/emx2/rdf/PrimaryKey.java
@@ -0,0 +1,73 @@
+package org.molgenis.emx2.rdf;
+
+import static org.molgenis.emx2.FilterBean.and;
+import static org.molgenis.emx2.FilterBean.f;
+import static org.molgenis.emx2.Operator.EQUALS;
+
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.molgenis.emx2.Filter;
+
+class PrimaryKey {
+
+  public static final String NAME_VALUE_SEPARATOR = "&";
+  public static final String KEY_PARTS_SEPARATOR = ";";
+  private final List<NameValuePair> keys;
+
+  static PrimaryKey makePrimaryKeyFromEncodedKey(String encodedValue) {
+    Base64.Decoder decoder = Base64.getDecoder();
+    String[] encodedPairs = encodedValue.split(KEY_PARTS_SEPARATOR);
+    if (encodedPairs.length == 0) {
+      throw new IllegalArgumentException("There must be at least one key.");
+    } else {
+      List<NameValuePair> pairs = new ArrayList<>();
+      for (var pair : encodedPairs) {
+        var parts = pair.split(NAME_VALUE_SEPARATOR);
+        if (parts.length != 2) {
+          throw new IllegalArgumentException(
+              "Can't decode the key, name value pair is incomplete.");
+        }
+        var name = new String(decoder.decode(parts[0]), StandardCharsets.UTF_8);
+        var value = new String(decoder.decode(parts[1]), StandardCharsets.UTF_8);
+        pairs.add(new BasicNameValuePair(name, value));
+      }
+      return new PrimaryKey(pairs);
+    }
+  }
+
+  PrimaryKey(List<NameValuePair> keys) {
+    if (keys.isEmpty()) {
+      throw new IllegalArgumentException("There must be at least one key.");
+    }
+    this.keys = keys;
+  }
+
+  String getEncodedValue() {
+    Base64.Encoder encoder = Base64.getEncoder();
+    List<String> encodedPairs = new ArrayList<>();
+    // Sort the list to have a stable order
+    var sortedList = keys.stream().sorted(new NameValuePairComparator()).toList();
+    for (var pair : sortedList) {
+      var name = encoder.encodeToString(pair.getName().getBytes(StandardCharsets.UTF_8));
+      var value = encoder.encodeToString(pair.getValue().getBytes(StandardCharsets.UTF_8));
+      encodedPairs.add(name + NAME_VALUE_SEPARATOR + value);
+    }
+    return String.join(KEY_PARTS_SEPARATOR, encodedPairs);
+  }
+
+  Filter getFilter() {
+    final List<Filter> filters =
+        keys.stream().map(param -> f(param.getName(), EQUALS, param.getValue())).toList();
+    return and(filters);
+  }
+
+  static class NameValuePairComparator implements Comparator<NameValuePair> {
+
+    @Override
+    public int compare(NameValuePair left, NameValuePair right) {
+      return left.getName().compareTo(right.getName());
+    }
+  }
+}
--- backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/MolgenisWebservice.java
+++ backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/MolgenisWebservice.java
@@ -220,15 +220,7 @@ public class MolgenisWebservice {
   /** get database either from session or based on token */
   // helper method used in multiple places
   public static Table getTable(Request request) {
-    String schemaName = request.params(SCHEMA);
-    Schema schema =
-        sessionManager.getSession(request).getDatabase().getSchema(sanitize(schemaName));
-    if (schema == null) {
-      throw new MolgenisException("Schema " + schemaName + " unknown or access denied");
-    }
-    Table table = schema.getTable(sanitize(request.params(TABLE)));
-    if (table == null) throw new MolgenisException("Table " + request.params(TABLE) + " unknown");
-    return table;
+    return getTable(request, request.params(TABLE));
   }
 
   /** alternative version for getTable */
@@ -239,7 +231,7 @@ public class MolgenisWebservice {
     if (schema == null) {
       throw new MolgenisException("Schema " + schemaName + " unknown or access denied");
     } else {
-      return schema.getTable(sanitize(tableName));
+      return schema.getTableById(sanitize(tableName));
     }
   }
 
