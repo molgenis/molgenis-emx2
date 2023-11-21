efcde849105f8b066e3de8c451c735504b491413
dcruvolo <davidruvolo51@gmail.com>
2023-11-21 16:05:12 +0100

    added support for labels and errors; added sytling for focus states

--- apps/molgenis-design-system/src/components/inputs/InputString.story.vue
+++ apps/molgenis-design-system/src/components/inputs/InputString.story.vue
@@ -1,8 +1,49 @@
 <template>
-  <Story title="Inputs/String">
-    <InputString id="string" />
+  <Story title="Inputs/String" :layout="{ type: 'grid', width: '400px' }">
+    <Variant title="Default">
+      <InputString
+        id="search-patients-default"
+        :label="label"
+        :placeholder="placeholder"
+      />
+    </Variant>
+    <Variant title="Disabled">
+      <InputString
+        id="search-patients-disabled"
+        :label="label"
+        :placeholder="placeholder"
+        :is-disabled="true"
+      />
+    </Variant>
+    <Variant title="Required">
+      <InputString
+        id="search-patients-required"
+        :label="label"
+        :placeholder="placeholder"
+        :is-required="true"
+      />
+    </Variant>
+    <Variant title="Valid">
+      <InputString
+        id="search-patients-valid"
+        :label="label"
+        :placeholder="placeholder"
+        :is-valid="true"
+      />
+    </Variant>
+    <Variant title="Error">
+      <InputString
+        id="search-patients-error"
+        :label="label"
+        :placeholder="placeholder"
+        :has-error="true"
+      />
+    </Variant>
   </Story>
 </template>
 <script setup lang="ts">
 import InputString from "./InputString.vue";
+
+const label="Search for patients"
+const placeholder="Enter one or more identifiers; e.g., 'A-001' or 'A-001,A-002'"
 </script>
--- apps/molgenis-design-system/src/components/inputs/InputString.vue
+++ apps/molgenis-design-system/src/components/inputs/InputString.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { CheckCircleIcon, ExclamationTriangleIcon } from "@heroicons/vue/24/solid";
 
-withDefaults(
-  defineProps<{
-    id: string;
-    required: boolean;
-    hasError?: boolean;
-  }>(),
+interface Props {
+  id: string;
+  label: string,
+  placeholder?: string,
+  isDisabled?: boolean,
+  isRequired: boolean;
+  isValid?: boolean, 
+  hasError?: boolean,
+  error?:string,
+}
+
+const props = withDefaults(
+  defineProps<Props>(),
   {
-    required: false,
+    isValid: false,
+    isDisabled: false,
+    isRequired: false,
     hasError: false,
   }
 );
@@ -17,12 +27,42 @@ const modelValue = ref("");
 </script>
 
 <template>
-  <input
-    v-model="modelValue"
-    type="text"
-    :id="id"
-    :required="required"
-    class="w-full pr-16 font-sans text-black text-gray-300 bg-white outline-none rounded-search-input h-10 ring-red-500 pl-3 shadow-search-input focus:shadow-search-input hover:shadow-search-input search-input-mobile border"
-    :class="{ 'border-red-500 text-red-500': hasError }"
-  />
+  <div 
+    :class="`
+      flex flex-row justify-center items-center w-full border-2 py-2 rounded-full
+      [&:first-child]:focus-within:border-gray-600
+      ${isDisabled ? 'bg-gray-200/10': ''}
+      ${hasError ? 'border-red-500 [&_label]:text-red-500 [&_label]:font-semibold [&_svg>path]:fill-red-500' : ''}
+      ${isValid ? 'border-green-800 [&_svg>path]:fill-green-800' : ''}
+    `"
+  >
+  <div class="flex flex-col-reverse w-5/6">
+    <input
+      v-model="modelValue"
+      type="text"
+      :id="id"
+      :placeholder="placeholder"
+      :required="isRequired"
+      :disabled="isDisabled"
+      class="peer grow block w-[90%] py-1 pl-3 ml-6 bg-transparent focus:outline-none"
+    />
+    <label
+      :for="id"
+      class="
+        block ml-9 grow 
+        peer-required:after:content-['*']
+        peer-required:after:text-red-500
+        peer-disabled:text-gray-400
+      "
+    >
+      {{ label }}
+    </label>
+  </div>
+  <div class="w-1/6 [&_svg]:w-[1.5rem]">
+    <component :is="isValid ? CheckCircleIcon : hasError ? ExclamationTriangleIcon : null" class="m-auto" />
+  </div>
+  </div>
+  <div v-if="hasError" class="w-full md:w-[92%] m-auto mt-2">
+    <p class="text-red-500 font-semibold">{{ error }}</p>
+  </div>
 </template>
