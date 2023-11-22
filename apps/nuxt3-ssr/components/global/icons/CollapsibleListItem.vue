511df84b642f826f15d10885ee544619f65f26b7
Morris Swertz <m.a.swertz@rug.nl>
2023-11-22 21:41:00 +0100

    fix: yarn parse-icons was failing because of previously updated svgo dependency (#3012)
    * updated svgo requires new settings
    
    * remove unneeded comment

--- apps/nuxt3-ssr/components/global/icons/CollapsibleListItem.vue
+++ apps/nuxt3-ssr/components/global/icons/CollapsibleListItem.vue
@@ -1,7 +1,7 @@
 <template>
-  <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 22">
+  <svg viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
-      d="M1 1v20h18"
+      d="M1 1V21H19"
       stroke="currentColor"
       stroke-linecap="round"
       stroke-linejoin="round"
