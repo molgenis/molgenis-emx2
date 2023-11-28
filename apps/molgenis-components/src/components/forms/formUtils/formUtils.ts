a244465df1de48fa0859878b88694243bcb9d783
konstantina-gkp <k.gkikopouli@umcg.nl>
2023-11-28 10:48:44 +0100

    Fix #2432 Array type input fields in molgenis-components were not properly validated when marked as required. The issue has been fixed. Each individual field in an array input component is now validated separately.

--- apps/molgenis-components/src/components/forms/formUtils/formUtils.ts
+++ apps/molgenis-components/src/components/forms/formUtils/formUtils.ts
@@ -18,18 +18,26 @@ export function getRowErrors(
 
 function getColumnError(
   column: IColumn,
-  values: Record<string, any>,
+  rowData: Record<string, any>,
   tableMetaData: ITableMetaData
 ) {
-  const value = values[column.id];
+  const value = rowData[column.id];
   const type = column.columnType;
   const isInvalidNumber = isInValidNumericValue(type, value);
   // FIXME: this function should also check all array types
   // FIXME: longs are not checked
-  const missesValue = value === undefined || value === null || value === "";
+
+  const isMissingValue = (value: any): boolean => {
+    if (Array.isArray(value)) {
+      return value.some(element => isMissingValue(element));
+    }
+    return value === undefined || value === null || value === "";
+  };
+
+  const missesValue = isMissingValue(value);
 
   try {
-    if (!isColumnVisible(column, values, tableMetaData)) {
+    if (!isColumnVisible(column, rowData, tableMetaData)) {
       return undefined;
     }
   } catch (error) {
@@ -58,9 +66,9 @@ function getColumnError(
     return "Invalid hyperlink";
   }
   if (column.validation) {
-    return getColumnValidationError(column.validation, values, tableMetaData);
+    return getColumnValidationError(column.validation, rowData, tableMetaData);
   }
-  if (isRefLinkWithoutOverlap(column, values)) {
+  if (isRefLinkWithoutOverlap(column, rowData)) {
     return `value should match your selection in column '${column.refLinkId}'`;
   }
 
