cb25a568c0fcf1a9768e1e31defc76db15f03681
mswertz <m.a.swertz@rug.nl>
2023-12-01 13:53:53 +0100

    fix: duplicate tables showing those from other schemas

--- apps/tables/src/components/ListTables.vue
+++ apps/tables/src/components/ListTables.vue
@@ -74,7 +74,7 @@ export default {
             )
           );
       } else {
-        return this.schema.tables;
+        return this.schema.tables          .filter((table) => table.schemaId === this.schema.id);
       }
     },
     tables() {
