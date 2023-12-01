af57902a07c200f8406b60b6893b9c1c16bb4d80
connoratrug <c.stroomberg@rug.nl>
2023-12-01 13:53:44 +0100

    fix: only show error of error ref has value
    plus run code format

--- apps/nuxt3-ssr/pages/[schema]/ssr-catalogue/[catalogue]/datasources/index.vue
+++ apps/nuxt3-ssr/pages/[schema]/ssr-catalogue/[catalogue]/datasources/index.vue
@@ -6,7 +6,8 @@ const router = useRouter();
 const config = useRuntimeConfig();
 const pageSize = 10;
 
-const titlePrefix = route.params.catalogue === "all" ? "" : route.params.catalogue + " ";
+const titlePrefix =
+  route.params.catalogue === "all" ? "" : route.params.catalogue + " ";
 useHead({ title: titlePrefix + "Data sources" });
 
 const currentPage = ref(1);
@@ -99,8 +100,8 @@ const { data, pending, error, refresh } = await useFetch(graphqlURL.value, {
     variables: { orderby, filter },
   },
 });
-if (error) {
-  console.log(error);
+if (error.value) {
+  console.log(error.value);
   console.log(query);
 }
 
