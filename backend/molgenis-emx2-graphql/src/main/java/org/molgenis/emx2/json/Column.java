bdcffec9661f9da1b8520a5633c0e0d369b63490
harmbrugge <harmbrugge@gmail.com>
2023-12-14 10:41:15 +0100

    Refactor column required field to String

--- backend/molgenis-emx2-graphql/src/main/java/org/molgenis/emx2/json/Column.java
+++ backend/molgenis-emx2-graphql/src/main/java/org/molgenis/emx2/json/Column.java
@@ -16,7 +16,7 @@ public class Column {
   private boolean drop = false; // needed in case of migrations
   private String oldName;
   private Integer key = 0;
-  private Boolean required = false;
+  private String required = null;
   private Boolean readonly = false;
   private String defaultValue;
   private String refSchemaId = null;
@@ -87,7 +87,7 @@ public class Column {
     this.refLabelDefault = column.getRefLabelDefault();
     // this.cascadeDelete = column.isCascadeDelete();
     this.validation = column.getValidation();
-    this.required = column.isRequired();
+    this.setRequired(column.isRequired());
     this.readonly = column.isReadonly();
     this.defaultValue = column.getDefaultValue();
     this.descriptions =
@@ -160,12 +160,20 @@ public class Column {
     this.key = key;
   }
 
-  public Boolean getRequired() {
-    return required;
+  public boolean isRequired() {
+    return required != null && required.equals("true");
   }
 
   public void setRequired(Boolean required) {
+    this.required = required.toString();
+  }
+
+  public void setRequired(String required) {
     this.required = required;
+  }
+
+  public String getRequired() {
+    return this.required;
   }
 
   public String getRefTableId() {
