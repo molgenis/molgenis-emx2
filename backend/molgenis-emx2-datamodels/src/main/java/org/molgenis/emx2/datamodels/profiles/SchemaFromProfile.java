11206070e3014453c840682636c36a1c6d8d4224
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2023-12-23 16:15:42 +0100

    Load models and profiles from a shared directory

--- backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/profiles/SchemaFromProfile.java
+++ backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/profiles/SchemaFromProfile.java
@@ -5,9 +5,12 @@ import static org.molgenis.emx2.datamodels.profiles.PostProcessProfiles.csvStrin
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import org.molgenis.emx2.MolgenisException;
 import org.molgenis.emx2.Row;
 import org.molgenis.emx2.SchemaMetadata;
@@ -45,42 +48,99 @@ public class SchemaFromProfile {
   }
 
   public SchemaMetadata create() throws MolgenisException {
+    String sharedModelsDir = File.separator + "_models" + File.separator + "shared";
+    String specificModelsDir = File.separator + "_models" + File.separator + "specific";
     List<Row> keepRows = new ArrayList<>();
-
-    for (String schemaLoc : profiles.modelsList) {
-      SchemaMetadata metadata =
-          Emx2.fromRowList(
-              CsvTableReader.read(
-                  new InputStreamReader(
-                      AbstractDataLoader.class.getClassLoader().getResourceAsStream(schemaLoc))));
-
-      for (Row row : Emx2.toRowList(metadata)) {
-        List<String> profiles = csvStringToList(row.getString("profiles"));
-
-        if (profiles.isEmpty()) {
-          throw new MolgenisException("No profiles for " + row);
-        }
-
-        boolean profileFound = false;
-        for (String profile : profiles) {
-          if (this.profiles.profilesList.contains(profile)) {
-            keepRows.add(row);
-            profileFound = true;
-            break;
-          }
-        }
-        if (!profileFound) {
-          System.out.println("discard: " + row);
-        }
-      }
+    try {
+      keepRows.addAll(getProfilesFromAllModels(sharedModelsDir));
+      keepRows.addAll(getProfilesFromAllModels(specificModelsDir));
+    } catch (Exception e) {
+      throw new MolgenisException(e.getMessage());
     }
-
     SchemaMetadata generatedSchema = Emx2.fromRowList(keepRows);
-
     return generatedSchema;
   }
 
-  public static void main(String args[]) throws IOException {
+  /**
+   * From a classpath dir, get all EMX2 model files and slice for profiles
+   *
+   * @param directory
+   * @return
+   */
+  public List<Row> getProfilesFromAllModels(String directory)
+      throws URISyntaxException, IOException {
+    List<Row> keepRows = new ArrayList<>();
+    String[] modelsList = getResourceListing(directory);
+    for (String schemaLoc : modelsList) {
+
+      Iterable<Row> rowIterable =
+          CsvTableReader.read(
+              new InputStreamReader(
+                  Objects.requireNonNull(
+                      getClass().getResourceAsStream(directory + File.separator + schemaLoc))));
+
+      for (Row row : rowIterable) {
+        List<String> profiles = csvStringToList(row.getString("profiles"));
+        if (profiles.isEmpty()) {
+          throw new MolgenisException("No profiles for " + row);
+        }
+        for (String profile : profiles) {
+          if (this.profiles.profilesList.contains(profile)) {
+            keepRows.add(row);
+            break;
+          }
+        }
+      }
+    }
+    return keepRows;
+  }
+
+  /**
+   * List directory contents for a resource folder. Not recursive. Works for regular files and JARs.
+   * Based on original by Greg Briggs, see:
+   * https://www.uofr.net/~greg/java/get-resource-listing.html
+   *
+   * @return Just the name of each member item, not the full paths.
+   */
+  String[] getResourceListing(String path) throws URISyntaxException, IOException {
+    URL dirURL = getClass().getResource(path);
+    if (dirURL == null) {
+      throw new MolgenisException(
+          "Import failed: Directory " + path + " doesn't exist in classpath");
+    }
+    if (dirURL.getProtocol().equals("file")) {
+      return new File(dirURL.toURI()).list();
+    }
+    if (dirURL.getProtocol().equals("jar")) {
+      String jarPath =
+          dirURL
+              .getPath()
+              .substring(5, dirURL.getPath().indexOf("!")); // strip out only the JAR file
+      String matchAgainstPath = path;
+      if (matchAgainstPath.startsWith(File.separator)) {
+        matchAgainstPath = path.substring(1);
+      }
+      JarFile jar = new JarFile(URLDecoder.decode(jarPath, "UTF-8"));
+      Enumeration<JarEntry> entries = jar.entries(); // gives ALL entries in jar
+      Set<String> result = new HashSet<String>(); // avoid duplicates in case it is a subdirectory
+      while (entries.hasMoreElements()) {
+        String name = entries.nextElement().getName();
+        if (name.startsWith(matchAgainstPath)) { // filter according to the path
+          String entry = name.substring(path.length());
+          int checkSubdir = entry.indexOf("/");
+          if (checkSubdir >= 0) {
+            // if it is a subdirectory, we just return the directory name
+            entry = entry.substring(0, checkSubdir);
+          }
+          result.add(entry);
+        }
+      }
+      return result.toArray(new String[result.size()]);
+    }
+    throw new UnsupportedOperationException("Cannot list files for URL " + dirURL);
+  }
+
+  public static void main(String args[]) throws IOException, URISyntaxException {
     SchemaFromProfile sfp = new SchemaFromProfile("fairdatahub/FAIRDataHub.yaml");
     SchemaMetadata generatedSchema = sfp.create();
     System.out.println("resulting schema: " + generatedSchema);
