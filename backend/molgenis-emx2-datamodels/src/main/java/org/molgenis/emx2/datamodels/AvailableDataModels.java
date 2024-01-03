daa5cc44551e14d231c70203015e37394d846d32
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2024-01-03 14:30:48 +0100

    Move and update profiles

--- backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/AvailableDataModels.java
+++ backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/AvailableDataModels.java
@@ -6,16 +6,16 @@ public enum AvailableDataModels {
   DIRECTORY(new DirectoryLoader()),
   DATA_CATALOGUE_COHORT_STAGING(new DataCatalogueCohortStagingLoader()),
   DATA_CATALOGUE_NETWORK_STAGING(new DataCatalogueNetworkStagingLoader()),
-  DATA_CATALOGUE(new ProfileLoader("_templates/DataCatalogue.yaml")),
+  DATA_CATALOGUE(new ProfileLoader("_profiles/DataCatalogue.yaml")),
   PET_STORE(new PetStoreLoader()),
-  FAIR_DATA_HUB(new ProfileLoader("_templates/FAIRDataHub.yaml")),
-  RD3(new ProfileLoader("_templates/RD3.yaml")),
-  JRC_COMMON_DATA_ELEMENTS(new ProfileLoader("_templates/JRC-CDE.yaml")),
-  BEACON_V2(new ProfileLoader("_templates/BeaconV2.yaml")),
+  FAIR_DATA_HUB(new ProfileLoader("_profiles/FAIRDataHub.yaml")),
+  RD3(new ProfileLoader("_profiles/RD3.yaml")),
+  JRC_COMMON_DATA_ELEMENTS(new ProfileLoader("_profiles/JRC-CDE.yaml")),
+  BEACON_V2(new ProfileLoader("_profiles/BeaconV2.yaml")),
   ERN_DASHBOARD(new DashboardLoader()),
   ERN_CRANIO(new ErnCranioLoader()),
   BIOBANK_DIRECTORY(new BiobankDirectoryLoader()),
-  SHARED_STAGING(new ProfileLoader("_templates/SharedStaging.yaml"));
+  SHARED_STAGING(new ProfileLoader("_profiles/SharedStaging.yaml"));
 
   private AbstractDataLoader installer;
 
