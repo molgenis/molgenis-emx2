67029d6e1cebe50a1dc76ffdb0b09098c5adef56
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2024-01-11 08:27:20 +0100

    Better reusable functions for resources and filter

---
+++ backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/profiles/ResourceListing.java
@@ -0,0 +1,106 @@
+package org.molgenis.emx2.datamodels.profiles;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+import org.molgenis.emx2.MolgenisException;
+
+public class ResourceListing {
+
+  /**
+   * List directory contents for a resource folder. Not recursive. Works for regular files and JARs.
+   * Based on original by Greg Briggs, see:
+   * https://www.uofr.net/~greg/java/get-resource-listing.html
+   *
+   * @return Just the name of each member item, not the full paths.
+   */
+  public String[] retrieve(String path) throws URISyntaxException, IOException {
+    URL dirURL = getClass().getResource(path);
+    if (dirURL == null) {
+      throw new MolgenisException(
+          "Import failed: Directory " + path + " doesn't exist in classpath");
+    }
+    if (dirURL.getProtocol().equals("file")) {
+      return new File(dirURL.toURI()).list();
+    }
+    if (dirURL.getProtocol().equals("jar")) {
+      String jarPath =
+          dirURL
+              .getPath()
+              .substring(5, dirURL.getPath().indexOf("!")); // strip out only the JAR file
+      String matchAgainstPath = path;
+      if (matchAgainstPath.startsWith(File.separator)) {
+        matchAgainstPath = path.substring(1);
+      }
+
+      // SonarCloud Recommended Secure Coding Practices preventing Zip Bomb attacks
+      int THRESHOLDENTRIES = 10000;
+      int THRESHOLDSIZE = 1000000000; // 1 GB
+      double THRESHOLDRATIO = 10;
+      int totalSizeArchive = 0;
+      int totalEntryArchive = 0;
+
+      try (JarFile jar = new JarFile(URLDecoder.decode(jarPath, StandardCharsets.UTF_8))) {
+        Enumeration<JarEntry> entries = jar.entries(); // gives ALL entries in jar
+        Set<String> result = new HashSet<>(); // avoid duplicates in case it is a subdirectory
+        while (entries.hasMoreElements()) {
+
+          ZipEntry ze = entries.nextElement();
+          InputStream in = new BufferedInputStream(jar.getInputStream(ze));
+          OutputStream out = new BufferedOutputStream(OutputStream.nullOutputStream());
+
+          totalEntryArchive++;
+
+          int nBytes;
+          byte[] buffer = new byte[2048];
+          int totalSizeEntry = 0;
+
+          while ((nBytes = in.read(buffer)) > 0) { // Compliant
+            out.write(buffer, 0, nBytes);
+            totalSizeEntry += nBytes;
+            totalSizeArchive += nBytes;
+
+            double compressionRatio = totalSizeEntry / (double) ze.getCompressedSize();
+            if (compressionRatio > THRESHOLDRATIO) {
+              // ratio between compressed and uncompressed data is highly suspicious, looks like a
+              // Zip
+              // Bomb Attack
+              break;
+            }
+          }
+
+          if (totalSizeArchive > THRESHOLDSIZE) {
+            // the uncompressed data size is too much for the application resource capacity
+            break;
+          }
+
+          if (totalEntryArchive > THRESHOLDENTRIES) {
+            // too much entries in this archive, can lead to inodes exhaustion of the system
+            break;
+          }
+
+          String name = ze.getName();
+          if (name.startsWith(matchAgainstPath)) { // filter according to the path
+            String entry = name.substring(path.length());
+            int checkSubdir = entry.indexOf("/");
+            if (checkSubdir >= 0) {
+              // if it is a subdirectory, we just return the directory name
+              entry = entry.substring(0, checkSubdir);
+            }
+            result.add(entry);
+          }
+        }
+        return result.toArray(new String[0]);
+      }
+    }
+    throw new UnsupportedOperationException("Cannot list files for URL " + dirURL);
+  }
+}
--- backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/profiles/SchemaFromProfile.java
+++ backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/profiles/SchemaFromProfile.java
@@ -6,13 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.*;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.zip.ZipEntry;
 import org.molgenis.emx2.MolgenisException;
 import org.molgenis.emx2.Row;
 import org.molgenis.emx2.SchemaMetadata;
@@ -22,8 +16,19 @@ import org.molgenis.emx2.io.readers.CsvTableReader;
 
 public class SchemaFromProfile {
 
+  public static final String SHARED_MODELS_DIR =
+      File.separator + "_models" + File.separator + "shared";
+  public static final String SPECIFIC_MODELS_DIR =
+      File.separator + "_models" + File.separator + "specific";
+
   private final Profiles profiles;
 
+  /** Constructor without specific YAML file when you want everything */
+  public SchemaFromProfile() {
+    this.profiles = new Profiles();
+  }
+
+  /** Constructor with a specific profile YAML file */
   public SchemaFromProfile(String yamlFileLocation) {
     if (!yamlFileLocation.endsWith(".yaml")) {
       throw new MolgenisException("Input YAML file name must end in '.yaml'");
@@ -44,25 +49,26 @@ public class SchemaFromProfile {
   }
 
   public SchemaMetadata create() throws MolgenisException {
-    String sharedModelsDir = File.separator + "_models" + File.separator + "shared";
-    String specificModelsDir = File.separator + "_models" + File.separator + "specific";
+    return create(true);
+  }
+
+  public SchemaMetadata create(boolean filterByProfiles) throws MolgenisException {
     List<Row> keepRows = new ArrayList<>();
     try {
-      keepRows.addAll(getProfilesFromAllModels(sharedModelsDir));
-      keepRows.addAll(getProfilesFromAllModels(specificModelsDir));
+      keepRows.addAll(getProfilesFromAllModels(SHARED_MODELS_DIR, filterByProfiles));
+      keepRows.addAll(getProfilesFromAllModels(SPECIFIC_MODELS_DIR, filterByProfiles));
     } catch (Exception e) {
       throw new MolgenisException(e.getMessage());
     }
     return Emx2.fromRowList(keepRows);
   }
 
-  /** From a classpath dir, get all EMX2 model files and slice for profiles */
-  public List<Row> getProfilesFromAllModels(String directory)
+  /** From a classpath dir, get all EMX2 model files and optionally slice for profiles */
+  public List<Row> getProfilesFromAllModels(String directory, boolean filterByProfiles)
       throws URISyntaxException, IOException {
     List<Row> keepRows = new ArrayList<>();
-    String[] modelsList = getResourceListing(directory);
+    String[] modelsList = new ResourceListing().retrieve(directory);
     for (String schemaLoc : modelsList) {
-
       Iterable<Row> rowIterable =
           CsvTableReader.read(
               new InputStreamReader(
@@ -75,7 +81,7 @@ public class SchemaFromProfile {
           throw new MolgenisException("No profiles for " + row);
         }
         for (String profile : profiles) {
-          if (this.profiles.getProfileTagsList().contains(profile)) {
+          if (!filterByProfiles || this.profiles.getProfileTagsList().contains(profile)) {
             keepRows.add(row);
             break;
           }
@@ -83,94 +89,5 @@ public class SchemaFromProfile {
       }
     }
     return keepRows;
-  }
-
-  /**
-   * List directory contents for a resource folder. Not recursive. Works for regular files and JARs.
-   * Based on original by Greg Briggs, see:
-   * https://www.uofr.net/~greg/java/get-resource-listing.html
-   *
-   * @return Just the name of each member item, not the full paths.
-   */
-  String[] getResourceListing(String path) throws URISyntaxException, IOException {
-    URL dirURL = getClass().getResource(path);
-    if (dirURL == null) {
-      throw new MolgenisException(
-          "Import failed: Directory " + path + " doesn't exist in classpath");
-    }
-    if (dirURL.getProtocol().equals("file")) {
-      return new File(dirURL.toURI()).list();
-    }
-    if (dirURL.getProtocol().equals("jar")) {
-      String jarPath =
-          dirURL
-              .getPath()
-              .substring(5, dirURL.getPath().indexOf("!")); // strip out only the JAR file
-      String matchAgainstPath = path;
-      if (matchAgainstPath.startsWith(File.separator)) {
-        matchAgainstPath = path.substring(1);
-      }
-
-      // SonarCloud Recommended Secure Coding Practices preventing Zip Bomb attacks
-      int THRESHOLDENTRIES = 10000;
-      int THRESHOLDSIZE = 1000000000; // 1 GB
-      double THRESHOLDRATIO = 10;
-      int totalSizeArchive = 0;
-      int totalEntryArchive = 0;
-
-      try (JarFile jar = new JarFile(URLDecoder.decode(jarPath, StandardCharsets.UTF_8))) {
-        Enumeration<JarEntry> entries = jar.entries(); // gives ALL entries in jar
-        Set<String> result = new HashSet<>(); // avoid duplicates in case it is a subdirectory
-        while (entries.hasMoreElements()) {
-
-          ZipEntry ze = entries.nextElement();
-          InputStream in = new BufferedInputStream(jar.getInputStream(ze));
-          OutputStream out = new BufferedOutputStream(OutputStream.nullOutputStream());
-
-          totalEntryArchive++;
-
-          int nBytes;
-          byte[] buffer = new byte[2048];
-          int totalSizeEntry = 0;
-
-          while ((nBytes = in.read(buffer)) > 0) { // Compliant
-            out.write(buffer, 0, nBytes);
-            totalSizeEntry += nBytes;
-            totalSizeArchive += nBytes;
-
-            double compressionRatio = totalSizeEntry / (double) ze.getCompressedSize();
-            if (compressionRatio > THRESHOLDRATIO) {
-              // ratio between compressed and uncompressed data is highly suspicious, looks like a
-              // Zip
-              // Bomb Attack
-              break;
-            }
-          }
-
-          if (totalSizeArchive > THRESHOLDSIZE) {
-            // the uncompressed data size is too much for the application resource capacity
-            break;
-          }
-
-          if (totalEntryArchive > THRESHOLDENTRIES) {
-            // too much entries in this archive, can lead to inodes exhaustion of the system
-            break;
-          }
-
-          String name = ze.getName();
-          if (name.startsWith(matchAgainstPath)) { // filter according to the path
-            String entry = name.substring(path.length());
-            int checkSubdir = entry.indexOf("/");
-            if (checkSubdir >= 0) {
-              // if it is a subdirectory, we just return the directory name
-              entry = entry.substring(0, checkSubdir);
-            }
-            result.add(entry);
-          }
-        }
-        return result.toArray(new String[0]);
-      }
-    }
-    throw new UnsupportedOperationException("Cannot list files for URL " + dirURL);
   }
 }
