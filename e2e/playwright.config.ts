385cff3af97212fcabe70d824f73662a79fd9c5d
connoratrug <c.stroomberg@rug.nl>
2024-01-12 16:52:58 +0100

    use baseurl from configwq

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -24,7 +24,13 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: process.env.CI
+      ? (() => {
+          const parts = process.env.CIRCLE_PULL_REQUEST.split("/");
+          const prNumber = parts[parts.length - 1]; // Or parts.pop();
+          return `https://preview-emx2-pr-${prNumber}.dev.molgenis.org`;
+        })()
+      : "http://localhost:8080",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -73,13 +79,7 @@ export default defineConfig({
     ? undefined
     : {
         command: "../gradlew run -p ../",
-        url: process.env.CI
-          ? (() => {
-              const parts = process.env.CIRCLE_PULL_REQUEST.split("/");
-              const prNumber = parts[parts.length - 1]; // Or parts.pop();
-              return `https://preview-emx2-pr-${prNumber}.dev.molgenis.org`;
-            })()
-          : "http://localhost:8080",
+        url: "http://localhost:8080",
         reuseExistingServer: true,
       },
 });
