c447e1da23c20ff67a418d6234ada07697070bd1
mswertz <m.a.swertz@rug.nl>
2024-01-22 09:58:46 +0100

    Revert "feat(graphql): can expand into the fields of subclasses"
    This reverts commit 1b106be9b12d73752a50043a850e639ce150a1b2.

--- backend/molgenis-emx2/src/main/java/org/molgenis/emx2/TableMetadata.java
+++ backend/molgenis-emx2/src/main/java/org/molgenis/emx2/TableMetadata.java
@@ -583,43 +583,4 @@ public class TableMetadata extends HasLabelsDescriptionsAndSettings<TableMetadat
       return null;
     }
   }
-
-  public List<Column> getColumnsIncludingSubclasses() {
-    // get all tables in current schema that inherit this
-    List<Column> result = new ArrayList<>();
-    result.addAll(this.getColumns());
-    result.addAll(getColumnsFromSubclasses());
-    return result;
-  }
-
-  public List<Column> getColumnsIncludingSubclassesExcludingHeadings() {
-    return getColumnsIncludingSubclasses().stream().filter(c -> !c.isHeading()).toList();
-  }
-
-  private List<Column> getColumnsFromSubclasses() {
-    List<Column> result = new ArrayList<>();
-    for (TableMetadata table : getSubclassTables()) {
-      result.addAll(table.getLocalColumns());
-      result.addAll(table.getColumnsFromSubclasses());
-    }
-    return result;
-  }
-
-  public Column getColumnIncludingSubclasses(String columnName) {
-    return getColumnsIncludingSubclasses().stream()
-        .filter(c -> c.getName().equals(columnName))
-        .findFirst()
-        .orElseGet(() -> null);
-  }
-
-  public List<TableMetadata> getSubclassTables() {
-    List<TableMetadata> result = new ArrayList();
-    for (TableMetadata table : getSchema().getTables()) {
-      if (this.getTableName().equals(table.getInheritName())) {
-        result.add(table);
-        result.addAll(table.getSubclassTables());
-      }
-    }
-    return result;
-  }
 }
