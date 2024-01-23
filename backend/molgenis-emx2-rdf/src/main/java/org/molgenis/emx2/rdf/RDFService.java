fb77980c0a23d3faf42524180d71daf8ff560600
mswertz <m.a.swertz@rug.nl>
2024-01-23 12:16:03 +0200

    fix

--- backend/molgenis-emx2-rdf/src/main/java/org/molgenis/emx2/rdf/RDFService.java
+++ backend/molgenis-emx2-rdf/src/main/java/org/molgenis/emx2/rdf/RDFService.java
@@ -284,10 +284,8 @@ public class RDFService {
     }
     builder.add(subject, RDFS.LABEL, table.getName());
 
-    if (table.getMetadata().getDescriptions() != null) {
-      for (final var entry : table.getMetadata().getDescriptions().entrySet()) {
-        builder.add(subject, DCTERMS.DESCRIPTION, Values.literal(entry.getValue(), entry.getKey()));
-      }
+    if (table.getMetadata().getDescription() != null) {
+      builder.add(subject, DCTERMS.DESCRIPTION, table.getMetadata().getDescription());
     }
   }
 
