0f730f2cbe84cbf884436d831985a58d315d2599
connoratrug <c.stroomberg@rug.nl>
2024-02-01 13:08:20 +0100

    fix: ssr-catalogue header data reload
    Move header/menubar data fetch down to page level to allow refresh on spa and html navigation.
    
    - fix ref to Readmore component
    - remove unused demo components
    - add null and error handling to data fetch
    - type header data fetch

--- apps/nuxt3-ssr/app.vue
+++ apps/nuxt3-ssr/app.vue
@@ -1,99 +1,3 @@
-<template>
-  <div
-    class="overflow-x-clip min-h-screen bg-base-gradient relative after:bg-app-wrapper after:w-full after:h-[166px] after:top-0 after:absolute after:opacity-20 after:z-20 xl:after:hidden"
-  >
-    <div
-      class="absolute top-0 left-0 z-10 w-screen h-screen overflow-hidden opacity-background-gradient"
-    >
-      <BackgroundGradient class="z-10" />
-    </div>
-    <div class="z-30 relative min-h-screen flex flex-col">
-      <slot name="header">
-        <HeaderCatalogue v-if="route.params.catalogue" />
-        <HeaderGlobal v-else />
-      </slot>
-      <main class="mb-auto">
-        <BottomModal
-          :show="showCookieWall"
-          :full-screen="false"
-          button-alignment="right"
-        >
-          <section
-            class="bg-white py-9 lg:px-12.5 px-4 text-gray-900 xl:rounded-3px"
-          >
-            <h2 class="mb-5 uppercase text-heading-4xl font-display">
-              Cookies 🍪🍪🍪
-            </h2>
-            <div class="mb-5 prose max-w-none">
-              We use cookies and similar technologies to enhance your browsing
-              experience, analyze website traffic, and personalize content. By
-              clicking "Accept," you consent to the use of cookies.
-            </div>
-            <div class="mb-5 prose max-w-none">
-              We value your privacy and are committed to protecting your
-              personal information. Our use of cookies is solely for improving
-              your experience on our website and ensuring its functionality. We
-              do not sell or share your data with third parties.
-            </div>
-            <div class="flex gap-2">
-              <Button @click="setAnalyticsCookie(true)" type="secondary"
-                >Accept</Button
-              >
-              <Button @click="setAnalyticsCookie(false)" type="tertiary"
-                >Reject</Button
-              >
-            </div>
-          </section>
-        </BottomModal>
-
-        <slot>
-          <NuxtPage />
-        </slot>
-      </main>
-      <slot name="footer">
-        <div class="bg-footer p-6">
-          <div class="mt-5 mb-0 text-center text-title text-body-lg">
-            This database was created using
-            <a
-              class="text-body-base text-footer-link hover:underline"
-              href="http://molgenis.org"
-              >MOLGENIS.org</a>
-            (<a
-              class="text-body-base text-footer-link hover:underline"
-              href="http://github.com/molgenis/molgenis-emx2"
-            >
-              github </a
-            >).
-          </div>
-          <div class="mb-0 text-center lg:pb-5 text-title text-body-lg">
-            Please cite
-            <a
-                class="text-body-base text-footer-link hover:underline"
-                href="https://www.ncbi.nlm.nih.gov/pubmed/36463884"
-            >
-              Swertz & Gini (2022)
-            </a>,
-            <a
-              class="text-body-base text-footer-link hover:underline"
-              href="https://www.ncbi.nlm.nih.gov/pubmed/30165396"
-            >
-              Van der Velde et al (2018)
-            </a>
-            or
-            <a
-              class="text-body-base text-footer-link hover:underline"
-              href="https://www.ncbi.nlm.nih.gov/pubmed/21210979"
-            >
-              Swertz et al (2010)
-            </a>
-            on use.
-          </div>
-        </div>
-      </slot>
-    </div>
-  </div>
-</template>
-
 <script setup lang="ts">
 import BackgroundGradient from "./components/BackgroundGradient.vue";
 import { hash } from "./.fingerprint.js";
@@ -118,9 +22,8 @@ function setAnalyticsCookie(value: boolean) {
 
 let themeFilename = "styles";
 if (route.query.theme) {
-  themeFilename += `.${ route.query.theme }`;
-}
-else if (config.public.emx2Theme) {
+  themeFilename += `.${route.query.theme}`;
+} else if (config.public.emx2Theme) {
   themeFilename += `.${config.public.emx2Theme}`;
 }
 
@@ -160,3 +63,56 @@ useHead({
       : [],
 });
 </script>
+
+<template>
+  <div
+    class="overflow-x-clip min-h-screen bg-base-gradient relative after:bg-app-wrapper after:w-full after:h-[166px] after:top-0 after:absolute after:opacity-20 after:z-20 xl:after:hidden"
+  >
+    <div
+      class="absolute top-0 left-0 z-10 w-screen h-screen overflow-hidden opacity-background-gradient"
+    >
+      <BackgroundGradient class="z-10" />
+    </div>
+    <div class="z-30 relative min-h-screen flex flex-col">
+
+      <main class="mb-auto">
+        <BottomModal
+          :show="showCookieWall"
+          :full-screen="false"
+          button-alignment="right"
+        >
+          <section
+            class="bg-white py-9 lg:px-12.5 px-4 text-gray-900 xl:rounded-3px"
+          >
+            <h2 class="mb-5 uppercase text-heading-4xl font-display">
+              Cookies 🍪🍪🍪
+            </h2>
+            <div class="mb-5 prose max-w-none">
+              We use cookies and similar technologies to enhance your browsing
+              experience, analyze website traffic, and personalize content. By
+              clicking "Accept," you consent to the use of cookies.
+            </div>
+            <div class="mb-5 prose max-w-none">
+              We value your privacy and are committed to protecting your
+              personal information. Our use of cookies is solely for improving
+              your experience on our website and ensuring its functionality. We
+              do not sell or share your data with third parties.
+            </div>
+            <div class="flex gap-2">
+              <Button @click="setAnalyticsCookie(true)" type="secondary"
+                >Accept</Button
+              >
+              <Button @click="setAnalyticsCookie(false)" type="tertiary"
+                >Reject</Button
+              >
+            </div>
+          </section>
+        </BottomModal>
+        <slot>      
+          <NuxtPage />
+        </slot>
+      </main>
+
+    </div>
+  </div>
+</template>
