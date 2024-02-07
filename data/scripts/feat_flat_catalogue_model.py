d01585a765e3332e1c82ec01f09cb12c1db5cc45
ypezijlstra <info@ypezijlstra.frl>
2024-02-07 14:37:06 +0100

    Modified Organisations, Models, etc.

--- data/scripts/feat_flat_catalogue_model.py
+++ data/scripts/feat_flat_catalogue_model.py
@@ -10,6 +10,54 @@ import pandas as pd
 
 MODELS_DIR = '../_models/shared'
 
+inherit_tables = {'Organisations': 'Resources', 'Data resources': 'Extended resources', 'Models': 'Extended resources',
+                  'Networks': 'Extended resources', 'Studies': 'Extended resources', 'Extended resources': 'Resources'
+                  }
+
+class Transformer:
+    """
+    Class to transform the DataCatalogue model to a flat, i.e. without inheritance, model.
+    """
+    df: pd.DataFrame
+
+    def __init__(self):
+        """Initializes the Transformer object by loading the original dataset."""
+        self.df = self._load_data()
+
+    def transform(self) -> pd.DataFrame:
+        """Performs the transformation.
+        Returns the updated model.
+        """
+        self.df = self._load_data()
+
+        for tables in inherit_tables.items():
+            self._duplicate_columns(*tables)
+
+
+        # Save result to file
+        self._save_df()
+
+        return self.df
+
+    def _duplicate_columns(self, new_tab: str, old_tab: str):
+        """Duplicates columns of an inherited table for the inheriting table."""
+        inheriting_tables = self.df.loc[self.df['tableName'] == old_tab]
+
+        for idx in reversed(inheriting_tables.index):
+            self.df.index = [*self.df.index[:idx+1], *list(self.df.index[idx+1:] + 1)]
+            self.df.loc[idx+1] = inheriting_tables.loc[idx]
+            self.df.loc[idx+1, 'tableName'] = new_tab
+            self.df.sort_index(inplace=True)
+        self.df = self.df.loc[(self.df['tableName'] != old_tab) | (self.df['tableExtends'] != new_tab)]
+
+    @staticmethod
+    def _load_data() -> pd.DataFrame:
+        return pd.read_csv(f"{MODELS_DIR}/DataCatalogue-TODO.csv")
+
+    def _save_df(self):
+        """Saves the pandas DataFrame of the model to disk."""
+        self.df.to_csv(f"{MODELS_DIR}/DataCatalogue-FLAT.csv", index=False)
+
 
 def transform_data_model():
 
@@ -21,12 +69,26 @@ def transform_data_model():
     df = data_catalogue.copy()
 
     # Transform Organisations, Models, Networks and Studies into standalone tables that do not inherit from *Resources
+    # Copy Organisations columns from Resources columns
+    resources = df.loc[df['tableName'] == 'Resources']
+    for idx in reversed(resources.index):
+        df.index = [*df.index[:idx+1], *list(df.index[idx+1:] + 1)]
+        df.loc[idx+1] = resources.loc[idx]
+        df.loc[idx+1, 'tableName'] = 'Organisations'
+        df.sort_index(inplace=True)
 
     # Change tableName from 'Cohorts' to 'Resources' and add 'Cohorts' as profile
 
-    # Save result to file
-    df.to_csv(f"{MODELS_DIR}/DataCatalogue-FLAT.csv")
+    df.to_csv(f"{MODELS_DIR}/DataCatalogue-FLAT.csv", index=False)
+
+
+def replace_table_name(old_tab: str, new_tab: str):
+    """
+    Duplicates rows of a table that are inherited by another table.
+    """
 
 
 if __name__ == '__main__':
-    transform_data_model()
+    # transform_data_model()
+    transformer = Transformer()
+    transformer.transform()
