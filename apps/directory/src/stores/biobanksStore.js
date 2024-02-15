a8d2ff76b3ccfc855ad6011864c0958aa745d26f
konstantina-gkp <k.gkikopouli@umcg.nl>
2024-02-15 11:54:59 +0100

    fixed searching filter on directory app fixed the total count of organizations

--- apps/directory/src/stores/biobanksStore.js
+++ apps/directory/src/stores/biobanksStore.js
@@ -129,8 +129,12 @@ export const useBiobanksStore = defineStore("biobanksStore", () => {
       biobankCards.value = [];
       const biobankResult = await baseQuery.execute();
 
-      /** also show biobanks that don't have collections */
-      const foundBiobanks = biobankResult.Biobanks || [];
+      /** depending on whether filters have been selected display the correct count of biobanks */
+      let foundBiobanks = biobankResult.Biobanks;
+      if (filtersStore.hasActiveFilters) {
+        foundBiobanks = foundBiobanks.filter(biobank => biobank.collections);
+      }
+
       biobankCards.value = filterWithdrawn(foundBiobanks);
       waitingForResponse.value = false;
 
