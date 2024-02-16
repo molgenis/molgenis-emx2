eb3c8c2fc4c0e1f1ad6ed14d78ead149ea7c65d0
connoratrug <c.stroomberg@rug.nl>
2024-02-16 10:30:53 +0100

    setup build to show preview app

--- apps/nuxt-components/histoire.config.ts
+++ apps/nuxt-components/histoire.config.ts
@@ -1,9 +1,23 @@
-import { defineConfig } from 'histoire'
-import { HstVue } from '@histoire/plugin-vue'
-import { HstNuxt } from '@histoire/plugin-nuxt'
+import { defineConfig } from "histoire"
+import { HstVue } from "@histoire/plugin-vue"
+import { HstNuxt } from "@histoire/plugin-nuxt"
 
 export default defineConfig({
-  setupFile: './histoire-setup.ts',
+  setupFile: "./histoire-setup.ts",
+  outDir: "showCase",
+  routerMode: "hash",
+  vite: {
+    base: "",
+    server: {
+      fs: {
+        allow: [
+          "../node_modules/histoire/dist/node/builtin-plugins/vanilla-support",
+          "../node_modules/@histoire/plugin-vue/dist/bundled/client/app",
+          "../node_modules/@histoire/controls/dist",
+        ],
+      },
+    },
+  },
   plugins: [
     HstVue(),
     HstNuxt(),
