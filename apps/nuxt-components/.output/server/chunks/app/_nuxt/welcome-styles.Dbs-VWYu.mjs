9019e0f019398cab6cdb541f566b04b138d5b13c
mswertz <m.a.swertz@rug.nl>
2024-02-24 17:29:11 +0100

    remove files that shouldn't have committed

--- apps/nuxt-components/.output/server/chunks/app/_nuxt/welcome-styles.Dbs-VWYu.mjs
+++
@@ -1,6 +0,0 @@
-import { w as welcome_vue_vue_type_style_index_0_scoped_46326706_lang } from './entry-styles-2.mjs--D68m_TR.mjs';
-
-const welcomeStyles_DbsVWYu = [welcome_vue_vue_type_style_index_0_scoped_46326706_lang];
-
-export { welcomeStyles_DbsVWYu as default };
-//# sourceMappingURL=welcome-styles.Dbs-VWYu.mjs.map
