f2c7c15dd008b2059b0b523205988540b2d91fa7
mswertz <m.a.swertz@rug.nl>
2024-02-24 17:26:53 +0100

    undo changes

---
+++ apps/nuxt-components/.output/server/chunks/app/styles.mjs
@@ -0,0 +1,13 @@
+const interopDefault = r => r.default || r || [];
+const styles = {
+  "../node_modules/nuxt/dist/app/entry.js": () => import('./_nuxt/entry-styles.Db_igHJB.mjs').then(interopDefault),
+  "../node_modules/@nuxt/ui-templates/dist/templates/welcome.vue": () => import('./_nuxt/welcome-styles.Dbs-VWYu.mjs').then(interopDefault),
+  "../node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./_nuxt/error-404-styles.mRgERIxh.mjs').then(interopDefault),
+  "../node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./_nuxt/error-500-styles.B65fSdPH.mjs').then(interopDefault),
+  "../node_modules/@nuxt/ui-templates/dist/templates/error-404.vue?vue&type=style&index=0&scoped=8e38b8ef&lang.css": () => import('./_nuxt/error-404-styles.mRgERIxh.mjs').then(interopDefault),
+  "../node_modules/@nuxt/ui-templates/dist/templates/error-500.vue?vue&type=style&index=0&scoped=5741bfc2&lang.css": () => import('./_nuxt/error-500-styles.B65fSdPH.mjs').then(interopDefault),
+  "../node_modules/@nuxt/ui-templates/dist/templates/welcome.vue?vue&type=style&index=0&scoped=46326706&lang.css": () => import('./_nuxt/welcome-styles.Dbs-VWYu.mjs').then(interopDefault)
+};
+
+export { styles as default };
+//# sourceMappingURL=styles.mjs.map
