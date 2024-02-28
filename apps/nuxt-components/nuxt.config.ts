129175e1f7997f8edeea1856e06c6e834b308299
connoratrug <c.stroomberg@rug.nl>
2024-02-28 14:27:50 +0100

    chore: stripout storybook

--- apps/nuxt-components/nuxt.config.ts
+++ apps/nuxt-components/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/storybook', '@nuxtjs/tailwindcss', 'floating-vue/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', 'floating-vue/nuxt'],
   components: [{ path: "./components", prefix: "UI" }],
+  tailwindcss: {
+    cssPath: '~/assets/css/main.css',
+  }
 })
