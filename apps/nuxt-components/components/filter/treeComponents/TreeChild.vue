09600e0b67789f2f6f9ff7941298305351beb7c3
connoratrug <c.stroomberg@rug.nl>
2024-02-28 08:50:44 +0100

    chore: cleanup code, remove unused constructs

--- apps/nuxt-components/components/filter/treeComponents/TreeChild.vue
+++ apps/nuxt-components/components/filter/treeComponents/TreeChild.vue
@@ -13,50 +13,37 @@ const props = withDefaults(
 
 const emit = defineEmits(["select", "deselect"]);
 
-var itemMap = props.nodes.reduce(function (
-  map: Record<string, ITreeNode>,
-  node
-) {
-  map[node.name] = node;
-  return map;
-},
-{});
-
-let terms = reactive(itemMap);
-
-let key = ref(1);
-function toggleExpand(term: ITreeNode) {
-  terms[term.name].expanded = !terms[term.name].expanded;
-  key.value++;
+function toggleExpand(node: ITreeNode) {
+  node.expanded = !node.expanded;
 }
 
-function toggleSelect(term: ITreeNode) {
+function toggleSelect(node: ITreeNode) {
   //if selecting then also expand
   //if deselection we keep it open
-  if (term.selected == "complete") {
-    emit("deselect", term.name);
+  if (node.selected == "complete") {
+    emit("deselect", node.name);
   } else {
-    emit("select", term.name);
+    emit("select", node.name);
   }
 }
 </script>
 
 <template>
   <div>
-    <li v-for="child in nodes" :key="child.name" class="mt-2.5 relative">
+    <li v-for="node in nodes" class="mt-2.5 relative">
       <span class="flex items-center">
         <span
-          v-if="child.children?.length"
-          @click="toggleExpand(child)"
+          v-if="node.children?.length"
+          @click="toggleExpand(node)"
           class="-left-[11px] top-0 text-search-filter-group-toggle rounded-full hover:bg-search-filter-group-toggle hover:cursor-pointer h-6 w-6 flex items-center justify-center absolute z-20"
         >
           <BaseIcon
-            :name="child.expanded ? 'caret-down' : 'caret-up'"
+            :name="node.expanded ? 'caret-down' : 'caret-up'"
             :width="20"
           />
         </span>
         <BaseIcon
-          v-if="child.children?.length"
+          v-if="node.children?.length"
           name="collapsible-list-item-sub"
           :width="20"
           class="text-blue-200 absolute -top-[9px]"
@@ -72,46 +59,36 @@ function toggleSelect(term: ITreeNode) {
         <div class="flex items-center">
           <input
             type="checkbox"
-            :id="child.name"
-            :name="child.name"
-            @click.stop="toggleSelect(child)"
+            :id="node.name"
+            :name="node.name"
+            @click.stop="toggleSelect(node)"
             :checked="
-              child.selected === 'complete' || child.selected === 'partial'
+              node.selected === 'complete' || node.selected === 'partial'
             "
             class="w-5 h-5 rounded-3px ml-2.5 mr-2.5 mt-0.5 text-search-filter-group-checkbox border border-checkbox"
           />
         </div>
-        <label
-          :for="child.name"
-          class="hover:cursor-pointer text-body-sm group"
-        >
-          <span class="group-hover:underline">{{ child.name }}</span>
+        <label :for="node.name" class="hover:cursor-pointer text-body-sm group">
+          <span class="group-hover:underline">{{ node.name }}</span>
           <div class="inline-flex items-center whitespace-nowrap">
-            <!-- <span
-              v-if="child?.children?.length"
-              class="inline-block mr-2 text-blue-200 group-hover:underline decoration-blue-200"
-            >
-              &nbsp;- {{ child.children.length }}
-            </span> -->
             <div class="inline-block">
               <CustomTooltip
-                v-if="child.description"
+                v-if="node.description"
                 label="Description"
                 hoverColor="white"
-                :content="child.description"
+                :content="node.description"
               />
             </div>
           </div>
         </label>
       </div>
       <ul
-        v-if="child.children"
-        :class="{ hidden: !terms[child.name].expanded }"
+        v-if="node.children"
+        :class="{ hidden: !node.expanded }"
         class="ml-[31px]"
       >
         <TreeChild
-          :key="key"
-          :nodes="child.children"
+          :nodes="node.children"
           @select="$emit('select', $event)"
           @deselect="$emit('deselect', $event)"
         />
