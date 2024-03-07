faab04b0ae86d5f788d304a7212f58db2c09be4f
dcruvolo <davidruvolo51@gmail.com>
2024-03-07 11:44:20 +0100

    feat: added component for search result counts

---
+++ apps/nuxt3-ssr/components/SearchResultsCount.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="mt-1 mb-0 lg:mb-3 text-body-lg flex flex-col text-title">
+    <p>{{ value }} {{ pluralizedLabel }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  value: Number;
+  label?: String;
+  suffix?: String;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  value: 0,
+  label: "result",
+  suffix: "s",
+});
+
+let pluralizedLabel = computed(() => {
+  return props.value > 1 || props.value === 0
+    ? `${props.label}${props.suffix}`
+    : label;
+});
+</script>
