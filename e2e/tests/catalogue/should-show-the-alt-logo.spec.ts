ababecc4ea2f63150bbe0596039e0f8cd818adab
connoratrug <c.stroomberg@rug.nl>
2024-03-07 17:10:58 +0100

    test for log region only

--- e2e/tests/catalogue/should-show-the-alt-logo.spec.ts
+++ e2e/tests/catalogue/should-show-the-alt-logo.spec.ts
@@ -2,5 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('should show the alt logo', async ({ page }) => {
   await page.goto('/catalogue-demo/ssr-catalogue/?logo=UMCGkort.woordbeeld');
-  await expect(page).toHaveScreenshot();
+  await page.getByRole('button', { name: 'Accept' }).click();
+  await expect(page).toHaveScreenshot({clip: {x: 0, y: 0, width: 200, height: 100}, threshold: 0.4});
 });
