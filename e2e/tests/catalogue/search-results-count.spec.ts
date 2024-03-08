0a2c92fd0f4b83ad3f3d31e403ac161b4b42e1fb
dcruvolo <davidruvolo51@gmail.com>
2024-03-08 12:41:56 +0100

    feat: improved count validation; added new tests

--- e2e/tests/catalogue/search-results-count.spec.ts
+++ e2e/tests/catalogue/search-results-count.spec.ts
@@ -1,7 +1,31 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('/catalogue-demo/ssr-catalogue/all/cohorts');
-  await page.getByRole('button', { name: 'Reject' }).click();
-  await expect(page.getByRole('main')).toContainText('53 cohorts');
-});
+const pattern = new RegExp(/^(([a-zA-Z]{1,})?(\s)?(([0-9]{1,})\s(cohort([s])?|variable([s])?|data\ssource([s])?|result([s])?)))$/);
+
+test('validate cohort search result counts @cohort-view @search-result-counts',
+  async ({ page }) => {
+    await page.goto('/catalogue-demo/ssr-catalogue/all/cohorts');
+    await page.getByRole('button', { name: 'Reject' }).click(); 
+    const text = await page.locator(".search-results-count").textContent();
+    await expect(text).toMatch(pattern);
+  }
+);
+
+test('validate data sources search result counts @data-sources-view @search-result-counts',
+  async ({ page }) => {
+    await page.goto('/catalogue-demo/ssr-catalogue/all/datasources');
+    await page.getByRole('button', { name: 'Reject' }).click();
+    const text = await page.locator(".search-results-count").textContent();
+    await expect(text).toMatch(pattern);
+  }
+);
+
+test('validate variables sources search result counts @variables-view @search-result-counts',
+  async ({ page }) => {
+    await page.goto('/catalogue-demo/ssr-catalogue/all/variables');
+    await page.getByRole('button', { name: 'Reject' }).click();
+    const text = await page.locator(".search-results-count").textContent();
+    await expect(text).toMatch(pattern);
+  }
+);
+
