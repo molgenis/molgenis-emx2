04719f22e4bc676da9be4458f8d8527d363641e0
connoratrug <47183404+connoratrug@users.noreply.github.com>
2024-03-08 09:46:35 +0100

    fix: load logo (#3471)
    fix: load logo
    
    - update usage of vite load response
    
    Closes #3470

--- e2e/tests/catalogue/show-landing-page.spec.ts
+++ e2e/tests/catalogue/show-landing-page.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('catalogue-demo/ssr-catalogue/');
+  await page.goto('/catalogue-demo/ssr-catalogue/');
   await expect(page.getByRole('heading', { name: 'European Health Research Data' })).toBeVisible();
 });
