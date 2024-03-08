76836ad52aeebd85bad3e1092f792074c3c69770
dcruvolo <davidruvolo51@gmail.com>
2024-03-08 15:51:32 +0100

    fix: added option to skip button click; removed extra space in reg expr

--- e2e/tests/catalogue/search-results-count.spec.ts
+++ e2e/tests/catalogue/search-results-count.spec.ts
@@ -1,11 +1,25 @@
+/**
+ * Test all instances where the component <SearchResultsCounts> is used.
+ * All values must follow this format: <value-prefix> <value> <label>. 
+ * Value prefixes are optional and used if you would like to add a label
+ * before the value (e.g., `Found 12 results`). If testing locally, disable
+ * the `enableRejectCookiesClick` as this does not exist when running the
+ * preview locally and the action will timeout.
+ */
+
 import { test, expect } from '@playwright/test';
 
-const numberOfResultsPattern = new RegExp(/^(([a-zA-Z]{1,})?(\s)?(([0-9]{1,})\s(cohort([s])?|variable([s])?|data\ssource([s])?|result([s])?|networks([s])? )))$/);
+const enableRejectCookiesClick = true;
+const numberOfResultsPattern = new RegExp(/^(([a-zA-Z]{1,})?(\s)?(([0-9]{1,})\s(cohort([s])?|variable([s])?|data\ssource([s])?|result([s])?|networks([s])?)))$/);
 
 test('validate cohort search result counts @cohort-view @search-result-counts',
   async ({ page }) => {
     await page.goto('/catalogue-demo/ssr-catalogue/all/cohorts');
-    await page.getByRole('button', { name: 'Reject' }).click(); 
+    
+    if (enableRejectCookiesClick) {
+      await page.getByRole('button', { name: 'Reject' }).click();
+    }
+    
     const text = await page.locator(".search-results-count").textContent();
     await expect(text).toMatch(numberOfResultsPattern);
   }
@@ -14,7 +28,11 @@ test('validate cohort search result counts @cohort-view @search-result-counts',
 test('validate data sources search result counts @data-sources-view @search-result-counts',
   async ({ page }) => {
     await page.goto('/catalogue-demo/ssr-catalogue/all/datasources');
-    await page.getByRole('button', { name: 'Reject' }).click();
+    
+    if (enableRejectCookiesClick) {
+      await page.getByRole('button', { name: 'Reject' }).click();
+    }
+    
     const text = await page.locator(".search-results-count").textContent();
     await expect(text).toMatch(numberOfResultsPattern);
   }
@@ -23,7 +41,11 @@ test('validate data sources search result counts @data-sources-view @search-resu
 test('validate variables sources search result counts @variables-view @search-result-counts',
   async ({ page }) => {
     await page.goto('/catalogue-demo/ssr-catalogue/all/variables');
-    await page.getByRole('button', { name: 'Reject' }).click();
+    
+    if (enableRejectCookiesClick) {
+      await page.getByRole('button', { name: 'Reject' }).click();
+    }
+    
     const text = await page.locator(".search-results-count").textContent();
     await expect(text).toMatch(numberOfResultsPattern);
   }
@@ -32,7 +54,11 @@ test('validate variables sources search result counts @variables-view @search-re
 test('validate networks sources search result counts @networks-view @search-result-counts',
   async ({ page }) => {
     await page.goto('/catalogue-demo/ssr-catalogue/all/networks');
-    await page.getByRole('button', { name: 'Reject' }).click();
+    
+    if (enableRejectCookiesClick) {
+      await page.getByRole('button', { name: 'Reject' }).click();
+    }
+    
     const text = await page.locator(".search-results-count").textContent();
     await expect(text).toMatch(numberOfResultsPattern);
   }
