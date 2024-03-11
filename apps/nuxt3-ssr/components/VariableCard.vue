b2c4b3c2b7f813aaf5ec6d88dc3832a479d736e9
dcruvolo <davidruvolo51@gmail.com>
2024-03-11 13:28:33 +0100

    fix: adjusted arrangement of text elements

--- apps/nuxt3-ssr/components/VariableCard.vue
+++ apps/nuxt3-ssr/components/VariableCard.vue
@@ -18,35 +18,29 @@ const resourcePathId = resourceIdPath(variableKey.value);
 
 <template>
   <article class="py-5 lg:px-12.5 p-5">
-    <header class="flex">
-      <div class="grow flex items-center">
-        <h2 class="min-w-[160px] mr-4 md:inline-block block">
+    <header class="flex md:flex-row gap-3 items-start md:items-center">
+      <div class="md:basis-1/5 p-2">
+        <h2 class="break-all">
           <NuxtLink
             :to="`/${schema}/ssr-catalogue/${catalogue}/variables/${resourcePathId}`"
-            class="text-body-base font-extrabold text-blue-500 hover:underline hover:bg-blue-50 break-words"
+            class="text-body-base font-extrabold text-blue-500 hover:underline hover:bg-blue-50"
           >
             {{ variable?.name }}
           </NuxtLink>
         </h2>
-
-        <span class="mr-4 text-body-base hidden md:block">
-          {{ variable?.label }}
-        </span>
       </div>
-      <div class="flex">
-        <!--
-        <IconButton
-          icon="star"
-          :class="iconStarClasses"
-          class="text-blue-500 xl:justify-end"
-        />
-        -->
+      <div class="hidden md:flex md:basis-3/5">
+        <p class="text-body-base">
+          {{ variable?.label }}
+        </p>
+      </div>
+      <div class="hidden basis-1/5 xl:flex xl:justify-end">
         <NuxtLink
           :to="`/${schema}/ssr-catalogue/${catalogue}/variables/${resourcePathId}`"
         >
-          <IconButton
+          <BaseIcon
             icon="arrow-right"
-            class="text-blue-500 hidden xl:flex xl:justify-end"
+            class="text-blue-500"
           />
         </NuxtLink>
       </div>
