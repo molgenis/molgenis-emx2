6fde55f8eb163a815730860b066b16c3a486823c
Harm Brugge <harmbrugge@gmail.com>
2024-03-25 08:50:11 +0100

    Added permitted values to filters

--- backend/molgenis-emx2-beacon-v2/src/main/java/org/molgenis/emx2/beaconv2/Concept.java
+++ backend/molgenis-emx2-beacon-v2/src/main/java/org/molgenis/emx2/beaconv2/Concept.java
@@ -1,24 +1,32 @@
 package org.molgenis.emx2.beaconv2;
 
+import java.util.List;
 import org.molgenis.emx2.MolgenisException;
 
 public enum Concept {
-  DISEASE("NCIT_C2991", "{ diseases: { diseaseCode: { ontologyTermURI: { like: \"%s\" } } } }"),
+  DISEASE(
+      "NCIT_C2991", "{ diseases: { diseaseCode: { ontologyTermURI: { like: \"%s\" } } } }", null),
   PHENOTYPE(
       "SIO_010056",
-      "{ phenotypicFeatures: { featureType: { ontologyTermURI: { like: \"%s\" } } } }"),
-  SEX("NCIT_C28421", "{ sex: { ontologyTermURI: { like: \"%s\" } } }"),
-  CAUSAL_GENE("data_2295", "{ diseaseCausalGenes: { name: { equals: \"%s\" } } }"),
-  AGE_THIS_YEAR("NCIT_C83164", null),
-  AGE_OF_ONSET("NCIT_C124353", null),
-  AGE_AT_DIAG("NCIT_C156420", null);
+      "{ phenotypicFeatures: { featureType: { ontologyTermURI: { like: \"%s\" } } } }",
+      null),
+  SEX(
+      "NCIT_C28421",
+      "{ sex: { ontologyTermURI: { like: \"%s\" } } }",
+      List.of("NCIT_C16576", "NCIT_C20197", "NCIT_C124294", "NCIT_C17998")),
+  CAUSAL_GENE("data_2295", "{ diseaseCausalGenes: { name: { equals: \"%s\" } } }", null),
+  AGE_THIS_YEAR("NCIT_C83164", null, null),
+  AGE_OF_ONSET("NCIT_C124353", null, null),
+  AGE_AT_DIAG("NCIT_C156420", null, null);
 
   private final String id;
   private final String graphQlQuery;
+  private final List<String> permittedValues;
 
-  Concept(String id, String graphQlQuery) {
+  Concept(String id, String graphQlQuery, List<String> permittedValues) {
     this.id = id;
     this.graphQlQuery = graphQlQuery;
+    this.permittedValues = permittedValues;
   }
 
   public static Concept findById(String id) {
@@ -40,5 +48,9 @@ public enum Concept {
 
   public String getGraphQlQuery() {
     return graphQlQuery;
+  }
+
+  public List<String> getPermittedValues() {
+    return permittedValues;
   }
 }
