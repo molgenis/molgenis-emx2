7ef6c919b2b9b9fff4ae2765fbb8d168df553e5d
dcruvolo <davidruvolo51@gmail.com>
2024-03-29 16:27:50 +0100

    added demo

--- apps/molgenis-viz/src/views/charts-home.vue
+++ apps/molgenis-viz/src/views/charts-home.vue
@@ -1,20 +1,62 @@
 <template>
   <Page>
     <PageSection :verticalPadding="2">
-      <BarChartEmx
+      <!-- <GeoMercatorEmx
+        chartId="patients-by-recruitment-center"
+        table="Organisations"
+        rowId="code"
+        latitude="latitude"
+        longitude="longitude"
+        group="organisationType"
+        :tooltipVariables="['name', 'city', 'country']"
+      /> -->
+      <!-- <BarChartEmx
         chartId="patients-by-recruitment-center"
         title="Number of patients submitted by research center"
         table="ClinicalData_groupBy"
         xvar="_sum { n }"
-        yvar="sampleType { name }"
-        y-axis-line-breaker=" "
+        yvar="researchCenter { name }"
+      />
+       -->
+      <!-- <ColumnChartEmx
+        chartId="sample-types-chart"
+        title="Overview of submitted samples"
+        table="ClinicalData_groupBy"
+        xvar="sampleType { name }"
+        yvar="_sum { n }"
+      /> -->
+      <!-- <GroupedColumnChartEmx
+        chartId="patients-by-recruitment-center-grouped"
+        title="Number of patients submitted by research center and sex"
+        table="ClinicalData_groupBy"
+        xvar="sex { name }"
+        group="researchCenter { name }"
+        yvar="_sum { n }"
+        xAxisLabel="Patients submitted"
+        :yTickValues="[0,500,1000,1500,2000,2500,3000,3500,4000]"
+        xAxisLineBreaker=" "        
         :chartMargins="{
-          top: 10,
-          right: 20,
-          bottom: 60,
+          top: 20,
+          right: 60,
+          bottom: 80,
           left: 90
         }"
-      />
+      /> -->
+      <!-- <ScatterPlotEmx
+        chartId="scatter-plot-demo"
+        table="Organisations" 
+        xvar="longitude"
+        yvar="latitude"
+      /> -->
+      <!-- <PieChart2Emx
+        chartId="number-of-patients-by-sex"
+        title="Number of Submitted Patients by Sex"
+        description="To date, all centers have submitted data."
+        table="ClinicalData_groupBy"
+        categories="sex { name }"
+        values="_sum { n }"
+        legendPosition="bottom"
+      /> -->
     </PageSection>
   </Page>
 </template>
@@ -28,8 +70,10 @@ import DashboardRow from "../components/layouts/DashboardRow.vue";
 import DashboardChart from "../components/layouts/DashboardChart.vue";
 import MessageBox from "../components/display/MessageBox.vue";
 
-import BarChartEmx from "../components/viz/BarChartEmx.vue";
-import PieChart2Emx from "../components/viz/PieChart2Emx.vue";
-import ColumnChartEmx from "../components/viz/ColumnChartEmx.vue";
-import GroupedColumnChartEmx from "../components/viz/GroupedColumnChartEmx.vue";
+// import BarChartEmx from "../components/viz/BarChartEmx.vue";
+// import PieChart2Emx from "../components/viz/PieChart2Emx.vue";
+// import ColumnChartEmx from "../components/viz/ColumnChartEmx.vue";
+// import GroupedColumnChartEmx from "../components/viz/GroupedColumnChartEmx.vue";
+// import ScatterPlotEmx from "../components/viz/ScatterPlotEmx.vue";
+// import GeoMercatorEmx from "../components/viz/GeoMercatorEmx.vue";
 </script>
