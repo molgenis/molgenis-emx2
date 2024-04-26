8173a3bc714128ea57217eebe70af6bf240d8ff9
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2024-04-26 09:59:19 +0200

    Clean up MIME types

--- backend/molgenis-emx2-fairdatapoint/src/main/java/org/molgenis/emx2/fairdatapoint/FormatMimeTypes.java
+++ backend/molgenis-emx2-fairdatapoint/src/main/java/org/molgenis/emx2/fairdatapoint/FormatMimeTypes.java
@@ -7,21 +7,7 @@ public class FormatMimeTypes {
 
   /** Possible download formats for EMX2 tables */
   public static final Set<String> FORMATS =
-      new TreeSet<>(
-          Set.of(
-              "csv",
-              "jsonld",
-              "ttl",
-              "excel",
-              "zip",
-              "rdf-ttl",
-              "rdf-n3",
-              "rdf-ntriples",
-              "rdf-nquads",
-              "rdf-xml",
-              "rdf-trig",
-              "rdf-jsonld",
-              "graphql"));
+      new TreeSet<>(Set.of("csv", "ttl", "excel", "zip", "graphql"));
 
   /**
    * Convert a format into its corresponding MIME type
@@ -36,13 +22,10 @@ public class FormatMimeTypes {
       case "csv":
         mediaType = "https://www.iana.org/assignments/media-types/text/csv";
         break;
-      case "jsonld":
-      case "rdf-jsonld":
       case "graphql":
         mediaType = "https://www.iana.org/assignments/media-types/application/ld+json";
         break;
       case "ttl":
-      case "rdf-ttl":
         mediaType = "https://www.iana.org/assignments/media-types/text/turtle";
         break;
       case "excel":
@@ -50,21 +33,6 @@ public class FormatMimeTypes {
         break;
       case "zip":
         mediaType = "https://www.iana.org/assignments/media-types/application/zip";
-        break;
-      case "rdf-n3":
-        mediaType = "https://www.iana.org/assignments/media-types/text/n3";
-        break;
-      case "rdf-ntriples":
-        mediaType = "https://www.iana.org/assignments/media-types/application/n-triples";
-        break;
-      case "rdf-nquads":
-        mediaType = "https://www.iana.org/assignments/media-types/application/n-quads";
-        break;
-      case "rdf-xml":
-        mediaType = "https://www.iana.org/assignments/media-types/application/rdf+xml";
-        break;
-      case "rdf-trig":
-        mediaType = "https://www.iana.org/assignments/media-types/application/trig";
         break;
       default:
         throw new Exception("MIME Type could not be assigned");
