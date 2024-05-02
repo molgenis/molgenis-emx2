06ff0e50986a607b815a968369d9ec9862119225
connoratrug <c.stroomberg@rug.nl>
2024-05-02 10:41:22 +0200

    update e2e test

--- e2e/tests/catalogue/variable-cohort-filter.spec.ts
+++ e2e/tests/catalogue/variable-cohort-filter.spec.ts
@@ -6,10 +6,16 @@ test.beforeEach(async ({ context, baseURL }) => {
 
 test('filter varaibles by cohort', async ({ page }) => {
   await page.goto('/catalogue-demo/ssr-catalogue/testNetwork1/variables');
+  await expect(page.getByRole('list')).toContainText('testVarCategorical_');
+  await expect(page.getByRole('list')).toContainText('testVarNoRepeats');
+  await expect(page.getByRole('list')).toContainText('testVarRepeats_');
   await page.getByRole('complementary').getByRole('img').nth(2).click();
-  await page.getByLabel('testCohort1').check();
-  await expect(page.getByRole('main')).toContainText('1 variablein 1 cohort');
+  await page.getByText('testCohort1').click();
+  await expect(page.getByRole('main')).toContainText('testVarRepeats_');
   await page.getByRole('button', { name: 'Harmonizations' }).click();
-  await expect(page.getByRole('cell', { name: 'testCohort1' }).locator('span')).toBeVisible();
+  await expect(page.locator('tbody')).toContainText('testVarNoRepeats');
+  await expect(page.locator('tbody')).toContainText('testVarRepeats_');
+  await page.getByText('Remove 1 selected').click();
+  await expect(page.locator('thead')).toContainText('testCohort4');
 });
 
