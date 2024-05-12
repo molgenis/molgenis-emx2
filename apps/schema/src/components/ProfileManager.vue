97fbbd9509e81cbd82e7105c84bd356283bbae41
mswertz <m.a.swertz@rug.nl>
2024-05-12 17:56:44 +0200

    draft2

--- apps/schema/src/components/ProfileManager.vue
+++ apps/schema/src/components/ProfileManager.vue
@@ -1,117 +1,134 @@
 <template>
-  <p>error: {{ error }} {{ search }}</p>
-  <p>
-    available profiles:
-    <span
-      class="badge mr-1"
-      :style="{ 'background-color': color(profile) }"
-      v-for="profile in profiles.filter(
-        (profile) => !selectedProfiles.includes(profile)
-      )"
-      @click="selectedProfiles.push(profile)"
-      >{{ profile }}</span
-    >
-  </p>
-  <p>
-    selected profiles:
-    <span
-      class="badge mr-1"
-      :style="{ 'background-color': color(profile) }"
-      v-for="profile in selectedProfiles"
-      @click="selectedProfiles.splice(selectedProfiles.indexOf(profile), 1)"
-      >{{ profile }}</span
-    >
-  </p>
-  <InputSearch v-model="search" />
-  <div class="row">
-    <div class="col-3">
-      <h2>Available tables</h2>
-    </div>
-    <div class="col-9" v-if="selectedTable">
-      <h2>Selected table: {{ selectedTable.name }}</h2>
-      <div>{{ selectedTable.description }}</div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-3">
-      <ul class="list-group">
-        <li
-          v-for="table in tables"
-          class="list-group-item list-group-item-action"
-          :class="{ active: table === selectedTable }"
-          @click="selectedTable = table"
-        >
-          <h5>
-            {{ table.name }}
-            <sup v-if="table.profiles"
-              ><span
-                class="badge mr-1"
-                :style="{ 'background-color': color(profile) }"
-                v-for="profile in table.profiles"
-                >{{ profile }}</span
-              ></sup
-            >
-          </h5>
-        </li>
-      </ul>
-    </div>
-    <div class="col-9" v-if="selectedTable">
-      <ul class="list-group">
-        <li
-          v-for="column in columns"
-          class="list-group-item list-group-item-action"
-        >
-          <h5>
-            {{ column.name }}
-            <sup
-              ><span
-                class="badge mr-1"
-                :style="{ 'background-color': color(profile) }"
-                v-for="profile in column.profiles"
-                >{{ profile }}</span
-              ></sup
-            >
-            <div class="float-right">
-              <small class="align-right"
-                ><a
-                  v-for="semantics in column.semantics"
-                  href="semantics"
-                  target="_blank"
-                  >{{ semantics }}</a
-                ></small
+  <div class="container-fluid">
+    <MessageError v-if="error">error: {{ error }} {{ search }}</MessageError>
+    <p>
+      available profiles:
+      <span
+        class="badge mr-1"
+        :style="{ 'background-color': color(profile) }"
+        v-for="profile in profiles.filter(
+          (profile) => !selectedProfiles.includes(profile)
+        )"
+        @click="selectedProfiles.push(profile)"
+        >{{ profile }}</span
+      >
+    </p>
+    <p>
+      selected profiles:
+      <span
+        class="badge mr-1"
+        :style="{ 'background-color': color(profile) }"
+        v-for="profile in selectedProfiles"
+        @click="selectedProfiles.splice(selectedProfiles.indexOf(profile), 1)"
+        >{{ profile }}</span
+      >
+    </p>
+    <div class="row">
+      <div class="col-4">
+        <ul class="list-group">
+          <li
+            v-for="table in tables"
+            class="list-group-item list-group-item-action"
+            :class="{
+              active: table.selected || table.columns?.some((c) => c.selected),
+            }"
+          >
+            <h5>
+              {{ table.name }}
+              <sup v-if="table.profiles"
+                ><span
+                  class="badge mr-1"
+                  :style="{ 'background-color': color(profile) }"
+                  v-for="profile in table.profiles"
+                >
+                  {{ profile }}
+                </span>
+              </sup>
+            </h5>
+          </li>
+        </ul>
+      </div>
+      <div class="col-6">
+        <InputSearch v-model="search" />
+        <div v-for="table in tables">
+          <h2>{{ table.name }}</h2>
+          <div>{{ table.description }}</div>
+          <ul class="list-group">
+            <template v-for="column in table.columns">
+              <li
+                v-if="isVisible(column)"
+                class="list-group-item list-group-item-action"
+                :class="{ active: column.selected }"
+                @click="
+                  column.selected
+                    ? (column.selected = null)
+                    : (column.selected = true)
+                "
               >
-            </div>
-          </h5>
+                <h5>
+                  {{ column.name }} {{}}
+                  <sup
+                    ><span
+                      class="badge mr-1"
+                      :style="{ 'background-color': color(profile) }"
+                      v-for="profile in column.profiles"
+                      >{{ profile }}</span
+                    ></sup
+                  >
+                  <div class="float-right">
+                    <small class="align-right"
+                      ><a
+                        v-for="semantics in column.semantics"
+                        href="semantics"
+                        target="_blank"
+                        >{{ semantics }}</a
+                      ></small
+                    >
+                  </div>
+                </h5>
 
-          {{ column.description }}
-          <div>
-            <small
-              ><i>{{
-                Object.keys(column)
-                  .filter((key) =>
-                    ["key", "refTableName", "columnType"].includes(key)
-                  )
-                  .map((key) => key + "=" + column[key])
-                  .join(", ")
-              }}</i></small
-            >
-          </div>
-        </li>
-      </ul>
+                {{ column.description }}
+                <div>
+                  <small
+                    ><i>{{
+                      Object.keys(column)
+                        .filter((key) =>
+                          ["key", "refTableName", "columnType"].includes(key)
+                        )
+                        .map((key) => key + "=" + column[key])
+                        .join(", ")
+                    }}</i></small
+                  >
+                </div>
+              </li>
+            </template>
+          </ul>
+        </div>
+      </div>
+      <div class="col-2">
+        <h2>My selection:</h2>
+        <a href="" @click.prevent="downloadCsv()">download</a>
+        <ul>
+          <li v-for="table in selectedTables">
+            {{ table.name }}
+            <ul v-if="table.columns?.some((c) => c.selected)">
+              <li v-for="column in table.columns?.filter((c) => c.selected)">
+                {{ column.name }}
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
-  <pre>
-  <p>{{ profileJson }}</p>
-    </pre>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
-import { InputSearch } from "molgenis-components";
+import { ref, computed, reactive } from "vue";
+import { InputSearch, MessageError } from "molgenis-components";
 
 const error = ref(null);
 const profileJson = ref({});
-const selectedTable = ref(null);
 const selectedProfiles = ref([]);
 const search = defineModel();
 
@@ -120,22 +137,32 @@ fetch("../api/profiles")
   .then((data) => {
     profileJson.value = data;
     profileJson.value.tables.forEach(
-      (t) => (t.columns = t.columns.filter((c) => !c.inherited))
+      (t) => (t.columns = t.columns?.filter((c) => !c.inherited))
     );
   })
   .catch((err) => (error.value = err));
 
 const tables = computed(() => {
-  return profileJson.value.tables?.filter(
-    (t) =>
-      ((!search.value || JSON.stringify(t).includes(search.value)) &&
-        selectedProfiles.value.length == 0) ||
-      selectedProfiles.value.some(
-        (p) =>
-          t.profiles?.includes(p) ||
-          t.columns?.some((c) => c.profiles?.includes(p))
-      )
-  );
+  if (profileJson.value) {
+    return profileJson.value.tables?.filter(
+      (t) =>
+        ((!search.value || JSON.stringify(t).includes(search.value)) &&
+          selectedProfiles.value.length == 0) ||
+        selectedProfiles.value.some(
+          (p) =>
+            t.profiles?.includes(p) ||
+            t.columns?.some((c) => c.profiles?.includes(p))
+        )
+    );
+  }
+});
+
+const selectedTables = computed(() => {
+  if (profileJson.value) {
+    return profileJson.value.tables?.filter(
+      (t) => t.selected || t.columns?.some((c) => c.selected)
+    );
+  }
 });
 
 const profiles = computed(() => {
@@ -158,18 +185,63 @@ const profiles = computed(() => {
   return Array.from(new Set(result));
 });
 
-const columns = computed(() => {
-  if (selectedTable.value) {
-    return profileJson.value.tables
-      .find((t) => t === selectedTable.value)
-      .columns.filter(
-        (c) =>
-          (selectedProfiles?.value.length == 0 ||
-            c.profiles?.some((p) => selectedProfiles?.value.includes(p))) &&
-          (!search.value || JSON.stringify(c).includes(search.value))
-      );
+const isVisible = function (column) {
+  return (
+    (selectedProfiles?.value.length == 0 ||
+      column.profiles?.some((p) => selectedProfiles?.value.includes(p))) &&
+    (!search.value || JSON.stringify(column).includes(search.value))
+  );
+};
+
+const v = function (value) {
+  if (value == null) {
+    return "";
+  } else if (Array.isArray(value)) {
+    if (value.length > 1) {
+      return `"${value.join(",")}"`;
+    } else {
+      return v(value[0]);
+    }
+  } else {
+    return value;
   }
-});
+};
+
+const convertToCsv = function () {
+  var csv =
+    "tableName,tableExtends,columnName,columnType,key,required,refSchema,refTable,refLink,refBack,validation,semantics,description,profiles";
+  selectedTables.value.forEach((t) => {
+    csv += `${v(t.name)},${v(t.inheritName)},,,,,,,,,${v(t.semantics)},${v(
+      t.descrpition
+    )},${v(t.profiles)}\n`;
+    t.columns
+      ?.filter((c) => c.selected)
+      .forEach((c) => {
+        csv += `${v(t.name)},,${v(c.name)},${v(c.columnType)},${v(c.key)},${v(
+          c.required
+        )},${v(c.refSchema)},${v(c.refTableName)},${v(c.refLink)},${v(
+          c.validation
+        )},${v(c.semantics)},${v(c.description)},${v(c.profiles)}\n`;
+      });
+  });
+  return csv;
+};
+
+const downloadCsv = function () {
+  const text = convertToCsv();
+  var element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", "molgenis.csv");
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+};
 
 const color = function (profile) {
   var colorArray = [
