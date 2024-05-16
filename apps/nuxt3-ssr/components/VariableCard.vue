d42997bb40fc081c10a365170390dbae2bce9607
connoratrug <c.stroomberg@rug.nl>
2024-05-16 10:38:52 +0200

    move count out of link and make name column wider

--- apps/nuxt3-ssr/components/VariableCard.vue
+++ apps/nuxt3-ssr/components/VariableCard.vue
@@ -19,17 +19,17 @@ const resourcePathId = resourceIdPath(variableKey.value);
 <template>
   <article class="py-5 lg:px-12.5 p-5">
     <header class="flex md:flex-row gap-3 items-start md:items-center">
-      <div class="md:basis-1/5 p-2">
+      <div class="md:basis-2/5 p-2">
         <h2 class="break-all">
           <NuxtLink
             :to="`/${schema}/ssr-catalogue/${catalogue}/variables/${resourcePathId}`"
             class="text-body-base font-extrabold text-blue-500 hover:underline hover:bg-blue-50"
           >
             {{ variable?.name }}
-            <span v-if="variable.repeats?.length"
-              >( {{ variable.repeats?.length }} repeats)</span
-            >
           </NuxtLink>
+          <span class="ml-1" v-if="variable.repeats?.length"
+            >+{{ variable.repeats?.length }}</span
+          >
         </h2>
       </div>
       <div class="hidden md:flex md:basis-3/5">
