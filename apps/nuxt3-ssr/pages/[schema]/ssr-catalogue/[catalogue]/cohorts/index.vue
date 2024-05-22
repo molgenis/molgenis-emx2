4fa135d49f846bf55390a9bef3cd4a5f587b151a
connoratrug <c.stroomberg@rug.nl>
2024-05-22 16:52:10 +0200

    fix: 3693 scroll to top on next page

--- apps/nuxt3-ssr/pages/[schema]/ssr-catalogue/[catalogue]/cohorts/index.vue
+++ apps/nuxt3-ssr/pages/[schema]/ssr-catalogue/[catalogue]/cohorts/index.vue
@@ -176,11 +176,11 @@ const { data } = await useFetch<any, IMgError>(
 const cohorts = computed(() => data.value.data.Cohorts || []);
 const numberOfCohorts = computed(() => data.value.data.Cohorts_agg.count || 0);
 
-function setCurrentPage(pageNumber: number) {
-  router.push({
-    path: route.path,
+async function setCurrentPage(pageNumber: number) {
+  await navigateTo({
     query: { ...route.query, page: pageNumber },
   });
+  window.scrollTo({ top: 0 });
 }
 
 function onFilterChange(filters: IFilter[]) {
