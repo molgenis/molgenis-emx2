39cbbb7b6a43a0d8d1ce349dd8331ada8e2ea054
dcruvolo <davidruvolo51@gmail.com>
2024-05-23 15:54:36 +0200

    feat: init structure for imdhub-public (#3784)
    * init structure for imdhub-public
    
    * fix: quicklink title and text

---
+++ apps/imdhub-public/src/views/view-about.vue
@@ -0,0 +1,20 @@
+<template>
+  <Page>
+    <PageHeader
+      title="Recon4imd: IMD-hub"
+      subtitle="The Inherited Metabolic Diseases database"
+      imageSrc="recon4imd_header.jpg"
+      height="medium"
+    />
+    <PageSection aria-labelledby="about-section-title">
+      <h2 id="about-section-title">About Us</h2>
+      <MessageBox type="warning">
+        <p>This page is under construction.</p>
+      </MessageBox>
+    </PageSection>
+  </Page>
+</template>
+
+<script setup lang="ts">
+import { Page, PageHeader, PageSection, MessageBox } from "molgenis-viz";
+</script>
