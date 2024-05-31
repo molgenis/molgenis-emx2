16b079a1d89a7698dce21913c67aabb24155c3af
Vittorio Meloni <vittorio.meloni@crs4.it>
2024-05-31 17:02:05 +0200

    feat(data/biobank-directory): fixes Study model

---
+++ apps/directory/src/components/report-components/ReportStudyDetails.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="d-flex">
+      <report-description
+        :description="study.description"
+        :maxLength="500"
+      ></report-description>
+    </div>
+
+    <!-- study information -->
+    <view-generator :viewmodel="studyModel.viewmodel" />
+  </div>
+</template>
+
+<script>
+import { getStudyDetails } from "../../functions/viewmodelMapper";
+import ReportDescription from "../report-components/ReportDescription.vue";
+import ViewGenerator from "../generators/ViewGenerator.vue";
+
+export default {
+  name: "ReportStudyDetails",
+  props: {
+    study: {
+      type: Object,
+      required: true,
+    },
+  },
+  components: {
+    ReportDescription,
+    ViewGenerator,
+  },
+  computed: {
+    studyModel() {
+      return this.study ? getStudyDetails(this.study) : {};
+    },
+  },
+};
+</script>
---
+++ apps/directory/src/stores/studyStore.js
@@ -0,0 +1,43 @@
+import { defineStore } from "pinia";
+import { QueryEMX2 } from "molgenis-components";
+import { useSettingsStore } from "./settingsStore";
+
+export const useStudyStore = defineStore("studyStore", () => {
+  const settingsStore = useSettingsStore();
+
+  const studyColumns = settingsStore.config.studyColumns;
+  const graphqlEndpoint = settingsStore.config.graphqlEndpoint;
+
+  function getStudyColumns() {
+    const properties = studyColumns
+      .filter((column) => column.column)
+      .flatMap((studyColumn) => studyColumn.column);
+
+    const rangeProperties = studyColumns.filter(
+      (column) => column.type === "range"
+    );
+
+    for (const property of rangeProperties) {
+      properties.push(property.min, property.max, property.unit_column);
+    }
+
+    return properties;
+  }
+
+  async function getStudyReport(id) {
+    const studyReportQuery = new QueryEMX2(graphqlEndpoint)
+      .table("Studies")
+      .select(getStudyColumns())
+      .orderBy("Studies", "id", "asc")
+      .where("id")
+      .like(id);
+    const reportResults = await studyReportQuery.execute();
+
+    return reportResults;
+  }
+
+  return {
+    getStudyColumns,
+    getStudyReport,
+  };
+});
