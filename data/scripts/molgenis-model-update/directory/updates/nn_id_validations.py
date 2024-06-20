4910bf696e7db8a20e6849fc7290b3af2a0db858
dtroelofsprins <d.t.roelofs-prins@umcg.nl>
2024-06-20 08:13:06 +0200

    chore: rename module and class

--- data/scripts/molgenis-model-update/directory/updates/nn_id_validations.py
+++ data/scripts/molgenis-model-update/directory/updates/nn_id_validations.py
@@ -2,13 +2,13 @@ import glob
 import os
 import pandas as pd
 import shutil
-import sys
-sys.path.insert(1, "/".join(os.path.realpath(__file__).split("/")[0:-2]) + "/util")
-from update_client import UpdateClient  # noqa: E402
-from zip_handling import Zip  # noqa: E402
+# import sys
+# sys.path.insert(1, "/".join(os.path.realpath(__file__).split("/")[0:-2]) + "/util")
+from util.update_client import UpdateClient  # noqa: E402
+from util.zip_handling import Zip  # noqa: E402
 
 
-class UpdateMeta:
+class NnIdValidations:
     """General function to add ID validation expressions to the Directory staging areas
     """
 
@@ -17,7 +17,7 @@ class UpdateMeta:
         self.database_type = database_type
         self.path = self.database + '_data/'
         self.session = update_session
-        self.model_file = f"{self.path}molgenis.csv"
+        self.meta_file = f"{self.path}molgenis.csv"
         self.zip_handling = Zip(self.database)
         extra_char = ""
         if len(self.database) == 3:
@@ -45,16 +45,6 @@ class UpdateMeta:
                        f"or bbmri-eric:contactID:EU_'",
                             }
 
-    def add_expression(self, table, column):
-        if column == "id":
-            return self.validations[table]
-
-    def remove_files(self):
-        """Delete all files except molgenis.csv"""
-        for file in glob.glob(f"{self.path}/*.csv"):
-            if not file.endswith(self.model_file):
-                os.remove(file)
-
     def add_validations(self):
         if self.database_type != "BIOBANK_DIRECTORY_STAGING":
             print("No ID validations need to be added")
@@ -68,12 +58,22 @@ class UpdateMeta:
         self.session.save_data(self.database)
         self.zip_handling.unzip_data()
         self.remove_files()
-        df_model = pd.read_csv(self.model_file, sep=",")
-        df_model["validation"] = (
-            df_model.apply(lambda row:
-                           self.add_expression(row.tableName, row.columnName), axis=1))
-        df_model = self.session.float_to_int(df_model)
-        df_model.to_csv(f"{self.path}molgenis.csv", index=False)
+        df_meta = pd.read_csv(self.meta_file, sep=",")
+        df_meta["validation"] = (
+            df_meta.apply(lambda row:
+                          self.add_expression(row.tableName, row.columnName), axis=1))
+        df_meta = self.session.float_to_int(df_meta)
+        df_meta.to_csv(f"{self.path}molgenis.csv", index=False)
         self.zip_handling.zip_data()
         print(f" ⬆️ Update {self.database} data model")
         self.session.upload_zip(self.database, f"{self.database}_upload.zip")
+
+    def add_expression(self, table, column):
+        if column == "id":
+            return self.validations[table]
+
+    def remove_files(self):
+        """Delete all files except molgenis.csv"""
+        for file in glob.glob(f"{self.path}/*.csv"):
+            if not file.endswith(self.meta_file):
+                os.remove(file)
