6d5fa1bdc30b3578bc11dd9150d1dd02f912d7b8
connoratrug <c.stroomberg@rug.nl>
2024-07-02 12:20:37 +0200

    remove merge

--- backend/molgenis-emx2-analytics/src/main/java/org/molgenis/emx2/analytics/model/actions/CreateTriggerAction.java
+++
@@ -1,6 +0,0 @@
-package org.molgenis.emx2.analytics.model.actions;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record CreateTriggerAction(
-    @JsonProperty("name") String name, @JsonProperty("cssSelector") String cssSelector) {}
--- backend/molgenis-emx2-analytics/src/main/java/org/molgenis/emx2/analytics/service/AnalyticsService.java
+++
@@ -1,8 +0,0 @@
-package org.molgenis.emx2.analytics.service;
-
-import org.molgenis.emx2.Schema;
-import org.molgenis.emx2.analytics.model.actions.CreateTriggerAction;
-
-public interface AnalyticsService {
-  void createTriggerForSchema(Schema schema, CreateTriggerAction createTriggerAction);
-}
