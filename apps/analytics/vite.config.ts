eb7dc36cbc2ab260659f75d7a6231005bb31eee9
connoratrug <c.stroomberg@rug.nl>
2024-07-08 16:48:54 +0200

    chore: setup for publishing

--- apps/analytics/vite.config.ts
+++ apps/analytics/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'; // Import the 'path' module
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,18 @@ export default defineConfig({
     proxy: {
       "^/[a-zA-Z0-9_.%-]+/api/trigger": "http://localhost:8080/",
     }
-  }
+  },
+  build: {
+    lib: {
+      //Defines the entry point for the library build. It resolves 
+      //to src/index.ts,indicating that the library starts from this file.
+      entry: path.resolve(__dirname, "src/lib/analytics.ts"), // Use 'path.resolve' to resolve the path
+      name: "analytics",
+      //A function that generates the output file
+      //name for different formats during the build
+      fileName: (format) => `index.${format}.js`,
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
 });
