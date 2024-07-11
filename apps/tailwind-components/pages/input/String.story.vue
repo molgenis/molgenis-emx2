464a7547383cccefe1792cc84960293ed23ce554
dcruvolo <davidruvolo51@gmail.com>
2024-07-11 15:07:52 +0200

    feat: merged string components

--- apps/tailwind-components/pages/input/String.story.vue
+++ apps/tailwind-components/pages/input/String.story.vue
@@ -1,60 +1,71 @@
 <script setup lang="ts">
-const id = "story-input-1";
-const hasError = ref(false);
-const required = ref(false);
-const fieldValue = ref("");
+const label = "Search for patients";
+const placeholder = "'A-001' or 'A-001,A-002'";
 </script>
 
 <template>
-  <div class="flex">
-    <div class="m-4 flex-1">
-      <div>
-        <InputString
-          :id="id"
-          v-model="fieldValue"
-          :has-error="hasError"
-          :required="required"
-        />
-      </div>
+  <h2>String component</h2>
+  <p>
+    The <code>String Component</code> enables you to create an input element that takes user input
+    via the keyboard or other device. The available input types are not only
+    text, but date, email, number, password, search, etc. However, checkbox and
+    radio types are in a separate component.
+  </p>
+  <div
+    class="grid grid-cols-2 gap-6 my-5 [&>div>div]:bg-white [&>div>div]:p-4 [&_h3]:font-semibold [&_h3]:my-2"
+  >
+    <div>
+      <h3>Input String: default use</h3>
+      <InputString
+        id="search-patients-default"
+        :label="label"
+        :placeholder="placeholder"
+      />
     </div>
-
-    <div class="m-4 flex-1">
-      <p>fieldValue value: {{ fieldValue }}</p>
-
-      <div class="mt-4">
-        <button
-          @click="fieldValue = ''"
-          class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
-        >
-          clear value
-        </button>
-      </div>
-
-      <fieldset class="mt-4 border border-gray-900 mb-2">
-        <legend class="m-2 px-2">Props</legend>
-        <div class="mb-2">
-          <label class="ml-1 hover:cursor-pointer" for="input-string-required">
-            required
-          </label>
-          <input
-            id="input-string-required"
-            class="ml-2 hover:cursor-pointer"
-            type="checkbox"
-            v-model="required"
-          />
-        </div>
-        <div class="mb-2">
-          <label class="ml-1 hover:cursor-pointer" for="input-string-has-error">
-            hasError
-          </label>
-          <input
-            id="input-string-has-error"
-            class="ml-2 hover:cursor-pointer"
-            type="checkbox"
-            v-model="hasError"
-          />
-        </div>
-      </fieldset>
+    <div>
+      <h3>Input String: disabled</h3>
+      <InputString
+        id="search-patients-disabled"
+        :label="label"
+        :placeholder="placeholder"
+        :disabled="true"
+      />
+    </div>
+    <div>
+      <h3>Input Text: as a required field</h3>
+      <InputString
+        id="search-patients-required"
+        :label="label"
+        :placeholder="placeholder"
+        :required="true"
+      />
+    </div>
+    <div>
+      <h3>Input String: positive validation result</h3>
+      <InputString
+        id="search-patients-valid"
+        :label="label"
+        :placeholder="placeholder"
+        :valid="true"
+      />
+    </div>
+    <div>
+      <h3>Input String: negative validation result</h3>
+      <InputString
+        id="search-patients-error"
+        :label="label"
+        :placeholder="placeholder"
+        :hasError="true"
+      />
+    </div>
+    <div>
+      <h3>Input String: visually hidden label</h3>
+      <InputString
+        id="search-patients-error"
+        :label="label"
+        :placeholder="placeholder"
+        :show-label="false"
+      />
     </div>
   </div>
 </template>
