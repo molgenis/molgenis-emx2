0199cdb3a8277ca4e836ed27882a134c4343d21b
dcruvolo <davidruvolo51@gmail.com>
2024-07-12 17:16:57 +0200

    feat: init checkbox component and story

---
+++ apps/tailwind-components/components/input/Checkbox.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  id: string;
+  value?: string;
+}>()
+
+const modelValue = ref<string | boolean>("");
+
+const emit = defineEmits<{
+  (e: "change", value: Event): void;
+}>()
+
+</script>
+
+<template>
+  <input
+    :id="id"
+    type="checkbox"
+    :name="id"
+    v-model="modelValue"
+    class="w-5 h-5 rounded-3px ml-[6px] mr-2.5 mt-0.5 accent-yellow-500 indeterminate:accent-yellow-500 border border-checkbox hover:cursor-pointer"
+    @change="$emit('change', $event.target.value)"
+  />
+  
+</template>
---
+++ apps/tailwind-components/pages/input/Checkbox.story.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const selections = ref<string>('');
+</script>
+
+<template>
+  <h2>Radio component</h2>
+  <div class="flex">
+    <div>
+      <InputCheckbox id="radio-default" />
+      <InputLabel for="radio-default" label="Check this box" @change="selections = event" />
+      <p>Is the box ticked? Answer: {{ selections }}</p>
+    </div>
+  </div>
+</template>
