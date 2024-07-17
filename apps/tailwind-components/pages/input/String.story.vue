b10a4f491a906aaffc702578824faa76c243740b
connoratrug <c.stroomberg@rug.nl>
2024-07-17 13:58:19 +0200

    fix(tailwind-components): string input data binding

--- apps/tailwind-components/pages/input/String.story.vue
+++ apps/tailwind-components/pages/input/String.story.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const label = "Search for patients";
 const placeholder = "'A-001' or 'A-001,A-002'";
+const demoValue = ref("");
 </script>
 
 <template>
@@ -11,50 +12,53 @@ const placeholder = "'A-001' or 'A-001,A-002'";
     types are not only text, but date, email, number, password, search, etc.
     However, checkbox and radio types are in a separate component.
   </p>
+  
   <div
     class="grid grid-cols-2 gap-6 my-5 [&>div]:bg-white [&>div]:p-4 [&_h3]:font-semibold [&_h3]:my-2"
   >
     <div>
-      <h3>Input String: default use</h3>
+      <h3>Input String: default use (model value: {{ demoValue }})</h3>
       <InputLabel for="input-string-default" :label="label" />
-      <InputString id="input-string-default" :placeholder="placeholder" />
+      <InputString id="input-string-default" :placeholder="placeholder" v-model="demoValue" />
     </div>
     <div>
-      <h3>Input String: disabled</h3>
+      <h3>Input String: disabled (model value: {{ demoValue }})</h3>
       <InputLabel for="input-string-disabled" :label="label" />
       <InputString
         id="input-string-disabled"
         :placeholder="placeholder"
         :disabled="true"
+        v-model="demoValue"
       />
     </div>
     <div>
-      <h3>Input Text: as a required field</h3>
+      <h3>Input Text: as a required field (model value: {{ demoValue }})</h3>
       <InputLabel for="input-string-required" :label="label" />
       <InputString
         id="input-string-required"
         :label="label"
         :placeholder="placeholder"
+        v-model="demoValue"
       />
     </div>
     <div>
-      <h3>Input String: positive validation result</h3>
+      <h3>Input String: positive validation result (model value: {{ demoValue }})</h3>
       <InputLabel for="input-string-valid" :label="label" :valid="true" />
-      <InputString id="input-string-valid" :placeholder="placeholder" />
+      <InputString id="input-string-valid" :placeholder="placeholder" v-model="demoValue" />
     </div>
     <div>
-      <h3>Input String: negative validation result</h3>
+      <h3>Input String: negative validation result (model value: {{ demoValue }})</h3>
       <InputLabel for="input-string-error" :label="label" :has-error="true" />
-      <InputString id="input-string-error" :placeholder="placeholder" />
+      <InputString id="input-string-error" :placeholder="placeholder" v-model="demoValue" />
     </div>
     <div>
-      <h3>Input String: visually hidden label</h3>
+      <h3>Input String: visually hidden label (model value: {{ demoValue  }})</h3>
       <InputLabel
         for="input-string-hidden-label"
         :label="label"
         :hide-label="true"
       />
-      <InputString id="input-string-hidden-label" :placeholder="placeholder" />
+      <InputString id="input-string-hidden-label" :placeholder="placeholder" v-model="demoValue" />
     </div>
   </div>
 </template>
