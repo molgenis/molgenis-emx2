2e4854f8514c9c527abe9087e9988912a469593e
Sander van den Hoek <savandenhoek@gmail.com>
2024-07-18 12:25:59 +0200

    Fix example test

--- e2e/tests/admin!example.spec.ts
+++ e2e/tests/admin!example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 
 test("go to admin settings webpage", async ({ page }) => {
-await page.goto('http://localhost:8080/apps/central/');
-await page.getByRole('link', { name: 'Admin' }).click();
-await page.getByRole('link', { name: 'Settings' }).click();
+  await page.goto('/apps/central/');
+  await page.getByRole('link', { name: 'Admin' }).click();
+  await page.getByRole('link', { name: 'Settings' }).click();
 });
