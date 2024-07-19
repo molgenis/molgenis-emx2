3215f29d231b2bad225790faa5b38db00e4d281d
dcruvolo <davidruvolo51@gmail.com>
2024-07-19 17:16:08 +0200

    feat: init viz legend component and story

---
+++ apps/tailwind-components/components/viz/Legend.vue
@@ -0,0 +1,237 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    legendId: string;
+    data: object;
+    stackLegend?: boolean;
+    enableClicks?: boolean;
+    enableHovering?: boolean;
+    enableMultiSelect?: boolean;
+    markerType?: "circle" | "square";
+  }>(),
+  {
+    stackLegend: false,
+    enableClicks: false,
+    enableHovering: false,
+    enableMultiSelect: false,
+    markerType: "circle",
+  }
+);
+
+const emit = defineEmits<{
+  (e: "legend-item-clicked", value: string): void;
+  (e: "legend-item-mouseover", value: string): void;
+  (e: "legend-item-mouseout", value: string): void;
+}>();
+
+const legendSelections = defineModel<string[] | number[]>();
+</script>
+
+<template>
+  <ul
+    class="list-style-none flex m-0 p-0"
+    :class="{
+      'flex-col gap-0': stackLegend,
+      'flex-row flex-wrap justify-start gap-3': !stackLegend,
+      'legend-clicked-enabled': enableClicks,
+      'legend-hovering-enabled': enableHovering,
+    }"
+  >
+    <li
+      class="relative cursor-default"
+      :class="{
+        'mb-2': !stackLegend,
+        'hover:cursor-pointer': enableHovering || enableClicks,
+      }"
+      data-legend-item="item"
+      v-for="key in Object.keys(data)"
+      :key="key"
+    >
+      <div
+        v-if="enableClicks"
+        class="flex flex-row gap-3 justify-start items-center"
+      >
+        <label :for="`legend-input-${legendId}-${key}`">
+          <vizLegendMarker :markerType="markerType" :fill="data[key]" />
+          <span class="text-body-base text-current">{{ key }}</span>
+        </label>
+        <input
+          :id="`legend-input-${legendId}-${key}`"
+          class="sr-only"
+          type="checkbox"
+          :data-group="key"
+          :value="key"
+          v-model="legendSelections"
+          @change="$emit('legend-item-clicked', key)"
+        />
+      </div>
+      <div
+        v-else
+        class="flex flex-row gap-3 justify-start items-center"
+        :class="{
+          'hover:underline': enableHovering,
+        }"
+        :data-value="key"
+        @mouseover="$emit('legend-item-mouseover', key)"
+        @mouseout="$emit('legend-item-mouseout', key)"
+      >
+        <vizLegendMarker :markerType="markerType" :fill="data[key]" />
+        <span class="item-label">
+          {{ key }}
+        </span>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<!-- <script>
+// Create a legend for a visualisation for use outside the chart element. This
+// component may be useful if you have several charts that display the
+// same groups.
+//
+// @group VISUALISATIONS
+export default {
+  name: "ChartLegend",
+  props: {
+    // a unique ID for the legend
+    legendId: {
+      type: String,
+      required: true,
+    },
+
+    // One or more key-value pairs
+    data: {
+      type: Object,
+      required: true,
+    },
+    // If true (default), all legend items will be stacked
+    stackLegend: {
+      type: Boolean,
+      default: true,
+    },
+
+    // If `true`, click events will be enabled for all labels. When a label is
+    // clicked, the row-level data for that label will be emitted.
+    // To access the data, use the event `@legend-item-clicked=(value) => ...`
+    enableClicks: {
+      type: Boolean,
+      // `false`
+      default: false,
+    },
+
+    // If `true`, mouseover event will be enabled for all labels. When a label is
+    // clicked, the row-level data for that item will be emitted.
+    // To access the data, use the event `legend-item-hovered=(value)=>...`
+    enableHovering: {
+      type: Boolean,
+      // `false`
+      default: false,
+    },
+  },
+  emits: [
+    "legend-item-clicked",
+    "legend-item-mouseover",
+    "legend-item-mouseout",
+  ],
+  data() {
+    return {
+      selection: [],
+    };
+  },
+  methods: {
+    emitSelection(event) {
+      const parent = event.target.parentNode;
+      parent.classList.toggle("checkbox-clicked");
+      this.$emit("legend-item-clicked", this.selection);
+    },
+    emitMouseOver(value) {
+      if (this.enableHovering) {
+        this.$emit("legend-item-mouseover", value);
+      }
+    },
+    emitMouseOut(value) {
+      if (this.enableHovering) {
+        this.$emit("legend-item-mouseout", value);
+      }
+    },
+  },
+  computed: {
+    classNames() {
+      const css = ["chart-legend"];
+      if (!this.stackLegend) {
+        css.push("legend-horizontal");
+      }
+      if (this.enableClicks) {
+        css.push("clicks-enabled");
+      }
+      return css.join(" ");
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.chart-legend {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+
+  .checkbox-item,
+  .text-item {
+    position: relative;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    column-gap: 4px;
+    margin-bottom: 5px;
+
+    &:last-child {
+      margin: 0;
+    }
+
+    .item-marker {
+      width: 16px;
+      margin-right: 6px;
+    }
+  }
+
+  .legend-item {
+    cursor: default;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+  &.legend-horizontal {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 0.5em;
+
+    .legend-item {
+      margin-bottom: 0;
+    }
+  }
+
+  &.clicks-enabled {
+    .checkbox-item {
+      .item-label {
+        cursor: pointer;
+      }
+      &.checkbox-clicked {
+        opacity: 0.65;
+        .item-label {
+          span {
+            text-decoration: line-through;
+          }
+        }
+      }
+    }
+  }
+}
+</style> -->
---
+++ apps/tailwind-components/pages/viz/Legend.story.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+const selections = ref<string[]>();
+
+const legendData = {
+  "Group A": "#f6eff7",
+  "Group B": "#bdc9e1",
+  "Group C": "#67a9cf",
+  "Group D": "#02818a",
+};
+</script>
+
+<template>
+  <div class="[&>p]:mb-3 [&>p]:mt-1">
+    <h4 class="text-heading-2xl">Legend Item Marker Component</h4>
+    <p>
+      The legend component has one child component:
+      <code>vizLegendMarker</code>. This component allows you to change the
+      symbol for each legend item. You can either render the markers as a square
+      or cirle. This is useful for visually linking chart types with the
+      legends. For example, if you are creating a scatter plot, it is better to
+      render the legend items using the circle item rather than the square. By
+      default, the marker type is a circle. The color (i.e., fill) of the marker
+      can be set by using the <code>fill</code> parameter.
+    </p>
+    <pre class="block bg-gray-200 p-1 mb-3 whitespace-pre-line">
+      {{  `<vizLegendMarker marker-type="circle" fill="orange"/>` }}
+      {{  `<vizLegendMarker marker-type="square" fill="orange"/>` }}
+    </pre>
+    <div class="flex flex-row gap-1 p-1 my-4">
+      <vizLegendMarker marker-type="circle" fill="orange" />
+      <vizLegendMarker marker-type="square" fill="orange" />
+    </div>
+    <h4 class="text-heading-2xl">The Legend Component</h4>
+    <p>
+      The purpose of the <code>vizLegend</code> component is to provide context
+      for charts that display grouped data. The color palette in the chart and
+      the legend should be the same. The input data is an object containing one
+      or more key-value pairs. The key must by the group name and the value is
+      the color. For example:
+    </p>
+    <pre class="block bg-gray-200 p-1 mb-3 whitespace-normal">
+      {{ legendData }}
+    </pre>
+    <p>This produces the following legend.</p>
+    <vizLegend
+      legend-id="default-legend"
+      :data="legendData"
+      :stack-legend="false"
+    />
+    <p>
+      The legend can also be stacked using <code>:stack-legend="true"</code>
+    </p>
+    <pre class="block bg-gray-200 p-1 mb-3 whitespace-normal">
+      {{ `<vizLegend legend-id="default-legend" :data="legendData" :stack-legend="true"/>` }}
+    </pre>
+    <vizLegend
+      legend-id="default-legend"
+      :data="legendData"
+      :stack-legend="true"
+    />
+    <p>
+      Interactive features are also available on the legend component. These
+      features can be used to control a chart by extracting the value of a
+      legend item (i.e., the label). You can enable/disable hovering and clicks
+      in a legend.
+    </p>
+  </div>
+</template>
