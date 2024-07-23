e00c34b710c86626b7f8357bc51219373051e7c7
dcruvolo <davidruvolo51@gmail.com>
2024-07-23 16:38:26 +0200

    fix: removed heights; added background styling

--- apps/tailwind-components/pages/index.vue
+++ apps/tailwind-components/pages/index.vue
@@ -11,8 +11,11 @@ const stories = Object.entries(modules).map(([key, value]) => {
 </script>
 
 <template>
-  <div v-for="story in stories" class="mt-6 pb-12 border-b border-y-gray-300">
-    <h3 class="text-heading-3xl my-3">{{ story.title }}</h3>
+  <div
+    v-for="story in stories"
+    class="mt-1 px-4 pt-2 pb-12 rounded bg-white first:mt-8 last:rounded-b-50px last:mb-8"
+  >
+    <h3 class="text-heading-4xl my-3">{{ story.title }}</h3>
 
     <div>
       <component :is="story.component" />
