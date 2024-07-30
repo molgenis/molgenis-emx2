9779ef6566940823f39f0ff9f0ff055abc83ad1e
mswertz <m.a.swertz@rug.nl>
2024-07-30 17:06:38 +0200

    fix icons

--- apps/nuxt3-ssr/nuxt.config.ts
+++ apps/nuxt3-ssr/nuxt.config.ts
@@ -2,9 +2,8 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  modules: ["@nuxt/image"],
   devtools: { enabled: true },
-  extends: ["../tailwind-components"],
+  modules: ["@nuxt/image",'@nuxtjs/tailwindcss', 'floating-vue/nuxt', '@nuxt/test-utils/module'],
   tailwindcss: {
     cssPath: '../tailwind-components/assets/css/main.css',
     configPath: '../tailwind-components/tailwind.config.js'
@@ -16,7 +15,7 @@ export default defineNuxtConfig({
       siteTitle: "MOLGENIS",
       analyticsKey: "",
       cohortOnly: false,
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || "https://data-catalogue-acc.molgeniscloud.org/",
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || "https://preview-emx2-pr-4090.dev.molgenis.org/",
     },
   },
   imports: {
@@ -25,12 +24,28 @@ export default defineNuxtConfig({
       exclude: [/\bmeta-data-utils\b/],
     },
   },
+  nitro: {
+    prerender: {
+      ignore: ['/_tailwind/']
+    }
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        'data-theme': ''
+      }
+    }
+  },
   components: [
     {
       path: "~/components"
     },
     {
       path: "../tailwind-components/components"
+    },
+    {
+      path: "../tailwind-components/components/global/icons",
+      global: true,
     }
   ],
 });
