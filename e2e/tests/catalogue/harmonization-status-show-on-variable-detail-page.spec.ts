26f4d5a923b8840e4cb1948309e70796c11644f0
mswertz <m.a.swertz@rug.nl>
2024-08-02 00:25:31 +0200

    fix test

--- e2e/tests/catalogue/harmonization-status-show-on-variable-detail-page.spec.ts
+++ e2e/tests/catalogue/harmonization-status-show-on-variable-detail-page.spec.ts
@@ -4,7 +4,9 @@ test('test hamonisation status is show in varaible on variable detail page', asy
   await page.goto('/catalogue-demo/ssr-catalogue/')
   await page.getByRole('button', { name: 'Accept' }).click();
   await page.getByText('ATHLETE').click();
-  await page.getByRole('button', { name: 'Variables' }).dblclick();
+  await page.getByRole('button', { name: 'Variables' }).click();
+  await page.getByPlaceholder('Type to search..').click();
+  await page.getByPlaceholder('Type to search..').fill('fetus_abd_circum_t2');
   await page.getByRole('link', { name: 'fetus_abd_circum_t2', exact: true }).click();
   await expect(page.getByRole('row', { name: 'fetus_abd_circum_t2 partial' }).getByRole('img')).toBeVisible();
 });
