c711efdf36c7e2e5ef069d3f0f5653a50328b80d
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2024-08-02 12:36:47 +0200

    Refactor and expand tests

--- backend/molgenis-emx2-rdf/src/test/java/org/molgenis/emx2/rdf/CatalogueComplianceTest.java
+++ backend/molgenis-emx2-rdf/src/test/java/org/molgenis/emx2/rdf/CatalogueComplianceTest.java
@@ -7,33 +7,33 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @Disabled
-public class FDPComplianceTest {
+public class CatalogueComplianceTest {
 
-  private static String fdpSchemaRDF;
+  private static String catalogueSchemaRDF;
 
   @BeforeAll
   public static void setup() {
-    fdpSchemaRDF =
-        CreateSchemaExportRDF("fairdatapoint_shacl_validation", "_profiles/FAIRDataPoint.yaml");
+    catalogueSchemaRDF =
+        CreateSchemaExportRDF("catalogue_shacl_validation", "_profiles/DataCatalogueFlat.yaml");
   }
 
   @Test
   public void FAIRDataPoint_Compliance() throws Exception {
-    TestCompliance(FAIR_DATA_POINT_SHACL_FILES, fdpSchemaRDF);
+    TestCompliance(FAIR_DATA_POINT_SHACL_FILES, catalogueSchemaRDF);
   }
 
   @Test
   public void DCAT_AP_Compliance() throws Exception {
-    TestCompliance(DCAT_AP_SHACL_FILES, fdpSchemaRDF);
+    TestCompliance(DCAT_AP_SHACL_FILES, catalogueSchemaRDF);
   }
 
   @Test
   public void Health_RI_core_plateau_1_Compliance() throws Exception {
-    TestCompliance(HEALTH_RI_SHACL_FILES, fdpSchemaRDF);
+    TestCompliance(HEALTH_RI_SHACL_FILES, catalogueSchemaRDF);
   }
 
   @Test
   public void EJP_RD_VP_Lvl1_Compliance() throws Exception {
-    TestCompliance(EJP_RD_VP_SHACL_FILES, fdpSchemaRDF);
+    TestCompliance(EJP_RD_VP_SHACL_FILES, catalogueSchemaRDF);
   }
 }
