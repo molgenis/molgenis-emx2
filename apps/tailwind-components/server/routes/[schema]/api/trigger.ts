b043833c3b41c1f0821b10d3f2dd3f7e333246eb
connoratrug <c.stroomberg@rug.nl>
2024-08-12 15:00:10 +0200

    remove dev code

--- apps/tailwind-components/server/routes/[schema]/api/trigger.ts
+++ apps/tailwind-components/server/routes/[schema]/api/trigger.ts
@@ -3,9 +3,7 @@ export default defineEventHandler((event) => {
  const config = useRuntimeConfig(event); 
   console.log("proxy trigger request : ", event.path);
   const schema = getRouterParam(event, "schema") || "";
-  const triggerPreviewLocation = "https://preview-emx2-pr-3946.dev.molgenis.org/"
-  //const location = joinURL(config.public.apiBase, schema, "api", "trigger")
-  const location = joinURL(triggerPreviewLocation, schema, "api", "trigger")
+  const location = joinURL(config.public.apiBase, schema, "api", "trigger")
   console.log("to : ", location);
   return proxyRequest(event, location);
 });
