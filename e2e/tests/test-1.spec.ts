39ce67819ed2e65145a8cd554ee9471a611fd343
mswertz <m.a.swertz@rug.nl>
2024-08-16 01:13:39 +0200

    more fixes

--- e2e/tests/test-1.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://preview-emx2-pr-4114.dev.molgenis.org/catalogue-demo/ssr-catalogue/EUChildNetwork/variables?page=2');
-  await page.getByRole('button', { name: 'Accept' }).click();
-
-});
