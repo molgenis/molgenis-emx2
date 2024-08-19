fbe453feb67d2bb350afdfee0e327ff4c9f9b354
connoratrug <c.stroomberg@rug.nl>
2024-08-19 16:22:53 +0200

    object cell type

--- apps/tailwind-components/components/table/cellTypes/EMX2.vue
+++ apps/tailwind-components/components/table/cellTypes/EMX2.vue
@@ -6,20 +6,33 @@ defineProps<{
 }>();
 </script>
 <template>
-  <td
-    v-if="typeof data === 'string'"
-    class="py-2.5 px-2.5 border-b border-gray-200 first:font-bold first:pl-0 last:pr-0 sm:first:pl-2.5 sm:last:pr-2.5 h-4 overflow-ellipsis whitespace-nowrap overflow-hidden"
-  >
-    {{ data }}
+
+  <td v-if="data === null || data === undefined"
+    class="py-2.5 px-2.5 border-b border-gray-200 first:pl-0 last:pr-0 sm:first:pl-2.5 sm:last:pr-2.5">
   </td>
-  <td
-    v-else-if="data === null || data === undefined"
-    class="py-2.5 px-2.5 border-b border-gray-200 first:pl-0 last:pr-0 sm:first:pl-2.5 sm:last:pr-2.5"
-  ></td>
-  <td
-    v-else
-    class="py-2.5 px-2.5 border-b border-gray-200 first:pl-0 last:pr-0 sm:first:pl-2.5 sm:last:pr-2.5"
-  >
-    {{ typeof data }}
+
+  <td v-else-if="metaData.columnType === 'STRING'"
+    class="py-2.5 px-2.5 border-b border-gray-200 first:pl-0 last:pr-0 sm:first:pl-2.5 sm:last:pr-2.5 h-4 overflow-ellipsis whitespace-nowrap overflow-hidden">
+    <TableCellTypesString :metaData="metaData" :data="data" />
   </td>
+
+  <td v-else-if="metaData.columnType === 'DECIMAL'"
+    class="py-2.5 px-2.5 border-b border-gray-200 first:pl-0 last:pr-0 sm:first:pl-2.5 sm:last:pr-2.5 h-4 overflow-ellipsis whitespace-nowrap overflow-hidden">
+    <TableCellTypesDecimal :metaData="metaData" :data="data" />
+  </td>
+
+  <td v-else-if="metaData.columnType === 'REF'"
+    class="py-2.5 px-2.5 border-b border-gray-200 first:pl-0 last:pr-0 sm:first:pl-2.5 sm:last:pr-2.5 h-4 overflow-ellipsis whitespace-nowrap overflow-hidden">
+    <TableCellTypesObject :metaData="metaData" :data="data" />
+  </td>
+
+  <td v-else-if="metaData.columnType === 'ONTOLOGY'"
+    class="py-2.5 px-2.5 border-b border-gray-200 first:pl-0 last:pr-0 sm:first:pl-2.5 sm:last:pr-2.5 h-4 overflow-ellipsis whitespace-nowrap overflow-hidden">
+    <TableCellTypesObject :metaData="metaData" :data="data" />
+  </td>
+
+  <td v-else class="py-2.5 px-2.5 border-b border-gray-200 first:pl-0 last:pr-0 sm:first:pl-2.5 sm:last:pr-2.5">
+    {{ metaData.columnType }}
+  </td>
+
 </template>
