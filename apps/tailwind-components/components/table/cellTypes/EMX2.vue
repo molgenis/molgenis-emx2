a53220577a038450a5c2960e9933f39a931fe375
connoratrug <c.stroomberg@rug.nl>
2024-08-21 10:48:41 +0200

    add coltype ts types and simple list column type

--- apps/tailwind-components/components/table/cellTypes/EMX2.vue
+++ apps/tailwind-components/components/table/cellTypes/EMX2.vue
@@ -12,6 +12,13 @@ defineProps<{
   ></td>
 
   <td
+    v-else-if="metaData.columnType.endsWith('ARRAY')"
+    class="py-2.5 px-2.5 border-b border-gray-200 first:pl-0 last:pr-0 sm:first:pl-2.5 sm:last:pr-2.5 h-4 overflow-ellipsis whitespace-nowrap overflow-hidden"
+  >
+    <TableCellTypesList :metaData="metaData" :data="data" />
+  </td>
+
+  <td
     v-else-if="metaData.columnType === 'STRING'"
     class="py-2.5 px-2.5 border-b border-gray-200 first:pl-0 last:pr-0 sm:first:pl-2.5 sm:last:pr-2.5 h-4 overflow-ellipsis whitespace-nowrap overflow-hidden"
   >
