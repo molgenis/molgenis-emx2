f708106add2075b561b588206763802c340ae7fc
connoratrug <c.stroomberg@rug.nl>
2024-08-21 13:25:38 +0200

    cleanup logger

--- apps/tailwind-components/server/routes/[schema]/graphql.ts
+++ apps/tailwind-components/server/routes/[schema]/graphql.ts
@@ -4,8 +4,6 @@ import { createConsola } from "consola";
 export default defineEventHandler((event) => {
  const config = useRuntimeConfig(event); 
  const logger = createConsola({ level: config.logLevel?? 3 });
-
-  logger.info(config.logLevel);
   logger.info("proxy schema gql request : ", event.path);
   if (event.method === "POST") {
     readBody(event).then((body) => {
