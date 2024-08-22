07992a8b5453958153bbfc8e09bb479b5354edf0
Harm Brugge <harmbrugge@gmail.com>
2024-08-22 12:41:20 +0200

    Updated test

--- backend/molgenis-emx2-webapi/src/test/java/org.molgenis.emx2.web/TestValidationWithJavaScriptBindings.java
+++ backend/molgenis-emx2-webapi/src/test/java/org.molgenis.emx2.web/TestValidationWithJavaScriptBindings.java
@@ -5,6 +5,7 @@ import static org.molgenis.emx2.Column.column;
 import static org.molgenis.emx2.Row.row;
 import static org.molgenis.emx2.TableMetadata.table;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.molgenis.emx2.*;
@@ -16,16 +17,15 @@ public class TestValidationWithJavaScriptBindings {
   private static final String schemaName =
       TestValidationWithJavaScriptBindings.class.getSimpleName();
   private static Schema schema;
+  private static Table table1;
+  private static Table table2;
 
   @BeforeAll
   public static void setup() {
     database = TestDatabaseFactory.getTestDatabase();
     schema = database.dropCreateSchema(schemaName);
     JavaScriptBindings.getBindingsForSession(new MolgenisSession(database));
-  }
 
-  @Test
-  public void testValidationWithSimplePostClient_shouldThrowWhenValidationFails() {
     // validation script that checks if the age inserted in the Test2 table is present in the Test1
     String validationScript =
         """
@@ -50,16 +50,16 @@ public class TestValidationWithJavaScriptBindings {
         """
             .formatted(schemaName);
 
-    Table test1 =
+    table1 =
         schema.create(
             table(
                 "Test1",
                 column("id").setPkey(),
                 column("name"),
                 column("age").setVisible("name").setType(ColumnType.INT)));
-    test1.insert(row("id", 1, "name", "jan", "age", 11));
+    table1.insert(row("id", 1, "name", "jan", "age", 11));
 
-    Table test2 =
+    table2 =
         schema.create(
             table(
                 "Test2",
@@ -69,11 +69,22 @@ public class TestValidationWithJavaScriptBindings {
                     .setValidation(validationScript)
                     .setVisible("name")
                     .setType(ColumnType.INT)));
+  }
 
+  @AfterAll
+  public static void after() {
+    database.dropSchema(schemaName);
+  }
+
+  @Test
+  public void testValidationWithSimplePostClient_shouldSucceed() {
     // Age 10 is not present in test1
-    test2.insert(row("id", 1, "name", "jan", "age", 10));
-    // Should throw exception as age 11 is present in Test1 table:
+    table2.insert(row("id", 1, "name", "jan", "age", 10));
+  }
+
+  @Test
+  public void testValidationWithSimplePostClient_shouldFail() {
     assertThrows(
-        MolgenisException.class, () -> test2.insert(row("id", 2, "name", "jan", "age", 11)));
+        MolgenisException.class, () -> table2.insert(row("id", 2, "name", "jan", "age", 11)));
   }
 }
