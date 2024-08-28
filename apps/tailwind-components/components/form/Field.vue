e308e6eb47338526e3f1de7f97bc78445a2330b3
connoratrug <c.stroomberg@rug.nl>
2024-08-28 16:08:41 +0200

    format folder

--- apps/tailwind-components/components/form/Field.vue
+++ apps/tailwind-components/components/form/Field.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { IColumn } from '../../../metadata-utils/src/types';
+import type { IColumn } from "../../../metadata-utils/src/types";
 
 defineProps<{
   column: IColumn;
@@ -11,18 +11,22 @@ const error = computed(() => {
 </script>
 
 <template>
-  <div class=" flex flex-col gap-1">
+  <div class="flex flex-col gap-1">
     <div>
-      <span class="capitalize text-title font-bold">{{ column.label }}</span><span class=" text-disabled ml-3" v-show="column.required">Required</span>
+      <span class="capitalize text-title font-bold">{{ column.label }}</span
+      ><span class="text-disabled ml-3" v-show="column.required">Required</span>
     </div>
-    <div class=" text-title " v-if="column.description">
+    <div class="text-title" v-if="column.description">
       {{ column.description }}
     </div>
     <div>
-      <FormFieldInput :type="column.columnType" :id="column.id"></FormFieldInput>
+      <FormFieldInput
+        :type="column.columnType"
+        :id="column.id"
+      ></FormFieldInput>
     </div>
     <div v-if="error">
-      <p class=" text-invalid">error</p>
+      <p class="text-invalid">error</p>
     </div>
   </div>
 </template>
--- apps/tailwind-components/pages/Form.story.vue
+++ apps/tailwind-components/pages/Form.story.vue
@@ -1,19 +1,30 @@
 <script setup lang="ts">
-import type { ISchemaMetaData, ITableMetaData } from '../../metadata-utils/src/types';
+import type {
+  ISchemaMetaData,
+  ITableMetaData,
+} from "../../metadata-utils/src/types";
 
 const sampleType = ref("complex");
 
- // just assuming that the table is there for the demo
-const schemaId = computed(() => sampleType.value === "simple" ? "pet store" : "catalogue");
-const tableId = computed(() => sampleType.value === "simple" ? "Pet" : "Collections");
+// just assuming that the table is there for the demo
+const schemaId = computed(() =>
+  sampleType.value === "simple" ? "pet store" : "catalogue"
+);
+const tableId = computed(() =>
+  sampleType.value === "simple" ? "Pet" : "Collections"
+);
 
+const { data: schemaMeta, refresh: refetchMetadata } = await useAsyncData(
+  "form sample",
+  () => fetchMetadata(schemaId.value)
+);
 
-const {
-  data: schemaMeta,
-  refresh: refetchMetadata,
-} = await useAsyncData("form sample", () => fetchMetadata(schemaId.value));
-
-const tableMeta = computed(() => ((schemaMeta.value as ISchemaMetaData).tables.find((table) => table.id === tableId.value)) as ITableMetaData);
+const tableMeta = computed(
+  () =>
+    (schemaMeta.value as ISchemaMetaData).tables.find(
+      (table) => table.id === tableId.value
+    ) as ITableMetaData
+);
 
 const updateSampleType = (event: Event) => {
   sampleType.value = (event.target as HTMLSelectElement).value;
@@ -24,13 +35,9 @@ const updateSampleType = (event: Event) => {
 <template>
   <div class="pb-10">
     <select @change="updateSampleType">
-      <option value="complex">
-         Complex form example
-      </option>
-      <option value="simple">
-         Simple form example
-      </option>
+      <option value="complex">Complex form example</option>
+      <option value="simple">Simple form example</option>
     </select>
   </div>
-  <FormFields class="  p-8" :meta-data="tableMeta"></FormFields>
+  <FormFields class="p-8" :meta-data="tableMeta"></FormFields>
 </template>
