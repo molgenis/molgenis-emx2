ff0959a1340f0614d6b70d46df9826544e85e324
Sander van den Hoek <savandenhoek@gmail.com>
2024-09-02 13:55:58 +0200

    updated e2e test to be identical to backend test

--- e2e/tests/home/admin!create-database.spec.ts
+++ e2e/tests/home/admin!create-database.spec.ts
@@ -1,18 +1,30 @@
 import { test, expect } from '@playwright/test';
 
-let regexErrorMessage='Table name must start with a letter, followed by letters/underscores/spaces/numbers (though no underscore preceded/followed by a space), i.e. ^(?!.* _|.*_ )[a-zA-Z][a-zA-Z0-9 _]*$. Maximum length: 31 characters'
+let regexErrorMessage='Table name must start with a letter, followed by letters/underscores/spaces/numbers (though no underscore preceded/followed by a space) and with a maximum of 31 characters, i.e. ^(?!.* _|.*_ )[a-zA-Z][a-zA-Z0-9 _]{0,30}$'
 
 test('database name regex validation', async ({ page }) => {
   await page.goto('http://localhost:8080/apps/central/#/');
   await page.getByRole('button', { name: '' }).click();
-  await page.getByLabel('name').fill('%');
-  await expect(page.locator('form')).toContainText(regexErrorMessage);
   await page.getByLabel('name').fill('a');
   await expect(page.locator('form')).not.toContainText(regexErrorMessage);
-  await page.getByLabel('name').fill('a_ b');
-  await expect(page.locator('form')).toContainText(regexErrorMessage);
-  await page.getByLabel('name').fill('a _b');
-  await expect(page.locator('form')).toContainText(regexErrorMessage);
-  await page.getByLabel('name').fill('a_b c');
+  await page.getByLabel('name').fill('first name');
   await expect(page.locator('form')).not.toContainText(regexErrorMessage);
+  await page.getByLabel('name').fill('yet_another name');
+  await expect(page.locator('form')).not.toContainText(regexErrorMessage);
+  await page.getByLabel('name').fill('#first name');
+  await expect(page.locator('form')).toContainText(regexErrorMessage);
+  await page.getByLabel('name').fill('first_  name');
+  await expect(page.locator('form')).toContainText(regexErrorMessage);
+  await page.getByLabel('name').fill('first   _name');
+  await expect(page.locator('form')).toContainText(regexErrorMessage);
+  await page.getByLabel('name').fill('first  _  name');
+  await expect(page.locator('form')).toContainText(regexErrorMessage);
+  await page.getByLabel('name').fill('first  __   name');
+  await expect(page.locator('form')).toContainText(regexErrorMessage);
+  await page.getByLabel('name').fill('aa    ____      ');
+  await expect(page.locator('form')).toContainText(regexErrorMessage);
+  await page.getByLabel('name').fill('abcdefghijklmnopqrstuvwzyx78901');
+  await expect(page.locator('form')).not.toContainText(regexErrorMessage);
+  await page.getByLabel('name').fill('abcdefghijklmnopqrstuvwzyx789012');
+  await expect(page.locator('form')).toContainText(regexErrorMessage);
 });
