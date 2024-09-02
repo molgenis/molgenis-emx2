6bd8886a00d182f3d3d3e60c16f25847788467c1
Sander van den Hoek <savandenhoek@gmail.com>
2024-09-02 14:02:12 +0200

    fixed e2e test

--- e2e/tests/home/admin!create-database.spec.ts
+++ e2e/tests/home/admin!create-database.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 let regexErrorMessage='Table name must start with a letter, followed by letters/underscores/spaces/numbers (though no underscore preceded/followed by a space) and with a maximum of 31 characters, i.e. ^(?!.* _|.*_ )[a-zA-Z][a-zA-Z0-9 _]{0,30}$'
 
 test('database name regex validation', async ({ page }) => {
-  await page.goto('http://localhost:8080/apps/central/#/');
+  await page.goto('/apps/central/');
   await page.getByRole('button', { name: '' }).click();
   await page.getByLabel('name').fill('a');
   await expect(page.locator('form')).not.toContainText(regexErrorMessage);
