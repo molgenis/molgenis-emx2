c16934d9cee0ff0c42903a5ed88045691c6cd3dd
Sander van den Hoek <savandenhoek@gmail.com>
2024-09-02 12:22:51 +0200

    added test for database creation name regex validation

---
+++ e2e/tests/home/admin!create-database.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+let regexErrorMessage='Table name must start with a letter, followed by letters/underscores/spaces/numbers (though no underscore preceded/followed by a space), i.e. ^(?!.* _|.*_ )[a-zA-Z][a-zA-Z0-9 _]*$. Maximum length: 31 characters'
+
+test('database name regex validation', async ({ page }) => {
+  await page.goto('http://localhost:8080/apps/central/#/');
+  await page.getByRole('button', { name: '' }).click();
+  await page.getByLabel('name').fill('%');
+  await expect(page.locator('form')).toContainText(regexErrorMessage);
+  await page.getByLabel('name').fill('a');
+  await expect(page.locator('form')).not.toContainText(regexErrorMessage);
+  await page.getByLabel('name').fill('a_ a');
+  await expect(page.locator('form')).toContainText(regexErrorMessage);
+  await page.getByLabel('name').fill('a _a');
+  await expect(page.locator('form')).toContainText(regexErrorMessage);
+  await page.getByLabel('name').fill('a_a');
+  await expect(page.locator('form')).not.toContainText(regexErrorMessage);
+});
