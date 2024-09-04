91b8fcfac9bb3aa20b8f6a6bd9580cf450b432b8
hslh <h.haagsma@umcg.nl>
2024-09-04 14:13:39 +0200

    Read server URL as cmd line argument

--- data/scripts/directory_to_flat_model.py
+++ data/scripts/directory_to_flat_model.py
@@ -9,10 +9,11 @@ def main():
     """Main function doing the conversion"""
     # Read password from command line
     parser = argparse.ArgumentParser(description="Command line arguments")
+    parser.add_argument("-url", type = str, required = True, dest="server", help="URL of server to access")
     parser.add_argument("-pw", type=str, dest="password", required=True, help="Password for server access")
     args = parser.parse_args()
     # Connect to server
-    with molgenis_emx2_pyclient.Client(url="https://directory.bbmri-eric.eu") as client:
+    with molgenis_emx2_pyclient.Client(args.server) as client:
         client.signin('admin', args.password)
         # Test stuff
         print(client.schema_names)
