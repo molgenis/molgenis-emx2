d9a0c22f4708751cf99c16ade3edb9ed8245989d
hslh <h.haagsma@umcg.nl>
2024-09-04 13:56:21 +0200

    Connect to BBMRI server

--- data/scripts/directory_to_flat_model.py
+++ data/scripts/directory_to_flat_model.py
@@ -2,6 +2,20 @@
 Script to convert the BBMRI-ERIC directory data to the flat model
 """
 
+import argparse
 import molgenis_emx2_pyclient
 
-print(molgenis_emx2_pyclient.__name__)
+def main():
+    """Main function doing the conversion"""
+    # Read password from command line
+    parser = argparse.ArgumentParser(description="Command line arguments")
+    parser.add_argument("-pw", type=str, dest="password", required=True, help="Password for server access")
+    args = parser.parse_args()
+    # Connect to server
+    with molgenis_emx2_pyclient.Client(url="https://directory.bbmri-eric.eu") as client:
+        client.signin('admin', args.password)
+        # Test stuff
+        print(client.schema_names)
+
+if __name__ == "__main__":
+    main()
