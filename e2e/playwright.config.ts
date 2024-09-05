17796f321de863260a349396d619f4790a617d5c
dcruvolo <davidruvolo51@gmail.com>
2024-09-05 10:14:53 +0200

    set default base url

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.E2E_BASE_URL || "https://preview-emx2-pr-4176.dev.molgenis.org/",
+    baseURL: process.env.E2E_BASE_URL || "https://emx2.dev.molgenis.org/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
