98f0d6b40d65aca46bb710fd89255cbd3c837d87
hslh <h.haagsma@umcg.nl>
2024-09-06 16:11:15 +0200

    Add transformation of Persons table and output to csv

--- data/scripts/directory_to_flat_model.py
+++ data/scripts/directory_to_flat_model.py
@@ -5,18 +5,61 @@ Script to convert the BBMRI-ERIC directory data to the flat model
 import argparse
 import molgenis_emx2_pyclient
 
+
+def map_persons_to_contacts(persons):
+    """Maps the BBRMI-ERIC Persons table to the flat data model's Collection Contacts"""
+    persons.rename(columns={'first_name': 'first name',
+                   'last_name': 'last name', 'role': 'role description'}, inplace=True)
+    # Add dummy values for missing first and last names
+    persons['first name'] = persons['first name'].replace(r'^$', 'FirstName', regex=True)
+    persons['last name'] = persons['last name'].replace(r'^$', 'LastName', regex=True)
+    # Do partial mappings of title_before_name to pre-defined titles
+    persons['title_before_name'] = persons['title_before_name'].str.lower(
+    ).str.replace('[^a-z]', '', regex=True)
+    title_mapping = {'dr': 'dr.', 'profdr': 'prof. dr.', 'prof': 'prof.', 'mwdr': 'dr.',
+                     'profdrmed': 'prof. dr.', 'pddr': 'dr.', 'drrernat': 'dr.', 'drmed': 'dr.'}
+    persons['title'] = persons['title_before_name'].map(title_mapping)
+    # Partial mappings of hand-entered roles to pre-defined roles
+    role_mapping = {'PI': 'Principal Investigator', 'Principal Investigator': 'Principal Investigator',
+                    'Principal Investigators': 'Principal Investigator', 'Principle Investigator': 'Principal Investigator',
+                    'Project coordinator': 'Project manager', 'Project Co-ordinator': 'Project manager'}
+    persons['role'] = persons['role description'].map(role_mapping)
+    # Add everyone to dummy organisation for now
+    persons['collection'] = 'INMA'
+    # Ugly temporary fix to make duplicate keys unique by appending id after last name
+    persons['last name'] = persons['last name'] + ' ' + persons['id']
+    return persons
+
+
 def main():
     """Main function doing the conversion"""
     # Read password from command line
     parser = argparse.ArgumentParser(description="Command line arguments")
-    parser.add_argument("-url", type = str, required = True, dest="server", help="URL of server to access")
-    parser.add_argument("-pw", type=str, dest="password", required=True, help="Password for server access")
+    parser.add_argument("-url", type=str, required=True,
+                        dest="server", help="URL of server to access")
+    parser.add_argument("-pw", type=str, dest="password",
+                        required=False, help="Password for server access")
     args = parser.parse_args()
     # Connect to server
     with molgenis_emx2_pyclient.Client(args.server) as client:
-        client.signin('admin', args.password)
-        # Test stuff
-        print(client.schema_names)
+        if args.password:
+            client.signin('admin', args.password)
+        # Get and transform Persons table first
+        client.set_schema('ERIC')
+        persons = client.get('Persons', as_df=True)
+        with molgenis_emx2_pyclient.Client("https://hessel1.molgenis.net") as cat_client:
+            cat_client.set_schema('catalogue-demo')
+            contacts = cat_client.get('Collection contacts', as_df=True)
+            mapped_contacts = map_persons_to_contacts(
+                persons.copy())  # Unnecessary copy?
+            # Output mapped contacts
+            mapped_contacts = mapped_contacts.reindex(columns=['collection', 'role', 'role description', 'display name', 'first name',
+                                                               'last name', 'prefix', 'initials', 'title', 'organisation', 'email',
+                                                               'orcid', 'homepage', 'photo', 'photo_filename', 'expertise'])
+            # cat_client.save_schema(table = 'Collection contacts', data = mapped_contacts)
+            mapped_contacts.to_csv(
+                './Collection contacts.csv.zip', index=False)
+            # cat_client.upload_file('./Collection contacts.csv')
 
 if __name__ == "__main__":
     main()
