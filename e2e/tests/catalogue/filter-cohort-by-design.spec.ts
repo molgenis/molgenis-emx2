ef109b0fdeead1364badc3d12dff060bad3f6199
connoratrug <c.stroomberg@rug.nl>
2024-09-16 10:45:05 +0200

    clean up test

--- e2e/tests/catalogue/filter-cohort-by-design.spec.ts
+++ e2e/tests/catalogue/filter-cohort-by-design.spec.ts
@@ -1,8 +1,18 @@
 import { test, expect } from '@playwright/test';
 
+test.beforeEach(async ({ context, baseURL }) => {
+  await context.addCookies([
+    {
+      name: "mg_allow_analytics",
+      value: "false",
+      domain: new URL(baseURL as string).hostname,
+      path: "/"
+    }
+  ]);
+});
+
 test('filter cohorts list page by design', async ({ page }) => {
   await page.goto('/catalogue-demo/ssr-catalogue/all/cohorts');
-  await page.getByRole('button', { name: 'Accept' }).click();
   await page.getByRole('heading', { name: 'Design' }).click();
   await page.getByText('Cross-sectional').click();
   await expect(page.getByRole('main')).toContainText('Cross-sectional');
