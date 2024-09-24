4b8281b5dc447180646a2cc75f40b727b47f12a3
hslh <h.haagsma@umcg.nl>
2024-09-24 15:13:57 +0200

    Change 'Collections'-table to 'Resources'

--- data/scripts/directory_to_flat_model.py
+++ data/scripts/directory_to_flat_model.py
@@ -9,7 +9,7 @@ import numpy as np
 
 
 def map_persons_to_contacts(persons):
-    """Maps the BBRMI-ERIC Persons table to the flat data model's Collection Contacts table"""
+    """Maps the BBRMI-ERIC Persons table to the flat data model's Contacts table"""
     persons.rename(columns={'first_name': 'first name',
                    'last_name': 'last name', 'role': 'role description'}, inplace=True)
     # Add dummy values for missing first and last names
@@ -29,7 +29,7 @@ def map_persons_to_contacts(persons):
                     'Project coordinator': 'Project manager', 'Project Co-ordinator': 'Project manager'}
     persons['role'] = persons['role description'].map(role_mapping)
     # Add everyone to dummy organisation for now
-    persons['collection'] = 'INMA'
+    persons['resource'] = 'INMA'
     # Ugly temporary fix to make duplicate keys unique by appending id after last name
     persons['last name'] = persons['last name'] + ' ' + persons['id']
 
@@ -37,10 +37,10 @@ def map_persons_to_contacts(persons):
 
 
 def map_collections_to_collections(collections):
-    """Maps the BBMRI-ERIC Collections table to the flat data model's Collections table"""
+    """Maps the BBMRI-ERIC Collections table to the flat data model's Resources table"""
     # Rename and create columns
     collections.rename(columns={'url': 'website'}, inplace=True)
-    collections['collections'] = ''
+    collections['resources'] = ''
     # Create a unique name
     collections['name'] = collections['name'] + ' ' + collections['biobank'] + \
         ' (' + collections['id'] + ')'
@@ -49,9 +49,9 @@ def map_collections_to_collections(collections):
     for idx, row in collections.iterrows():
         p_c = row['parent_collection']
         if p_c:
-            if collections.loc[p_c, 'collections'] != '':
-                collections.loc[p_c, 'collections'] += ','
-            collections.loc[p_c, 'collections'] += idx
+            if collections.loc[p_c, 'resources'] != '':
+                collections.loc[p_c, 'resources'] += ','
+            collections.loc[p_c, 'resources'] += idx
     return collections
 
 
@@ -74,30 +74,30 @@ def main():
             client.signin('admin', args.source_password)
         client.set_schema('ERIC')
         # Connect to target server
-        with molgenis_emx2_pyclient.Client(args.target_server) as cat_client:
+        with molgenis_emx2_pyclient.Client(args.target_server) as catalogue_client:
             if args.target_password:
-                cat_client.signin('admin', args.target_password)
-            cat_client.set_schema('catalogue-demo-hsl')
+                catalogue_client.signin('admin', args.target_password)
+            catalogue_client.set_schema('catalogue-demo-hsl')
             # Map persons to contacts
             persons = client.get('Persons', as_df=True)
             mapped_contacts = map_persons_to_contacts(
                 persons.copy())  # Unnecessary copy?
-            mapped_contacts = mapped_contacts.reindex(columns=['collection', 'role', 'role description', 'display name', 'first name',
+            mapped_contacts = mapped_contacts.reindex(columns=['resource', 'role', 'role description', 'display name', 'first name',
                                                                'last name', 'prefix', 'initials', 'title', 'organisation', 'email',
                                                                'orcid', 'homepage', 'photo', 'photo_filename', 'expertise'])
-            # Map collections to collections
+            # Map collections to resources
             collections = client.get('Collections', as_df=True)
             mapped_collections = map_collections_to_collections(
                 collections.copy())  # Unnecessary copy?
             mapped_collections = mapped_collections.reindex(
-                columns=['id', 'name', 'acronym', 'description', 'website', 'collections'])
+                columns=['id', 'name', 'acronym', 'description', 'website', 'resources'])
             # Mappings involving multiple tables
-            # TODO: add role for 'contact's in collections (also for 'head's?)
-            # TODO: Link contacts and collections
+            # TODO: add role for 'contact's in resources (also for 'head's?)
+            # TODO: Link contacts and resources
             # Upload mapped tables
-            cat_client.save_schema(
-                table='Collection contacts', data=mapped_contacts)
-            cat_client.save_schema(table='Collections',
+            catalogue_client.save_schema(
+                table='Contacts', data=mapped_contacts)
+            catalogue_client.save_schema(table='Resources',
                                    data=mapped_collections)
 
 
