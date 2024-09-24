4509030021b46e7365bd06e7f6e8d36ff73541de
Joris de Keijser <chinook25@gmail.com>
2024-09-24 14:44:19 +0200

    wip: use useFetch (not working)

--- apps/ui/pages/admin.vue
+++ apps/ui/pages/admin.vue
@@ -2,17 +2,23 @@
   <PageHeader title="Admin Tools" />
   <Container class="flex flex-col items-center">
     <ContentBlock class="w-full mt-3" title="User management">
+      {{ users }}
       <h2>User List</h2>
       <Table>
         <template #head>
           <TableHeadRow>
-            <TableHead>name</TableHead>
-            <TableHead>description</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Roles</TableHead>
+            <TableHead>Tokens</TableHead>
           </TableHeadRow>
         </template>
         <template #body>
+          <TableRow>
+            <TableCell>admin</TableCell>
+            <TableCell>admin.username :)</TableCell>
+          </TableRow>
           <TableRow v-for="user in users">
-            <TableCell>{{ user.userName }}</TableCell>
+            <TableCell>{{ user.email }}</TableCell>
             <TableCell>{{ user }}</TableCell>
           </TableRow>
         </template>
@@ -21,8 +27,9 @@
   </Container>
 </template>
 
-<script lang="ts">
-import { request } from "graphql-request";
+<script setup lang="ts">
+import gql from "graphql-tag";
+
 definePageMeta({
   middleware: "admin-only",
 });
@@ -33,13 +40,27 @@ const userCount = ref(0);
 getUsers();
 
 async function getUsers() {
-  const result: any = await request(
-    "graphql",
-    `{_admin{users{email},userCount}}`
-  ).catch((error) => {
-    console.log("Error loading users: ", error);
+  const { data, error } = await useFetch("central/graphql", {
+    method: "post",
+    body: {
+      query: gql`
+        {
+          _admin {
+            users(limit: 20, offset: 0) {
+              email
+            }
+            userCount
+          }
+        }
+      `,
+    },
   });
-  users.value = result?._admin?.users || [];
-  userCount.value = result?._admin?.userCount ?? 0;
+  if (error) {
+    console.log("Error loading users: ", error);
+  } else {
+    console.log("data ", data.value);
+    users.value = data.value?._admin?.users || [];
+    userCount.value = data.value?._admin?.userCount ?? 0;
+  }
 }
 </script>
