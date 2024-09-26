8f26624e50e375f21e2b55e812ca9a0f1426cb77
mswertz <m.a.swertz@rug.nl>
2024-09-26 10:50:56 +0200

    fix(sql): change insert/update batch size from 100 to 1000

--- backend/molgenis-emx2-io/src/main/java/org/molgenis/emx2/io/ImportTableTask.java
+++ backend/molgenis-emx2-io/src/main/java/org/molgenis/emx2/io/ImportTableTask.java
@@ -186,7 +186,7 @@ public class ImportTableTask extends Task {
         }
         batch.add(row);
         index++;
-        if (batch.size() >= 100) {
+        if (batch.size() >= 1000) {
           table.save(batch);
           task.setProgress(index);
           task.setDescription("Imported " + task.getProgress() + " rows into " + table.getName());
