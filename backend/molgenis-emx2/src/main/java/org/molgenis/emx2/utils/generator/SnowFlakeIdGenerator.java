c1c8e85e7b80a23a87c968508c5795e1faeac4e0
Harm Brugge <harmbrugge@gmail.com>
2024-09-27 16:27:41 +0200

    implemented SnowFlake IdGenerator

--- backend/molgenis-emx2/src/main/java/org/molgenis/emx2/utils/generator/SnowFlakeIdGenerator.java
+++ backend/molgenis-emx2/src/main/java/org/molgenis/emx2/utils/generator/SnowFlakeIdGenerator.java
@@ -10,19 +10,18 @@ public class SnowFlakeIdGenerator implements IdGenerator {
 
   private static final String BASE62_CHARACTERS =
       "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
   private static final long EPOCH = 1727446121186L;
 
   // With base62 encoding 64 bits results in a ~10 character id
   private static final int TOTAL_BITS = 64;
   private static final int TIMESTAMP_BITS = 41; // 69 years of ids
   private static final int SEQUENCE_BITS = 8; // 2^8 (256) ids per ms
-  private static final int HASHED_STRING_BITS =
+  private static final int ID_BITS =
       TOTAL_BITS - TIMESTAMP_BITS - SEQUENCE_BITS; // 15 bits left for tableId ~= 32K unique hashes
 
   private static final long MAX_TIMESTAMP = (1L << TIMESTAMP_BITS) - 1;
   private static final long MAX_SEQUENCE = (1L << SEQUENCE_BITS) - 1;
-  private static final long MAX_HASHED_STRING_ID = (1L << HASHED_STRING_BITS) - 1;
+  private static final long MAX_ID = (1L << ID_BITS) - 1;
 
   // For the schemaId hashing
   private static final long FNV_OFFSET_BASIS = 0xcbf29ce484222325L;
@@ -50,12 +49,11 @@ public class SnowFlakeIdGenerator implements IdGenerator {
     lastTimestamp = currentTimestamp;
 
     // Use BigInteger to avoid overflow
-    BigInteger timestampPart =
-        BigInteger.valueOf(currentTimestamp).shiftLeft(HASHED_STRING_BITS + SEQUENCE_BITS);
-    BigInteger tablePart = BigInteger.valueOf(tableBits).shiftLeft(SEQUENCE_BITS);
+    BigInteger timePart = BigInteger.valueOf(currentTimestamp).shiftLeft(ID_BITS + SEQUENCE_BITS);
+    BigInteger tableIdPart = BigInteger.valueOf(tableBits).shiftLeft(SEQUENCE_BITS);
     BigInteger sequencePart = BigInteger.valueOf(sequence);
 
-    BigInteger snowflakeId = timestampPart.or(tablePart).or(sequencePart);
+    BigInteger snowflakeId = timePart.or(tableIdPart).or(sequencePart);
 
     return base62EncodeSnowflakeId(snowflakeId.longValue());
   }
@@ -91,13 +89,12 @@ public class SnowFlakeIdGenerator implements IdGenerator {
   private long hashStringToBits(String stringId) {
     long hash = FNV_OFFSET_BASIS;
 
-    // Convert the string to bytes and calculate the hash
     byte[] bytes = stringId.getBytes(UTF_8);
     for (byte b : bytes) {
       hash ^= b;
       hash *= FNV_PRIME;
     }
 
-    return hash & MAX_HASHED_STRING_ID;
+    return hash & MAX_ID;
   }
 }
