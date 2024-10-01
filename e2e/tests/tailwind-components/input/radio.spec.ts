f1fd18d2b1d9652e9607fd066ca7c6d84ac7d839
dcruvolo <davidruvolo51@gmail.com>
2024-10-01 08:53:49 +0200

    feat: add e2e test for radio component

--- e2e/tests/tailwind-components/input/radio.spec.ts
+++ e2e/tests/tailwind-components/input/radio.spec.ts
@@ -4,12 +4,8 @@ test(
   "Radio Input: values are properly defined @tw-components @tw-forms @input-radio",
   async ({ page }) => {
     await page.goto('/apps/tailwind-components/#/input/Radio.story');
-    
-    await page.getByLabel('No').check();
-    await expect(page.getByLabel('No')).toHaveValue('No')
-    
-    await page.getByLabel('Yes').check();
-    await expect(page.getByLabel('Yes')).toHaveValue('Yes')
+    await expect(page.getByText('no', { exact: true })).toHaveText('no');
+    await expect(page.getByText('yes')).toHaveText('yes');
   }
 );
 
@@ -17,10 +13,19 @@ test(
   'Radio Input: labels are properly displayed @tw-components @tw-forms @input-radio',
   async ({ page }) => {
     await page.goto('/apps/tailwind-components/#/input/Radio.story');
-    await expect(page.getByText('No', { exact: true })).toHaveText('No');
-    await expect(page.getByText('Yes')).toHaveText('Yes');
+    await expect(page.getByText('Level 1 (A)')).toHaveValue('level-1');
+    await expect(page.getByText('Level 2 (AA)')).toHaveValue('level-2');
+    await expect(page.getByText('Level 3 (AAA)')).toHaveValue('level-3');
   }
 );
+
+test(
+  "Radio Input: default value is set @tw-components @tw-forms @input-radio",
+  async ({ page }) => {
+    await page.goto('/apps/tailwind-components/#/input/Radio.story');
+    await expect(page.getByLabel('Experimental cohort')).toBeChecked({checked: true});
+  }
+)
 
 test(
   'Radio Input: clear button is shown when indicated @tw-components @tw-forms @input-radio',
@@ -34,8 +39,10 @@ test(
   'Radio Input: inputs are properly reset @tw-components @tw-forms @input-radio',
   async ({ page }) => {
     await page.goto('/apps/tailwind-components/#/input/Radio.story');
-    await page.getByLabel("Healthy controls").check();
+    await page.getByText('Healthy controls').click();
     await page.getByRole('button', { name: 'Clear' }).click();
-    await expect(page.getByLabel('Healthy controls')).toBeChecked({checked: false});
+    await expect(page.getByLabel('Healthy controls')).toBeChecked({ checked: false });
+    await expect(page.getByLabel('Experimental cohort')).toBeChecked({ checked: false });
+    await expect(page.getByLabel('Placebo cohort')).toBeChecked({ checked: false }); 
   }
 )
