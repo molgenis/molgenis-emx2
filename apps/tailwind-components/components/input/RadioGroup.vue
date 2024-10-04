224575a37f551592beedeb2e5cc1d76f7cacad5b
dcruvolo <davidruvolo51@gmail.com>
2024-10-04 10:50:35 +0200

    fix: moved icon into label to enable clicks

--- apps/tailwind-components/components/input/RadioGroup.vue
+++ apps/tailwind-components/components/input/RadioGroup.vue
@@ -13,11 +13,14 @@
         v-model="modelValue"
         :checked="option.value === modelValue"
       />
-      <InputRadioIcon :checked="modelValue === option.value" />
       <InputLabel
-        :for="`${id}-radio-group-${option.value}`"
-        class="hover:cursor-pointer"
+      :for="`${id}-radio-group-${option.value}`"
+      class="hover:cursor-pointer flex flex-row gap-1"
       >
+        <InputRadioIcon 
+          :checked="modelValue === option.value"
+          class="mr-2.5"
+        />
         <template v-if="option.label">
           {{ option.label }}
         </template>
