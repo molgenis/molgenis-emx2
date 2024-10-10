44ec74a96c42e04ee02a938ecbe20468c2b703b7
ypezijlstra <info@ypezijlstra.frl>
2024-10-10 11:02:13 +0200

    added check for recent changes in staging area

--- tools/staging_migrator/dev/migrate_UMCG.py
+++ tools/staging_migrator/dev/migrate_UMCG.py
@@ -9,14 +9,17 @@ import os
 import sys
 
 from dotenv import load_dotenv
+from datetime import datetime
 
 from tools.staging_migrator.src.molgenis_emx2_staging_migrator import StagingMigrator
 
 
 CATALOGUE = 'UMCG'
 
+changelog_query = """{_changes(limit: 1) {operation, stamp, userId, tableName}}"""
 
-def main(args):
+
+def main(staging_areas: list = None, args=None):
 
     # Set up the logger
     logging.basicConfig(level='DEBUG')
@@ -27,15 +30,25 @@ def main(args):
     server_url = os.environ.get('MG_URL')
     token = os.environ.get('MG_TOKEN')
 
-    staging_areas = args[-1].split(',')
+    if staging_areas is None:
+        staging_areas = args[-1].split(',')
 
-    with StagingMigrator(url=server_url, token=token, catalogue=CATALOGUE) as migrator:
+    with StagingMigrator(url=server_url, token=token, catalogue=CATALOGUE, table='Resources') as migrator:
 
         for sa in staging_areas:
             migrator.set_staging_area(sa)
+            response = migrator.session.post(url=f"{migrator.url}/{sa}/settings/graphql",
+                                             json={"query": changelog_query}, headers=migrator.session.headers)
+            changelog = response.json().get('data').get('_changes')
+            change_datetime = changelog[0].get('stamp')
+            change_datetime = datetime.strptime(change_datetime, '%Y-%m-%d %H:%M:%S.%f')
 
-            migrator.migrate()
+            if (datetime.now() - change_datetime).total_seconds() < 23 * 60 * 60:
+                migrator.migrate(keep_zips=True)
+            else:
+                logging.info(f"Skipping {sa}.")
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    cohorts = 'MLPREDNEC,JOZO,TRANSPLANTLINES'.split(',')
+    main(staging_areas=cohorts, args=sys.argv)
