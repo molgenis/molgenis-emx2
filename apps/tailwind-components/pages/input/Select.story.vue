905858907bf3052f68184d5e3d6108e45e2d31cc
dcruvolo <davidruvolo51@gmail.com>
2024-10-11 10:54:39 +0200

    initial revisions of the select component

--- apps/tailwind-components/pages/input/Select.story.vue
+++ apps/tailwind-components/pages/input/Select.story.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-const id = "story-input-1";
-const hasError = ref(false);
-const required = ref(false);
-const fieldValue = ref("");
+const hasError = ref<boolean>(false);
+const required = ref<boolean>(false);
+const fieldValue = ref<string>("");
 
-const options = ["option 1", "option 2", "option 3", "option 4"];
+const exampleData = [
+  { value: "level-1", label: "Level 1 (A)" },
+  { value: "level-2", label: "Level 2 (AA)" },
+  { value: "level-3", label: "Level 3 (AAA)" },
+];
 
 function clearValue() {
   fieldValue.value = "";
@@ -12,56 +15,56 @@ function clearValue() {
 </script>
 
 <template>
-  <div class="flex">
-    <div class="m-4 flex-3">
-      <div>
-        <InputSelect
-          :id="id"
-          :options="options"
-          v-model="fieldValue"
-          :has-error="hasError"
-          :required="required"
-        />
-      </div>
-    </div>
+  <div class="flex gap-5">
+    <form class="grow">
+      <legend class="text-title">Select the level of security required</legend>
+      <InputSelect
+        id="example-level-input"
+        :selectOptions="exampleData"
+        v-model="fieldValue"
+        :has-error="hasError"
+        :required="required"
+      />
+    </form>
 
-    <div class="m-4 flex-1">
-      <p>fieldValue value: {{ fieldValue }}</p>
-
-      <div class="mt-4">
-        <button
-          @click="clearValue"
-          class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
-        >
-          clear value
-        </button>
-      </div>
-
-      <fieldset class="mt-4 border border-gray-900 mb-2">
-        <legend class="m-2 px-2">Props</legend>
-        <div class="mb-2">
-          <label class="ml-1 hover:cursor-pointer" for="input-select-required">
-            required
-          </label>
+    <form class="m-4 flex-1 flex flex-col">
+      <legend class="text-title">Configure the select component</legend>
+      <fieldset class="my-2 [&_div]:my-1">
+        <legend class="text-title">Adjust props</legend>
+        <div>
           <input
             id="input-select-required"
-            class="ml-2 hover:cursor-pointer"
+            class="hover:cursor-pointer"
             type="checkbox"
             v-model="required"
           />
-        </div>
-        <div class="mb-2">
-          <label class="ml-1 hover:cursor-pointer" for="input-select-has-error">
-            hasError
+          <label class="hover:cursor-pointer" for="input-select-required">
+            required
           </label>
+        </div>
+        <div>
           <input
             id="input-select-has-error"
-            class="ml-2 hover:cursor-pointer"
+            class="hover:cursor-pointer"
             type="checkbox"
             v-model="hasError"
           />
+          <label class="ml-1 hover:cursor-pointer" for="input-select-has-error">
+            hasError
+          </label>
         </div>
       </fieldset>
-    </div>
+      <h3 class="text-title">Output</h3>
+      <output class="my-2 border border-gray-900 py-2 pl-2">
+        Selection: {{ fieldValue }}
+      </output>
+      <Button
+        type="outline"
+        size="small"
+        class="mt-4 mr-auto"
+        label="Clear value"
+        @click.prevent="clearValue"
+      />
+    </form>
   </div>
 </template>
