85f58d186c5d0d1d16ff7b833d6a94ee1795fe5c
connoratrug <c.stroomberg@rug.nl>
2024-10-11 16:10:37 +0200

    try fix with absolute (just to store the idea)

--- apps/tailwind-components/components/table/cellTypes/String.vue
+++ apps/tailwind-components/components/table/cellTypes/String.vue
@@ -10,18 +10,30 @@ const divRef = useTemplateRef("overflow-ref");
 const isOverFlow = computed(() => {
   return (divRef.value?.scrollWidth ?? 0) > (divRef.value?.offsetWidth ?? 0);
 });
+
+function onMoreClick() {
+  alert("More clicked");
+}
 </script>
 
 <template>
   <td
     class="py-2.5 px-2.5 border-b border-gray-200 first:pl-0 last:pr-0 sm:first:pl-2.5 sm:last:pr-2.5 h-4"
   >
-    <div
-      ref="overflow-ref"
-      class="overflow-ellipsis whitespace-nowrap overflow-hidden"
-    >
-      {{ data }}
+    <div style="position: relative; padding-right: 40px">
+      <div
+        ref="overflow-ref"
+        class="overflow-ellipsis whitespace-nowrap overflow-hidden"
+      >
+        {{ data }}
+      </div>
+      <span
+        v-if="isOverFlow"
+        @click="onMoreClick"
+        style="position: absolute; right: 0px; top: 0px"
+        class="text-blue-500 underline"
+        >More</span
+      >
     </div>
-    <span class="text-blue-500 underline">{{ isOverFlow ? "More" : "" }}</span>
   </td>
 </template>
