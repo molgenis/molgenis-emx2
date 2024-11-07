5dda8a793c362ebc51b5b673fbd0c7a559cf1ff3
connoratrug <c.stroomberg@rug.nl>
2024-11-07 12:15:31 +0100

    fix: pick render for ontology vaue
    depending on the actual value of the ontology field pick the correct dom render
    - case single item, render as item
    - case list ( no depth ), render as list
    - case tree, render as a tree

--- apps/nuxt3-ssr/components/content/TreeNode.vue
+++ apps/nuxt3-ssr/components/content/TreeNode.vue
@@ -28,7 +28,7 @@ const toggleCollapse = () => {
         v-if="node.children?.length"
         @click="toggleCollapse()"
         class="text-blue-500 mr-1 mt-0.5 rounded-full hover:bg-blue-50 hover:cursor-pointer p-0.5"
-        :class="{ 'rotate-180': collapsed }"
+        :class="{ 'rotate-180': collapsed, 'ml-[-0.5rem]': isRootNode }"
       >
         <BaseIcon name="caret-up" :width="20" />
       </span>
