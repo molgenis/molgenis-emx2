e177dcdfb889ab4ac99bd7ce4fa88cf03eadd0f0
BrendaHijmans <42644481+BrendaHijmans@users.noreply.github.com>
2024-11-18 10:00:35 +0100

    Update DataModels.java

--- backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/DataModels.java
+++ backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/DataModels.java
@@ -13,7 +13,7 @@ public class DataModels {
     DATA_CATALOGUE_NETWORK_STAGING("_profiles/NetworksStaging.yaml"),
     DATA_CATALOGUE_AGGREGATES("_profiles/DataCatalogueAggregates.yaml"),
     DATA_CATALOGUE("_profiles/DataCatalogue.yaml"),
-    UMCU_COHORTS_STAGING("_profiles/UMCUCohortsStaging.yaml"),
+    UMCU_COHORTS_STAGING("_profiles/UMCUCohorts.yaml"),
     FAIR_DATA_HUB("_profiles/FAIRDataHub.yaml"),
     RD3("_profiles/RD3.yaml"),
     JRC_COMMON_DATA_ELEMENTS("_profiles/JRC-CDE.yaml"),
