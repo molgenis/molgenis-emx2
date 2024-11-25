0e31872c699697ccf2da7666ca390650dce8bdfb
dcruvolo <davidruvolo51@gmail.com>
2024-11-25 15:38:54 +0000

    feat: added function for flattening datasets

--- apps/cranio-provider/src/utils/index.ts
+++ apps/cranio-provider/src/utils/index.ts
@@ -1,3 +1,5 @@
+import type { IKeyValuePair } from "../interfaces/index";
+
 /**
  * @name uniqueValues
  * @description Return an array of unique values from a specific property in a dataset
@@ -17,4 +19,25 @@
 export function uniqueValues(data: any, key: string): string[] {
   const values = data.map((row: Record<string, any>) => row[key]);
   return Array.from(new Set(values)).sort() as string[];
+}
+
+/**
+ * @name asKeyValuePairs
+ * @description Convert an array of objects to an object of key-values
+ * 
+ * @param data array of objects
+ * @param key name of the column containing the values that will be used as keys
+ * @param value name of the column containing the values to map to the new keys
+ * 
+ * @returns object of key-value pairs
+ */
+export function asKeyValuePairs(
+  data: any,
+  key: string,
+  value: string
+): IKeyValuePair {
+  const values = data.map((row: Record<string, any>) => {
+    return [row[key], row[value]];
+  });
+  return Object.fromEntries(values);
 }
