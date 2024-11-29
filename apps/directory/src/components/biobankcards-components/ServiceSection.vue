8bee98ec81b02dc28b98354f4e335b73ef6c4f23
connoratrug <c.stroomberg@rug.nl>
2024-11-29 17:07:54 +0100

    feat: select all services in biobank card
    - add select all
    - todo fix styling

--- apps/directory/src/components/biobankcards-components/ServiceSection.vue
+++ apps/directory/src/components/biobankcards-components/ServiceSection.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { IServices } from "../../interfaces/directory";
 import CardItem from "../CardItem.vue";
+import CheckBox from "../CheckBox.vue";
 import Service from "./Service.vue";
 
-withDefaults(
+const props = withDefaults(
   defineProps<{
     services?: IServices[];
     selectedServices?: string[];
@@ -23,12 +25,39 @@ function addService(serviceId: string) {
 function removeService(serviceId: string) {
   emit("update:removeServices", [serviceId]);
 }
+
+const isAllSelected = computed(
+  () =>
+    props.services.length > 0 &&
+    props.services.length === props.selectedServices.length
+);
+
+function handleSelectAll(isChecked: boolean) {
+  if (isChecked) {
+    emit(
+      "update:addServices",
+      props.services.map((service) => service.id)
+    );
+  } else {
+    emit(
+      "update:removeServices",
+      props.services.map((service) => service.id)
+    );
+  }
+}
 </script>
 
 <template>
   <div>
     <div v-if="services.length > 1" class="pl-2 pt-2 d-flex">
       <h6>{{ services.length }} services available</h6>
+      <div class="ml-auto">
+        <CheckBox
+          id="all"
+          :is-checked="isAllSelected"
+          @change="handleSelectAll"
+        ></CheckBox>
+      </div>
     </div>
 
     <CardItem v-for="(service, index) in services">
