2654d72869b3f4ac9e5192c1cf92f2401c6a59bd
connoratrug <c.stroomberg@rug.nl>
2024-12-05 15:06:46 +0100

    chore: better directory e2e local dev setup
    - make it easier to run agains local dev sever
    - set local dev server as default

--- apps/directory/tests/view-biobank-services.spec.ts
+++ apps/directory/tests/view-biobank-services.spec.ts
@@ -1,16 +1,14 @@
 import { test, expect } from '@playwright/test';
-import { getSchemaName } from './getSchemaName';
-
-const schemaName = getSchemaName();
+import { getAppRoute } from './getAppRoute';
 
 test("should show a biobank card with a service tab", async ({ page }) => {
-  await page.goto(`/${schemaName}/directory/`);
+  await page.goto(getAppRoute());
   await expect(page.getByRole('main')).toContainText('Biobank1');
   await expect(page.getByRole('button', { name: 'Services' }).first()).toBeVisible();
 });
 
 test("selecting the service tab should show the servie details", async ({ page }) => {
-    await page.goto(`/${schemaName}/directory/`);
+    await page.goto(getAppRoute());
     await page.getByRole('button', { name: 'Services' }).first().click();
     await expect(page.getByRole('main')).toContainText('Biobank Service');
     await expect(page.getByText('Sample Storage, Microbiome')).toBeVisible();
