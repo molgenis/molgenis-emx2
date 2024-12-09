06a0d0b94b5509485656a4a27246d48ebcec44a1
Harm Brugge <harmbrugge@gmail.com>
2024-12-09 16:13:27 +0100

    template form

--- apps/central/src/components/admin/ManageTemplates.vue
+++ apps/central/src/components/admin/ManageTemplates.vue
@@ -1,24 +1,62 @@
 <template>
   <div>Manage scripts</div>
-  <RoutedTableExplorer
-    v-if="session"
+  <InputSelect :options="schemas"></InputSelect>
+  <TableExplorer
     tableId="Templates"
     schemaId="_SYSTEM_"
     :canEdit="true"
     :canManage="false"
   >
-  </RoutedTableExplorer>
+    <template v-slot:rowheader="slotProps">
+      <TemplateEditButton :taskId="slotProps.row.id" />
+    </template>
+  </TableExplorer>
 </template>
 
 <script>
-import { RoutedTableExplorer } from "molgenis-components";
+import { TableExplorer, InputSelect } from "molgenis-components";
+import TemplateEditButton from "./TemplateEditButton.vue";
+import { request } from "graphql-request";
 
 export default {
   components: {
-    RoutedTableExplorer,
+    TemplateEditButton,
+    TableExplorer,
+    InputSelect,
   },
   props: {
     session: Object,
+  },
+  data: function () {
+    return {
+      schemas: [],
+      loading: false,
+      graphqlError: null,
+    };
+  },
+  created() {
+    this.getSchemaList();
+  },
+  methods: {
+    getSchemaList() {
+      this.loading = true;
+      const schemaFragment = "_schemas{id,label,description}";
+      const lastUpdateFragment =
+        "_lastUpdate{schemaName, tableName, stamp, userId, operation}";
+      request(
+        "graphql",
+        `{${schemaFragment} ${this.showChangeColumn ? lastUpdateFragment : ""}}`
+      )
+        .then((data) => {
+          this.schemas = data._schemas;
+          this.loading = false;
+        })
+        .catch((error) => {
+          console.error("internal server error", error);
+          this.graphqlError = "internal server error" + error;
+          this.loading = false;
+        });
+    },
   },
 };
 </script>
