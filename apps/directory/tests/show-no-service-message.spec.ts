5419c654efa97c20a5d81fbdc77922c3194bb602
connoratrug <47183404+connoratrug@users.noreply.github.com>
2024-12-12 10:00:14 +0100

    Update show-no-service-message.spec.ts

--- apps/directory/tests/show-no-service-message.spec.ts
+++ apps/directory/tests/show-no-service-message.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { getAppRoute } from "./getAppRoute";
 
-test("show message if biobanik has no services", async ({ page }) => {
+test("show message if biobank has no services", async ({ page }) => {
   await page.goto(getAppRoute());
   await page.getByRole("button", { name: "Services" }).nth(2).click();
   await expect(page.getByRole("main")).toContainText(
