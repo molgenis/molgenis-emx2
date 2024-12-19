b71518ad736085bf227e2eea7ca9221880fb815f
Joris de Keijser <chinook25@gmail.com>
2024-12-19 10:36:35 +0100

    chore: add e2e tests

---
+++ apps/directory/tests/test-error-state.spec.ts
@@ -0,0 +1,42 @@
+import test, { expect } from "@playwright/test";
+import { getAppRoute } from "./getAppRoute";
+
+test.afterAll(async ({ page }) => {
+  await page.close();
+});
+
+test("should show an error when navigating to an unknown biobank", async ({
+  page,
+}) => {
+  await page.goto(getAppRoute() + "#/biobank/nonexistentBiobank");
+  await expect(page.getByText("Biobank not found")).toContainText(
+    "Biobank not found"
+  );
+});
+
+test("should show an error when navigating to an unknown collection", async ({
+  page,
+}) => {
+  await page.goto(getAppRoute() + "#/collection/nonexistentCollection");
+  await expect(page.getByText("Collection not found")).toContainText(
+    "Collection not found"
+  );
+});
+
+test("should show an error when navigating to an unknown study", async ({
+  page,
+}) => {
+  await page.goto(getAppRoute() + "#/study/nonexistentStudy");
+  await expect(page.getByText("Study not found")).toContainText(
+    "Study not found"
+  );
+});
+
+test("should show an error when navigating to an unknown service", async ({
+  page,
+}) => {
+  await page.goto(getAppRoute() + "#/service/nonexistentService");
+  await expect(page.getByText("Service not found")).toContainText(
+    "Service not found"
+  );
+});
