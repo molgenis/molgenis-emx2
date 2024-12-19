dc903c1085b7af612e7693fa38533ec63954f0e8
Joris de Keijser <chinook25@gmail.com>
2024-12-19 11:18:12 +0100

    simplify tests

--- apps/directory/tests/test-error-state.spec.ts
+++ apps/directory/tests/test-error-state.spec.ts
@@ -5,34 +5,26 @@ test("should show an error when navigating to an unknown biobank", async ({
   page,
 }) => {
   await page.goto(getAppRoute() + "#/biobank/nonexistentBiobank");
-  await expect(page.getByText("Biobank not found")).toContainText(
-    "Biobank not found"
-  );
+  await expect(page.getByText("Biobank not found")).toBeDefined();
 });
 
 test("should show an error when navigating to an unknown collection", async ({
   page,
 }) => {
   await page.goto(getAppRoute() + "#/collection/nonexistentCollection");
-  await expect(page.getByText("Collection not found")).toContainText(
-    "Collection not found"
-  );
+  await expect(page.getByText("Collection not found")).toBeDefined();
 });
 
 test("should show an error when navigating to an unknown study", async ({
   page,
 }) => {
   await page.goto(getAppRoute() + "#/study/nonexistentStudy");
-  await expect(page.getByText("Study not found")).toContainText(
-    "Study not found"
-  );
+  await expect(page.getByText("Study not found")).toBeDefined();
 });
 
 test("should show an error when navigating to an unknown service", async ({
   page,
 }) => {
   await page.goto(getAppRoute() + "#/service/nonexistentService");
-  await expect(page.getByText("Service not found")).toContainText(
-    "Service not found"
-  );
+  await expect(page.getByText("Service not found")).toBeDefined();
 });
