84e9a916c1cc22296b1b2fd88c00def43595350c
mswertz <m.a.swertz@rug.nl>
2024-12-30 17:13:43 +0100

    feat: first version of RefArrayInput

---
+++ apps/tailwind-components/pages/input/RefArray.story.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const schemaId = ref("pet store");
+const tableId = ref("Pet");
+const labelTemplate = ref("${name}");
+const value = ref([{ name: "spike" }]);
+</script>
+
+<template>
+  <div class="flex">
+    <div class="m-4 flex-3">
+      <div>
+        <InputRefArray
+          id="story-ref"
+          v-model="value"
+          :schemaId="schemaId"
+          :tableId="tableId"
+          :labelTemplate="labelTemplate"
+        />
+        {{ value }}
+      </div>
+    </div>
+  </div>
+</template>
