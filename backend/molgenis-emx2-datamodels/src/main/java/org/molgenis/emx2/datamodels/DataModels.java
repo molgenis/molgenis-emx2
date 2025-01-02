8842836dc60b3fd9f180c2673512df401337303a
connoratrug <c.stroomberg@rug.nl>
2025-01-02 09:36:41 +0100

    fix test, keep non profile loader

--- backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/DataModels.java
+++ backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/DataModels.java
@@ -49,6 +49,7 @@ public class DataModels {
 
   public enum Regular {
     DIRECTORY(DirectoryLoader::new),
+    PET_STORE(PetStoreLoader::new),
     ERN_DASHBOARD(DashboardLoader::new),
     PROJECTMANAGER(ProjectManagerLoader::new),
     BIOBANK_DIRECTORY(BiobankDirectoryLoader::new),
