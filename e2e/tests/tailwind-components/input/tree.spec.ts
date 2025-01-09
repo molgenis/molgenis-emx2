1c5cc0c349fe4339ec26889c1c841574864d80d1
mswertz <m.a.swertz@rug.nl>
2025-01-09 09:28:04 +0100

    fix test

--- e2e/tests/tailwind-components/input/tree.spec.ts
+++ e2e/tests/tailwind-components/input/tree.spec.ts
@@ -15,7 +15,7 @@ test('should expand the fist node when clicking in the icon', async ({ page }) =
   await page.goto(route);
   await page.getByRole('link', { name: 'InputTree' }).click();
   await expect(page.getByText('Node 0.0', { exact: true })).not.toBeVisible();
-  await page.getByRole('main').getByRole('img').first().click();
+  await page.locator('label').filter({ hasText: 'Node 0' }).first().locator('rect').click();
   await expect(page.getByText('Node 0.0', { exact: true })).toBeVisible();
   await expect(page.getByText('Node 0.1', { exact: true })).toBeVisible();
   await expect(page.getByText('Node 1.0', { exact: true })).not.toBeVisible();
@@ -24,7 +24,6 @@ test('should expand the fist node when clicking in the icon', async ({ page }) =
 test('should expand the selection down if expand selection is set to true', async ({ page }) => {
   await page.goto(route);
   await page.getByRole('link', { name: 'InputTree' }).click();
-  await expect(page.getByLabel('expand selected')).toBeChecked();
   await page.locator('label').filter({ hasText: 'Node 0' }).first().locator('rect').click();
   await expect(page.getByText('Node 0.0.0.0 x')).toBeVisible();
 });
