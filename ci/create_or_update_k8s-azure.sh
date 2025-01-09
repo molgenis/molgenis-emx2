32ad7a309f21b91c0f24105f973130c345679109
connoratrug <47183404+connoratrug@users.noreply.github.com>
2025-01-09 11:04:10 +0100

    Update create_or_update_k8s-azure.sh

--- ci/create_or_update_k8s-azure.sh
+++ ci/create_or_update_k8s-azure.sh
@@ -46,7 +46,7 @@ helm upgrade --install ${NAME} ./helm-chart --namespace ${NAME} \
 --set oidc.enabled=false \
 --set oidc.clientId="emx2-previews" \
 --set oidc.discovery_url="https://auth1.molgenis.net/realms/Molgenis/.well-known/openid-configuration" \
---set oidc.clientSecret=$OIDC_SECRET
+--set oidc.clientSecret=${OIDC_SECRET}
 
 rm /tmp/cert_key
 rm /tmp/cert_pem
