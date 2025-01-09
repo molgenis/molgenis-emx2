b0b1163f92cab0a9e234ecfa70a74325c96506e2
Postema Max <max@elynor.com>
2025-01-09 12:46:22 +0100

    fix(e2e): also fixed broken forms e2e tests

--- e2e/tests/tailwind-components/form/renderForm.spec.ts
+++ e2e/tests/tailwind-components/form/renderForm.spec.ts
@@ -6,12 +6,12 @@ const route = playwrightConfig?.use?.baseURL?.startsWith('http://localhost') ? '
 test("it should render the form", async ({ page }) => {
   await page.goto(`${route}Form.story`);
   await expect(page.getByText('nameRequiredthe name')).toBeVisible();
-  await expect(page.locator('div').filter({ hasText: /^statusRequired$/ }).nth(1)).toBeVisible();
+  await expect(page.getByText('WeightRequired')).toBeVisible();
 });
 
 test("it should update the model value when a field is filled out", async ({ page }) => {
   await page.goto(`${route}Form.story`);
   await page.getByLabel('name').click();
   await page.getByLabel('name').fill('test');
-  await expect(page.getByRole('main')).toContainText('dataMap: { "name": "test", "category": "", "photoUrls": "", "details": "", "status": "", "tags": "", "weight": "", "orders": "", "mg_draft": "", "mg_insertedBy": "", "mg_insertedOn": "", "mg_updatedBy": "", "mg_updatedOn": "" } errorMap: { "name": [], "category": [], "photoUrls": [], "details": [], "status": [], "tags": [], "weight": [], "orders": [], "mg_draft": [], "mg_insertedBy": [], "mg_insertedOn": [], "mg_updatedBy": [], "mg_updatedOn": [] }');
+  await expect(page.getByRole('main')).toContainText('dataMap:  { "bool": "", "boolarray": "", "date": "", "name": "test", "category": "", "photoUrls": "", "details": "", "tags": "", "weight": "", "heading2": "", "orders": "", "autoid": "", "mg_draft": "", "mg_insertedBy": "", "mg_insertedOn": "", "mg_updatedBy": "", "mg_updatedOn": "" }');
 });
