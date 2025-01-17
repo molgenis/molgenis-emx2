c4cd3795cbea6691439fd1a251f9e43d154591c2
dcruvolo <davidruvolo51@gmail.com>
2025-01-17 15:37:13 +0100

    init new dashboard data model

---
+++ backend/molgenis-emx2-datamodels/src/main/java/org/molgenis/emx2/datamodels/UiDashboardLoader.java
@@ -0,0 +1,33 @@
+package org.molgenis.emx2.datamodels;
+
+import org.molgenis.emx2.MolgenisException;
+import org.molgenis.emx2.Privileges;
+import org.molgenis.emx2.Schema;
+import org.molgenis.emx2.io.ImportDataModelTask;
+import org.molgenis.emx2.io.MolgenisIO;
+import org.molgenis.emx2.sql.SqlDatabase;
+
+public class UiDashboardLoader extends ImportDataModelTask {
+
+  public UiDashboardLoader(Schema schema, Boolean includeDemoData) {
+    super(schema, includeDemoData);
+  }
+
+  @Override
+  public void run() {
+    this.start();
+    try {
+      createSchema(getSchema(), "ui_dashboards/molgenis.csv");
+      getSchema().addMember(SqlDatabase.ANONYMOUS, Privileges.VIEWER.toString());
+      MolgenisIO.fromClasspathDirectory("ui_dashboards/ontologies", getSchema(), false);
+
+      if (isIncludeDemoData()) {
+        MolgenisIO.fromClasspathDirectory("ui_dashboards/demodata", getSchema(), false);
+      }
+      this.complete();
+    } catch (Exception e) {
+      this.completeWithError(e.getMessage());
+      throw new MolgenisException("Create profile failed", e);
+    }
+  }
+}
