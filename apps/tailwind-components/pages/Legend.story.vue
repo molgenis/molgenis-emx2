f2886c5b391f9b5e780378129194936616e06f16
connoratrug <c.stroomberg@rug.nl>
2025-01-23 11:07:35 +0100

    capitalize

--- apps/tailwind-components/pages/Legend.story.vue
+++ apps/tailwind-components/pages/Legend.story.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-row">
     <div class="basis-1/2 bg-sidebar-gradient">
-      <FormLegend :sections="sections" @go-to-section="handleGotoRequest" />
+      <FormLegend :sections="sections" @go-to-section="handleGoToRequest" />
     </div>
     <div class="basis-1/2 text-title p-4">
       <h3>Active section: {{ sections.find((s) => s.isActive) }}</h3>
@@ -12,7 +12,7 @@
 <script lang="ts" setup>
 import type { IFormLegendSection } from "../../metadata-utils/src/types";
 
-function handleGotoRequest(section: IFormLegendSection) {
+function handleGoToRequest(section: IFormLegendSection) {
   sections.value.forEach((s) => {
     if (s.domId === section.domId) {
       s.isActive = true;
