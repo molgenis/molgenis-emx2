0c3ec8a2bce2fa622080d8075a5563288262d3cd
Joris de Keijser <chinook25@gmail.com>
2025-01-27 10:23:16 +0100

    chore: add e2e test

---
+++ apps/tailwind-components/tests/components/form/inputHyperlink.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from "@playwright/test";
+import playwrightConfig from "../../../playwright.config";
+const route = playwrightConfig?.use?.baseURL?.startsWith("http://localhost")
+  ? ""
+  : "/apps/tailwind-components/#/";
+
+test("the inputHyperLink", async ({ page }) => {
+  await page.goto(`${route}/input/Hyperlink.story`);
+  await page.getByRole("button", { name: "focus" }).click({ delay: 500 }); // wait for hydration to complete
+  await expect(page.getByText("Error:")).not.toContainText(
+    "Error: Invalid hyperlink"
+  );
+  await page.fill("#input-hyperlink", "blaat");
+  await expect(page.getByText("Error: Invalid hyperlink")).toContainText(
+    "Error: Invalid hyperlink"
+  );
+  await page.getByPlaceholder("https://example.com").clear();
+  await page.fill("#input-hyperlink", "https://molgenis.net");
+  await expect(page.getByText("Error:")).not.toContainText(
+    "Error: Invalid hyperlink"
+  );
+});
