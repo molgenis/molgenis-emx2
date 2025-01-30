936dbf9d02fd7fd71a17a552e061fbdff64d3587
mswertz <m.a.swertz@rug.nl>
2025-01-30 09:26:29 +0100

    remove aspecific test

--- e2e/tests/tailwind-components/form/renderForm.spec.ts
+++ e2e/tests/tailwind-components/form/renderForm.spec.ts
@@ -13,7 +13,8 @@ test("it should update the model value when a field is filled out", async ({ pag
   await page.goto(`${route}Form.story`);
   await page.getByLabel('name').click();
   await page.getByLabel('name').fill('test');
-  await expect(page.getByRole('main')).toContainText('dataMap: { "name": "test", "category": "", "photoUrls": "", "details": "", "status": "", "tags": "", "weight": "", "orders": "", "mg_draft": "", "mg_insertedBy": "", "mg_insertedOn": "", "mg_updatedBy": "", "mg_updatedOn": "" } errorMap: { "name": [], "category": [], "photoUrls": [], "details": [], "status": [], "tags": [], "weight": [], "orders": [], "mg_draft": [], "mg_insertedBy": [], "mg_insertedOn": [], "mg_updatedBy": [], "mg_updatedOn": [] }');
+  //this test will be flakey, replace by specific tests?
+  //await expect(page.getByRole('main')).toContainText('dataMap: { "name": "test", "category": "", "photoUrls": "", "details": "", "status": "", "tags": "", "weight": "", "orders": "", "mg_draft": "", "mg_insertedBy": "", "mg_insertedOn": "", "mg_updatedBy": "", "mg_updatedOn": "" } errorMap: { "name": [], "category": [], "photoUrls": [], "details": [], "status": [], "tags": [], "weight": [], "orders": [], "mg_draft": [], "mg_insertedBy": [], "mg_insertedOn": [], "mg_updatedBy": [], "mg_updatedOn": [] }');
   await page.locator('label').filter({ hasText: 'dog' }).getByRole('img').click();
   await expect(page.getByRole('button', { name: 'dog' })).toBeVisible();
 });
