52907152cc321338d1945c28f7482559cc5e0e86
connoratrug <c.stroomberg@rug.nl>
2025-01-31 15:59:14 +0100

    chore: allow selection of form demo schema and table
    - select via ui
    - select via query params

--- apps/tailwind-components/tests/components/form/renderForm.spec.ts
+++ apps/tailwind-components/tests/components/form/renderForm.spec.ts
@@ -6,34 +6,34 @@ const route = playwrightConfig?.use?.baseURL?.startsWith("http://localhost")
   : "/apps/tailwind-components/#/";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto(`${route}Form.story`);
-  await page.getByLabel("name").click({ delay: 500 }); // wait for hydration to complete
+  await page.goto(`${route}Form.story?schema=catalogue&table=Resources`);
+  await page.getByText("Jump to", { exact: true }).click({ delay: 300 });
 });
 
 test("it should render the form", async ({ page }) => {
-  await expect(page.getByRole("main")).toContainText("name");
-  await expect(page.getByRole("main")).toContainText("the name");
-  await expect(page.getByRole("main")).toContainText("date");
-  await expect(page.getByLabel("name")).toBeVisible();
+  await expect(page.getByRole("main")).toContainText("id");
+  await expect(page.getByRole("main")).toContainText("pid");
+  await expect(page.getByRole("main")).toContainText("Name");
+  await expect(page.getByLabel("id", { exact: true })).toBeVisible();
 });
 
 test("it should handle input", async ({ page }) => {
-  await page.getByLabel("name").pressSequentially("test");
-  await expect(page.getByLabel("name")).toHaveValue("test");
+  await page.getByLabel("name", { exact: true }).pressSequentially("test");
+  await expect(page.getByLabel("name", { exact: true })).toHaveValue("test");
 });
 
 test("it should show the chapters in the legend", async ({ page }) => {
   await expect(
-    page.locator("span").filter({ hasText: "details" })
+    page.locator("span").filter({ hasText: "Overview" })
   ).toBeVisible();
-  await expect(page.getByText("Heading2", { exact: true })).toBeVisible();
+  await expect(
+    page.getByText("design and structure", { exact: true })
+  ).toBeVisible();
 });
 
 test("the legend should show number of errors per chapter (if any)", async ({
   page,
 }) => {
-  await page.getByLabel("Demo data").selectOption("complex", { force: true });
-  // touch the form
   await page.getByLabel("name", { exact: true }).click();
   // skip a required field
   await page.getByLabel("name", { exact: true }).press("Tab");
@@ -43,6 +43,6 @@ test("the legend should show number of errors per chapter (if any)", async ({
 test("clicking on the chapter should scroll to the chapter", async ({
   page,
 }) => {
-  await page.getByText("Heading2", { exact: true }).click();
-  await expect(page.getByRole("heading", { name: "heading2" })).toBeVisible();
+  await page.getByText("population", { exact: true }).first().click();
+  await expect(page.getByRole("heading", { name: "population" })).toBeVisible();
 });
