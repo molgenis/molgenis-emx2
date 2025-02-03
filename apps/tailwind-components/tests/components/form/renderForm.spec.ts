17066513c07be06341463da4381f18bbf15babb0
connoratrug <c.stroomberg@rug.nl>
2025-02-03 10:26:19 +0100

    update test

--- apps/tailwind-components/tests/components/form/renderForm.spec.ts
+++ apps/tailwind-components/tests/components/form/renderForm.spec.ts
@@ -60,7 +60,6 @@ test("it should update the model value when a field is filled out", async ({
 test("should also work for refs", async ({ page }) => {
   await page.goto(`${route}Form.story?schema=pet+store&table=Pet`);
   await page.getByText("Jump to", { exact: true }).click({ delay: 300 });
-  await expect(page.getByText("cat", { exact: true })).toBeVisible();
   await page.getByText("dog").click();
   await expect(
     page.locator("#category-radio-group").getByText("dog")
