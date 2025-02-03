6f8aa2c6f0219e789ba5ef2969872f1b7e6c7e7a
dtroelofsprins <57137088+dtroelofsprins@users.noreply.github.com>
2025-02-03 12:51:14 +0100

    fix: documents visible in SKIN and GENTURIS dashboards (#4655)
    * fix: documents visible in SKIN and GENTURIS dashboards
    
    * fix: missing address on GENTURIS contact page
    
    * fix: also fix documents for ithaca and reconnet
    
    * fix: added missing styles
    
    * fix: change texts
    
    * fix: change texts
    
    ---------
    
    Co-authored-by: dcruvolo <davidruvolo51@gmail.com>

--- apps/ern-skin/src/views/view-documents.vue
+++ apps/ern-skin/src/views/view-documents.vue
@@ -14,17 +14,15 @@
       aria-labelledby="section-documents-title"
       :verticalPadding="2"
     >
-      <h2 id="section-documents-title">Documents</h2>
+      <h2 id="section-documents-title"></h2>
       <p>Download additional information about the ERRAS Registry.</p>
-      <MessageBox class="page-warning" type="warning">
-        <p>This page is under construction.</p>
-      </MessageBox>
+      <FileList table="Files" labelsColumn="name" fileColumn="file" />
     </PageSection>
   </Page>
 </template>
 
 <script setup lang="ts">
 // @ts-ignore
-import { Page, PageSection, MessageBox } from "molgenis-viz";
+import { Page, PageSection, FileList } from "molgenis-viz";
 import CustomPageHeader from "../components/CustomPageHeader.vue";
 </script>
