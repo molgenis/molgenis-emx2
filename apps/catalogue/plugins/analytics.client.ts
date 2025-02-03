f9101a2a8a42955942786a8b6dd8f57d4845deb1
connoratrug <c.stroomberg@rug.nl>
2025-02-03 16:40:30 +0100

    link local

--- apps/catalogue/plugins/analytics.client.ts
+++ apps/catalogue/plugins/analytics.client.ts
@@ -1,5 +1,6 @@
 import { defineNuxtPlugin } from "#imports";
-import { setupAnalytics } from "@molgenis/emx2-analytics";
+import { setupAnalytics } from "../../emx2-analytics/src/lib/analytics";
+import type { Provider } from "../../emx2-analytics/src/types/Provider";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook("page:loading:end", () => {
@@ -7,7 +8,9 @@ export default defineNuxtPlugin((nuxtApp) => {
     const analyticsKey = nuxtApp.$config.public.analyticsKey;
     if (schema && analyticsKey) {
       console.log("Setup Analytics for: " + schema);
-      const providers = [{ id: "site-improve", options: { analyticsKey } }];
+      const providers: Provider[] = [
+        { id: "site-improve", options: { analyticsKey } },
+      ];
       setupAnalytics(schema, providers);
     }
   });
