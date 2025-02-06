fe06d94460e2b36f7107b010ca4663657ba26658
mswertz <m.a.swertz@rug.nl>
2025-02-06 01:12:45 +0100

    fix email test

---
+++ apps/tailwind-components/pages/input/Email.story.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>Email component</h2>
+  <p>Using a form with input of type email you can have email validation</p>
+  <FormFields
+    :metadata="{
+      columns: [
+        {
+          id: 'input-email',
+          columnType: 'EMAIL',
+          label: 'Input an email address',
+        },
+      ],
+    }"
+  />
+</template>
+
+<script setup lang="ts"></script>
