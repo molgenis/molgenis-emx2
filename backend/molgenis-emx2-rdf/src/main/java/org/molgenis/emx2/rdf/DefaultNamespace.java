eb2c48b52a421121e93e1c425697fb30f6414320
Sander van den Hoek <savandenhoek@gmail.com>
2025-02-13 12:41:30 +0100

    updated remaining namespaces

--- backend/molgenis-emx2-rdf/src/main/java/org/molgenis/emx2/rdf/DefaultNamespace.java
+++ backend/molgenis-emx2-rdf/src/main/java/org/molgenis/emx2/rdf/DefaultNamespace.java
@@ -18,7 +18,23 @@ public enum DefaultNamespace {
   FOAF("foaf", "http://xmlns.com/foaf/0.1/"),
   VCARD("vcard", "http://www.w3.org/2006/vcard/ns#"),
   ORG("org", "http://www.w3.org/ns/org#"),
-  FDP("fdp-o", "https://w3id.org/fdp/fdp-o#");
+  OBO("obo", "http://purl.obolibrary.org/obo/"),
+  ORDO("ordo", "http://www.orpha.net/ORDO/"),
+  EDAM("edam", "http://edamontology.org/"),
+  AFR("arf", "http://purl.allotrope.org/ontologies/result#"),
+  AFRL("afrl", "http://purl.allotrope.org/ontologies/role#"),
+  EFO("efo", "http://www.ebi.ac.uk/efo/"),
+  FG("fg", "https://w3id.org/fair-genomes/resource/"),
+  SCHEMA("schema", "http://schema.org/"),
+  HL7("hl7", "http://purl.bioontology.org/ontology/HL7/"),
+  SNOMEDCT("snomedct", "http://purl.bioontology.org/ontology/SNOMEDCT/"),
+  LNC("lnc", "http://purl.bioontology.org/ontology/LNC/"),
+  LDP("ldp", "http://www.w3.org/ns/ldp#"),
+  EJP("ejp", "https://w3id.org/ejp-rd/vocabulary#"),
+  ENSEMBL("ensembl", "http://ensembl.org/glossary/"),
+  MESH("mesh", "http://purl.bioontology.org/ontology/MESH/"),
+  FDP("fdp-o", "http://w3id.org/fdp/fdp-o#"),
+  HEALTH_DCAT_AP("healthDCAT-AP", "urn:uuid:a7ef52b2-bd43-4294-a80f-3e7299af35e4#");
 
   private final Namespace namespace;
 
