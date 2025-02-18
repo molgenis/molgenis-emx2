123fa83fd0231338a71f603fff22eedff47ff48d
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2025-02-18 11:22:06 +0100

    Enabled FDP compliance test on Data Catalogue

--- backend/molgenis-emx2-rdf/src/test/java/org/molgenis/emx2/rdf/CatalogueComplianceTest.java
+++ backend/molgenis-emx2-rdf/src/test/java/org/molgenis/emx2/rdf/CatalogueComplianceTest.java
@@ -6,15 +6,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
 public class CatalogueComplianceTest {
 
   private static String catalogueSchemaRDF;
 
   @BeforeAll
   public static void setup() {
-    catalogueSchemaRDF =
-        createSchemaExportRDF("catalogue_shacl_validation", "_profiles/DataCatalogueFlat.yaml");
+    catalogueSchemaRDF = createSchemaExportRDF("datacatl", "_profiles/DataCatalogue.yaml");
   }
 
   @Test
@@ -22,16 +20,19 @@ public class CatalogueComplianceTest {
     testCompliance(FAIR_DATA_POINT_SHACL_FILES, catalogueSchemaRDF);
   }
 
+  @Disabled
   @Test
   public void DCAT_AP_Compliance() throws Exception {
     testCompliance(DCAT_AP_SHACL_FILES, catalogueSchemaRDF);
   }
 
+  @Disabled
   @Test
   public void Health_RI_core_plateau_1_Compliance() throws Exception {
     testCompliance(HEALTH_RI_SHACL_FILES, catalogueSchemaRDF);
   }
 
+  @Disabled
   @Test
   public void EJP_RD_VP_Lvl1_Compliance() throws Exception {
     testCompliance(EJP_RD_VP_SHACL_FILES, catalogueSchemaRDF);
