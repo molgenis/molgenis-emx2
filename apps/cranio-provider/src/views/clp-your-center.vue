a0951ad0eae703f03c81c73f00d430e15ecd0e63
dcruvolo <davidruvolo51@gmail.com>
2025-02-21 12:25:38 +0100

    minor adjustments on filtering; connected chart title

--- apps/cranio-provider/src/views/clp-your-center.vue
+++ apps/cranio-provider/src/views/clp-your-center.vue
@@ -74,26 +74,29 @@
       </DashboardChart>
     </DashboardRow>
     <DashboardRow :columns="1">
-      <DashboardChart v-if="!loading && currentVisibleMeter === 'cleftq'">
-        <ProgressMeter
-          :chartId="cleftqCompletionChart?.chartId"
-          :title="`% of patients that completed the CLEFT-Q (${selectedAgeGroup})`"
-          :value="progressMeterValue"
-          :totalValue="100"
-          :barHeight="25"
-          barFill="#66c2a4"
-        />
-      </DashboardChart>
-      <DashboardChart v-if="!loading && currentVisibleMeter === 'ics'">
-        <ProgressMeter
-          :chartId="icsCompletionChart?.chartId"
-          :title="`% of patients that completed the ICS (${selectedAgeGroup})`"
-          :value="progressMeterValue"
-          :totalValue="100"
-          :barHeight="25"
-          barFill="#9f6491"
-        />
-      </DashboardChart>
+      <LoadingScreen v-if="loading" style="height: 100%" />
+      <template v-else>
+        <DashboardChart v-if="currentVisibleMeter === 'ics'">
+          <ProgressMeter
+            :chartId="icsCompletionChart?.chartId"
+            :title="icsCompletionChart?.chartTitle?.replace('${ageGroup}', (selectedAgeGroup as string))"
+            :value="icsCompletionChartData?.dataPointValue"
+            :totalValue="100"
+            :barHeight="25"
+            barFill="#9f6491"
+          />
+        </DashboardChart>
+        <DashboardChart v-else>
+          <ProgressMeter
+            :chartId="cleftqCompletionChart?.chartId"
+            :title="cleftqCompletionChart?.chartTitle?.replace('${ageGroup}', (selectedAgeGroup as string))"
+            :value="cleftqCompletionChartData?.dataPointValue"
+            :totalValue="100"
+            :barHeight="25"
+            barFill="#66c2a4"
+          />
+        </DashboardChart>
+      </template>
     </DashboardRow>
   </ProviderDashboard>
 </template>
@@ -135,8 +138,9 @@ const patientsByGenderChart = ref<ICharts>();
 const patientsByGenderChartData = ref<IKeyValuePair>();
 const patientsByGenderPalette = ref<IKeyValuePair>();
 const icsCompletionChart = ref<ICharts>();
+const icsCompletionChartData = ref<IChartData>();
 const cleftqCompletionChart = ref<ICharts>();
-const progressMeterValue = ref<number>(0);
+const cleftqCompletionChartData = ref<IChartData>();
 
 const currentVisibleMeter = computed<clpChartTypes>(() => {
   if (["3-4 years", "5-6 years"].includes(selectedAgeGroup.value as string)) {
@@ -215,25 +219,17 @@ function updateGenderChart() {
 }
 
 function updateProgressMeter() {
-  console.log(currentVisibleMeter.value, selectedAgeGroup.value);
   if (currentVisibleMeter.value === "ics") {
-    const icsFiltered = icsCompletionChart.value?.dataPoints?.filter(
+    icsCompletionChartData.value = icsCompletionChart.value?.dataPoints?.filter(
       (row: IChartData) => {
         return row.dataPointPrimaryCategory === selectedAgeGroup.value;
       }
-    ) as IChartData[];
-    if (icsFiltered[0].dataPointValue) {
-      progressMeterValue.value = icsFiltered[0].dataPointValue;
-    }
+    )[0] as IChartData;
   } else {
-    const cleftqFilterd = cleftqCompletionChart.value?.dataPoints?.filter(
-      (row: IChartData) => {
+    cleftqCompletionChartData.value =
+      cleftqCompletionChart.value?.dataPoints?.filter((row: IChartData) => {
         return row.dataPointPrimaryCategory === selectedAgeGroup.value;
-      }
-    ) as IChartData[];
-    if (cleftqFilterd[0].dataPointValue) {
-      progressMeterValue.value = cleftqFilterd[0].dataPointValue;
-    }
+      })[0] as IChartData;
   }
 }
 
