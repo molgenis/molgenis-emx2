b66b84dc2c6fd3b19b8346d71afe54a909c8765a
Harm Brugge <harmbrugge@gmail.com>
2025-02-26 15:06:49 +0100

    exists -> exist

--- backend/molgenis-emx2-cafevariome/src/main/java/org/molgenis/emx2/cafevariome/response/RecordResponse.java
+++ backend/molgenis-emx2-cafevariome/src/main/java/org/molgenis/emx2/cafevariome/response/RecordResponse.java
@@ -4,4 +4,4 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import org.molgenis.emx2.cafevariome.Range;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record RecordResponse(Integer recordCount, Range recordRange, boolean exists) {}
+public record RecordResponse(Integer recordCount, Range recordRange, boolean exist) {}
