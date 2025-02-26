ed4364510a575d7cff6c63a3c6b9bac24792c94e
Postema Max <max@elynor.com>
2025-02-26 10:31:51 +0100

    fix: remove and add buttons

--- apps/tailwind-components/components/input/Array.vue
+++ apps/tailwind-components/components/input/Array.vue
@@ -1,39 +1,16 @@
 <template>
-  <div v-for="(value, index) in values" :key="index">
-    <div>hoi</div>
+  <div v-if="values" v-for="(value, index) in values" :key="index" class="flex">
     <Input
+      :id="id"
       v-model="values[index]"
-      v-bind="$props"
-      :type="inputType(props.type||'STRING_ARRAY')"
+      v-bind="partialProps($props)"
+      :type="nonArrayType(props.type || 'STRING_ARRAY')"
       @blur="emit('blur')"
       @focus="emit('focus')"
     />
-
-<!--
-    <Input
-      v-bind="$props"
-      @blur="emit('blur')"
-      @focus="emit('focus')"
-    >
-      <template v-slot:append>
-        <button
-          v-if="values.length > 1"
-          @click="clearInput(index)"
-          class="btn btn-outline-primary"
-          type="button"
-        >
-          <i class="fas fa-fw fa-times"></i>
-        </button>
-        <button
-          @click="addItem(index)"
-          class="btn btn-outline-primary"
-          type="button"
-        >
-          <i class="fas fa-fw fa-plus"></i>
-        </button>
-      </template>
-    </Input>
-    -->
+    <Button iconOnly icon="trash" label="Remove"  v-if="values.length > 1"
+      @click="clearInput(values, index)" />
+    <Button iconOnly icon="plus" label="add" @click="addItem(values, index)" />
   </div>
 </template>
 
@@ -48,71 +25,40 @@ let props = defineProps<
   }
 >();
 const emit = defineEmits(["focus", "blur", "update:modelValue"]);
-function inputType(type:string):CellValueType {
-      return (({
-        BOOL_ARRAY: "BOOL",
-        DATE_ARRAY: "DATE",
-        DATETIME_ARRAY: "DATETIME",
-        DECIMAL_ARRAY: "DECIMAL",
-        PERIOD_ARRAY: "PERIOD",
-        EMAIL_ARRAY: "EMAIL",
-        HYPERLINK_ARRAY: "HYPERLINK",
-        INT_ARRAY: "INT",
-        LONG_ARRAY: "LONG",
-        STRING_ARRAY: "STRING",
-        TEXT_ARRAY: "TEXT",
-        UUID_ARRAY: "UUID",
-      }[type])||"STRING") as CellValueType;
-    }
+
+function addItem(values: any, index: number) {
+  values.splice(index + 1, 0, null);
+  emit("update:modelValue", values);
+}
+
+function clearInput(values: any, index: number) {
+  if (values.length > 1) {
+    values.splice(index, 1);
+  }
+  emit("update:modelValue", values);
+}
+
+function partialProps(props: any): { [key: string]: string } {
+  let clone = { ...props };
+  delete clone.type;
+  delete clone.modelValue;
+  return clone;
+}
+
+function nonArrayType(type: string): CellValueType {
+  return ({
+    BOOL_ARRAY: "BOOL",
+    DATE_ARRAY: "DATE",
+    DATETIME_ARRAY: "DATETIME",
+    DECIMAL_ARRAY: "DECIMAL",
+    PERIOD_ARRAY: "PERIOD",
+    EMAIL_ARRAY: "EMAIL",
+    HYPERLINK_ARRAY: "HYPERLINK",
+    INT_ARRAY: "INT",
+    LONG_ARRAY: "LONG",
+    STRING_ARRAY: "STRING",
+    TEXT_ARRAY: "TEXT",
+    UUID_ARRAY: "UUID",
+  }[type] || "STRING") as CellValueType;
+}
 </script>
-<!--
-export default {
-  name: "ArrayInput",
-  extends: BaseInput,
-  components: { FormGroup },
-  data() {
-    return { values: this.modelValue?.length ? this.modelValue : [null] };
-  },
-  props: {
-    columnType: {
-      type: String,
-      required: true,
-    },
-  },
-  computed: {
-    inputType() {
-      return {
-        BOOL_ARRAY: "BOOL",
-        DATE_ARRAY: "DATE",
-        DATETIME_ARRAY: "DATETIME",
-        DECIMAL_ARRAY: "DECIMAL",
-        PERIOD_ARRAY: "PERIOD",
-        EMAIL_ARRAY: "EMAIL",
-        HYPERLINK_ARRAY: "HYPERLINK",
-        INT_ARRAY: "INT",
-        LONG_ARRAY: "LONG",
-        STRING_ARRAY: "STRING",
-        TEXT_ARRAY: "TEXT",
-        UUID_ARRAY: "UUID",
-      }[this.columnType];
-    },
-  },
-  methods: {
-    addItem(index) {
-      this.values.splice(index + 1, 0, null);
-      this.$emit("update:modelValue", this.values);
-    },
-    clearInput(index) {
-      if (this.values.length > 1) {
-        this.values.splice(index, 1);
-      }
-      this.$emit("update:modelValue", this.values);
-    },
-    handleUpdate(event, index) {
-      this.values[index] = event;
-      this.$emit("update:modelValue", this.values);
-    },
-  },
-  emits: ["update:modelValue"],
-};
--->
