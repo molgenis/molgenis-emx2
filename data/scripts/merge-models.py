372c178bf54db7bca553ab71a0bcc079da671fcd
YpeZ <info@ypezijlstra.frl>
2025-03-19 16:10:09 +0100

    * started merge_models script

--- data/scripts/merge-models.py
+++ data/scripts/merge-models.py
@@ -4,6 +4,7 @@ Script to merge and update the data models.
 import logging
 from pathlib import Path, PosixPath
 
+import numpy as np
 import pandas as pd
 
 DATA_DIR = Path(__file__).parent.parent.absolute()
@@ -29,10 +30,43 @@ def identify_non_catalogue() -> list:
 
     return non_catalogue
 
+def merge_models():
+    """
+    Merges the portal and data catalogue data models.
+    Iterates over the tables in the portals directory.
+    If the table is not present in _models/shared the table is moved there and the profile 'portal' is added
+    to each of its columns' metadata.
+    Else iterates of the columns in that table.
+    If a portal column's metadata does not fully match the corresponding catalogue column's metadata,
+    or if the column does not exist in the catalogue model the column's metadata is added as a new row
+    and given the 'portal' profile.
+    Else the profile 'portal' is appended to the list of profiles of the corresponding column.
+    """
+    portal_dir = DATA_DIR / "portal"
+    shared_dir = DATA_DIR / "_models" / "shared"
+
+    for table_file in portal_dir.glob("*.csv"):
+        if (shared_dir / table_file.name).exists():
+            logger.info(f"Merging table {table_file.name!r}.")
+
+            portal_df = pd.read_csv(table_file).replace({np.nan: None})
+            cat_df = pd.read_csv(shared_dir / table_file.name).replace({np.nan: None})
+
+            print(portal_df.head(5).to_string())
+            print(cat_df.head(5).to_string())
+
+            cols = portal_df.columns.drop("profiles")
+
+            # Matching columns
+            portal_match = portal_df.loc[portal_df['columnName'].isin(cat_df['columnName'])]
+            portal_add = portal_df.loc[~portal_df['columnName'].isin(cat_df['columnName'])]
+        else:
+            logger.info(f"Moving table {table_file.name!r} to folder 'shared'.")
 
 def main():
     tables = identify_non_catalogue()
     print(tables)
+    merge_models()
 
 
 if __name__ == '__main__':
