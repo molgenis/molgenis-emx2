e77deb24b18343966e4c73dc5210470e1a29be8c
YpeZ <info@ypezijlstra.frl>
2025-03-19 14:31:06 +0100

    * removed the actual removing of tables

--- data/scripts/merge-models.py
+++ data/scripts/merge-models.py
@@ -11,29 +11,28 @@ DATA_DIR = Path(__file__).parent.parent.absolute()
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("merge")
 
-def move_fdp_tables():
-    """Moves the tables in the _models/shared directory that are not part of the data catalogue model."""
+def identify_non_catalogue() -> list:
+    """Identifies the tables in the _models/shared directory that are not part of the data catalogue model."""
 
-    logger.info("Moving tables.")
-    # Create the directory
-    if not (fdp_dir := (DATA_DIR / "_models" / "specific" / "fdp")).exists():
-        fdp_dir.mkdir()
 
     # Iterate over the tables in _models/shared and check if any of the columns have the 'DataCatalogueFlat' profile.
     # If not, move to the new directory
     shared_dir = DATA_DIR / "_models" / "shared"
 
+    non_catalogue = list()
     table_path: PosixPath
-    for table_path in shared_dir.iterdir():
+    for table_path in shared_dir.glob("*.csv"):
         df = pd.read_csv(table_path)
         if df["profiles"].str.contains("DataCatalogueFlat").any():
             continue
-        # Move to new directory
-        logger.info(f"Moving file 'shared/{table_path.name}' to 'specific/fdp{table_path.name}'.")
-        table_path.rename(Path(str(table_path).replace("shared", "specific/fdp")))
+        non_catalogue.append(table_path.name)
+
+    return non_catalogue
+
 
 def main():
-    move_fdp_tables()
+    tables = identify_non_catalogue()
+    print(tables)
 
 
 if __name__ == '__main__':
