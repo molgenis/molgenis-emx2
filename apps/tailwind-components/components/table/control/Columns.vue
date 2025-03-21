5525127fb3fff6f797e72268503f0403629be5c5
Joris de Keijser <chinook25@gmail.com>
2025-03-21 12:25:13 +0100

    chore: add comment

--- apps/tailwind-components/components/table/control/Columns.vue
+++ apps/tailwind-components/components/table/control/Columns.vue
@@ -27,6 +27,7 @@
                     :id="element.id"
                     class="w-5 h-5 rounded-3px ml-[6px] mr-2.5 mt-0.5 accent-yellow-500 border border-checkbox"
                   />
+                  <!-- TODO move styling to checkbox component -->
                 </div>
                 <label
                   class="hover:cursor-pointer text-body-sm group"
