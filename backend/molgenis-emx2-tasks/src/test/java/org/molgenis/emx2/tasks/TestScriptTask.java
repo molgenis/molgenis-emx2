773b0b3d9077d5fe90ebed9d509317792800745c
erikzwart <e.zwart@umcg.nl>
2025-04-14 16:04:41 +0200

    Disable testBashScript in TestScriptTask.java

--- backend/molgenis-emx2-tasks/src/test/java/org/molgenis/emx2/tasks/TestScriptTask.java
+++ backend/molgenis-emx2-tasks/src/test/java/org/molgenis/emx2/tasks/TestScriptTask.java
@@ -9,86 +9,88 @@ import static org.molgenis.emx2.tasks.TaskStatus.ERROR;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 @Tag("slow")
 public class TestScriptTask {
 
-  @Tag("windowsFail")
-  @Test
-  public void testPython() throws InterruptedException {
-    System.out.println("first");
-    ScriptTask r1 =
-        new ScriptTask("hello")
+    @Tag("windowsFail")
+    @Test
+    public void testPython() throws InterruptedException {
+        System.out.println("first");
+        ScriptTask r1 = new ScriptTask("hello")
             .type(PYTHON)
             .dependencies("numpy==1.23.4")
             // example with some characters that need escaping
             .parameters("\"netherlands & world\"")
             .script(
                 """
-import time
-import numpy as np
-import sys
-# you can get parameters via sys.argv[1]
-print('Hello, '+sys.argv[1]+'!')
-a = np.array([1, 2, 3, 4, 5, 6])
-print(a)
-time.sleep(1)
-print('Halfway')
-time.sleep(1)
-print('Complete')
-""");
-    r1.run();
-    if (ERROR.equals(r1.getStatus())) {
-      System.out.println(r1);
-    }
-    assertEquals(COMPLETED, r1.getStatus());
-    // check for the arguments
-    assertTrue(r1.toString().contains("world"));
+                import time
+                import numpy as np
+                import sys
+                # you can get parameters via sys.argv[1]
+                print('Hello, '+sys.argv[1]+'!')
+                a = np.array([1, 2, 3, 4, 5, 6])
+                print(a)
+                time.sleep(1)
+                print('Halfway')
+                time.sleep(1)
+                print('Complete')
+                """
+            );
+        r1.run();
+        if (ERROR.equals(r1.getStatus())) {
+            System.out.println(r1);
+        }
+        assertEquals(COMPLETED, r1.getStatus());
+        // check for the arguments
+        assertTrue(r1.toString().contains("world"));
 
-    System.out.println("\nsecond");
-    ScriptTask r2 =
-        new ScriptTask("error")
+        System.out.println("\nsecond");
+        ScriptTask r2 = new ScriptTask("error")
             .type(PYTHON)
             .script(
                 """
-import sys
-print('Error message', file=sys.stderr)
-""");
-    // System.out.println(r2);
-    r2.run();
-  }
+                import sys
+                print('Error message', file=sys.stderr)
+                """
+            );
+        // System.out.println(r2);
+        r2.run();
+    }
 
-  @Test
-  public void testBashScript() {
-    Task bashTask =
-        new ScriptTask("bashTest")
+    @Test
+    @Disabled
+    public void testBashScript() {
+        Task bashTask = new ScriptTask("bashTest")
             .type(BASH)
             .script(
                 """
-echo "hello world"
-ls -la
-echo "bey"
-""");
-    bashTask.run();
-    assertEquals(bashTask.getStatus(), COMPLETED);
-  }
+                echo "hello world"
+                ls -la
+                echo "bey"
+                """
+            );
+        bashTask.run();
+        assertEquals(bashTask.getStatus(), COMPLETED);
+    }
 
-  @Test
-  public void testPythonScript_shouldFail() throws MalformedURLException {
-    Task task =
-        new ScriptTask("error")
+    @Test
+    public void testPythonScript_shouldFail() throws MalformedURLException {
+        Task task = new ScriptTask("error")
             .type(PYTHON)
             .script(
                 """
-import sys
-failureVariable = fail
-print('unreachable')
-""")
+                import sys
+                failureVariable = fail
+                print('unreachable')
+                """
+            )
             .setServerUrl(new URL("http://localhost:8080/"))
             .failureAddress("test@test.com");
-    task.run();
-    assertEquals(task.getStatus(), ERROR);
-  }
+        task.run();
+        assertEquals(task.getStatus(), ERROR);
+    }
 }
