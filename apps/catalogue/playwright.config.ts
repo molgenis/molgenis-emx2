ff21ffaff93eda7a829fd3db5c808c521fd07146
mswertz <m.a.swertz@rug.nl>
2025-04-26 19:39:55 +0200

    wip

--- apps/catalogue/playwright.config.ts
+++ apps/catalogue/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig<ConfigOptions>({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.E2E_BASE_URL || "https://emx2.dev.molgenis.org/", // change to specific http://localhost:*/, preview, etc.
+    baseURL: process.env.E2E_BASE_URL || "https://preview-emx2-pr-4995.dev.molgenis.org/", // change to specific http://localhost:*/, preview, etc.
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
