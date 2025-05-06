7358fe21d0155f0653a5a339a7793aad31326f0c
Harm Brugge <harmbrugge@gmail.com>
2025-05-06 19:36:30 +0200

    wip

--- backend/molgenis-emx2-sql/src/main/java/org/molgenis/emx2/sql/SqlDatabase.java
+++ backend/molgenis-emx2-sql/src/main/java/org/molgenis/emx2/sql/SqlDatabase.java
@@ -585,23 +585,13 @@ public class SqlDatabase extends HasSettings<Database> implements Database {
       }
     }
     this.connectionProvider.setActiveUser(username);
-    this.connectionProvider.setAdmin(isAdmin());
+    this.connectionProvider.setAdmin(getUser(username).isAdmin());
     this.clearCache();
   }
 
   @Override
   public String getActiveUser() {
-    String user = jooq.fetchOne("SELECT CURRENT_USER").get(0, String.class);
-    if (user.equals(postgresUser)) {
-      return ADMIN_USER;
-    } else if (user.contains(MG_USER_PREFIX)) {
-      String userName = user.substring(MG_USER_PREFIX.length());
-      if (!userName.isEmpty()) {
-        return userName;
-      }
-    }
-    // user is either valid MG_USER_* or postgresUser, otherwise error state
-    throw new MolgenisException("Unexpected user found as activeUser " + user);
+    return connectionProvider.getActiveUser();
   }
 
   @Override
