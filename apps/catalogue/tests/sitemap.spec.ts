205d9459a1c8796cb7287ffc7678adcb673f8a14
connoratrug <c.stroomberg@rug.nl>
2025-05-08 15:33:51 +0200

    fix path issue, double slash

--- apps/catalogue/tests/sitemap.spec.ts
+++ apps/catalogue/tests/sitemap.spec.ts
@@ -3,10 +3,10 @@ import { test, expect } from "@nuxt/test-utils/playwright";
 test("test sitemap generation", async ({ request }) => {
   const location = process.env.CI
     ? process.env.E2E_BASE_URL
-    : "http://localhost:8080";
+    : "http://localhost:8080/";
   const resp = await request.get(location + "/catalogue/sitemap.xml");
   const xml = await resp.text();
   expect(xml).toContain(
-    `<loc>${location}/catalogue/catalogue/all/collections/ABCD</loc>`
+    `<loc>${location}catalogue/catalogue/all/collections/ABCD</loc>`
   );
 });
