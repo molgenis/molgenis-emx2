beeccba4898ab5784d82f4b7a9efbdc3de349d10
connoratrug <c.stroomberg@rug.nl>
2025-05-09 16:23:15 +0200

    add missing schema name to test call

--- apps/catalogue/tests/sitemap.spec.ts
+++ apps/catalogue/tests/sitemap.spec.ts
@@ -4,7 +4,7 @@ test("test sitemap generation", async ({ request }) => {
   const location = process.env.CI
     ? process.env.E2E_BASE_URL
     : "http://localhost:8080/";
-  const resp = await request.get(location + "catalogue/sitemap.xml");
+  const resp = await request.get(location + "catalogue/catalogue/sitemap.xml");
   const xml = await resp.text();
   expect(xml).toContain(
     `<loc>${location}catalogue/catalogue/all/collections/ABCD</loc>`
