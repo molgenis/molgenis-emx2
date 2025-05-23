1b2f511ad82207aeb5596c04bdfe34071f019c84
de Keijser <j.j.de.keijser@umcg.nl>
2025-05-23 12:39:24 +0200

    chore: add e2e test

---
+++ apps/directory/tests/diseases-filter-tree.spec.ts
@@ -0,0 +1,105 @@
+import { test, expect } from "@playwright/test";
+
+test("The tree state of the Diagnosis Available filter", async ({ page }) => {
+  await page.goto("http://localhost:5173/#/catalogue");
+  await page.locator("#Diagnosisavailable svg").click();
+
+  // Open the first item of the list
+  await page
+    .getByRole("listitem")
+    .filter({ hasText: "▲I Certain infectious and" })
+    .locator("span")
+    .first()
+    .click();
+  await page
+    .getByRole("listitem")
+    .filter({ hasText: /^▲A00-A09 Intestinal infectious diseases$/ })
+    .locator("span")
+    .first()
+    .click();
+  await page
+    .getByRole("listitem")
+    .filter({ hasText: /^▲A00 Cholera$/ })
+    .locator("span")
+    .first()
+    .click();
+
+  // Select A00 Cholera and its children
+  await page
+    .getByRole("listitem")
+    .filter({ hasText: /^▼A00 Cholera$/ })
+    .getByRole("checkbox")
+    .check();
+  await expect(
+    page
+      .getByRole("listitem")
+      .filter({ hasText: /^▼A00 Cholera$/ })
+      .getByRole("checkbox")
+  ).toBeChecked();
+  await expect(
+    page
+      .getByRole("listitem")
+      .filter({ hasText: /^▼A00-A09 Intestinal infectious diseases$/ })
+      .getByRole("checkbox")
+  ).toBeChecked({ indeterminate: true });
+
+  // Deselect its children and check indeterminate state
+  await page
+    .getByRole("listitem")
+    .filter({
+      hasText: /^A00\.0 Cholera due to Vibrio cholerae 01, biovar cholerae$/,
+    })
+    .getByRole("checkbox")
+    .uncheck();
+  await expect(
+    page
+      .getByRole("listitem")
+      .filter({ hasText: /^▼A00 Cholera$/ })
+      .getByRole("checkbox")
+  ).toBeChecked({ indeterminate: true });
+  await expect(
+    page
+      .getByRole("listitem")
+      .filter({ hasText: /^▼A00-A09 Intestinal infectious diseases$/ })
+      .getByRole("checkbox")
+  ).toBeChecked({ indeterminate: true });
+  await expect(
+    page
+      .getByRole("listitem")
+      .filter({ hasText: "▼I Certain infectious and" })
+      .getByRole("checkbox")
+  ).toBeChecked({ indeterminate: true });
+
+  await page
+    .getByRole("listitem")
+    .filter({
+      hasText: /^A00\.1 Cholera due to Vibrio cholerae 01, biovar eltor$/,
+    })
+    .getByRole("checkbox")
+    .uncheck();
+  await page
+    .getByRole("listitem")
+    .filter({ hasText: /^A00\.9 Cholera, unspecified$/ })
+    .getByRole("checkbox")
+    .uncheck();
+
+  // Nothing should be checked
+  await expect(
+    page
+      .getByRole("listitem")
+      .filter({ hasText: /^▼A00 Cholera$/ })
+      .getByRole("checkbox")
+  ).not.toBeChecked({ indeterminate: false });
+  await expect(
+    page
+      .getByRole("listitem")
+      .filter({ hasText: /^▼A00-A09 Intestinal infectious diseases$/ })
+      .getByRole("checkbox")
+  ).not.toBeChecked({ indeterminate: false });
+  await expect(
+    page
+      .getByRole("listitem")
+      .filter({ hasText: "▼I Certain infectious and" })
+      .getByRole("checkbox")
+  ).not.toBeChecked({ indeterminate: false });
+});
