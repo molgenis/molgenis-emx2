7eb8397f53179b92d03ebd323d010f4e89452e6b
dcruvolo <davidruvolo51@gmail.com>
2025-05-27 14:05:50 +0200

    initial migration script

--- backend/molgenis-emx2-sql/src/main/java/org/molgenis/emx2/sql/Migrations.java
+++ backend/molgenis-emx2-sql/src/main/java/org/molgenis/emx2/sql/Migrations.java
@@ -183,6 +183,13 @@ public class Migrations {
             executeMigrationFile(tdb, "migration28.sql", "Update admin field original admin user");
           }
 
+          if (version < 30) {
+            executeMigrationFile(
+                tdb,
+                "migration29.sql",
+                "For custom pages, the option to defer js dependencies is added");
+          }
+
           // if success, update version to SOFTWARE_DATABASE_VERSION
           updateDatabaseVersion((SqlDatabase) tdb, SOFTWARE_DATABASE_VERSION);
         });
---
+++ backend/molgenis-emx2-sql/src/main/resources/org/molgenis/emx2/sql/migration29.sql
@@ -0,0 +1,26 @@
+UPDATE "MOLGENIS"."schema_metadata"
+SET settings = updated.new_settings
+FROM (
+  SELECT
+    ctid,
+    to_jsonb(jsonb_object_agg(
+      key,
+      CASE
+        WHEN key LIKE 'page.%'
+         AND jsonb_typeof(value) = 'object'
+         AND jsonb_typeof(value->'dependencies') = 'object'
+        THEN jsonb_set(
+          value,
+          '{dependencies}',
+          (value->'dependencies') || jsonb_build_object('jsDefer', true),
+          true
+        )
+        ELSE value
+      END
+    )) AS new_settings
+  FROM "MOLGENIS"."schema_metadata",
+       LATERAL jsonb_each(settings::jsonb)
+  GROUP BY ctid
+) AS updated
+WHERE "MOLGENIS"."schema_metadata".ctid = updated.ctid;
+
