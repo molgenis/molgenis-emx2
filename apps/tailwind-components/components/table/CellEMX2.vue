98221e7dc36a8a2f53e1ebd4d2c3ddd75e32943a
connoratrug <c.stroomberg@rug.nl>
2025-06-02 12:33:00 +0200

    flip sfc

--- apps/tailwind-components/components/table/CellEMX2.vue
+++ apps/tailwind-components/components/table/CellEMX2.vue
@@ -1,16 +1,3 @@
-<script setup lang="ts">
-import type { IColumn, IRefColumn } from "../../../metadata-utils/src/types";
-import type { RefPayload } from "../../types/types";
-defineProps<{
-  metaData: IColumn;
-  data: any;
-}>();
-
-defineEmits<{
-  (e: "cellClicked", payload: RefPayload): void;
-}>();
-</script>
-
 <template>
   <td
     class="h-4 px-2.5 border-b border-gray-200 first:pl-0 last:pr-0 sm:first:pl-2.5 sm:last:pr-2.5 overflow-ellipsis whitespace-nowrap overflow-hidden"
@@ -106,3 +93,16 @@ defineEmits<{
     </div>
   </td>
 </template>
+
+<script setup lang="ts">
+import type { IColumn, IRefColumn } from "../../../metadata-utils/src/types";
+import type { RefPayload } from "../../types/types";
+defineProps<{
+  metaData: IColumn;
+  data: any;
+}>();
+
+defineEmits<{
+  (e: "cellClicked", payload: RefPayload): void;
+}>();
+</script>
