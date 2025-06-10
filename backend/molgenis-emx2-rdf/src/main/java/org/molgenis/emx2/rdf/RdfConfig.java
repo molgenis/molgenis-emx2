c0dfdeede1d9e36897103b76c2999b5671af91cd
Sander van den Hoek <savandenhoek@gmail.com>
2025-06-10 11:32:08 +0200

    auto-formatting

--- backend/molgenis-emx2-rdf/src/main/java/org/molgenis/emx2/rdf/RdfConfig.java
+++ backend/molgenis-emx2-rdf/src/main/java/org/molgenis/emx2/rdf/RdfConfig.java
@@ -6,7 +6,8 @@ import org.molgenis.emx2.rdf.writers.WriterFactory;
 // todo: replace placeholder with full implementation that uses advaned setting to store values
 public class RdfConfig {
   private final WriterFactory writerFactory = WriterFactory.valueOf("MODEL");
-  private final RdfApiGeneratorFactory rdfApiGeneratorFactory = RdfApiGeneratorFactory.valueOf("EMX2");
+  private final RdfApiGeneratorFactory rdfApiGeneratorFactory =
+      RdfApiGeneratorFactory.valueOf("EMX2");
 
   public WriterFactory getWriterFactory() {
     return writerFactory;
