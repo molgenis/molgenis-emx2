6ffebc29ff329d2904d88e2cb827cb92d69bcbb9
connoratrug <c.stroomberg@rug.nl>
2025-07-02 13:32:01 +0200

    update perf test ( local only)

--- backend/molgenis-emx2-webapi/src/test/java/org.molgenis.emx2.web/PerformanceTest.java
+++ backend/molgenis-emx2-webapi/src/test/java/org.molgenis.emx2.web/PerformanceTest.java
@@ -3,7 +3,9 @@ package org.molgenis.emx2.web;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.restassured.RestAssured;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -21,19 +23,47 @@ public class PerformanceTest {
   void testPerformance() {
     RestAssured.baseURI = BASE_URL;
 
+    List<Integer> responseTimes = new ArrayList<>();
+
     long t0 = new Date().getTime();
 
-    for (int i = 1; i <= 10000; i++) {
+    int numRequests = 1000;
+
+    for (int i = 1; i <= numRequests; i++) {
       long t1 = new Date().getTime();
       String resp = doDemoRequest();
       long t2 = new Date().getTime();
       assertTrue(resp.contains("Contacts"), "Response does not contain expected data: " + resp);
-      logger.info(
-          "Response time: " + (t2 - t1) + " ms, duration : " + (t2 - t0) + " ms, request: " + i);
-      assertTrue(t2 - t1 < 1000, "Request took too long: " + (t2 - t1) + " ms for request " + i);
+      logger.info("Response time: " + (t2 - t1) + " ms, ms, request: " + i);
+      responseTimes.add((int) (t2 - t1));
     }
 
-    long tn = new Date().getTime();
+    long mean = responseTimes.stream().mapToInt(Integer::intValue).sum() / responseTimes.size();
+    logger.info("Mean response time: " + mean + " ms for " + numRequests + " requests");
+
+    long max = responseTimes.stream().mapToInt(Integer::intValue).max().orElse(0);
+
+    logger.info("Max response time: " + max + " ms for " + numRequests + " requests");
+
+    long variance =
+        responseTimes.stream()
+                .mapToInt(Integer::intValue)
+                .map(i -> i - (int) mean)
+                .map(i -> i * i)
+                .sum()
+            / responseTimes.size();
+
+    long stdDev = (long) Math.sqrt(variance);
+    logger.info(
+        "Standard deviation of response times: "
+            + stdDev
+            + " ms for  "
+            + numRequests
+            + "  requests");
+
+    assertTrue(
+        mean < 3000,
+        "Request took too long: " + mean + " ms (sd: " + stdDev + "ms, max: " + max + "ms)");
   }
 
   private String doDemoRequest() {
