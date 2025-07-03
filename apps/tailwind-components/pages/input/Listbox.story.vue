edde6154d0236d1001bcee380f7362253f559aa4
dcruvolo <davidruvolo51@gmail.com>
2025-07-03 13:40:32 +0200

    feat(tw): integrate search component into listbox (#5185)
    * Added search field to listbox component and updated story

--- apps/tailwind-components/pages/input/Listbox.story.vue
+++ apps/tailwind-components/pages/input/Listbox.story.vue
@@ -1,24 +1,84 @@
+<script lang="ts" setup>
+import { ref, onBeforeMount, watch } from "vue";
+
+const modelValue = ref<string>("");
+const listboxData = ref<string[]>([]);
+const listboxState = ref<string>("");
+const searchTerm = ref<string>("");
+
+type Resp<T> = {
+  data: Record<string, T[]>;
+};
+
+interface Pet {
+  name: string;
+}
+
+interface PostBody {
+  query: string;
+  variables?: { filter: Record<string, any> };
+}
+
+async function getExampleData() {
+  const body: PostBody = {
+    query: `query (\$filter: PetFilter){ Pet(filter: $filter) { name } }`,
+  };
+
+  if (searchTerm.value) {
+    body.variables = {
+      filter: {
+        name: { like: searchTerm.value },
+      },
+    };
+  }
+
+  const response = await $fetch("/pet store/graphql", {
+    method: "POST",
+    body: body,
+  });
+
+  listboxData.value = response.data.Pet.map((entry: Pet) => entry.name).sort();
+}
+
+watch(searchTerm, async () => {
+  getExampleData();
+});
+
+onBeforeMount(() => {
+  getExampleData();
+});
+</script>
+
 <template>
   <h2 class="text-heading-2xl">Listbox component</h2>
-  <form class="mb-6 rounded p-4 text-title">
-    <legend class="mb-2 text-heading-lg">
-      Configure the listbox component
-    </legend>
-    <div class="flex flex-row flex-wrap justify-center items-start gap-4">
-      <div class="p-2 grow">
-        <InputLabel for="listbox-data-type" class="pl-0">
-          <span>Select the input data type</span>
-        </InputLabel>
-        <InputRadioGroup
-          id="listbox-data-type"
-          v-model="listboxDataType"
-          :options="[
-            { value: 'string', label: 'String array' },
-            { value: 'array', label: 'Array of options' },
-          ]"
-        />
-      </div>
-      <div class="p-2 grow">
+  <div
+    class="flex flex-row flex-wrap md:flex-nowrap justify-start items-start gap-4"
+  >
+    <div class="sm:w-[65%]">
+      <h3 class="text-heading-lg mb-2"></h3>
+      <InputLabel
+        id="listbox-input-label"
+        for="listbox-input"
+        class="block mb-3 pl-0"
+        :disabled="listboxState === 'disabled'"
+        :has-error="listboxState === 'error'"
+      >
+        <span>Select a pet from the pet store</span>
+      </InputLabel>
+      <InputListbox
+        id="listbox-input"
+        labelId="listbox-input-label"
+        :options="listboxData"
+        :invalid="listboxState === 'error'"
+        :valid="listboxState === 'valid'"
+        :disabled="listboxState === 'disabled'"
+        @update:model-value="(value: any) => (modelValue = value)"
+        @search="(value: string) => searchTerm = value"
+      />
+    </div>
+    <form class="[&>div]:py-2 [&>div>label]:block">
+      <legend class="mb-2 text-heading-lg">Settings</legend>
+      <div class="p-2">
         <InputLabel for="listbox-component-state" class="pl-0">
           Change the component state
         </InputLabel>
@@ -42,80 +102,15 @@
           :showClearButton="true"
         />
       </div>
-      <div class="p-2 grow">
-        <InputLabel for="listbox-placeholder">
-          Change the default placeholder text
-        </InputLabel>
-        <InputString id="listbox-placeholder" v-model="listboxPlaceholder" />
-      </div>
-    </div>
-  </form>
-  <div class="mb-6 rounded px-6 py-8 text-title">
-    <h3 class="text-heading-lg mb-2">Listbox example</h3>
-    <InputLabel
-      id="listbox-input-label"
-      for="listbox-input"
-      class="block mb-3 pl-0"
-      :disabled="listboxState === 'disabled'"
-      :has-error="listboxState === 'error'"
-    >
-      <span>Select a group assignment</span>
-    </InputLabel>
-    <InputListbox
-      id="listbox-input"
-      labelId="listbox-input-label"
-      :options="listboxData"
-      :invalid="listboxState === 'error'"
-      :valid="listboxState === 'valid'"
-      :disabled="listboxState === 'disabled'"
-      :placeholder="listboxPlaceholder"
-      @update:model-value="(value: any) => (modelValue = value)"
-    />
-    <output class="block w-full mt-6 border py-3 px-2 pl-6">
-      <code
-        >Output {{ listboxDataType === "true" ? "Value" : "Object" }}:
-        {{ modelValue }}</code
-      >
-    </output>
+    </form>
   </div>
-  <div class="mb-2 rounded p-6 text-title">
-    <h3 class="text-heading-lg mb-2">Input data structure</h3>
-    <p>Based on the selection above, the input data is shown below.</p>
-    <output
-      class="block w-full mt-6 border py-3 px-2 pl-6 h-60 overflow-y-scroll shadow-inner"
-    >
-      <pre class="indent-[-5em]">
-        {{ listboxData }}
-      </pre>
+  <div>
+    <h3 class="text-heading-lg mb-2">Component output</h3>
+    <output class="block w-full mt-2 border py-3 px-2 pl-6">
+      <code>Search Term: {{ searchTerm }}</code>
+    </output>
+    <output class="block w-full mt-6 border py-3 px-2 pl-6">
+      <code> User selection: {{ modelValue }} </code>
     </output>
   </div>
 </template>
-
-<script lang="ts" setup>
-import { ref, computed } from "vue";
-import type {
-  IInputValue,
-  IInputValueLabel,
-} from "../../../metadata-utils/src/types";
-
-const modelValue = defineModel<IInputValue | IInputValueLabel>();
-const letters: string[] = [...Array(26).keys()].map((num) =>
-  String.fromCharCode(num + 65)
-);
-
-const lettersWithLabels: IInputValueLabel[] = letters.map((letter: string) => {
-  return { value: letter, label: `Group ${letter}` };
-});
-
-const listboxState = ref<string>("");
-const listboxPlaceholder = ref<string>("Select an option");
-const listboxDataType = ref<string>("string");
-
-const listboxData = computed<IInputValue[] | IInputValueLabel[]>(() => {
-  if (listboxDataType.value === "string") {
-    return letters as IInputValue[];
-  } else {
-    return lettersWithLabels as IInputValueLabel[];
-  }
-});
-</script>
