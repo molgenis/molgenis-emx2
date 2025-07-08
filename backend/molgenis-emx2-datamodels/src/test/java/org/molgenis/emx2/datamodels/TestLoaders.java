deb23cbc7dc07ad0d9c65ac29bd0b2f230027116
Harm Brugge <harmbrugge@gmail.com>
2025-07-08 09:55:02 +0200

    fixed test

--- backend/molgenis-emx2-datamodels/src/test/java/org/molgenis/emx2/datamodels/TestLoaders.java
+++ backend/molgenis-emx2-datamodels/src/test/java/org/molgenis/emx2/datamodels/TestLoaders.java
@@ -29,6 +29,7 @@ public class TestLoaders {
   public static final String DIRECTORY_ONTOLOGIES = "DirectoryOntologies";
   public static final String DASHBOARD_TEST = "UiDashboardTest";
   public static final String PATIENT_REGISTRY_DEMO = "patientRegistryDemo";
+  public static final String PATIENT_REGISTRY_TEST = "patientRegistryTest";
 
   static Database database;
 
@@ -48,6 +49,7 @@ public class TestLoaders {
     database.dropSchemaIfExists(PROJECT_MANAGER);
     database.dropSchemaIfExists(DASHBOARD_TEST);
     database.dropSchemaIfExists(PATIENT_REGISTRY_DEMO);
+    database.dropSchemaIfExists(PATIENT_REGISTRY_TEST);
     // delete ontologies last
     database.dropSchemaIfExists(CATALOGUE_ONTOLOGIES);
   }
