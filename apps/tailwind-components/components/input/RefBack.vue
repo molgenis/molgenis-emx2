ec77d8a54a56fea86382758478e497c7febcea21
connoratrug <c.stroomberg@rug.nl>
2025-07-08 15:13:19 +0200

    feat: taiwind refback input
    - Add Refback input (wip)
    - Bind to form by column type
    - Add Rebback list item component

---
+++ apps/tailwind-components/components/input/RefBack.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import type { IInputProps } from "~/types/types";
+import type { columnValueObject } from "../../../metadata-utils/src/types";
+
+const props = withDefaults(
+  defineProps<
+    IInputProps & {
+      refSchemaId: string;
+      refTableId: string;
+      refLabel: string;
+      canEdit?: boolean;
+    }
+  >(),
+  {
+    canEdit: true,
+  }
+);
+
+const modelValue = defineModel<columnValueObject[]>();
+</script>
+<template>
+  <Button v-if="canEdit" icon="plus" type="text" size="small"
+    >Add{{ props.refTableId }}</Button
+  >
+  <ul class="border divide-y divide-gray-200">
+    <InputRefListItem
+      v-for="ref in modelValue"
+      :data="ref"
+      :refLabel="props.refLabel"
+      :canEdit="props.canEdit"
+    />
+  </ul>
+</template>
---
+++ apps/tailwind-components/components/input/ref/ListItem.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { rowToString } from "~/utils/rowToString";
+import type { columnValueObject } from "../../../../metadata-utils/src/types";
+
+const props = withDefaults(
+  defineProps<{
+    data: columnValueObject;
+    refLabel: string;
+    canEdit?: boolean;
+  }>(),
+  {
+    canEdit: true,
+  }
+);
+
+function toLabel(row: columnValueObject) {
+  return rowToString(row, props.refLabel);
+}
+</script>
+
+<template>
+  <li class="py-5 px-[30px]">
+    <div class="flex items-center justify-between">
+      <h3 class="text-title-contrast font-bold">{{ toLabel(data) }}</h3>
+      <div class="flex items-center gap-4">
+        <div class="flex items-center gap-2" v-if="props.canEdit">
+          <Button
+            :icon-only="true"
+            icon="trash"
+            type="outline"
+            size="small"
+          ></Button>
+          <Button
+            :icon-only="true"
+            icon="copy"
+            type="outline"
+            size="small"
+          ></Button>
+          <Button
+            :icon-only="true"
+            icon="edit"
+            type="outline"
+            size="small"
+          ></Button>
+        </div>
+        <div class="flex items-center gap-2">
+          <Button
+            :icon-only="true"
+            icon="caret-down"
+            type="outline"
+            size="small"
+          ></Button>
+        </div>
+      </div>
+    </div>
+  </li>
+</template>
