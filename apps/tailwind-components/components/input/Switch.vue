8eac68c6a23aabcf04bc32ba70111cb009cc7e76
dcruvolo <davidruvolo51@gmail.com>
2025-07-09 09:31:34 +0200

    added styles based on group hover and peer checked status

--- apps/tailwind-components/components/input/Switch.vue
+++ apps/tailwind-components/components/input/Switch.vue
@@ -22,8 +22,10 @@ const modelValue = defineModel<IInputValue>();
 </script>
 
 <template>
-  <div class="relative flex items-center border rounded w-fit group">
-    <div v-for="option in options" class="w-[36px]">
+  <div
+    class="relative flex items-center border rounded w-fit group bg-button-switch"
+  >
+    <div v-for="option in options" class="w-[36px] z-10">
       <input
         :id="`${id}-switch-${option.value}`"
         class="peer sr-only"
@@ -35,7 +37,7 @@ const modelValue = defineModel<IInputValue>();
       />
       <label
         :for="`${id}-switch-${option.value}`"
-        class="cursor-pointer block transition-all duration-500 ease-in-out bg-button-switch hover:bg-button-switch-hover text-button-switch hover:text-button-switch-hover border border-transparent rounded hover:border-button-switch-hover peer-checked:bg-button-switch-selected peer-checked:text-button-switch-selected peer-checked:border-button-switch-selected"
+        class="cursor-pointer block transition-all duration-700 ease-in-out bg-button-switch text-button-switch border border-transparent rounded hover:bg-button-switch-hover hover:text-button-switch-hover hover:border-button-switch-hover group-hover:peer-checked:border-transparent group-hover:peer-checked:hover:border-button-switch-hover peer-checked:bg-button-switch-selected peer-checked:text-button-switch-selected peer-checked:border-button-switch-selected peer-checked:hover:bg-button-switch-hover peer-checked:hover:text-button-switch-hover peer-checked:hover:border-button-switch-hover"
       >
         <span class="sr-only">{{ option.label || option.value }}</span>
         <BaseIcon :name="option.icon" :width="36" class="p-2 text-current" />
