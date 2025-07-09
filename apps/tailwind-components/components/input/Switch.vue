3c09664c9c15a5a75d3f475effcf27b0be86aff8
dcruvolo <davidruvolo51@gmail.com>
2025-07-09 12:29:52 +0200

    add tooltip

--- apps/tailwind-components/components/input/Switch.vue
+++ apps/tailwind-components/components/input/Switch.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import { ref } from "vue";
 import { BaseIcon } from "#components";
 import type { IInputProps } from "~/types/types";
 import type { IInputValue } from "../../../metadata-utils/src/types";
@@ -17,7 +16,6 @@ defineProps<
 >();
 
 const emits = defineEmits(["update:modelValue"]);
-
 const modelValue = defineModel<IInputValue>();
 </script>
 
@@ -25,7 +23,7 @@ const modelValue = defineModel<IInputValue>();
   <div
     class="relative flex items-center border rounded w-fit group bg-button-switch"
   >
-    <div v-for="option in options" class="w-[36px] z-10">
+    <div v-for="option in options" class="w-[36px]">
       <input
         :id="`${id}-switch-${option.value}`"
         class="peer sr-only"
@@ -37,6 +35,7 @@ const modelValue = defineModel<IInputValue>();
       />
       <label
         :for="`${id}-switch-${option.value}`"
+        v-tooltip.top="option.label || option.value"
         class="cursor-pointer block transition-all duration-default ease-in-out bg-button-switch text-button-switch border border-transparent rounded hover:bg-button-switch-hover hover:text-button-switch-hover hover:border-button-switch-hover group-hover:peer-checked:border-transparent group-hover:peer-checked:hover:border-button-switch-hover peer-checked:bg-button-switch-selected peer-checked:text-button-switch-selected peer-checked:border-button-switch-selected peer-checked:hover:bg-button-switch-hover peer-checked:hover:text-button-switch-hover peer-checked:hover:border-button-switch-hover"
       >
         <span class="sr-only">{{ option.label || option.value }}</span>
