b134cc5dde3a6698caf2c363e028c441bb7f5481
dcruvolo <davidruvolo51@gmail.com>
2025-07-09 12:49:20 +0200

    added support for reduced motion preference

--- apps/tailwind-components/components/input/Switch.vue
+++ apps/tailwind-components/components/input/Switch.vue
@@ -36,7 +36,7 @@ const modelValue = defineModel<IInputValue>();
       <label
         :for="`${id}-switch-${option.value}`"
         v-tooltip.top="option.label || option.value"
-        class="cursor-pointer block transition-all duration-default ease-in-out bg-button-switch text-button-switch border border-transparent rounded hover:bg-button-switch-hover hover:text-button-switch-hover hover:border-button-switch-hover group-hover:peer-checked:border-transparent group-hover:peer-checked:hover:border-button-switch-hover peer-checked:bg-button-switch-selected peer-checked:text-button-switch-selected peer-checked:border-button-switch-selected peer-checked:hover:bg-button-switch-hover peer-checked:hover:text-button-switch-hover peer-checked:hover:border-button-switch-hover"
+        class="cursor-pointer block transition-all duration-default ease-in-out motion-reduce:transition-none motion-reduce:duration-0 bg-button-switch text-button-switch border border-transparent rounded hover:bg-button-switch-hover hover:text-button-switch-hover hover:border-button-switch-hover group-hover:peer-checked:border-transparent group-hover:peer-checked:hover:border-button-switch-hover peer-checked:bg-button-switch-selected peer-checked:text-button-switch-selected peer-checked:border-button-switch-selected peer-checked:hover:bg-button-switch-hover peer-checked:hover:text-button-switch-hover peer-checked:hover:border-button-switch-hover"
       >
         <span class="sr-only">{{ option.label || option.value }}</span>
         <BaseIcon :name="option.icon" :width="36" class="p-2 text-current" />
