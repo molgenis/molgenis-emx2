f8302dd8f6f14299c36ac51711cd1ea20e484119
Harm Brugge <harmbrugge@gmail.com>
2025-07-10 11:49:05 +0200

    chore(beacon): migrate beacon api to rd3 datamodel (#4825)
    * migrated genomic variants
    
    * started analysis migration
    
    * migrated individuals test to datamodels module
    
    * Migrated individuals tests
    
    * Migrated authority tests
    
    * migrated many tests
    
    * migrated analyses
    
    * migrated runs
    
    * migrated datasets
    
    * fix map and biosamples
    
    * Updated rd3 model changes
    
    * Updated tests
    
    * Fix test
    
    * changed documentation
    
    * added template documentation
    
    * updated docs
    
    * fixed test
    
    * migrated testloaders
    
    * removed test
    
    * removed always admin
    
    * Added getColumnByIdIncludingSubclasses
    
    * fixed test

--- backend/molgenis-emx2-datamodels/src/test/java/org/molgenis/emx2/datamodels/beacon/BeaconInfoTest.java
+++ backend/molgenis-emx2-datamodels/src/test/java/org/molgenis/emx2/datamodels/beacon/BeaconInfoTest.java
@@ -1,4 +1,4 @@
-package org.molgenis.emx2.beaconv2;
+package org.molgenis.emx2.datamodels.beacon;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -6,15 +6,15 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.molgenis.emx2.beaconv2.endpoints.Info;
-import org.molgenis.emx2.beaconv2.entrytypes.BeaconModelEndPointTest;
+import org.molgenis.emx2.datamodels.TestLoaders;
 
 @Disabled
-public class Beaconv2_InfoTest extends BeaconModelEndPointTest {
+public class BeaconInfoTest extends TestLoaders {
 
   @Test
   public void testInfoRootEndpoint() {
     Info info = new Info(database);
-    JsonNode response = info.getResponse(beaconSchema);
+    JsonNode response = info.getResponse(patientRegistry);
 
     assertEquals("https://molgenis.org/", response.get("response").get("welcomeUrl").asText());
     assertEquals("MOLGENIS EMX2 Beacon v2", response.get("response").get("name").asText());
