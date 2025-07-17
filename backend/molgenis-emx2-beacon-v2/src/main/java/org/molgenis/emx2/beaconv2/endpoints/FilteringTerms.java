0e7fe993e3a58ae9a60694c339c610b546863313
Harm Brugge <harmbrugge@gmail.com>
2025-07-17 15:23:43 +0200

    Adjusted filtering terms

--- backend/molgenis-emx2-beacon-v2/src/main/java/org/molgenis/emx2/beaconv2/endpoints/FilteringTerms.java
+++ backend/molgenis-emx2-beacon-v2/src/main/java/org/molgenis/emx2/beaconv2/endpoints/FilteringTerms.java
@@ -1,6 +1,8 @@
 package org.molgenis.emx2.beaconv2.endpoints;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import java.util.Collections;
+import java.util.Map;
 import org.molgenis.emx2.Database;
 import org.molgenis.emx2.beaconv2.common.misc.Handover;
 import org.molgenis.emx2.beaconv2.endpoints.datasets.DatasetsMeta;
@@ -13,14 +15,15 @@ public class FilteringTerms {
 
   private DatasetsMeta meta;
   private ResponseSummary responseSummary;
-  private FilteringTerm[] response;
+  private Map<String, FilteringTerm[]> response;
   private Handover beaconHandovers;
 
   public FilteringTerms(Database database) {
     database.tx(
         db -> {
           this.meta = new DatasetsMeta("../beaconFilteringTermsResponse.json", "filteringterms");
-          this.response = new FilteringTermsFetcher(database).getAllFilteringTerms();
+          FilteringTerm[] terms = new FilteringTermsFetcher(database).getAllFilteringTerms();
+          this.response = Collections.singletonMap("filteringTerms", terms);
           this.responseSummary = new ResponseSummary();
           this.beaconHandovers = new Handover();
         });
@@ -34,7 +37,7 @@ public class FilteringTerms {
     return responseSummary;
   }
 
-  public FilteringTerm[] getResponse() {
+  public Map<String, FilteringTerm[]> getResponse() {
     return response;
   }
 
