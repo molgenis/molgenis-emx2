ccedb922c8999d532e139f556ec87e8fe50f86cc
Harm Brugge <harmbrugge@gmail.com>
2025-07-22 15:07:32 +0200

    Updated test sql script

--- backend/molgenis-emx2-sql/src/test/resources/org/molgenis/emx2/sql/migration_xx_permissions_test.sql
+++ backend/molgenis-emx2-sql/src/test/resources/org/molgenis/emx2/sql/migration_xx_permissions_test.sql
@@ -1,8 +1,18 @@
+RESET ROLE;
 CREATE ROLE "MG_USER_test@test.com" WITH NOLOGIN;
+CREATE ROLE "MG_USER_test2@test.com" WITH NOLOGIN;
 GRANT "MG_USER_user" TO "MG_USER_test@test.com";
+GRANT "MG_USER_user" TO "MG_USER_test2@test.com";
 
 INSERT INTO "MOLGENIS".users_metadata (username, password, enabled, settings, admin)
 VALUES ('test@test.com', null, true, '{}', false);
+INSERT INTO "MOLGENIS".users_metadata (username, password, enabled, settings, admin)
+VALUES ('test2@test.com', null, true, '{}', false);
+
+UPDATE "MOLGENIS".group_metadata
+SET users = '{test@test.com, test2@test.com}'
+WHERE group_name LIKE '"pet store"#_VIEWER' ESCAPE '#';
+
 INSERT INTO "MOLGENIS".group_metadata (group_name, group_description, users)
 VALUES ('pet_store_special', 'pet store special', '{test@test.com}');
 INSERT INTO "MOLGENIS".group_permissions (group_name, table_schema, table_name, has_select, has_insert, has_update,
@@ -10,12 +20,17 @@ INSERT INTO "MOLGENIS".group_permissions (group_name, table_schema, table_name, 
 VALUES ('pet_store_special', 'pet store', 'Pet', true, true, true,
         false, true, true, false, false);
 
+-- needs _ALL_ column in table_metadata
 SELECT "MOLGENIS".create_or_update_schema_groups('pet store');
 SELECT "MOLGENIS".enable_RLS_on_table('pet store', 'Pet');
 
-SET ROLE "MG_USER_test@test.com";
 UPDATE "pet store"."Pet"
 SET mg_group = 'pet_store_special'
 WHERE name = 'fire ant';
 
+SET ROLE "MG_USER_test@test.com";
 SELECT COUNT(*) FROM "pet store"."Pet";
+
+SET ROLE "MG_USER_test2@test.com";
+SELECT COUNT(*) FROM "pet store"."Pet";
+
