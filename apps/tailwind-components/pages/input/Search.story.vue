53a6bec5e538dd04d9aa08cb52545832efc72461
mswertz <m.a.swertz@rug.nl>
2025-07-29 16:28:19 +0200

    decoubple the search story search values

--- apps/tailwind-components/pages/input/Search.story.vue
+++ apps/tailwind-components/pages/input/Search.story.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const searchValue = ref<string>("");
+const searchValue1 = ref<string>("");
+const searchValue2 = ref<string>("");
+const searchValue3 = ref<string>("");
 </script>
 
 <template>
@@ -12,17 +14,17 @@ const searchValue = ref<string>("");
   >
     <label for="search-input"> Search </label>
     <InputSearch
-      v-model="searchValue"
+      v-model="searchValue1"
       :placeholder="placeholder"
       :valid="valid"
       :invalid="invalid"
       :disabled="disabled"
       id="search-input"
     />
-    value: {{ searchValue }}
+    value: {{ searchValu1e }}
     <h2>large</h2>
     <InputSearch
-      v-model="searchValue"
+      v-model="searchValue2"
       size="large"
       :placeholder="placeholder"
       :valid="valid"
@@ -30,10 +32,10 @@ const searchValue = ref<string>("");
       :disabled="disabled"
       id="search-input"
     />
-    value: {{ searchValue }}
+    value: {{ searchValue2 }}
     <h2>tiny</h2>
     <InputSearch
-      v-model="searchValue"
+      v-model="searchValue3"
       size="tiny"
       :placeholder="placeholder"
       :valid="valid"
@@ -41,6 +43,6 @@ const searchValue = ref<string>("");
       :disabled="disabled"
       id="search-input"
     />
-    value: {{ searchValue }}
+    value: {{ searchValue3 }}
   </InputTestContainer>
 </template>
