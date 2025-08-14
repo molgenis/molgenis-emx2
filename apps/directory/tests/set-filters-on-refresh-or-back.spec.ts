b103924e187da1dbce0411259af34f233b3dcb58
de Keijser <j.j.de.keijser@umcg.nl>
2025-08-14 12:03:01 +0200

    chore: readd click

--- apps/directory/tests/set-filters-on-refresh-or-back.spec.ts
+++ apps/directory/tests/set-filters-on-refresh-or-back.spec.ts
@@ -5,16 +5,23 @@ test("Refreshing and going back should correctly set the filters in the url", as
   page,
 }) => {
   await page.goto(getAppRoute());
+
+  //filter
   await expect(page.getByRole("main")).toContainText("Organisations: 3");
   await page.getByText("Material type").click();
   await page.getByRole("checkbox", { name: "DNA", exact: true }).check();
   await expect(page.getByRole("main")).toContainText("Organisations: 1");
   await page.reload();
   await expect(page.getByRole("main")).toContainText("Organisations: 1");
+
+  //cart
+  await page.getByRole("button", { name: "Select all collections" }).click();
   await expect(page.getByRole("main")).toContainText("Request1");
   await page.reload();
   await expect(page.getByRole("main")).toContainText("Organisations: 1");
   await expect(page.getByRole("main")).toContainText("Request1");
+
+  //back
   await page.getByRole("link", { name: "Biobank1", exact: true }).click();
   await expect(page.getByRole("main")).toContainText(
     "bbmri-eric:ID:DE_biobank1"
