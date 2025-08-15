40197b88d273f62bbfcaf938582255aea2ecc045
dcruvolo <davidruvolo51@gmail.com>
2025-08-15 15:34:07 +0200

    added test to ensure aggregates dashboard still works

---
+++ apps/catalogue/tests/aggregates-url.spec.ts
@@ -0,0 +1,52 @@
+import { expect, test } from "@nuxt/test-utils/playwright";
+
+test.describe("Aggregates URL", () => {
+  test.beforeEach(async ({ context, baseURL, goto }) => {
+    await context.addCookies([
+      {
+        name: "mg_allow_analytics",
+        value: "false",
+        domain: new URL(baseURL as string).hostname,
+        path: "/",
+      },
+    ]);
+
+    await goto("/FORCE-NEN%20collections", { waitUntil: "hydration" });
+  });
+
+  test.afterEach(async ({ goto }) => {
+    await goto("/FORCE-NEN%20collections", { waitUntil: "hydration" });
+  });
+
+  test("Aggregates URLs are properly defined", async ({ page }) => {
+    const targetHref = "/Aggregates/aggregates/#/";
+    await expect(
+      page.getByRole("link", { name: "Aggregates" }).nth(0)
+    ).toHaveAttribute("href", targetHref);
+    await expect(
+      page.getByRole("link", { name: "Aggregates" }).nth(1)
+    ).toHaveAttribute("href", targetHref);
+  });
+
+  test("First link properly resolves to dashboard when clicked", async ({
+    page,
+  }) => {
+    await page.getByRole("link", { name: "Aggregates" }).first().click();
+    const aggPagePromise = page.waitForEvent("popup");
+    const aggPage = await aggPagePromise;
+    await expect(
+      aggPage.getByRole("button", { name: "Apply Filters" })
+    ).toBeVisible();
+  });
+
+  test("Second link properly resolves to dashboard when clicked", async ({
+    page,
+  }) => {
+    await page.getByRole("link", { name: "Aggregates" }).nth(1).click();
+    const aggPagePromise = page.waitForEvent("popup");
+    const aggPage = await aggPagePromise;
+    await expect(
+      aggPage.getByRole("button", { name: "Apply Filters" })
+    ).toBeVisible();
+  });
+});
