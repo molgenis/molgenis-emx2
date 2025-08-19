e9e51aa5c2dfa090294395682e7e796c417c17b7
YpeZ <info@ypezijlstra.frl>
2025-08-19 14:19:06 +0200

    created test skeletons

---
+++ tools/pyclient/tests/test_client.py
@@ -0,0 +1,99 @@
+"""
+Tests for the Pyclient.
+"""
+
+from tools.pyclient.src.molgenis_emx2_pyclient import Client
+
+import pytest
+
+
+def test_signin():
+    """Tests the `signin` method."""
+    ...
+
+def test_signout():
+    """Tests the `signout` method."""
+    ...
+
+def test_status():
+    """Tests the `status` property."""
+    ...
+
+def test_get_schemas():
+    """Tests the `get_schemas` method."""
+    ...
+
+def test_set_token():
+    """Tests the `set_token` method."""
+    ...
+
+def test_save_schema():
+    """Tests the `save_schema` method."""
+    ...
+
+def test_upload_file():
+    """Tests the `upload_file` method."""
+    ...
+
+def test_truncate():
+    """Tests the `truncate` method."""
+    ...
+
+def test_upload_csv():
+    """Tests the `_upload_csv` method."""
+    ...
+
+def test_delete_records():
+    """Tests the `delete_records` method."""
+    ...
+
+def test_get():
+    """Tests the `get` method."""
+    ...
+
+def test_get_graphql():
+    """Tests the `get_graphql` method."""
+    ...
+
+def test_export():
+    """Tests the `export` method."""
+    ...
+
+def test_create_schema():
+    """Tests the `create_schema` method."""
+    ...
+
+def test_delete_schema():
+    """Tests the `delete_schema` method."""
+    ...
+
+def test_update_schema():
+    """Tests the `update_schema` method."""
+    ...
+
+def test_recreate_schema():
+    """Tests the `recreate_schema` method."""
+    ...
+
+def test_get_schema_metadata():
+    """Tests the `get_schema_metadata` method."""
+    ...
+
+def test_prepare_filter():
+    """Tests the `_prepare_filter` method."""
+    ...
+
+def test_set_schema():
+    """Tests the `set_schema` method."""
+    ...
+
+def test_report_task_progress():
+    """Tests the `_report_task_progress` method."""
+    ...
+
+def test_validate_graphql_response():
+    """Tests the `_validate_graphql_response` method."""
+    ...
+
+
+
