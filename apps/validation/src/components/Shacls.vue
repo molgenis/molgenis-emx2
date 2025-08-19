59324135190d0040e942b7a8440e2b067da91bc6
Sander van den Hoek <savandenhoek@gmail.com>
2025-08-19 15:23:42 +0200

    available shacl sets are displayed

--- apps/validation/src/components/Shacls.vue
+++ apps/validation/src/components/Shacls.vue
@@ -1,16 +1,44 @@
 <template>
-  {{ shaclSets }}
+  <div v-for="shaclSet in shaclSets">
+    <Accordion :id="shaclSet.name" :title="shaclSet.name">
+      <table class="table">
+        <tr>
+        <th style="width:10%">name</th>
+        <th style="width:30%">description</th>
+        <th style="width:10%">version</th>
+        <th>sources</th>
+        </tr>
+        <tr>
+          <td>{{shaclSet.name}}</td>
+          <td>{{shaclSet.description}}</td>
+          <td>{{shaclSet.version}}</td>
+          <td><ul><li v-for="source in shaclSet.sources"><a :href="source" target="_blank">{{source}}</a></li></ul></td>
+        </tr>
+      </table>
+      <LayoutCard title="SHACL output" />
+    </Accordion>
+    <button
+        type="button"
+        :class="light ? 'btn btn-outline-light' : 'btn btn-outline-primary'"
+        :disabled="disabled"
+        @click.prevent="onClick"
+    >
+      validate
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import { parse } from "yaml";
+import { TableDisplay, LayoutCard } from "molgenis-components";
+import { Accordion } from "molgenis-viz";
 
-// todo: use correct implementation (that uses dev-proxy?)
 const shaclSets = ref("");
 async function fetchShacls() {
   const res = await fetch("/api/rdf?shacls");
   shaclSets.value = await res.text();
-  console.log(res.text());
+  shaclSets.value = parse(shaclSets.value);
 }
 
 fetchShacls();
--- apps/validation/vite.config.js
+++ apps/validation/vite.config.js
@@ -3,6 +3,18 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig(({ command }) => ({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+      @import "../molgenis-viz/src/styles/palettes.scss";
+      @import "../molgenis-viz/src/styles/variables.scss";
+      @import "../molgenis-viz/src/styles/mixins.scss";
+      @import "../molgenis-viz/src/styles/resets.scss";
+    `,
+      },
+    },
+  },
   base: command === "serve" ? "/" : "apps/validation/",
   server: {
     proxy: require("../dev-proxy.config"),
