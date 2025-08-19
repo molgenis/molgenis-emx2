92145ff5180910ad33fd72fe201d9c6b04562f19
dcruvolo <davidruvolo51@gmail.com>
2025-08-19 14:43:55 +0200

    added next/previous actions; moved clear button to component

--- apps/tailwind-components/components/button/FilterWellContainer.vue
+++ apps/tailwind-components/components/button/FilterWellContainer.vue
@@ -1,66 +1,97 @@
 <script lang="ts" setup>
-import {
-  ref,
-  useTemplateRef,
-  computed,
-  onMounted,
-  onUpdated,
-  onBeforeUnmount,
-} from "vue";
+import { ref, useTemplateRef, watch, onMounted, onBeforeUnmount } from "vue";
 
-const clearButton = useTemplateRef<HTMLDivElement>("clearButtonColumn");
-const btnContainer = useTemplateRef<HTMLDivElement>("filterButtonContainer");
+const buttonContainer = useTemplateRef<HTMLDivElement>("filterButtonContainer");
 const widthObserver = ref();
-const buttonObserver = ref();
 const showButtons = ref<boolean>(false);
-const hasClearButton = ref<boolean>(false);
-// const filterButtons = computed<HTMLButtonElement[]>(() => {
-//   if (btnContainer.value) {
-//     return btnContainer.value.children as HTMLCollection;
-//   }
-// })
+const hasFilterWellButtons = ref<boolean>(false);
+const filterButtons = ref<HTMLCollection>();
+const focusCounter = ref<number>(0);
+const minFocusCounter = ref<number>(0);
+const maxFocusCounter = ref<number>(0);
 
-// console.log(filterButtons.value);
+const scrollConfig = {
+  behavior: "smooth",
+  block: "nearest",
+  inline: "center",
+} as ScrollOptions;
 
-function observeClearButton() {
-  if (clearButton.value) {
-    hasClearButton.value = clearButton.value.children.length > 0;
-  }
-}
+const observerConfig = {
+  childList: true,
+  subtree: true,
+  attributes: false,
+  characterData: false,
+};
+
+const emit = defineEmits(["clear"]);
 
 function observeScrollWidth() {
   if (
-    btnContainer.value &&
-    btnContainer.value.scrollWidth > btnContainer.value.offsetWidth
+    buttonContainer.value &&
+    buttonContainer.value.scrollWidth > buttonContainer.value.offsetWidth
   ) {
     showButtons.value = true;
   } else {
     showButtons.value = false;
   }
+
+  updateFilterButtons();
 }
 
-function showPreviousItem() {}
+function updateFilterButtons() {
+  filterButtons.value = buttonContainer.value?.children as HTMLCollection;
+  maxFocusCounter.value = filterButtons.value.length;
 
-function showNextItem() {}
+  if (buttonContainer.value) {
+    hasFilterWellButtons.value = buttonContainer.value.children.length > 1;
+  }
+}
+
+function showPreviousItem() {
+  focusCounter.value -= 1;
+  if (focusCounter.value <= minFocusCounter.value) {
+    focusCounter.value = minFocusCounter.value;
+  }
+
+  if (filterButtons.value) {
+    const previousButton = filterButtons.value[
+      focusCounter.value as number
+    ] as HTMLButtonElement;
+    previousButton.focus();
+    previousButton.scrollIntoView(scrollConfig);
+  }
+}
+
+function showNextItem() {
+  focusCounter.value += 1;
+  if (focusCounter.value >= maxFocusCounter.value - 1) {
+    focusCounter.value = maxFocusCounter.value - 1;
+  }
+
+  if (filterButtons.value) {
+    const nextButton = filterButtons.value[
+      focusCounter.value as number
+    ] as HTMLButtonElement;
+    nextButton.focus();
+    nextButton.scrollIntoView(scrollConfig);
+  }
+}
 
 onMounted(() => {
   widthObserver.value = new MutationObserver(observeScrollWidth);
-  buttonObserver.value = new MutationObserver(observeClearButton);
-
-  const config = {
-    childList: true,
-    subtree: true,
-    attributes: false,
-    characterData: false,
-  };
-  widthObserver.value.observe(btnContainer.value, config);
-  buttonObserver.value.observe(clearButton.value, config);
+  widthObserver.value.observe(buttonContainer.value, observerConfig);
 });
 
-onBeforeUnmount(() => {
-  widthObserver.value.disconnect();
-  buttonObserver.value.disconnect();
-});
+onBeforeUnmount(() => widthObserver.value.disconnect());
+
+watch(
+  () => buttonContainer.value,
+  () => {
+    if (buttonContainer.value) {
+      updateFilterButtons();
+    }
+  }
+);
 </script>
 
 <template>
@@ -69,36 +100,48 @@ onBeforeUnmount(() => {
     role="group"
     class="grid flex-nowrap justify-start items-center gap-2 mb-2 py-3 pr-2"
     :class="{
-      'grid-cols-1': !hasClearButton,
-      'grid-cols-[100px_1fr]': hasClearButton && !showButtons,
-      'grid-cols-[100px_35px_1fr_35px]': hasClearButton && showButtons,
+      'grid-cols-1': !hasFilterWellButtons,
+      'grid-cols-[100px_1fr]': hasFilterWellButtons && !showButtons,
+      'grid-cols-[100px_35px_1fr_35px]': hasFilterWellButtons && showButtons,
     }"
   >
-    <div ref="clearButtonColumn">
-      <slot name="column-clear-button"></slot>
+    <div v-if="hasFilterWellButtons">
+      <Button
+        @click="emit('clear', true)"
+        type="filterWell"
+        size="tiny"
+        icon="cross"
+        iconPosition="right"
+      >
+        Clear all
+      </Button>
     </div>
     <div v-if="showButtons">
       <Button
         :icon-only="true"
         type="filterWell"
         icon="CaretLeft"
-        label="Show previous"
+        label="Show previous term"
         size="tiny"
+        :disabled="focusCounter === minFocusCounter"
+        @click="showPreviousItem"
       />
     </div>
     <div
       ref="filterButtonContainer"
       class="flex flex-row flex-nowrap gap-2 justify-start items-center overflow-x-auto"
     >
-      <slot name="filterButtons"></slot>
+      <slot></slot>
     </div>
     <div v-if="showButtons">
       <Button
         :icon-only="true"
         type="filterWell"
         icon="CaretRight"
-        label="Show next"
+        label="Show next term"
         size="tiny"
+        :disabled="focusCounter === maxFocusCounter"
+        @click="showNextItem"
       />
     </div>
   </div>
