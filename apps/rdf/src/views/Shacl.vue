168374a9ef54eca66ec9041a232c693d06780272
Sander van den Hoek <savandenhoek@gmail.com>
2025-08-22 13:40:55 +0200

    Added a check whether shaclSets have been retrieved

--- apps/rdf/src/views/Shacl.vue
+++ apps/rdf/src/views/Shacl.vue
@@ -4,19 +4,26 @@
     >output is deemed valid if nodes adhere to the requirements or those nodes
     are not present</InfoPopover
   >
+  <MessageError v-if="error">{{ error }}</MessageError>
   <ShaclSetItem v-for="shaclSet in shaclSets" :shaclSet="shaclSet" />
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import { parse } from "yaml";
-import { InfoPopover } from "molgenis-components";
+import { InfoPopover, MessageError } from "molgenis-components";
 import ShaclSetItem from "../components/ShaclSetItem.vue";
 
 const shaclSets = ref(null);
+const error = ref("");
 async function fetchShacls() {
   const res = await fetch("/api/rdf?shacls");
-  shaclSets.value = parse(await res.text());
+  if (res.status === 200) {
+    shaclSets.value = parse(await res.text());
+  } else {
+    error.value =
+      "Could not load available SHACL sets. Please check if you have access to any schema's to validate.";
+  }
 }
 fetchShacls();
 </script>
