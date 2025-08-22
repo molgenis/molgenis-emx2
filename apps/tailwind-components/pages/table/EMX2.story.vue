b575fe7a054923b21609e69e67e6e7c476587219
connoratrug <c.stroomberg@rug.nl>
2025-08-22 14:10:49 +0200

    use relative path

--- apps/tailwind-components/pages/table/EMX2.story.vue
+++ apps/tailwind-components/pages/table/EMX2.story.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed, ref, watch } from "vue";
 import type { ITableSettings, Resp, Schema } from "../../types/types";
-import DemoDataControles from "~/DemoDataControles.vue";
+import DemoDataControles from "../../DemoDataControles.vue";
 import type { ITableMetaData } from "../../../metadata-utils/src/types";
 
 const tableSettings = ref<ITableSettings>({
