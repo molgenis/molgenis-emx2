c0813ec0c043ff97420e1f81787097cb7568f4bd
Joris de Keijser <chinook25@gmail.com>
2025-08-27 11:38:53 +0200

    fix(directory): filters on browser back button (#5314)

--- apps/directory/src/App.vue
+++ apps/directory/src/App.vue
@@ -16,19 +16,17 @@ import { useFavicon, usePreferredDark } from "@vueuse/core";
 //@ts-expect-error
 import { Molgenis } from "molgenis-components";
 import { computed, onMounted, ref, watch } from "vue";
-import { LocationQuery, useRoute } from "vue-router";
+import { useRoute } from "vue-router";
 import Error from "./components/Error.vue";
-import { applyBookmark, createBookmark } from "./functions/bookmarkMapper";
-import { useCheckoutStore } from "./stores/checkoutStore";
+import { applyBookmark } from "./functions/bookmarkMapper";
+import router from "./router";
 import { useFiltersStore } from "./stores/filtersStore";
 import { useSettingsStore } from "./stores/settingsStore";
-import router from "./router";
 
 const route = useRoute();
 const query = computed(() => route.query);
 
 const filtersStore = useFiltersStore();
-const checkoutStore = useCheckoutStore();
 const settingsStore = useSettingsStore();
 
 const banner = computed(() => settingsStore.config.banner);
@@ -36,44 +34,19 @@ const footer = computed(() => settingsStore.config.footer);
 
 const session = ref({});
 
+window.onpopstate = function () {
+  filtersStore.bookmarkWaitingForApplication = true;
+  applyBookmark(query.value);
+};
+
 watch(session, () => {
   settingsStore.setSessionInformation(session.value);
 });
 
-watch(
-  query,
-  (newQuery: LocationQuery, oldQuery) => {
-    if (newQuery && Object.keys(newQuery).length) {
-      const remainingKeys = Object.keys(newQuery).filter(
-        (key) => key !== "cart"
-      );
-      /** if we only have a cart we do not need to wait for the filters to be applied before updating the biobank cards. */
-      if (remainingKeys.length > 0) {
-        filtersStore.bookmarkWaitingForApplication = true;
-      }
-    } else if (
-      oldQuery &&
-      Object.keys(oldQuery).length > 0 &&
-      newQuery &&
-      Object.keys(newQuery).length === 0
-    ) {
-      createBookmark(
-        filtersStore.filters,
-        checkoutStore.selectedCollections,
-        checkoutStore.selectedServices,
-        filtersStore.filterType
-      );
-    }
-
-    if (filtersStore.filterOptionsReady && !checkoutStore.cartUpdated) {
-      applyBookmark(newQuery);
-    }
-  },
-  { immediate: true, deep: true }
-);
-
 onMounted(async () => {
-  await settingsStore.configurationPromise;
+  filtersStore.bookmarkWaitingForApplication = true;
+  await router.isReady();
+  applyBookmark(query.value);
   initMatomo();
   changeFavicon();
 });
