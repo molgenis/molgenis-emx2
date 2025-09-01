03b25757bd8b6c0b67e9ba1507ade0ad28544062
Oudijk <w.f.oudijk@MacBook-Pro-van-Oudijk.local>
2025-09-01 11:21:04 +0200

    Fix backend test

--- backend/molgenis-emx2-datamodels/src/test/java/org/molgenis/emx2/datamodels/PatientRegistryTest.java
+++ backend/molgenis-emx2-datamodels/src/test/java/org/molgenis/emx2/datamodels/PatientRegistryTest.java
@@ -15,6 +15,6 @@ public class PatientRegistryTest extends TestLoaders {
 
   @Test
   void testPatientRegistryLoader() {
-    assertEquals(49, patientRegistry.getTableNames().size());
+    assertEquals(50, patientRegistry.getTableNames().size());
   }
 }
