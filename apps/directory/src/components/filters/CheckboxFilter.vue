1b836b7fa80be0fd0f1df11afb702cd79681c742
de Keijser <j.j.de.keijser@umcg.nl>
2025-09-01 10:37:07 +0200

    chore: add ts to toggle and checkbox filters

--- apps/directory/src/components/filters/CheckboxFilter.vue
+++ apps/directory/src/components/filters/CheckboxFilter.vue
@@ -27,133 +27,87 @@
   </div>
 </template>
 
-<script>
-import MatchTypeRadiobutton from "./base/MatchTypeRadiobutton.vue";
-import CheckboxComponent from "./base/CheckboxComponent.vue";
-import { useSettingsStore } from "../../stores/settingsStore";
+<script setup lang="ts">
+import { computed, ref, watch } from "vue";
+import { IFilterOption } from "../../interfaces/interfaces";
 import { useFiltersStore } from "../../stores/filtersStore";
+import { useSettingsStore } from "../../stores/settingsStore";
+import CheckboxComponent from "./base/CheckboxComponent.vue";
+import MatchTypeRadiobutton from "./base/MatchTypeRadiobutton.vue";
 
-export default {
-  setup() {
-    const settingsStore = useSettingsStore();
-    const filtersStore = useFiltersStore();
-    return { settingsStore, filtersStore };
-  },
-  name: "CheckboxFilter",
-  components: {
-    MatchTypeRadiobutton,
-    CheckboxComponent,
-  },
-  props: {
-    facetTitle: {
-      type: String,
-      required: true,
-    },
-    /** a JSON friendly identifier */
-    facetIdentifier: {
-      type: String,
-      required: true,
-    },
-    /**
-     * A Promise-function that resolves with an array of options.
-     * { text: 'foo', value: 'bar' }
-     */
-    options: {
-      type: [Function],
-      required: true,
-    },
-    currentlyActive: {
-      type: Boolean,
-      required: false,
-      default: () => false,
-    },
-    /**
-     * An array that contains values of options
-     * which is used to only show the checkboxes that match
-     * these values
-     */
-    optionsFilter: {
-      type: Array,
-      required: false,
-    },
-    /**
-     * Whether to use (De)Select All or not.
-     */
-    selectAll: {
-      type: Boolean,
-      required: false,
-      default: () => true,
-    },
-    showMatchTypeSelector: {
-      type: Boolean,
-      default: () => false,
-    },
-  },
-  data() {
-    return {
-      resolvedOptions: [],
-      reducedOptions: [],
-    };
-  },
-  watch: {
-    optionsFilter(newValue) {
-      if (!this.currentlyActive || this.filterSelection.length === 0) {
-        this.reducedOptions = newValue;
-      }
-    },
-  },
-  computed: {
-    uiText() {
-      return this.settingsStore.uiText;
-    },
-    selectAllText() {
-      if (this.filterSelection && this.filterSelection.length > 0) {
-        return this.uiText["deselect_all"];
-      } else {
-        return this.uiText["select_all"];
-      }
-    },
-    checkboxOptions() {
-      if (this.reducedOptions.length) {
-        const selectedValues = this.filterSelection.map(
-          (selection) => selection.value
-        );
+const settingsStore = useSettingsStore();
+const filtersStore = useFiltersStore();
 
-        return this.resolvedOptions.filter(
-          (option) =>
-            this.reducedOptions.includes(option.value) ||
-            selectedValues.includes(option.value)
-        );
-      } else {
-        return this.resolvedOptions;
-      }
-    },
-    filterSelection: {
-      get() {
-        return this.filtersStore.getFilterValue(this.facetIdentifier) || [];
-      },
-      set(value) {
-        this.filtersStore.updateFilter(this.facetIdentifier, value);
-      },
-    },
-  },
-  methods: {
-    toggleSelect() {
-      if (this.filterSelection && this.filterSelection.length > 0) {
-        this.filterSelection = [];
-      } else {
-        this.filterSelection = this.checkboxOptions;
-      }
-    },
-  },
-  created() {
-    this.reducedOptions = this.optionsFilter || [];
+const props = defineProps<{
+  facetTitle: string;
+  facetIdentifier: string;
+  options: () => Promise<Array<{ text: string; value: string }>>;
+  currentlyActive?: boolean;
+  optionsFilter?: Array<string>;
+  selectAll?: boolean;
+  showMatchTypeSelector?: boolean;
+}>();
 
-    this.options().then((response) => {
-      this.resolvedOptions = response;
-    });
+const resolvedOptions = ref<Array<{ text: string; value: string }>>([]);
+const reducedOptions = ref<Array<string>>(props.optionsFilter || []);
+
+props.options().then((response) => {
+  resolvedOptions.value = response;
+});
+
+watch(
+  () => props.optionsFilter,
+  (newValue) => {
+    if (
+      !props.currentlyActive ||
+      (filtersStore.getFilterValue(props.facetIdentifier) || []).length === 0
+    ) {
+      reducedOptions.value = newValue || [];
+    }
   },
-};
+  { immediate: true }
+);
+
+const selectAllText = computed(() => {
+  if (filterSelection.value && filterSelection.value.length > 0) {
+    return settingsStore.uiText.value["deselect_all"];
+  } else {
+    return settingsStore.uiText.value["select_all"];
+  }
+});
+
+const checkboxOptions = computed(() => {
+  if (reducedOptions.value.length) {
+    const selectedValues = filterSelection.value.map(
+      (selection: IFilterOption) => selection.value
+    );
+
+    return resolvedOptions.value.filter(
+      (option) =>
+        reducedOptions.value.includes(option.value) ||
+        selectedValues.includes(option.value)
+    );
+  } else {
+    return resolvedOptions.value;
+  }
+});
+
+const filterSelection = computed({
+  get() {
+    return filtersStore.getFilterValue(props.facetIdentifier) || [];
+  },
+  set(value) {
+    filtersStore.updateFilter(props.facetIdentifier, value);
+  },
+});
+
+function toggleSelect() {
+  if (filterSelection.value?.length) {
+    filterSelection.value = [];
+  } else {
+    filterSelection.value = checkboxOptions.value;
+  }
+}
 </script>
 
 <style scoped>
