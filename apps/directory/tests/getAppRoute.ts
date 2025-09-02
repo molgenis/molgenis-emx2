73a567b76432a76445a4ad957a5f95f434e86bd3
Joris de Keijser <chinook25@gmail.com>
2025-09-02 15:38:24 +0200

    Update getAppRoute.ts

--- apps/directory/tests/getAppRoute.ts
+++ apps/directory/tests/getAppRoute.ts
@@ -1,5 +1,4 @@
 export function getAppRoute() {
-  return "http://localhost:5173/";
   return process.env.E2E_BASE_URL
     ? "directory-demo/directory/"
     : process.env.MOLGENIS_APPS_SCHEMA ?? "";
