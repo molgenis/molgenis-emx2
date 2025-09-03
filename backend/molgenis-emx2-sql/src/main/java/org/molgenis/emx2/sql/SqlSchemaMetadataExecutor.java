b8cd5f96913ef9049b33b4e1fcd05c0a06dbe662
Harm Brugge <harmbrugge@gmail.com>
2025-09-03 10:18:02 +0200

    longer timeout for jooq for failing tests

--- backend/molgenis-emx2-sql/src/main/java/org/molgenis/emx2/sql/SqlSchemaMetadataExecutor.java
+++ backend/molgenis-emx2-sql/src/main/java/org/molgenis/emx2/sql/SqlSchemaMetadataExecutor.java
@@ -21,9 +21,9 @@ class SqlSchemaMetadataExecutor {
   }
 
   static void executeCreateSchema(SqlDatabase db, SchemaMetadata schema) {
-    db.getJooq().createSchema(schema.getName()).execute();
+    db.getJooqWithExtendedTimeout().createSchema(schema.getName()).execute();
 
-    MetadataUtils.saveSchemaMetadata(db.getJooq(), schema);
+    MetadataUtils.saveSchemaMetadata(db.getJooqWithExtendedTimeout(), schema);
   }
 
   static void executeAddMembers(DSLContext jooq, Schema schema, Member member) {
