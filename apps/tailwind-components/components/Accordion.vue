aca4933935f13bfc1949190984ee33998a1e4a57
dcruvolo <davidruvolo51@gmail.com>
2025-09-26 11:25:59 +0200

    fix: corrected display type

--- apps/tailwind-components/components/Accordion.vue
+++ apps/tailwind-components/components/Accordion.vue
@@ -48,7 +48,7 @@ const elemId = computed<string>(() => {
       :id="`${elemId}-content`"
       class="px-5 pb-5"
       :class="{
-        static: isExpanded,
+        block: isExpanded,
         hidden: !isExpanded,
       }"
     >
