8798e582bed7d4806e45fa57aa0062b86aa0603b
dcruvolo <davidruvolo51@gmail.com>
2025-10-02 15:02:01 +0200

    added navigation link to pages

--- apps/ui/layouts/default.vue
+++ apps/ui/layouts/default.vue
@@ -114,6 +114,7 @@ const navigation = computed(() => {
   ];
   if (schema.value && isAdmin.value) {
     items.push({ label: "Analytics", link: `/${schema.value}/analytics` });
+    items.push({ label: "Pages", link: `/${schema.value}/pages` });
   }
   if (isAdmin.value) {
     items.push({ label: "Admin", link: `/admin` });
