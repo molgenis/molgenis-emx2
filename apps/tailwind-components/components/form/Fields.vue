c5cce5789a916a3ec4bc4b45f72771b6bd3ac89c
mswertz <m.a.swertz@rug.nl>
2025-10-02 20:38:48 +0200

    primary key field should be disabled in update form

--- apps/tailwind-components/components/form/Fields.vue
+++ apps/tailwind-components/components/form/Fields.vue
@@ -86,6 +86,7 @@ const isRequired = (value: string | boolean): boolean =>
         :type="column.columnType"
         :label="column.label"
         :description="column.description"
+        :disabled="column.readonly || (rowKey && column.key === 1)"
         :rowKey="rowKey"
         :required="isRequired(column.required ?? false)"
         :error-message="errorMap[column.id]"
