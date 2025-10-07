6c10f0c624f9bbfa4748a5c0065e4877d5a9235d
Sander van den Hoek <savandenhoek@gmail.com>
2025-10-07 12:05:15 +0200

    fixed multi-level deep pages (within schema in new UI) to load

---
+++ apps/ui/tests/e2e/multi-layer-deep-page.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test";
+
+// Simple test that ensures 2-layer deep pages (counting from schema) are reachable.
+// Smoketest was not sufficient (missing js files & returned path deviated from actual path)
+test("test 2 level deep schema page being available", async ({ page }) => {
+  await page.goto("/apps/ui/pet%20store/rdf/shacl");
+  await Promise.all([
+    expect(page.locator("html")).not.toContainText("File not found:"),
+    expect(page.locator("h2")).toContainText("SHACL validation"),
+  ]);
+});
