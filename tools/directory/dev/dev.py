c300a3befc72f98fc89c1233acfea402bd6775d6
Hessel Haagsma <h.haagsma@umcg.nl>
2025-10-08 13:14:56 +0200

    feat(directory): add file ingest functionality to EMX2 directory client (#5250)
    * Don't stage external nodes again at publishing stage
    
    * Add docstring
    
    * ?
    
    * Check for data_refresh rather than dns being non-empty
    
    * Use data_refresh instead of dns column
    
    * Specify where to find .env
    
    * Change pid service setup
    
    * Create functioning file ingest
    
    * Cleanup code
    
    * Handle error when server not reachable/file not found
    
    * Don't publish if staging fails
    
    * Update changelog
    
    * Add missing docstrings to stager and model modules
    
    * Assert prepare is called rather than stage (which isn't called anymore anyway)
    
    * No need for ANY, use state
    
    * Don't test staging during publishing anymore
    
    * No more staging during publishing, so test removed
    
    * Make PID service type environment variable
    
    * Retro-actively update changelog
    
    * Re-order dev script to separate staging and publishing steps
    
    * Handle empty files, neater URL join, update TableMeta calls
    
    * Remove test on network inheritance
    
    * Remove on parent_network
    
    * Update version
    
    * Add a trailing slash to the base url so URLs with and without trailing slash are joined correctly
    
    * TableType instead of List[TableType]
    
    * _file_to_table is static
    
    * TableMeta.meta can also be list[Column]

--- tools/directory/dev/dev.py
+++ tools/directory/dev/dev.py
@@ -14,6 +14,8 @@ from tools.directory.src.molgenis_emx2.directory_client.directory_client import 
 )
 from tools.directory.src.molgenis_emx2.directory_client.pid_service import (
     DummyPidService,
+    NoOpPidService,
+    PidService,
 )
 
 # Get credentials from .env
@@ -23,9 +25,18 @@ target = os.getenv("TARGET")
 username = os.getenv("USERNAME")
 password = os.getenv("PASSWORD")
 directory_schema = os.getenv("DIRECTORY")
+pid_service_type = os.getenv(
+    "PID_SERVICE"
+)  # Select from: ['production', 'dummy', 'none']
 
 
 async def sync_directory():
+    """
+    Stage external nodes and publish them to the directory schema
+
+    Note: when staging an external server-type node the .env file should include
+    a "node"_user="token" with view permissions on the external staging area
+    """
     # Set up the logger
     logging.basicConfig(level="INFO", format=" %(levelname)s: %(name)s: %(message)s")
     logging.getLogger("requests").setLevel(logging.WARNING)
@@ -36,28 +47,29 @@ async def sync_directory():
         # Apply the 'signin' method with the username and password
         session.signin(username, password)
 
-        # Get the nodes you want to work with
-        # When staging a node the .env file should include a "node"_user="token"
-        # with view permissions on the external staging area
-        nodes_to_stage = session.get_external_nodes()
-        nodes_to_publish = session.get_nodes()
-
         # Create PidService
-        # pid_service = PidService.from_credentials("pyhandle_creds.json")
-        # print(f"Script runs on server {pid_service.base_url}")
-        # Use the DummyPidService if testing without interacting with a handle server
-        pid_service = DummyPidService()
-        # Use the NoOpPidService if you want to turn off the PID features completely
-        # pid_service = NoOpPidService()
+        if pid_service_type == 'production':
+            pid_service = PidService.from_credentials("pyhandle_creds.json")
+            print(f"Script runs on server {pid_service.base_url}")
+        elif pid_service_type == 'dummy':
+            # Use DummyPidService if testing without interacting with a handle server
+            pid_service = DummyPidService()
+        else:
+            # Use NoOpPidService if you want to turn off the PID features completely
+            pid_service = NoOpPidService()
 
-        # Instantiate the Directory class and do some work
+        # Instantiate the Directory class
         directory = Directory(session, pid_service)
+
+        # Stage
+        nodes_to_stage = session.get_external_nodes()
         staging_report = await directory.stage_external_nodes(nodes_to_stage)
-        publishing_report = await directory.publish_nodes(nodes_to_publish)
-
         if staging_report.has_errors():
-            raise ValueError("Some nodes did not publish correctly")
+            raise ValueError("Some nodes did not stage correctly")
 
+        # Publish
+        nodes_to_publish = session.get_nodes()
+        publishing_report = await directory.publish_nodes(nodes_to_publish)
         if publishing_report.has_errors():
             raise ValueError("Some nodes did not publish correctly")
 
