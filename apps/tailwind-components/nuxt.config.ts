cf906ed8eafa6530f8bd4cf2066b8ea9332fa6d4
dcruvolo <davidruvolo51@gmail.com>
2025-10-22 13:57:33 +0200

    undo accidental commit

--- apps/tailwind-components/nuxt.config.ts
+++ apps/tailwind-components/nuxt.config.ts
@@ -74,8 +74,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      // apiBase: "https://emx2.dev.molgenis.org/",
-      apiBase: "http://localhost:8080",
+      apiBase: "https://emx2.dev.molgenis.org/",
       sourceCodeMap: sourceCodeMap,
     },
   },
