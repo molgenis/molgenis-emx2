635db9b98541b6c3ebb03893d92f6c9ec636e3ca
YpeZ <info@ypezijlstra.frl>
2025-10-28 15:15:07 +0100

    * created skeleton for tests * wrote test `test_schema_fail` * wrote test `test_table_fail` * wrote test `test_columns_fail`

---
+++ tools/pyclient/tests/test_get.py
@@ -0,0 +1,88 @@
+"""
+Tests the Pyclient `get` method.
+"""
+
+import os
+from pathlib import Path
+
+import pandas as pd
+import pytest
+from dotenv import load_dotenv
+
+from src.molgenis_emx2_pyclient import Client
+from src.molgenis_emx2_pyclient.exceptions import NoSuchSchemaException, \
+    NoSuchTableException, NoSuchColumnException
+
+load_dotenv()
+server_url = os.environ.get("MG_SERVER")
+username = os.environ.get("MG_USERNAME")
+password = os.environ.get("MG_PASSWORD")
+
+RESOURCES_DIR = Path(__file__).parent / "resources"
+
+
+def test_schema_fail():
+    """Tests failing get by giving no/incorrect schema name."""
+    with Client(url=server_url) as client:
+        client.signin(username, password)
+        with pytest.raises(NoSuchSchemaException) as excinfo:
+            client.get(schema="Pet Store", table="Pet")
+        assert excinfo.value.msg == "Schema 'Pet Store' not available."
+
+def test_table_fail():
+    """Tests failing get by giving no/incorrect table name."""
+    with Client(url=server_url) as client:
+        client.signin(username, password)
+        with pytest.raises(NoSuchTableException) as excinfo:
+            client.get(schema="pet store", table="Pets")
+        assert excinfo.value.msg == "Table 'Pets' not found in schema 'pet store'."
+
+def test_columns_fail():
+    """Tests failing get by giving incorrect column names."""
+    with Client(url=server_url) as client:
+        client.signin(username, password)
+        with pytest.raises(NoSuchColumnException) as excinfo:
+            client.get(schema="pet store", table="Pet", columns=["Name"])
+        assert excinfo.value.msg == "Columns 'Name' not found."
+        with pytest.raises(NoSuchColumnException) as excinfo:
+            client.get(schema="pet store", table="Pet", columns=["Name", "name"])
+        assert excinfo.value.msg == "Columns ['Name'] not in index"
+        with pytest.raises(NoSuchColumnException) as excinfo:
+            client.get(schema="pet store", table="Pet", columns=["Name", "name2"])
+        assert excinfo.value.msg == "Columns 'Name', 'name2' not found."
+
+def test_columns_okay():
+    """Tests get with specifying columns."""
+    ...
+
+def test_query_filter_fail():
+    """Tests get fail with incorrect query filter."""
+    ...
+
+def test_equals_filter():
+    """Tests the 'equals' filter for the query filter parameter."""
+    ...
+
+def test_greater_filter():
+    """Tests the 'greater than' filter for the query filter parameter."""
+    ...
+
+def test_smaller_filter():
+    """Tests the 'smaller than' filter for the query filter parameter."""
+    ...
+
+def test_unequal_filter():
+    """Tests the 'unequal' filter for the query filter parameter."""
+    ...
+
+def test_between_filter():
+    """Tests the 'between' filter for the query filter parameter."""
+    ...
+
+def test_multiple_filters():
+    """Tests the query filter parameter with multiple filters."""
+    ...
+
+def test_as_df():
+    """Tests the method where the results are returned as pandas DataFrame."""
+    ...
