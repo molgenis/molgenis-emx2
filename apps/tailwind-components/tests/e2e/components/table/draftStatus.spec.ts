93b64ab2bfc9ed84727113bb5343e6e68b0de205
connoratrug <c.stroomberg@rug.nl>
2025-11-05 12:10:44 +0100

    fix: show/hide draft label in edit form

--- apps/tailwind-components/tests/e2e/components/table/draftStatus.spec.ts
+++ apps/tailwind-components/tests/e2e/components/table/draftStatus.spec.ts
@@ -6,16 +6,13 @@ const route = playwrightConfig?.use?.baseURL?.startsWith("http://localhost")
   ? ""
   : "/apps/tailwind-components/#/";
 
+test("the draft status is displayed correctly", async ({ page }) => {
+  await page.goto(`${route}table/EMX2.story?schema=pet+store&table=Pet`);
 
-test("the draft status is displayed correctly", async ({
-  page,
-}) => {
-   await page.goto(`${route}table/EMX2.story?schema=pet+store&table=Pet`);
-
-   // Verify the expected row is present
-   await expect(page.getByRole('cell', { name: 'yakul' })).toBeVisible();
-   // Verify the draft status is on the expected row
-   await expect(page.locator(' table > tbody > tr:nth-child(9)')).toContainText("Draft yakulherbivorous mammals ");
-
+  // Verify the expected row is present
+  await expect(page.getByRole("cell", { name: "yakul" })).toBeVisible();
+  // Verify the draft status is on the expected row
+  await expect(page.locator(" table > tbody > tr:nth-child(9)")).toContainText(
+    "Draft yakulherbivorous mammals "
+  );
 });
-   
