e00af9e58210367b1a1951b00866b49e57ea4bad
Sander van den Hoek <savandenhoek@gmail.com>
2025-11-18 13:57:30 +0100

    added component page for hyperlink

---
+++ apps/tailwind-components/app/pages/text/Hyperlink.story.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <span>Some text with </span><Hyperlink to="./" label="an internal hyperlink" type="static" /><span> in it.</span>
+  </div>
+  <div>
+    <span>Some text with </span><Hyperlink to="./" label="an external hyperlink" /><span> in it.</span>
+  </div>
+  <div>
+    <span>Some text with </span><Hyperlink to="./" label="an external hyperlink with icon" icon="handshake" /><span> in it.</span>
+  </div>
+
+</template>
+<script setup lang="ts">
+import ButtonPageheaderBack from "../components/button/PageHeaderBack.vue";
+import CustomTooltip from "../components/CustomTooltip.vue";
+import Hyperlink from "~/components/text/Hyperlink.vue";
+</script>
