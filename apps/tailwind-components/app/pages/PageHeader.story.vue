0d023dff0d690eed69bb036c2f753a6867b17c26
Sander van den Hoek <savandenhoek@gmail.com>
2025-11-19 16:00:15 +0100

    removed example

--- apps/tailwind-components/app/pages/PageHeader.story.vue
+++ apps/tailwind-components/app/pages/PageHeader.story.vue
@@ -46,33 +46,4 @@ import TextHyperlink from "../components/text/Hyperlink.vue";
       </PageHeader>
     </div>
   </div>
-  <div>
-    <p class="pb-3 text-title">
-      Center-aligned Pageheader with text-based (title) prefix/suffix & long
-      description that is truncated:
-    </p>
-    <div>
-      <PageHeader
-        title="My Title"
-        description='This is some dummy text to create a string of over 250 characters long to show that it is initially truncated. You can view the full text by pressing "..." at the end of this description to view the text that is initially hidden but then suddenly appears and allows the user to read the full description.'
-        backPath="/samples/rowEdit"
-      >
-        <template #title-prefix>
-          <span>title-prefix text</span>
-        </template>
-        <template #title-suffix>
-          <span
-            >a much longer text in title-suffix slot (title stays
-            centered)</span
-          >
-        </template>
-        <template #prefix>
-          <span>prefix</span>
-        </template>
-        <template #suffix>
-          <span>suffix</span>
-        </template>
-      </PageHeader>
-    </div>
-  </div>
 </template>
