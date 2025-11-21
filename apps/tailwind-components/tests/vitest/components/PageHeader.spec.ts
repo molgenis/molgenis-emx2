007182c44565be053e0e6f0cfa3afa6faeba02c3
Sander van den Hoek <savandenhoek@gmail.com>
2025-11-21 14:51:27 +0100

    added simple test for PageHeader

---
+++ apps/tailwind-components/tests/vitest/components/PageHeader.spec.ts
@@ -0,0 +1,15 @@
+import {mount} from "@vue/test-utils";
+import { expect, test } from "vitest";
+import PageHeader from "../../../app/components/PageHeader.vue";
+import ContentReadMore from "../../../app/components/ContentReadMore.vue";
+
+const pageHeaderSimple = mount(PageHeader, {
+  props: {
+    title: "My Title",
+    description: "My description"
+  },
+});
+
+test("PageHeader should contain truncation component by default (for description)", async () => {
+  expect(pageHeaderSimple.findComponent(ContentReadMore).exists()).toBe(true);
+});
