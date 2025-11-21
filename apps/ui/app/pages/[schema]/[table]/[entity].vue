eb6eef8605908603c79ebe805b6ebc7a7e83ce6d
connoratrug <c.stroomberg@rug.nl>
2025-11-21 15:08:30 +0100

    add some btns when signed in as admin

--- apps/ui/app/pages/[schema]/[table]/[entity].vue
+++ apps/ui/app/pages/[schema]/[table]/[entity].vue
@@ -9,11 +9,13 @@ import BreadCrumbs from "../../../../../tailwind-components/app/components/Bread
 import PageHeader from "../../../../../tailwind-components/app/components/PageHeader.vue";
 import fetchTableMetadata from "../../../../../tailwind-components/app/composables/fetchTableMetadata";
 import fetchRowData from "../../../../../tailwind-components/app/composables/fetchRowData";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import DefinitionList from "../../../../../tailwind-components/app/components/DefinitionList.vue";
 import DefinitionListTerm from "../../../../../tailwind-components/app/components/DefinitionListTerm.vue";
 import DefinitionListDefinition from "../../../../../tailwind-components/app/components/DefinitionListDefinition.vue";
 import ValueEMX2 from "../../../../../tailwind-components/app/components/value/EMX2.vue";
+import InputSearch from "../../../../../tailwind-components/app/components/input/Search.vue";
+import Button from "../../../../../tailwind-components/app/components/Button.vue";
 
 const route = useRoute();
 const schemaId = route.params.schema as string;
@@ -66,6 +68,8 @@ const sections = computed(() => {
       return section.fields.length > 0;
     });
 });
+
+const filterValue = ref("");
 </script>
 <template>
   <section class="mx-auto lg:px-[30px] px-0">
@@ -81,6 +85,21 @@ const sections = computed(() => {
         />
       </template>
     </PageHeader>
+
+    <div class="flex pb-[30px] gap-[10px] items-center" v-if="isAdmin">
+      <InputSearch
+        class="w-3/5 xl:w-2/5 2xl:w-1/5"
+        v-model="filterValue"
+        :placeholder="`Filter fields...`"
+        id="filter-input"
+      />
+
+      <div class="flex gap-[10px]">
+        <Button type="secondary" icon="edit" @click="">Edit </Button>
+        <Button type="secondary" icon="trash" @click="">Delete </Button>
+      </div>
+    </div>
+
     <section
       v-for="section in sections"
       class="first:pt-[50px] last:pb-[100px]"
