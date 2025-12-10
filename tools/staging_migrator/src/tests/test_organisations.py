e1242303cc9509fda9d50f6ef8294c38727b7b4a
YpeZ <info@ypezijlstra.frl>
2025-12-10 09:26:20 +0100

    wip: created test data for Organisations migration

---
+++ tools/staging_migrator/src/tests/test_organisations.py
@@ -0,0 +1,58 @@
+"""
+Tests the correct publication of an Organisation from a staging area onto a catalogue schema.
+"""
+
+
+import logging
+import os
+
+import pytest
+import sys
+
+from dotenv import load_dotenv
+
+from staging_migrator.src.molgenis_emx2_staging_migrator import StagingMigrator
+from staging_migrator.src.tests.utils import RESOURCES_PATH, zip_folder
+
+CATALOGUE = 'catalogue'
+STAGING_AREA = 'cohortStaging'
+
+log = logging.getLogger('publisher')
+
+# Set up the logger
+logging.basicConfig(level='DEBUG')
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+load_dotenv()
+
+@pytest.mark.asyncio
+async def test_organisations():
+    """
+    Creates a staging area schema.
+    Uploads data to this staging area.
+    Migrates the data using the StagingMigrator.
+    Asserts the data has been copied correctly to the catalogue.
+    Deletes the entries from the catalogue.
+    Deletes the staging area schema.
+    :return:
+    """
+
+    server_url = os.environ.get('MG_URL')
+    token = os.environ.get('MG_TOKEN')
+
+    zip_folder("test_organisations")
+
+    with StagingMigrator(url=server_url, token=token, target=CATALOGUE) as migrator:
+
+        if STAGING_AREA in migrator.schema_names:
+            await migrator.delete_schema(STAGING_AREA)
+        await migrator.create_schema(name=STAGING_AREA, template="DATA_CATALOGUE_COHORT_STAGING")
+        migrator.set_source(STAGING_AREA)
+        await migrator.upload_file(RESOURCES_PATH /"test_organisations.zip")
+
+        assert len(migrator.get(table="Resources")) == 1
+
+        migrator.migrate(keep_zips=True)
+
+
+
