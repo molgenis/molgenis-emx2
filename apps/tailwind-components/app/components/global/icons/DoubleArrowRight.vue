f8335f932c780c1c15f5bc2eece140bedc4af827
Sander van den Hoek <savandenhoek@gmail.com>
2025-12-10 16:03:23 +0100

    re-parsed icons after formatting SVGs

--- apps/tailwind-components/app/components/global/icons/DoubleArrowRight.vue
+++ apps/tailwind-components/app/components/global/icons/DoubleArrowRight.vue
@@ -1,3 +1,5 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="M383-480 200-664l56-56 240 240-240 240-56-56 183-184Zm264 0L464-664l56-56 240 240-240 240-56-56 183-184Z"/></svg> 
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+    <path d="M383-480 200-664l56-56 240 240-240 240-56-56 183-184Zm264 0L464-664l56-56 240 240-240 240-56-56 183-184Z"/>
+</svg> 
 </template>
