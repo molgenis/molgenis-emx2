14d373f15cd4e656e5272dda56a7af07b80b39b8
Sander van den Hoek <savandenhoek@gmail.com>
2025-12-16 13:11:27 +0100

    more formatting

--- apps/metadata-utils/src/generic.ts
+++ apps/metadata-utils/src/generic.ts
@@ -3,7 +3,12 @@
 // DONE -> process finished
 // INVALID -> process finished (no error): input/output validation failed
 // ERROR -> process did not finish correctly (f.e. non-200 HTTP code or non-0 exit code)
-export type ProcessStatus = "UNKNOWN" | "RUNNING" | "DONE" | "INVALID" | "ERROR";
+export type ProcessStatus =
+  | "UNKNOWN"
+  | "RUNNING"
+  | "DONE"
+  | "INVALID"
+  | "ERROR";
 
 export interface ProcessData {
   status: ProcessStatus;
