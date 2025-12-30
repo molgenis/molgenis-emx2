f17a5927457ca58dc9a4215887d26733ae9f0f60
p237959 <47183404+connoratrug@users.noreply.github.com>
2025-12-30 11:35:11 +0100

    fix: tw select input type (#5711)
    - add missing prop to render as select
    
    Closes #5705

--- apps/tailwind-components/app/components/Input.vue
+++ apps/tailwind-components/app/components/Input.vue
@@ -190,7 +190,7 @@
     :refLabel="refLabel"
     @focus="emit('focus')"
     @blur="emit('blur')"
-    :align="align"
+    :is-array="false"
   />
   <InputRef
     v-else-if="'MULTISELECT' === typeUpperCase"
