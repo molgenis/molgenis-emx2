0d556f7cbc07cdfc12afccf2b58373dc1b9eac91
connoratrug <c.stroomberg@rug.nl>
2026-01-07 16:42:55 +0100

    add missing param

--- apps/tailwind-components/tests/e2e/components/form/renderForm.spec.ts
+++ apps/tailwind-components/tests/e2e/components/form/renderForm.spec.ts
@@ -5,7 +5,7 @@ const route = playwrightConfig?.use?.baseURL?.startsWith("http://localhost")
   ? ""
   : "/apps/tailwind-components/#/";
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ page }, testInfo) => {
   await page.goto(`${route}Form.story?schema=catalogue-demo&table=Resources`);
   testInfo.setTimeout(testInfo.timeout + 30_000);
 });
