113fa2d58168415758a5e2516a46a96cdb215c96
dcruvolo <davidruvolo51@gmail.com>
2026-01-08 15:06:18 +0100

    added simple file input test

---
+++ apps/tailwind-components/tests/e2e/components/input/file.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from "@playwright/test";
+import playwrightConfig from "../../../../playwright.config";
+import * as path from "path";
+import { setTimeout } from "timers/promises";
+
+const route = playwrightConfig?.use?.baseURL?.startsWith("http://localhost")
+  ? ""
+  : "/apps/tailwind-components/#/";
+
+test.describe(
+  "File input",
+  {
+    tag: "@tw-components @tw-forms @tw-file",
+  },
+  () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto(`${route}input/File.story`);
+    });
+
+    test("input is empty by default", async ({ page }) => {
+      const inputValueContainer = page.locator(
+        "div[data-elem='current-file-container']"
+      );
+      expect(
+        (inputValueContainer as unknown as HTMLInputElement).children
+      ).toBeFalsy();
+    });
+
+  }
+);
