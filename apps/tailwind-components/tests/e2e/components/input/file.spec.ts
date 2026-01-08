2020355056d28095d2b45730792f991ee8a2a83e
dcruvolo <davidruvolo51@gmail.com>
2026-01-08 15:24:25 +0100

    removed unused imports

--- apps/tailwind-components/tests/e2e/components/input/file.spec.ts
+++ apps/tailwind-components/tests/e2e/components/input/file.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect } from "@playwright/test";
 import playwrightConfig from "../../../../playwright.config";
-import * as path from "path";
-import { setTimeout } from "timers/promises";
 
 const route = playwrightConfig?.use?.baseURL?.startsWith("http://localhost")
   ? ""
