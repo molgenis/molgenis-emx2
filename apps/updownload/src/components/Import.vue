86bc288062673d60f15ed92b3234523e127feb89
mswertz <m.a.swertz@rug.nl>
2026-01-10 23:03:59 +0100

    remove unused stuff

--- apps/updownload/src/components/Import.vue
+++ apps/updownload/src/components/Import.vue
@@ -74,12 +74,6 @@
               <a :href="`/${schema}/api/zip`">csv.zip</a> /
               <a :href="`/${schema}/api/ttl`">ttl</a> /
               <a :href="`/${schema}/api/jsonld`">jsonld</a>
-              <a :href="`/${schema}/api/ttl2`"
-                >ttl filtered by graphql query (experimental)</a
-              >
-              <a :href="`/${schema}/api/ttl2/_schema`"
-                >json-ld schema @context file</a
-              >
             </p>
 
             <div>
