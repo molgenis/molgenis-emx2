95e7f4bbb099f3ad8ea4056216fed7ba91669391
Hessel Haagsma <h.haagsma@umcg.nl>
2026-01-12 11:40:37 +0100

    Tweak footer text

--- apps/directory/src/components/Footer.vue
+++ apps/directory/src/components/Footer.vue
@@ -18,7 +18,7 @@
       >
         Holub et al. (2016)
       </a>
-      on directory use, or
+      for Directory use or
       <a href="https://www.ncbi.nlm.nih.gov/pubmed/30165396" target="_blank">
         Van der Velde et al. (2016)
       </a>
