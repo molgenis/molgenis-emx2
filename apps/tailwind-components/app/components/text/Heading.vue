de7c7dfbe918355c3e4b7483aec210f9572f9b65
dcruvolo <davidruvolo51@gmail.com>
2026-01-12 15:02:43 +0100

    feat: initial text components

---
+++ apps/tailwind-components/app/components/text/Heading.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    headingLevel: number;
+    isCentered?: boolean;
+  }>(),
+  {
+    headingLevel: 2,
+    isCentered: false,
+  }
+);
+
+const h = `h${props.headingLevel}`;
+</script>
+
+<template>
+  <component
+    :is="h"
+    :class="{
+      'text-title text-heading-6xl': h === 'h1',
+      'text-title text-heading-5xl': h === 'h2',
+      'text-title text-heading-4xl': h === 'h3',
+      'text-title text-heading-3xl': h === 'h4',
+      'text-title text-heading-2xl': h === 'h5',
+      'text-title text-heading-xl': h === 'h6',
+      'text-center': isCentered,
+    }"
+  >
+    <slot></slot>
+  </component>
+</template>
---
+++ apps/tailwind-components/app/components/text/Paragraph.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ isCentered: boolean }>(), { isCentered: false });
+</script>
+
+<template>
+  <p><slot /></p>
+</template>
