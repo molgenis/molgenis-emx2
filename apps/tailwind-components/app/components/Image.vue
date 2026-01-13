e38a5a6c0084eb236df21b22d3d6095c951f4272
dcruvolo <davidruvolo51@gmail.com>
2026-01-13 15:24:17 +0100

    feat: init image component

---
+++ apps/tailwind-components/app/components/Image.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { Img } from "~~/types/pages";
+
+withDefaults(defineProps<Img>(), {
+  isCentered: false,
+});
+</script>
+
+<template>
+  <img
+    :src="src"
+    :class="[
+      {
+        'text-center': isCentered,
+      },
+      width,
+      height,
+    ]"
+    :alt="alt"
+  />
+</template>
---
+++ apps/tailwind-components/app/pages/Image.story.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Image from "../../app/components/Image.vue";
+</script>
+
+<template>
+  <Image
+    src="_nuxt/assets/img/molgenis-logo-blue-small.svg"
+    alt="MOLGENIS: open source software"
+    width="w-[150px]"
+    height="h-[150px]"
+  />
+</template>
