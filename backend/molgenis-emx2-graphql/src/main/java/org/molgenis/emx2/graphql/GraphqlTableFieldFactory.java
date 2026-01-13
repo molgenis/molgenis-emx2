f2dfaa0bb72fa7ee1248686f2afd57f28301f220
Sander van den Hoek <savandenhoek@gmail.com>
2026-01-13 13:12:56 +0100

    auto-formatting

--- backend/molgenis-emx2-graphql/src/main/java/org/molgenis/emx2/graphql/GraphqlTableFieldFactory.java
+++ backend/molgenis-emx2-graphql/src/main/java/org/molgenis/emx2/graphql/GraphqlTableFieldFactory.java
@@ -1060,12 +1060,7 @@ public class GraphqlTableFieldFactory {
       case LONG_ARRAY -> GraphQLList.list(GraphQLLong);
       case DECIMAL_ARRAY -> GraphQLList.list(Scalars.GraphQLFloat);
       case JSON -> GraphqlCustomTypes.GraphQLJsonAsString;
-      case STRING_ARRAY,
-              TEXT_ARRAY,
-              DATE_ARRAY,
-              DATETIME_ARRAY,
-              PERIOD_ARRAY,
-              UUID_ARRAY ->
+      case STRING_ARRAY, TEXT_ARRAY, DATE_ARRAY, DATETIME_ARRAY, PERIOD_ARRAY, UUID_ARRAY ->
           GraphQLList.list(Scalars.GraphQLString);
       default ->
           throw new MolgenisException(
