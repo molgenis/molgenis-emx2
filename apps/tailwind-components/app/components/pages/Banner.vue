3bfd041023dfb1ffb95f0371fa66332ac0688143
dcruvolo <davidruvolo51@gmail.com>
2026-01-14 11:17:55 +0100

    fix: adjusted page banner component and story

--- apps/tailwind-components/app/components/pages/Banner.vue
+++ apps/tailwind-components/app/components/pages/Banner.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { Header } from "../../../types/pages";
+import Heading from "../text/Heading.vue";
 
 const props = withDefaults(defineProps<Header>(), {
   enableFullScreenWidth: true,
@@ -8,16 +9,20 @@ const props = withDefaults(defineProps<Header>(), {
 
 <template>
   <header
-    class="relative flex justify-start items-center bg-cover bg-center text-title h-60"
+    class="relative flex justify-start items-center bg-cover bg-center h-72"
+    :class="{
+      'text-gray-100': backgroundImage,
+      'text-title bg-gray-100': !backgroundImage,
+    }"
     :style="backgroundImage ? `background-image: url(${backgroundImage})` : ''"
   >
     <div class="w-page-sections mx-8 z-10">
-      <h1 class="font-display text-heading-3xl">{{ title }}</h1>
-      <p class="text-heading-5xl">{{ subtitle }}</p>
+      <h1 class="font-display text-heading-4xl">{{ title }}</h1>
+      <p class="text-heading-2xl">{{ subtitle }}</p>
     </div>
     <div
       v-if="backgroundImage"
-      class="absolute top-0 left-0 w-full h-full bg-blue-500 bg-opacity-50"
+      class="absolute top-0 left-0 w-full h-full bg-black bg-opacity-60"
     />
   </header>
 </template>
---
+++ apps/tailwind-components/app/pages/pages/Banner.story.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import PageBanner from "../../components/pages/Banner.vue";
+import DemoImage from "../../assets/img/molgenis-banner.jpg";
+import Heading from "../../components/text/Heading.vue";
+</script>
+
+<template>
+  <Heading heading-level="H2" class="my-5"
+    >Page Banner without image (default)</Heading
+  >
+  <PageBanner
+    id="page-banner-demo"
+    title="About Us"
+    subtitle="Duis aliquip reprehenderit nulla et voluptate ex eu qui cillum minim."
+  />
+  <Heading heading-level="H2" class="my-5">Page Banner with image</Heading>
+  <PageBanner
+    id="page-banner-demo"
+    :background-image="DemoImage"
+    title="About Us"
+    subtitle="Nostrud aute exercitation ipsum voluptate commodo et laborum ad labore ad elit."
+  />
+</template>
