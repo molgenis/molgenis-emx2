c0cb33208572c65471504b4eb94b1fedb0d8fe99
connoratrug <c.stroomberg@rug.nl>
2026-01-16 09:25:06 +0100

    fix: consistent page margens
    - use the template to set consistent marging between content and footer, as per design
    - remove component spesific margen that double / break page design

--- apps/ui/app/layouts/default.vue
+++ apps/ui/app/layouts/default.vue
@@ -56,11 +56,11 @@
         </template>
       </Header>
 
-      <main class="mb-auto">
+      <main>
         <slot />
       </main>
 
-      <FooterComponent />
+      <FooterComponent class="mt-[7.8125rem]" />
     </div>
   </div>
 </template>
