c86410cb32ed4aec90f66129567ce402f30d44e1
dcruvolo <davidruvolo51@gmail.com>
2026-01-20 10:58:24 +0100

    fix: updated styles to match figma designs

--- apps/tailwind-components/app/components/pages/Banner.vue
+++ apps/tailwind-components/app/components/pages/Banner.vue
@@ -11,13 +11,13 @@ withDefaults(defineProps<Header>(), {
     class="relative flex justify-start items-center h-72"
     :class="{
       'text-gray-100 bg-cover bg-center': backgroundImage,
-      'text-title bg-gray-100': !backgroundImage,
+      'text-title': !backgroundImage,
     }"
     :style="backgroundImage ? `background-image: url(${backgroundImage})` : ''"
   >
-    <div class="w-page-sections mx-8 z-10">
-      <h1 class="font-display text-heading-4xl">{{ title }}</h1>
-      <p class="text-heading-2xl">{{ subtitle }}</p>
+    <div class="w-page-sections mx-7.5 z-10">
+      <h1 class="font-display text-heading-6xl">{{ title }}</h1>
+      <p class="text-body-lg">{{ subtitle }}</p>
     </div>
     <div
       v-if="backgroundImage"
