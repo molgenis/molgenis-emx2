16a6881dcecd691575b7afbe4fb86614a58dfc49
jhhaanstra <janhendrikhaanstra@gmail.com>
2026-01-30 12:45:52 +0100

    Remove unneeded var

--- backend/molgenis-emx2-webapi/src/test/java/org/molgenis/emx2/web/ApiTestBase.java
+++ backend/molgenis-emx2-webapi/src/test/java/org/molgenis/emx2/web/ApiTestBase.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.molgenis.emx2.Database;
 import org.molgenis.emx2.sql.TestDatabaseFactory;
 import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
-import uk.org.webcompere.systemstubs.jupiter.SystemStub;
 import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
 /**
@@ -47,9 +46,6 @@ public abstract class ApiTestBase {
   protected static Database database;
   protected static final int PORT = 8081; // other than default so we can see effect
   private static MolgenisWebservice service;
-
-  @SystemStub
-  protected static EnvironmentVariables environmentVariables = new EnvironmentVariables();
 
   @BeforeAll
   static void setupService() throws Exception {
