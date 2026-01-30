d523ea7face76c63382629e70d129525846cbdaf
Postema Max <max@elynor.com>
2026-01-30 12:04:34 +0100

    chore: cleanup code

--- apps/molgenis-components/src/components/forms/formUtils/formUtils.ts
+++ apps/molgenis-components/src/components/forms/formUtils/formUtils.ts
@@ -24,6 +24,10 @@ const {
 } = constants;
 const BIG_INT_ERROR = `Invalid long: must be value from ${MIN_LONG} to ${MAX_LONG}`;
 const INT_ERROR = `Invalid integer: must be value from ${MIN_INT} to ${MAX_INT}`;
+const PERIOD_EXPLANATION =
+  "should start with a P and should contain at least a Y(year), M(month) or D(day): e.g. 'P1Y3M14D'";
+const UUID_EXPLANATION =
+  "should be a valid UUID format (rfc9562): e.g. '123e4567-e89b-12d3-a456-426614174000'";
 export const NON_NEGATIVE_INT_ERROR = `Invalid non negative integer: must be value from ${MIN_NON_NEGATIVE_INT} to ${MAX_INT}`;
 
 export function getRowErrors(
@@ -112,35 +116,39 @@ export function getColumnError(
   }
 
   if (type === "PERIOD" && isInvalidPeriod(value)) {
-    return "Invalid Period: should start with a P and should contain at least a Y(year), M(month) or D(day): e.g. 'P1Y3M14D'";
+    return "Invalid Period: " + PERIOD_EXPLANATION;
   }
   let invalidPeriods = arrayWithInvalidPeriod(value);
   if (type === "PERIOD_ARRAY" && invalidPeriods.length == 1) {
     return (
       readableStringArray(invalidPeriods) +
-      " is an invalid Period: should start with a P and should contain at least a Y(year), M(month) or D(day): e.g. 'P1Y3M14D'"
+      " is an invalid Period: " +
+      PERIOD_EXPLANATION
     );
   }
   if (type === "PERIOD_ARRAY" && invalidPeriods.length > 1) {
     return (
       readableStringArray(invalidPeriods) +
-      " are invalid Periods: should start with a P and should contain at least a Y(year), M(month) or D(day): e.g. 'P1Y3M14D'"
+      " are invalid Periods: " +
+      PERIOD_EXPLANATION
     );
   }
   if (type === "UUID" && isInvalidUUID(value)) {
-    return "Invalid UUID: should be a valid UUID format (rfc9562): e.g. '123e4567-e89b-12d3-a456-426614174000'";
+    return "Invalid UUID: " + UUID_EXPLANATION;
   }
   let invalidUUIDs = arrayWithInvalidUUIDs(value);
   if (type === "UUID_ARRAY" && invalidUUIDs.length == 1) {
     return (
       readableStringArray(invalidUUIDs) +
-      " is an invalid UUID: should be a valid UUID format (rfc9562): e.g. '123e4567-e89b-12d3-a456-426614174000'"
+      " is an invalid UUID: " +
+      UUID_EXPLANATION
     );
   }
   if (type === "UUID_ARRAY" && invalidUUIDs.length > 1) {
     return (
       readableStringArray(invalidUUIDs) +
-      " are invalid UUIDs: should be a valid UUID format (rfc9562): e.g. '123e4567-e89b-12d3-a456-426614174000'"
+      " are invalid UUIDs: " +
+      UUID_EXPLANATION
     );
   }
   if (type === "JSON") {
@@ -207,17 +215,14 @@ function readableStringArray(strings: string[]): string {
   if (strings.length === 0) {
     return "";
   } else if (strings.length === 1) {
-    return "'" + strings[0] + "'";
+    return `'${strings[0]}'`;
   } else {
-    return (
-      "'" +
-      strings.slice(0, strings.length - 1).join("', '") +
-      "' and '" +
-      strings[strings.length - 1] +
-      "'"
-    );
+    return `'${strings.slice(0, strings.length - 1).join("', '")}' and '${
+      strings[strings.length - 1]
+    }'`;
   }
 }
+
 export function isInvalidBigInt(value?: string): boolean {
   const isValidRegex = /^-?\d+$/;
   if (!value) {
