7768ee362b20cc0abde6d0534aa8a3563d306343
connoratrug <c.stroomberg@rug.nl>
2026-01-30 12:58:23 +0100

    fix: adding refabck item should update the parent
    - wait for save before reload
    - add test adding refback item

---
+++ apps/tailwind-components/tests/e2e/components/form/admin.inputRefback.spec.ts
@@ -0,0 +1,32 @@
+import { expect, test } from "@playwright/test";
+import playwrightConfig from "../../../../playwright.config";
+const route = playwrightConfig?.use?.baseURL?.startsWith("http://localhost")
+  ? ""
+  : "/apps/tailwind-components/#/";
+
+test.use({ storageState: "playwright/.auth/user.json" });
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(`${route}Form.story?schema=pet+store&table=Pet&rowIndex=1`);
+});
+
+test("adding an order via the pet.order refback should update the parent (pet), order list", async ({
+  page,
+}) => {
+  await expect(page.getByText("orders Add OrderORDER:")).toBeVisible();
+  await page.getByRole("button", { name: "Add Order" }).click();
+  await page
+    .getByRole("textbox", { name: "how many would you like ?" })
+    .click();
+  await page
+    .getByRole("textbox", { name: "how many would you like ?" })
+    .fill("3");
+  await page.getByRole("textbox", { name: "price" }).click();
+  await page.getByRole("textbox", { name: "price" }).fill("3");
+  await page.locator("#fields-container #status-form-field-input").click();
+  await page.locator("#fields-container #status-form-field-input").fill("e2e");
+  await page.getByRole("button", { name: "Save", exact: true }).click();
+  // test the order appears in the pet order refback list
+  await page.locator("#accordion__v-0-1-5-2-toggle-icon-only").click();
+  await expect(page.getByText("e2e", { exact: true })).toBeVisible();
+});
