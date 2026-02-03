34f0570f77ed56294af76f477e94065a6766a7fe
dcruvolo <davidruvolo51@gmail.com>
2026-02-03 15:59:22 +0100

    feat: added component for rendering chart title and description

---
+++ apps/tailwind-components/app/components/viz/ChartContext.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import type { ChartContext } from "../../../types/viz";
+
+defineProps<ChartContext>();
+</script>
+
+<template>
+  <div>
+    <h3 class="text-heading-2xl" :class="{ 'mb-5': description }">
+      {{ title }}
+    </h3>
+    <p v-if="description">{{ description }}</p>
+  </div>
+</template>
