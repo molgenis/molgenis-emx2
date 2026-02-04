0bf80deb68a2aa22d3e325d94f020e3899a2a6b2
dcruvolo <davidruvolo51@gmail.com>
2026-02-04 16:47:40 +0100

    fix: added tw classes

--- apps/tailwind-components/app/components/viz/ChartContext.vue
+++ apps/tailwind-components/app/components/viz/ChartContext.vue
@@ -6,9 +6,9 @@ defineProps<ChartContext>();
 
 <template>
   <div>
-    <h3 class="text-heading-2xl" :class="{ 'mb-5': description }">
+    <h3 class="text-title text-heading-4xl" :class="{ 'mb-5': !description }">
       {{ title }}
     </h3>
-    <p v-if="description">{{ description }}</p>
+    <p v-if="description" class="text-title-contrast">{{ description }}</p>
   </div>
 </template>
