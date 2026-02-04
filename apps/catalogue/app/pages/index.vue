c7e2804ace403c011ccfe7da0219648b0e2e17aa
mswertz <m.a.swertz@rug.nl>
2026-02-04 10:10:18 +0100

    fix filter of main

--- apps/catalogue/app/pages/index.vue
+++ apps/catalogue/app/pages/index.vue
@@ -67,10 +67,10 @@ const { data } = await useFetch<Resp<IResources, IResources_agg>>(
   }
 );
 
-const catalogues = data.value?.data?.Resources as IResources[];
+const catalogues = data.value?.data?.Resources as IResources[].filter(c => !c.mainCatalogue);
 const groupedCatalogues = catalogues
-  ? Object.groupBy(catalogues, (c) => c.catalogueType?.name ?? "other")
-  : { theme: [], project: [], organisation: [], other: [] };
+  ? Object.groupBy(catalogues, (c) => c.catalogueType?.name ?? "theme")
+  : { theme: [], project: [], organisation: [] };
 Object.keys(groupedCatalogues).forEach((key) => {
   groupedCatalogues[key]?.sort((a, b) => a.id.localeCompare(b.id));
 });
