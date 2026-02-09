565c3216f5b3fe14af94fa6d92d62b981a0030ba
Jelmer Veen <jelmerveen@pennions.com>
2026-02-09 10:37:45 +0100

    (W.I.P): refactored jooq implementation to sqlfile implementation

---
+++ backend/molgenis-emx2-sql/src/main/resources/org/molgenis/emx2/sql/utility-sql/clean-db-remove-all-roles.sql
@@ -0,0 +1,16 @@
+DO $$
+DECLARE
+    r RECORD;
+    dbname TEXT := current_database();
+BEGIN
+    FOR r IN
+        SELECT rolname
+        FROM pg_roles
+        WHERE rolname LIKE 'MG\_%' ESCAPE '\'
+           OR rolname LIKE 'test%'
+           OR rolname LIKE 'user_%'
+    LOOP
+        EXECUTE format('REVOKE ALL PRIVILEGES ON DATABASE "%I" FROM "%I";', dbname, r.rolname);
+        EXECUTE format('DROP ROLE "%I";', r.rolname);
+    END LOOP;
+END$$;
---
+++ backend/molgenis-emx2-sql/src/main/resources/org/molgenis/emx2/sql/utility-sql/clean-db-remove-foreign-keys.sql
@@ -0,0 +1,18 @@
+DO $$
+DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN
+        SELECT
+            table_schema, table_name, constraint_name
+        FROM information_schema.table_constraints
+        WHERE constraint_type = 'FOREIGN KEY'
+            AND table_schema NOT LIKE 'pg_%'
+            AND table_schema <> 'information_schema'
+            AND table_schema <> 'public'
+    LOOP
+        EXECUTE
+            format('ALTER TABLE "%I"."%I" DROP CONSTRAINT "%I";',
+                r.table_schema, r.table_name, r.constraint_name);
+    END LOOP;
+END$$;
