ef71c077d7cd264c8ba432e362b2acb9d2fa55ba
mswertz <m.a.swertz@rug.nl>
2026-02-11 10:52:25 +0100

    format

--- apps/catalogue/app/utils/resourceTypeUtils.ts
+++ apps/catalogue/app/utils/resourceTypeUtils.ts
@@ -2,7 +2,9 @@ export interface ResourceType {
   name?: string;
 }
 
-export function isCatalogueResource(types: ResourceType[] | undefined): boolean {
+export function isCatalogueResource(
+  types: ResourceType[] | undefined
+): boolean {
   if (!types) return false;
   return types.some((t) => t.name === "Catalogue");
 }
