679a4dbf769480cd30c9b8f2641f30f36f2f250a
Jelmer Veen <jelmerveen@pennions.com>
2026-02-23 14:03:38 +0100

    chore: added tests for staticfilemapper

---
+++ backend/molgenis-emx2-webapi/src/test/java/org/molgenis/emx2/web/StaticFileMapperTest.java
@@ -0,0 +1,43 @@
+package org.molgenis.emx2.web;
+
+import graphql.Assert;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import java.util.Objects;
+import org.junit.jupiter.api.Test;
+
+public class StaticFileMapperTest extends ApiTestBase {
+
+  @Test
+  void testCanFetchFileFromPublicHtmlFolder() {
+    Response restResponse = RestAssured.get("/test-app/test-assets/styling.css");
+
+    String mimeType = restResponse.getHeader("Content-type");
+    Assert.assertTrue(Objects.equals(mimeType, "text/css"));
+
+    int status = restResponse.statusCode();
+    Assert.assertTrue(status == 200);
+  }
+
+  @Test
+  void testCanFetchIndexByDefaultFromPublicHtmlFolder() {
+    Response restResponse = RestAssured.get("/schema/test-app/");
+
+    String mimeType = restResponse.getHeader("Content-type");
+    Assert.assertTrue(Objects.equals(mimeType, "text/html"));
+
+    int status = restResponse.statusCode();
+    Assert.assertTrue(status == 200);
+  }
+
+  @Test
+  void testDefaultRedirectToCentralApps() {
+    Response restResponse = RestAssured.given().redirects().follow(false).when().get("/");
+
+    int status = restResponse.getStatusCode();
+    Assert.assertTrue(status == 302);
+
+    String locationHeader = restResponse.getHeader("Location");
+    Assert.assertTrue(Objects.equals(locationHeader, "/apps/central/"));
+  }
+}
