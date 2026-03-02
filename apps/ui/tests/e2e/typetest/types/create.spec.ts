4e23bf9cf510a51a514543a47ddfd2aebc5ef2f5
p237959 <47183404+connoratrug@users.noreply.github.com>
2026-03-02 09:20:48 +0100

    fix: add null checks and handle null cases (#5993)
    fix: add null checks and handle null cases
    
    - Add minimal type checking setup for molgenis-components, to get type checking started
    - Add e2e test for insert typetest record with only required fields filled in
    
    Closes #https://github.com/molgenis/molgenis-emx2/issues/5989

--- apps/ui/tests/e2e/typetest/types/create.spec.ts
+++ apps/ui/tests/e2e/typetest/types/create.spec.ts
@@ -7,7 +7,27 @@ const route = playwrightConfig?.use?.baseURL?.startsWith("http://localhost")
 
 test.beforeEach(async ({ page }) => {});
 
-test.afterEach(async ({ page }) => {});
+test.afterEach(async ({ page }) => {
+  const cellLocator = page.getByRole("cell", { name: /^.*foobar.*/i });
+  if ((await cellLocator.count()) > 0 && (await cellLocator.isVisible())) {
+    await cellLocator.hover();
+
+    const deleteButton = page.getByRole("button", {
+      name: /^.*deletefoobar.*/i,
+    });
+    if ((await deleteButton.count()) > 0 && (await deleteButton.isVisible())) {
+      await deleteButton.click();
+    }
+
+    const confirmButton = page.getByRole("button", { name: "Delete" });
+    if (
+      (await confirmButton.count()) > 0 &&
+      (await confirmButton.isVisible())
+    ) {
+      await confirmButton.click();
+    }
+  }
+});
 
 test.describe("period input type", () => {
   test("it should be possible to create a period", async ({ page }) => {
@@ -77,5 +97,25 @@ test.describe("array input types", () => {
     await expect(
       page.locator('[id="type test-Types-stringArrayType-form-field-input_1"]')
     ).toHaveValue("string3");
+  });
+});
+
+test.describe("Insert type record with only required fields", () => {
+  test("it should be possible to create a record with only required fields", async ({
+    page,
+  }) => {
+    await page.goto(`${route}type%20test/Types`);
+    await page.getByRole("button", { name: "Add Types" }).click();
+    await page.getByRole("textbox", { name: "string type Required" }).click();
+    await page
+      .getByRole("textbox", { name: "string type Required" })
+      .fill("foobar");
+    await page.getByRole("button", { name: "Save", exact: true }).click();
+    // wait for save to complete
+    await page.waitForTimeout(3000);
+    await page.getByRole("button", { name: "Cancel" }).click();
+    await expect(
+      page.getByRole("cell", { name: /^.*foobar.*/i })
+    ).toBeVisible();
   });
 });
