774fcdd9a383dedc7b716b8446eb9229659dccf3
chinook25 <chinook25@gmail.com>
2026-03-04 10:49:06 +0100

    chore: undo change

--- apps/tailwind-components/nuxt.config.ts
+++ apps/tailwind-components/nuxt.config.ts
@@ -73,7 +73,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: "http://localhost:8080/",
+      apiBase: "https://emx2.dev.molgenis.org/",
       sourceCodeMap: sourceCodeMap,
     },
   },
