00316de3243e333756b35161a12bee9844259968
jhhaanstra <janhendrikhaanstra@gmail.com>
2026-03-05 16:15:24 +0100

    Optimized imports

--- backend/molgenis-emx2/src/test/java/org/molgenis/emx2/utils/generator/AutoIdFormatTest.java
+++ backend/molgenis-emx2/src/test/java/org/molgenis/emx2/utils/generator/AutoIdFormatTest.java
@@ -2,9 +2,6 @@ package org.molgenis.emx2.utils.generator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.List;
-import java.util.stream.IntStream;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.molgenis.emx2.MolgenisException;
