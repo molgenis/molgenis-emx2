84e14b5b3e5920eb3e6c1da6769a6112d1fea233
jhhaanstra <janhendrikhaanstra@gmail.com>
2026-03-12 15:00:07 +0100

    fix: Added commits in the clean script for-loops to clear transaction locks and prevent the script to break on a high number of schemas to delete.
    move the schemas removal to be the first thing to do when cleaning the db.

--- backend/molgenis-emx2-sql/src/main/resources/org/molgenis/emx2/sql/utility-sql/clean-molgenis-database.sql
+++ backend/molgenis-emx2-sql/src/main/resources/org/molgenis/emx2/sql/utility-sql/clean-molgenis-database.sql
@@ -1,52 +1,53 @@
-    DO $$
-    DECLARE
-        r RECORD;
-    BEGIN
-        FOR r IN
-            SELECT
-                table_schema, table_name, constraint_name
-            FROM information_schema.table_constraints
-            WHERE constraint_type = 'FOREIGN KEY'
-                AND table_schema NOT LIKE 'pg_%'
-                AND table_schema <> 'information_schema'
-                AND table_schema <> 'public'
-        LOOP
-            EXECUTE
-                format('ALTER TABLE %I.%I DROP CONSTRAINT %I;',
-                    r.table_schema, r.table_name, r.constraint_name);
-        END LOOP;
-    END$$;
+DROP SCHEMA IF EXISTS MOLGENIS CASCADE;
 
-    DROP SCHEMA IF EXISTS MOLGENIS CASCADE;
-
-    DO $$
-    DECLARE
-        r RECORD;
-    BEGIN
-        FOR r IN
-            SELECT schema_name
-            FROM information_schema.schemata
-            WHERE schema_name NOT LIKE 'pg_%'
-              AND schema_name <> 'information_schema'
-              AND schema_name <> 'public'
+DO $$
+DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN
+        SELECT schema_name
+        FROM information_schema.schemata
+        WHERE schema_name NOT LIKE 'pg_%'
+          AND schema_name <> 'information_schema'
+          AND schema_name <> 'public'
         LOOP
             EXECUTE format('DROP SCHEMA %I CASCADE;', r.schema_name);
+            COMMIT;
         END LOOP;
-    END$$;
+END$$;
 
-    DO $$
-    DECLARE
-        r RECORD;
-        dbname TEXT := current_database();
-    BEGIN
-        FOR r IN
-            SELECT rolname
-            FROM pg_roles
-            WHERE rolname LIKE 'MG\_%' ESCAPE '\'
-               OR rolname LIKE 'test%'
-               OR rolname LIKE 'user_%'
-        LOOP
-            EXECUTE format('REVOKE ALL PRIVILEGES ON DATABASE %I FROM %I;', dbname, r.rolname);
-            EXECUTE format('DROP ROLE %I;', r.rolname);
-        END LOOP;
-    END$$;
+DO $$
+DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN
+        SELECT
+            table_schema, table_name, constraint_name
+        FROM information_schema.table_constraints
+        WHERE constraint_type = 'FOREIGN KEY'
+            AND table_schema NOT LIKE 'pg_%'
+            AND table_schema <> 'information_schema'
+            AND table_schema <> 'public'
+    LOOP
+        EXECUTE
+            format('ALTER TABLE %I.%I DROP CONSTRAINT %I;',
+                r.table_schema, r.table_name, r.constraint_name);
+    END LOOP;
+END$$;
+
+DO $$
+DECLARE
+    r RECORD;
+    dbname TEXT := current_database();
+BEGIN
+    FOR r IN
+        SELECT rolname
+        FROM pg_roles
+        WHERE rolname LIKE 'MG\_%' ESCAPE '\'
+           OR rolname LIKE 'test%'
+           OR rolname LIKE 'user_%'
+    LOOP
+        EXECUTE format('REVOKE ALL PRIVILEGES ON DATABASE %I FROM %I;', dbname, r.rolname);
+        EXECUTE format('DROP ROLE %I;', r.rolname);
+    END LOOP;
+END$$;
