baea76c768c49c0dfd2ae72cd451282574d7a292
dcruvolo <davidruvolo51@gmail.com>
2026-03-16 10:36:42 +0100

    added e2e test

---
+++ apps/tailwind-components/tests/e2e/components/viz/ProgressMeter.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+import playwrightConfig from "../../../../playwright.config";
+
+const route = playwrightConfig?.use?.baseURL?.startsWith("http://localhost")
+  ? ""
+  : "/apps/tailwind-components/#/";
+
+test.describe("ColumnChart", { tag: "@tw-components @tw-viz" }, () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`${route}viz/ProgressMeter.story`);
+    await page
+      .getByRole("heading", { name: "VizProgressMeter" })
+      .click({ delay: 500 });
+  });
+
+  test("rects are rendered", async ({ page }) => {
+    const rects = await page.locator("g rect").all();
+    expect(rects.length).toEqual(8);
+  });
+
+  test("labels are rendered", async ({ page }) => {
+    const labels = await page.locator("g text").all();
+    expect(labels.length).toEqual(4);
+  });
+});
