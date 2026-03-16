eefd1b57ea8667c6dd951afc88094ef6f086dbfc
connoratrug <c.stroomberg@rug.nl>
2026-03-16 16:34:39 +0100

    chore: update css/scss loading for viz lib and skin app

--- apps/ern-skin/src/main.js
+++ apps/ern-skin/src/main.js
@@ -2,10 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
 
-import "molgenis-components";
-import "molgenis-viz";
-import "./styles/index.scss";
-
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
--- apps/ern-skin/vite.config.js
+++ apps/ern-skin/vite.config.js
@@ -1,22 +1,44 @@
 import { defineConfig } from "vite";
+import path from "path";
 import vue from "@vitejs/plugin-vue";
-import devProxy from "../dev-proxy.config";
 import dotenv from "dotenv";
+import devProxy from "./dev-proxy.config";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig((command) => {
   // Load environment variables
   dotenv.config({ path: "./.env" });
 
   return {
+    resolve: {
+      alias: {
+        viz: path.resolve(__dirname, "node_modules/molgenis-viz/src"),
+        vizdist: path.resolve(__dirname, "node_modules/molgenis-viz/dist"),
+        skin: path.resolve(__dirname, "src/styles"),
+        molgenis: path.resolve(
+          __dirname,
+          "node_modules/molgenis-components/dist"
+        ),
+      },
+    },
     css: {
       preprocessorOptions: {
         scss: {
-          api: "legacy",
           additionalData: `
-          @import "../molgenis-viz/src/styles/palettes.scss";
-          @import "../molgenis-viz/src/styles/variables.scss";
-          @import "../molgenis-viz/src/styles/mixins.scss";
-          @import "src/styles/variables.scss";
+           @import "viz/styles/heightwidth.scss";
+           @import "viz/styles/mixins.scss";
+           @import "viz/styles/padding.scss";
+           @import "viz/styles/palettes.scss";
+           @import "viz/styles/resets.scss";
+           @import "viz/styles/textPosition.scss";
+           @import "viz/styles/variables.scss";
+           @import "molgenis/molgenis-components.css";
+           @import "vizdist/molgenis-viz.css";
+           @import "skin/variables.scss";
+           @import "skin/index.scss";
         `,
         },
       },
