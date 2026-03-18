fc1912f0b05e5ac28720be16efcb0e0bc4055d85
connoratrug <c.stroomberg@rug.nl>
2026-03-18 11:13:25 +0100

    chore: upgrade vite from v6 to v7
    # Conflicts:
    #	apps/pnpm-lock.yaml

--- apps/nestor-public/vite.config.js
+++ apps/nestor-public/vite.config.js
@@ -1,20 +1,29 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
+const dir = path.dirname(new URL(import.meta.url).pathname);
 
 export default defineConfig((command) => {
   require("dotenv").config({ path: `./.env` });
 
   return {
+    resolve: {
+      alias: {
+        viz: path.resolve(dir, "node_modules/molgenis-viz/src"),
+        vizdist: path.resolve(dir, "node_modules/molgenis-viz/dist"),
+        molgenis: path.resolve(dir,"node_modules/molgenis-components/dist"),
+        nes: path.resolve(dir, "src/styles"),
+      }  
+    },
     css: {
       preprocessorOptions: {
         scss: {
-          api: 'legacy',
           additionalData: `
-          @import "../molgenis-viz/src/styles/palettes.scss";
-          @import "../molgenis-viz/src/styles/variables.scss";
-          @import "../molgenis-viz/src/styles/mixins.scss";
-          @import "../molgenis-viz/src/styles/resets.scss";
-          @import "src/styles/variables.scss";
+          @import "viz/styles/palettes.scss";
+          @import "viz/styles/variables.scss";
+          @import "viz/styles/mixins.scss";
+          @import "viz/styles/resets.scss";
+          @import "nes/variables.scss";
         `,
         },
       },
