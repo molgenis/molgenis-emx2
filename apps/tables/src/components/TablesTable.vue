fd1e078b5fd7e0473afd8b7f85e9c9dae3e22fa0
Harm Brugge <harmbrugge@gmail.com>
2026-03-30 11:05:28 +0200

    Added id to table permission graphql api

--- apps/tables/src/components/TablesTable.vue
+++ apps/tables/src/components/TablesTable.vue
@@ -35,7 +35,7 @@ export default {
   methods: {
     hasPermission(table) {
       if (!this.tablePermissions?.length) return true;
-      const perm = this.tablePermissions.find((p) => p.name === table.name);
+      const perm = this.tablePermissions.find((p) => p.id === table.id);
       return perm?.canView || table.tableType === "ONTOLOGIES";
     },
   },
