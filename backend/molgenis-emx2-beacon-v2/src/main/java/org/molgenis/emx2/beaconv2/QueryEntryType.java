8d964ae3f46cdf38c05c09d356824f2ffe289b70
Harm Brugge <harmbrugge@gmail.com>
2026-03-30 12:03:49 +0200

    Fixed beacon auth bug

--- backend/molgenis-emx2-beacon-v2/src/main/java/org/molgenis/emx2/beaconv2/QueryEntryType.java
+++ backend/molgenis-emx2-beacon-v2/src/main/java/org/molgenis/emx2/beaconv2/QueryEntryType.java
@@ -240,7 +240,7 @@ public class QueryEntryType {
     return switch (this.granularity) {
       case BOOLEAN -> level.ordinal() >= AggregateLevel.EXISTS.ordinal();
       case COUNT, AGGREGATED -> level.ordinal() >= AggregateLevel.RANGE.ordinal();
-      case RECORD, UNDEFINED -> level == AggregateLevel.COUNT;
+      case RECORD, UNDEFINED -> evaluator.canView(table);
     };
   }
 }
