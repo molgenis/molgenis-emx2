49609284374c165826de9bf360fc1811acc6f527
jhhaanstra <janhendrikhaanstra@gmail.com>
2026-04-03 11:08:09 +0200

    feat: dont generate iri's but raw string values for predicates and objects

--- backend/molgenis-emx2-harvester/src/main/java/org/molgenis/emx2/harvester/TableSparqlQuery.java
+++ backend/molgenis-emx2-harvester/src/main/java/org/molgenis/emx2/harvester/TableSparqlQuery.java
@@ -3,8 +3,8 @@ package org.molgenis.emx2.harvester;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.eclipse.rdf4j.model.ValueFactory;
-import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import java.util.stream.Stream;
+import org.eclipse.rdf4j.model.Namespace;
 import org.eclipse.rdf4j.sparqlbuilder.core.SparqlBuilder;
 import org.eclipse.rdf4j.sparqlbuilder.core.Variable;
 import org.eclipse.rdf4j.sparqlbuilder.core.query.Queries;
@@ -12,11 +12,12 @@ import org.eclipse.rdf4j.sparqlbuilder.core.query.SelectQuery;
 import org.eclipse.rdf4j.sparqlbuilder.graphpattern.GraphPattern;
 import org.eclipse.rdf4j.sparqlbuilder.graphpattern.GraphPatterns;
 import org.eclipse.rdf4j.sparqlbuilder.graphpattern.TriplePattern;
+import org.eclipse.rdf4j.sparqlbuilder.rdf.RdfObject;
+import org.eclipse.rdf4j.sparqlbuilder.rdf.RdfPredicate;
 import org.molgenis.emx2.*;
+import org.molgenis.emx2.rdf.DefaultNamespace;
 
 public class TableSparqlQuery {
-
-  private static final ValueFactory VALUE_FACTORY = SimpleValueFactory.getInstance();
 
   private final SchemaMetadata schema;
   private final TableMetadata table;
@@ -28,12 +29,22 @@ public class TableSparqlQuery {
   private final Variable mainVar;
 
   public TableSparqlQuery(SchemaMetadata schema, String tableName) {
+    this(schema, tableName, DefaultNamespace.streamAll());
+  }
+
+  public TableSparqlQuery(SchemaMetadata schema, String tableName, Stream<Namespace> namespaces) {
     this.schema = schema;
     this.table = schema.getTableMetadata(tableName);
     this.mainVar = SparqlBuilder.var(tableName);
+    namespaces.forEach(select::prefix);
   }
 
   public void build() {
+    if (table.getSemantics() != null) {
+      String semantic = table.getSemantics()[0];
+      mainPattern = mainVar.isA(object(semantic));
+    }
+
     for (Column column : table.getColumns()) {
       if (column.getSemantics() == null) {
         continue;
@@ -47,7 +58,7 @@ public class TableSparqlQuery {
       } else if (column.isRequired()) {
         addToMainPattern(semantic, columnVar);
       } else {
-        TriplePattern triple = mainVar.has(VALUE_FACTORY.createIRI(semantic), columnVar);
+        TriplePattern triple = mainVar.has(predicate(semantic), columnVar);
         optionalPatterns.add(GraphPatterns.optional(triple));
       }
 
@@ -80,18 +91,26 @@ public class TableSparqlQuery {
   }
 
   private TriplePattern triple(Variable subject, String predicate, Variable object) {
-    return subject.has(VALUE_FACTORY.createIRI(predicate), object);
+    return subject.has(predicate(predicate), object);
   }
 
-  private void addToMainPattern(String semantic, Variable var) {
+  private void addToMainPattern(String semantic, Variable variable) {
     if (mainPattern == null) {
-      mainPattern = triple(mainVar, semantic, var);
+      mainPattern = triple(mainVar, semantic, variable);
     } else {
-      mainPattern.andHas(VALUE_FACTORY.createIRI(semantic), var);
+      mainPattern.andHas(predicate(semantic), variable);
     }
   }
 
   public String asString() {
     return select.getQueryString();
+  }
+
+  private RdfPredicate predicate(String semantic) {
+    return () -> semantic;
+  }
+
+  private RdfObject object(String semantic) {
+    return () -> semantic;
   }
 }
--- backend/molgenis-emx2-harvester/src/test/java/org/molgenis/emx2/harvester/TableSparqlQueryTest.java
+++ backend/molgenis-emx2-harvester/src/test/java/org/molgenis/emx2/harvester/TableSparqlQueryTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.stream.Stream;
 import org.eclipse.rdf4j.query.QueryLanguage;
 import org.eclipse.rdf4j.query.TupleQuery;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
@@ -15,6 +16,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.molgenis.emx2.Database;
 import org.molgenis.emx2.Schema;
+import org.molgenis.emx2.rdf.DefaultNamespace;
 import org.molgenis.emx2.sql.TestDatabaseFactory;
 
 class TableSparqlQueryTest {
@@ -53,6 +55,30 @@ class TableSparqlQueryTest {
     query.build();
     TupleQuery tupleQuery = conn.prepareTupleQuery(QueryLanguage.SPARQL, query.asString());
     tupleQuery.evaluate().stream().forEach(System.out::println);
+  }
+
+  @Test
+  void givenPerson_thenDontAddPointBrackets() {
+    TableSparqlQuery query =
+        new TableSparqlQuery(
+            schema.getMetadata(), "User", Stream.of(DefaultNamespace.FOAF.getNamespace()));
+
+    query.build();
+    assertEquals(
+        """
+                PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+                SELECT ?username ?firstName ?lastName ?picture ?email ?phone ?pets
+                WHERE { ?User a foaf:Person ;
+                    foaf:accountName ?username .
+                OPTIONAL { ?User foaf:firstName ?firstName . }
+                OPTIONAL { ?User foaf:lastName ?lastName . }
+                OPTIONAL { ?User foaf:img ?picture . }
+                OPTIONAL { ?User foaf:mbox ?email . }
+                OPTIONAL { ?User foaf:phone ?phone . }
+                OPTIONAL { ?pets http://example.com/petstore#hasPets ?nameRef .
+                ?nameRef http://example.com/petstore#hasName ?name . } }
+                """,
+        query.asString());
   }
 
   private InputStream readTtl(String path) {
