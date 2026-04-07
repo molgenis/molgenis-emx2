0191dd183311b76886d82cebebec62c09c99d550
YpeZ <info@ypezijlstra.frl>
2026-04-07 14:54:08 +0200

    fixed utils tests and moved to own file

--- tools/staging_migrator/src/tests/tests.py
+++ tools/staging_migrator/src/tests/tests.py
@@ -3,16 +3,13 @@ Tests the correct publication of an Organisation from a staging area onto a cata
 """
 import io
 import logging
-import os
-
 import numpy as np
+import os
 import pandas as pd
 import pytest
 from dotenv import load_dotenv
 
 from staging_migrator.src.molgenis_emx2_staging_migrator import StagingMigrator
-from staging_migrator.src.molgenis_emx2_staging_migrator.exceptions import MissingContactException
-from staging_migrator.src.molgenis_emx2_staging_migrator.utils import check_hricore, process_contacts
 
 CATALOGUE = 'UMCG'
 STAGING_AREA = 'testUMCGCohort'
@@ -73,48 +70,6 @@ def test_process_organisations():
     assert processed_orgs["organisation pid"].values.tolist() == ['ROR:05xvt9f17', 'ROR:03cv38k47', None, None]
     assert processed_orgs["organisation website"].values.tolist() == ['https://www.lumc.nl', 'https://www.umcg.nl', None, None]
     assert migrator.warnings == ['No organisation for (resource, id) = (R4, O4)']
-
-def test_process_contacts():
-    """Unit test for the `process_contacts` method."""
-    contacts_csv = ("""resource,role,first name,last name,statement of consent personal data,email\n"""
-                    """R1,Principal Investigator,A1,B1,true,A1B1@R1.com\n"""
-                    """R2,Primary contact,A2,B2,true,A2B2@R2.com\n"""
-                    """R3,Participant,A3,B3,false,\n"""
-                    """R4,Participant,A4,B4,false,A4B4@R4.com""")
-    contacts_df = pd.read_csv(io.StringIO(contacts_csv))
-    resources_csv = ("""id,contact point.resource,contact point.first name,contact point.last name\n"""
-                     """R1,R1,A1,B1\n"""
-                     """R2,R2,A2,B2\n"""
-                     """R3,R3,A3,B3\n"""
-                     """R4,R4,A4,B4\n""")
-    resources_df = pd.read_csv(io.StringIO(resources_csv))
-
-    with pytest.raises(MissingContactException) as e_info:
-        process_contacts(contacts_df, resources_df)
-    assert e_info.value.msg == "Cannot migrate resource due to missing email or consent for contact (Resource, first name, last name) = (R3, A3, B3), (R4, A4, B4)."
-
-    resources_csv = ("""id,contact point.resource,contact point.first name,contact point.last name\n"""
-                     """R1,R1,A1,B1\n"""
-                     """R2,R2,A2,B2\n""")
-    resources_df = pd.read_csv(io.StringIO(resources_csv))
-    processed_contacts = process_contacts(contacts_df, resources_df)
-    assert processed_contacts["mg_delete"].values.tolist() == [False, False, True, True]
-
-
-
-def test_check_hricore():
-    """Tests the `check_hricore` utility function."""
-    resources_csv = (
-        """hricore,id,name\n"""
-        """true,A,A\n"""
-        """false,B,B\n"""
-        """,C,C"""
-    )
-    resources_df = pd.read_csv(io.StringIO(resources_csv))
-
-    with pytest.raises(ValueError) as e:
-        check_hricore(resources_df)
-    assert str(e.value) == "Value 'hricore' not set to 'true' for resource B, C"
 
 def test_add_resource():
     """Tests the `add_resource` method."""
