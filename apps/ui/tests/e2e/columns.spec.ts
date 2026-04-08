739ddde6d27551a52b476344b1d714c06f00ca67
Postema Max <max@elynor.com>
2026-04-08 10:36:51 +0200

    test: add dragdrop test

--- apps/ui/tests/e2e/columns.spec.ts
+++ apps/ui/tests/e2e/columns.spec.ts
@@ -19,3 +19,13 @@ test("Can hide and show columns", async ({ page }) => {
   await page.getByRole("button", { name: "Save" }).click();
   await expect(page.getByRole("columnheader", { name: "name" })).toBeVisible();
 });
+
+test("Can change order of columns", async ({ page }) => {
+  await page.goto(`${route}pet%20store/Pet`);
+  await expect(page.getByRole("heading", { level: 1 })).toContainText("Pet");
+  await expect(page.locator('table.table-fixed tr').first().getByRole("columnheader").first()).toHaveText("name");
+  await page.getByRole("button", { name: "Columns" }).click();
+  await page.locator('label:has-text("name")').dragTo(page.locator('label:has-text("tags")'));
+  await page.getByRole("button", { name: "Save" }).click();
+  await expect(page.locator('table.table-fixed tr').first().getByRole("columnheader").first()).toHaveText("category");
+});
