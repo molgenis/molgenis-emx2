25513c55f3d520126f0dc8c5006d8334b9e2969a
p237959 <47183404+connoratrug@users.noreply.github.com>
2026-04-20 13:14:50 +0200

    fix: catalogue site map (#6200)
    fix: fix catalogue sitemap variable fetch
    
    - Update endpoint fetch query

--- backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/service/CatalogueSiteMap.java
+++ backend/molgenis-emx2-webapi/src/main/java/org/molgenis/emx2/web/service/CatalogueSiteMap.java
@@ -1,6 +1,7 @@
 package org.molgenis.emx2.web.service;
 
 import static org.molgenis.emx2.FilterBean.f;
+import static org.molgenis.emx2.FilterBean.or;
 import static org.molgenis.emx2.SelectColumn.s;
 import static org.molgenis.emx2.web.util.EncodingHelpers.encodePathSegment;
 import static org.molgenis.emx2.web.util.EncodingHelpers.encodeQueryParam;
@@ -64,7 +65,18 @@ public class CatalogueSiteMap {
         schema
             .query("Variables")
             .select(s("name"), s(RESOURCE), s("dataset"))
-            .where(f(RESOURCE, f("type", f("name", Operator.EQUALS, TYPE_NETWORK))))
+            .where(
+                f(
+                    RESOURCE,
+                    or(
+                        f(
+                            Constants.MG_TABLECLASS,
+                            Operator.EQUALS,
+                            schema.getName() + "." + "Networks"),
+                        f(
+                            Constants.MG_TABLECLASS,
+                            Operator.EQUALS,
+                            schema.getName() + "." + "Catalogues"))))
             .retrieveRows()
             .forEach(
                 variable -> {
