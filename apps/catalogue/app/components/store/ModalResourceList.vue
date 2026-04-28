b1ac020736b5547520f2eced952fd645cc187621
chinook25 <chinook25@gmail.com>
2026-04-28 11:39:03 +0200

    chore: clean up

--- apps/catalogue/app/components/store/ModalResourceList.vue
+++ apps/catalogue/app/components/store/ModalResourceList.vue
@@ -4,13 +4,12 @@
       v-for="resource in datasetStore.datasets"
       class="border-b-[1px] border-b-gray-200 mb-2 last:border-none last:mb-none"
     >
-      <StoreModalResourceListItem :resource="(resource as IResources)" />
+      <StoreModalResourceListItem :resource="resource" />
     </li>
   </ul>
 </template>
 
 <script setup lang="ts">
-import type { IResources } from "../../../interfaces/catalogue";
 import { useDatasetStore } from "../../stores/useDatasetStore";
 import StoreModalResourceListItem from "./ModalResourceListItem.vue";
 const datasetStore = useDatasetStore();
