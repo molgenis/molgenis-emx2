06edbfc0a5e4f6556afd9296a3a3b741ef90d814
jhhaanstra <janhendrikhaanstra@gmail.com>
2026-04-29 17:25:23 +0200

    feat: added strict mode to graphql delete mutation

--- backend/molgenis-emx2-graphql/src/test/java/org/molgenis/emx2/graphql/TestGraphqlSchemaFields.java
+++ backend/molgenis-emx2-graphql/src/test/java/org/molgenis/emx2/graphql/TestGraphqlSchemaFields.java
@@ -668,6 +668,26 @@ class TestGraphqlSchemaFields {
   }
 
   @Test
+  void strictDeleteRows() throws IOException {
+    String message =
+        execute("mutation{delete(Tag:{name:\"non-existent\"}){message}}")
+            .get("delete")
+            .get("message")
+            .textValue();
+    assertEquals("delete 0 records from Tag\n", message);
+  }
+
+  @Test
+  void nonStrictDeleteRows() {
+    MolgenisException exception =
+        assertThrows(
+            MolgenisException.class,
+            () -> execute("mutation{delete(strict: true, Tag:{name:\"non-existent\"}){message}}"));
+    assertTrue(
+        exception.getMessage().contains("attempted to delete 1 rows but only deleted 0 rows"));
+  }
+
+  @Test
   void testAddAlterDropColumn() throws IOException {
     execute("mutation{change(columns:{table:\"Pet\",name:\"test\"}){message}}");
     assertNotNull(database.getSchema(schemaName).getTable("Pet").getMetadata().getColumn("test"));
