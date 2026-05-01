16f0c476973596698a53755f2ed475e68ef8110d
jhhaanstra <janhendrikhaanstra@gmail.com>
2026-05-01 13:05:48 +0200

    feat: added support for multi layered and multi semantic references

--- backend/molgenis-emx2-rdf/src/main/java/org/molgenis/emx2/rdf/generators/query/mappers/ReferenceMapper.java
+++ backend/molgenis-emx2-rdf/src/main/java/org/molgenis/emx2/rdf/generators/query/mappers/ReferenceMapper.java
@@ -8,42 +8,67 @@ import org.eclipse.rdf4j.sparqlbuilder.core.Variable;
 import org.eclipse.rdf4j.sparqlbuilder.graphpattern.GraphPattern;
 import org.eclipse.rdf4j.sparqlbuilder.graphpattern.GraphPatterns;
 import org.molgenis.emx2.Column;
-import org.molgenis.emx2.Reference;
-import org.molgenis.emx2.SchemaMetadata;
 import org.molgenis.emx2.TableMetadata;
 
 public class ReferenceMapper implements ColumnMapper {
 
   private final Variable variable;
-  private final TableMetadata targetTable;
-  private final boolean isRequired;
+  private final Column rootColumn;
+  private final List<String> path;
+
   private final List<GraphPattern> patterns = new ArrayList<>();
   private final List<Projectable> selectors = new ArrayList<>();
 
-  public ReferenceMapper(Variable variable, Reference reference, SchemaMetadata metadata) {
+  public ReferenceMapper(Variable variable, Column rootColumn) {
+    this(variable, rootColumn, new ArrayList<>());
+  }
+
+  private ReferenceMapper(Variable variable, Column rootColumn, List<String> path) {
     this.variable = variable;
-    this.targetTable = metadata.getTableMetadata(reference.getTargetTable());
-    this.isRequired = reference.toPrimitiveColumn().isRequired();
+    this.rootColumn = rootColumn;
+    this.path = path;
     map();
   }
 
   private void map() {
-    for (Column column : targetTable.getPrimaryKeyColumns()) {
-      Variable object = getRefVariable(column);
-      ColumnMapper columnMapper = new PlainColumnMapper(variable, column, object, isRequired);
-      patterns.addAll(columnMapper.getPattern());
+    PlainColumnMapper mapper = new PlainColumnMapper(variable, rootColumn, columnVariable(), true);
+    patterns.addAll(mapper.getPattern());
 
+    mapPrimaryKeys();
+  }
+
+  private void mapPrimaryKeys() {
+    TableMetadata refTable = rootColumn.getRefTable();
+    for (Column column : refTable.getPrimaryKeyColumns()) {
+      ArrayList<String> columnPath = columnPath();
+      Variable subject = columnVariable();
+
+      ColumnMapper mapper;
       if (column.isReference()) {
-        Variable ref = SparqlBuilder.var(variable.getVarName() + "_" + column.getName());
-        Reference firstRef = column.getReferences().getFirst();
-        ColumnMapper reference = new ReferenceMapper(ref, firstRef, targetTable.getSchema());
-
-        patterns.addAll(reference.getPattern());
-        selectors.addAll(reference.getSelectors());
+        mapper = new ReferenceMapper(subject, column, columnPath);
       } else {
-        selectors.addAll(columnMapper.getSelectors());
+        Variable ref = SparqlBuilder.var(String.join("_", columnPath));
+        mapper = new PlainColumnMapper(ref, column, extendVar(subject, column), true);
       }
+
+      patterns.addAll(mapper.getPattern());
+      selectors.addAll(mapper.getSelectors());
     }
+  }
+
+  private Variable columnVariable() {
+    ArrayList<String> newPath = columnPath();
+    return SparqlBuilder.var(String.join("_", newPath));
+  }
+
+  private ArrayList<String> columnPath() {
+    ArrayList<String> newPath = new ArrayList<>(path);
+    newPath.add(rootColumn.getName());
+    return newPath;
+  }
+
+  private Variable extendVar(Variable toExtend, Column column) {
+    return SparqlBuilder.var(toExtend.getVarName() + "_" + column.getName());
   }
 
   @Override
@@ -53,14 +78,10 @@ public class ReferenceMapper implements ColumnMapper {
 
   @Override
   public List<GraphPattern> getPattern() {
-    if (isRequired) {
+    if (rootColumn.isRequired()) {
       return new ArrayList<>(patterns);
     } else {
       return List.of(GraphPatterns.and(patterns.toArray(new GraphPattern[0])).optional());
     }
-  }
-
-  private Variable getRefVariable(Column column) {
-    return SparqlBuilder.var(variable.getVarName() + "_" + column.getName().replace(".", "_"));
   }
 }
