d7081ce3f43c324f6267af84097cd3d3c54ecc45
chinook25 <chinook25@gmail.com>
2026-05-04 12:48:53 +0200

    chore: simplify function

--- apps/catalogue/app/composables/fetchSetting.ts
+++ apps/catalogue/app/composables/fetchSetting.ts
@@ -1,18 +1,9 @@
 import { useRuntimeConfig } from "#app";
+import { fetchSettings } from "#imports";
 import type { ISetting } from "../../../metadata-utils/src";
 
 export const fetchSetting = (
   settingKey: string
 ): Promise<{ data: { _settings: ISetting[] } }> => {
-  const body = {
-    query: `{_settings (keys: ["${settingKey}"]){ key, value }}`,
-  };
-
-  const config = useRuntimeConfig();
-  const schema = config.public.schema;
-
-  return $fetch(`/${schema}/graphql`, {
-    method: "POST",
-    body,
-  });
+  return fetchSettings([settingKey]);
 };
