c7023574390251cc3cc92ebca78fea1b24e02a15
connoratrug <c.stroomberg@rug.nl>
2026-05-12 11:11:55 +0200

    fix: no not use settings route from inside the app

--- apps/ui/app/middleware/landingPage.ts
+++ apps/ui/app/middleware/landingPage.ts
@@ -3,10 +3,15 @@ import { useSchemaSettings } from "../../../tailwind-components/app/composables/
 const SCHEMA_LANDING_PAGE = "SCHEMA_LANDING_PAGE";
 
 // Navigate to location specified in schema settings.SCHEMA_LANDING_PAGE if it exists
-export default defineNuxtRouteMiddleware(async (to) => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  console.log("Running landing page middleware");
+  console.log("From:", from);
+  console.log("To:", to);
+
   const setting = await useSchemaSettings(new Set([SCHEMA_LANDING_PAGE]), to);
 
   if (
+    from.path === "/" &&
     typeof setting?.SCHEMA_LANDING_PAGE === "string" &&
     setting.SCHEMA_LANDING_PAGE !== ""
   ) {
