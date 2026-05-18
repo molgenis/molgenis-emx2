9e90fd16f1725461e4f6ea255fe0ecbd064c3e26
chinook25 <chinook25@gmail.com>
2026-05-18 10:05:38 +0200

    chore: remove comments

--- apps/catalogue/app/components/store/ModalResourceListItem.vue
+++ apps/catalogue/app/components/store/ModalResourceListItem.vue
@@ -1,35 +1,7 @@
 <template>
   <div class="flex justify-center items-start">
     <div class="grow mb-2 self-center">
-      <!-- TODO: Disabled html below is needed when we want to 
-         implement a connection with the REMS store-->
       <span class="block font-bold">{{ resource.name }}</span>
-      <!-- <div v-if="resource.datasets?.length">
-          <div v-for="dataset in resource.datasets">
-            <InputLabel
-              :for="`${resource.id}-${dataset.name}`"
-              class="group flex justify-start items-center relative"
-            >
-              <input
-                type="checkbox"
-                :id="`${resource.id}-${dataset.name}`"
-                :name="resource.id"
-                :value="dataset.name"
-                v-model="modelValue"
-                class="sr-only"
-              />
-              <InputCheckboxIcon
-                :checked="modelValue!.includes(dataset.name)"
-              />
-              <span class="block" v-if="dataset.label">
-                {{ dataset.label }}
-              </span>
-              <span class="block" v-else>
-                {{ dataset.name }}
-              </span>
-            </InputLabel>
-          </div>
-        </div> -->
     </div>
     <div>
       <IconButton
