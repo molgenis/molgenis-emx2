c791a969ad7a1029801b27f002ac338c3a66a9a2
chinook25 <chinook25@gmail.com>
2026-05-19 10:56:53 +0200

    request button colors

--- apps/catalogue/app/components/store/HeaderButton.vue
+++ apps/catalogue/app/components/store/HeaderButton.vue
@@ -1,7 +1,7 @@
 <template>
   <Button label="Request " icon="shopping-cart" @click="$emit('click')">
-    <span class="inline-block w-6 h-6 bg-white rounded-full">
-      <span class="body-xs/1 text-black">
+    <span class="inline-block w-6 h-6 bg-button-cart-count rounded-full">
+      <span class="body-xs/1 text-button-cart-count">
         {{ numberOfItemsInStore }}
       </span>
     </span>
