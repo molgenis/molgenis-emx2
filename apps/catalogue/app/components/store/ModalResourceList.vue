56001528d3ec3c1977f4132fd6b3845c855a93ee
chinook25 <chinook25@gmail.com>
2026-05-19 10:51:45 +0200

    chore: remove invisible border

--- apps/catalogue/app/components/store/ModalResourceList.vue
+++ apps/catalogue/app/components/store/ModalResourceList.vue
@@ -2,7 +2,7 @@
   <ul class="list-style-none">
     <li
       v-for="resource in datasetStore.datasets"
-      class="border-b-[1px] border-b-gray-200 mb-2 last:border-none last:mb-none"
+      class="border-b-[1px] mb-2 last:border-none last:mb-none"
     >
       <StoreModalResourceListItem :resource="resource" />
     </li>
