1aaaf6a188eff3d5899b6035b6f1dc3cceeb4aac
chinook25 <chinook25@gmail.com>
2026-06-03 12:32:46 +0200

    fix: tests

---
+++ apps/tailwind-components/tests/vitest/components/text/Hyperlink.spec.ts
@@ -0,0 +1,28 @@
+import { mountSuspended } from "@nuxt/test-utils/runtime";
+import { expect, test } from "vitest";
+import BaseIcon from "../../../../app/components/BaseIcon.vue";
+import Hyperlink from "../../../../app/components/text/Hyperlink.vue";
+
+test("External links should have specific rels for security/privacy", async () => {
+  const hyperlinkExternal = await mountSuspended(Hyperlink, {
+    props: {
+      to: "https://molgenis.org",
+      type: "external",
+    },
+  });
+  expect(hyperlinkExternal.html()).toContain(
+    'rel="external noopener noreferrer"'
+  );
+});
+
+test("External links should have icon to indicate it is external", async () => {
+  const hyperlinkExternal = await mountSuspended(Hyperlink, {
+    props: {
+      to: "https://molgenis.org",
+      type: "external",
+    },
+  });
+  expect(hyperlinkExternal.findComponent(BaseIcon).props()["name"]).toBe(
+    "ExternalLink"
+  );
+});
