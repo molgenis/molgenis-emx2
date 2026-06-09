193d23c4dd6d6770bab1177ca45a057d280b831a
jhhaanstra <janhendrikhaanstra@gmail.com>
2026-06-09 15:17:54 +0200

    fix: added feedback

--- backend/molgenis-emx2-rdf/src/main/java/org/molgenis/emx2/rdf/generators/query/FileBasedQueryGenerator.java
+++ backend/molgenis-emx2-rdf/src/main/java/org/molgenis/emx2/rdf/generators/query/FileBasedQueryGenerator.java
@@ -3,23 +3,29 @@ package org.molgenis.emx2.rdf.generators.query;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 import org.molgenis.emx2.MolgenisException;
 import org.molgenis.emx2.TableMetadata;
 
 public class FileBasedQueryGenerator implements QueryGenerator {
 
-  private final Path path;
+  private final Map<String, Path> paths;
 
-  public FileBasedQueryGenerator(Path path) {
-    this.path = path;
+  public FileBasedQueryGenerator(Map<String, Path> paths) {
+    this.paths = paths;
   }
 
   @Override
   public String generate(TableMetadata tableMetadata) {
+    if (!paths.containsKey(tableMetadata.getTableName())) {
+      throw new MolgenisException(
+          "No file path defined for table: " + tableMetadata.getTableName());
+    }
+
     try {
-      return Files.readString(path);
+      return Files.readString(paths.get(tableMetadata.getTableName()));
     } catch (IOException e) {
-      throw new MolgenisException("No file on path: " + path, e);
+      throw new MolgenisException("No file on path: " + paths, e);
     }
   }
 }
