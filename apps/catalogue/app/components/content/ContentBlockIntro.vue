14b5f8a1b0aa864778ccfd99d305dbe873ec9703
connoratrug <c.stroomberg@rug.nl>
2026-06-15 10:30:06 +0200

    feat: Add server location to contact request body
    Closes #https://github.com/molgenis/GCC/issues/2951

--- apps/catalogue/app/components/content/ContentBlockIntro.vue
+++ apps/catalogue/app/components/content/ContentBlockIntro.vue
@@ -126,6 +126,7 @@ const submitForm = async () => {
       \nOrganization: ${fields.organization.fieldValue}
       \nTopic: ${fields.topic.fieldValue}
       \nMessage: ${fields.senderMessage.fieldValue}
+      \nServer: ${window?.location?.origin || "Unknown origin"}
     `,
     });
   } catch (error) {
