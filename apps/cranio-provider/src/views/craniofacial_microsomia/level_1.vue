5d6c3b346137a0e845f3df05f4de8c0b2f78813b
dcruvolo <davidruvolo51@gmail.com>
2026-06-16 16:10:06 +0200

    feat: added title using patient registration counts

--- apps/cranio-provider/src/views/craniofacial_microsomia/level_1.vue
+++ apps/cranio-provider/src/views/craniofacial_microsomia/level_1.vue
@@ -1,9 +1,57 @@
 <script setup lang="ts">
+import { ref, computed } from "vue";
+
 import LevelTemplate from "../../components/LevelTemplate.vue";
+
+import { getDashboardChart } from "../../utils/getDashboardData.js";
+
+import type { ICharts, IChartData } from "../../types/schema.js";
 import type { IAppPage } from "../../types/app.js";
+
 const props = defineProps<IAppPage>();
+
+const siteCfm = ref<IChartData>();
+const ernCfm = ref<IChartData>();
+
+const chartDescription = computed<string>(() => {
+  const description: string[] = ["Number of Craniofacial Microsomia patients"];
+  if (siteCfm.value) {
+    description.push(`from your site (n=${siteCfm.value.dataPointValue})`);
+  }
+
+  if (ernCfm.value) {
+    description.push(`and in the ERN (n=${ernCfm.value.dataPointValue})`);
+  }
+  return description.join(" ");
+});
+
+async function getData() {
+  const sitePatientCounts = await getDashboardChart(
+    props.api.graphql.current,
+    "patients-by-workstream"
+  );
+
+  const ernPatientCounts = await getDashboardChart(
+    props.api.graphql.providers,
+    "patients-by-workstream"
+  );
+
+  siteCfm.value = sitePatientCounts[0].dataPoints?.filter((row: IChartData) => {
+    return row.dataPointName === "Craniofacial Microsomia";
+  })[0];
+
+  ernCfm.value = ernPatientCounts[0].dataPoints?.filter((row: IChartData) => {
+    return row.dataPointName === "Craniofacial Microsomia";
+  })[0];
+}
+
+getData();
 </script>
 
 <template>
-  <LevelTemplate name="Craniofacial microsomia level 1" :props="props" />
+  <LevelTemplate
+    name="Craniofacial microsomia level 1"
+    :props="props"
+    :chart-description="chartDescription"
+  />
 </template>
