0058ace96abbc65608d93344cafaac325438c493
Jelmer Veen <jelmerveen@pennions.com>
2026-06-17 09:58:55 +0200

    fix: removed unused icon and adjusted the link

--- apps/ern-ithaca/src/views/view-home.vue
+++ apps/ern-ithaca/src/views/view-home.vue
@@ -4,8 +4,7 @@ import { Page, PageHeader, PageSection, LinkCard } from "molgenis-viz";
 import {
   PlusIcon,
   FingerPrintIcon,
-  ClipboardDocumentListIcon,
-  UserIcon,
+  ClipboardDocumentListIcon
 } from "@heroicons/vue/24/outline";
 </script>
 
@@ -41,7 +40,7 @@ import {
       </h2>
       <div class="quick-links-container ithaca-quick-links">
         <LinkCard>
-          <a href="/tables/#/Subject">Register patient </a>
+          <a href="../tables/#/Subject">Register patient </a>
         </LinkCard>
         <LinkCard>
           <router-link :to="{ name: 'documents' }">Documents</router-link>
