006348e32a083eba82a7cf7b54c63d1596b30269
chinook25 <chinook25@gmail.com>
2026-06-22 11:02:07 +0200

    wip: almost all inputs work

--- apps/tailwind-components/tests/e2e/components/form/admin.add-type-test.spec.ts
+++ apps/tailwind-components/tests/e2e/components/form/admin.add-type-test.spec.ts
@@ -322,5 +322,317 @@ test("it should be able to fill out all input types", async ({ page }) => {
     .locator('[id="type test-Types-periodArrayType-form-field-input_1"]')
     .fill("P13Y37M42D");
 
+  // REFS
+  // REF
+  await page
+    .locator('[id="type test-Types-refType-form-field-input-radio-group"]')
+    .getByText("AT", { exact: true })
+    .click();
+
+  // Select
+  await page
+    .locator('[id="search-for-type test-Types-selectType-form-field-input"]')
+    .click();
+  await page
+    .locator('[id="type test-Types-selectType-form-field-input-radio-group"]')
+    .getByText("FR")
+    .click();
+
+  // Select Label
+  await page
+    .locator('[id="type test-Types-radioType-form-field-input-radio-group"]')
+    .getByText("HU")
+    .click();
+  await page
+    .locator(
+      '[id="search-for-type test-Types-selectTypeLabel-form-field-input"]'
+    )
+    .click();
+
+  // Radio
+  await page
+    .locator(
+      '[id="type test-Types-selectTypeLabel-form-field-input-radio-group"]'
+    )
+    .getByText("Belgium")
+    .click();
+
+  // Radio label
+  await page
+    .locator(
+      '[id="type test-Types-radioTypeLabel-form-field-input-radio-group"] > div > .text-body-base > .mt-1\\.5 > .fill-transparent'
+    )
+    .first()
+    .click();
+
+  // Ref array
+  await page
+    .locator(
+      '[id="type test-Types-refArrayType-form-field-input-checkbox-group"]'
+    )
+    .getByText("CY")
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-refArrayType-form-field-input-checkbox-group"]'
+    )
+    .getByText("GR", { exact: true })
+    .click();
+
+  // Checkbox
+  await page
+    .locator(
+      '[id="type test-Types-checkboxType-form-field-input-checkbox-group"]'
+    )
+    .getByText("ES", { exact: true })
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-checkboxType-form-field-input-checkbox-group"]'
+    )
+    .getByText("DE", { exact: true })
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-checkboxType-form-field-input-checkbox-group"]'
+    )
+    .getByText("CY", { exact: true })
+    .click();
+
+  // Checkbox label
+  await page
+    .locator(
+      '[id="type test-Types-checkboxTypeLabel-form-field-input-checkbox-group"]'
+    )
+    .getByText("Greece")
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-checkboxTypeLabel-form-field-input-checkbox-group"]'
+    )
+    .getByText("France")
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-checkboxTypeLabel-form-field-input-checkbox-group"]'
+    )
+    .getByText("Germany")
+    .click();
+
+  // Multiselect
+  await page
+    .locator(
+      '[id="type test-Types-multiselectType-form-field-input-ref"] > .flex.items-center.justify-between'
+    )
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-multiselectType-form-field-input-checkbox-group"] span'
+    )
+    .filter({ hasText: "CY" })
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-multiselectType-form-field-input-checkbox-group"] > div:nth-child(5) > .text-body-base > .w-\\[20px\\] > .fill-input'
+    )
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-multiselectType-form-field-input-checkbox-group"]'
+    )
+    .getByText("LT", { exact: true })
+    .click();
+
+  // Multiselect label
+  // await page
+  //   .locator(
+  //     '[id="type test-Types-multiselectTypeLabel-form-field-input-ref"] > .flex.items-center.justify-between > div:nth-child(2) > svg:nth-child(2)'
+  //   )
+  //   .click();
+  // await page
+  //   .locator(
+  //     '[id="type test-Types-multiselectTypeLabel-form-field-input-checkbox-group"] > div:nth-child(4) > .text-body-base > .w-\\[20px\\] > .fill-input'
+  //   )
+  //   .click();
+  // await page
+  //   .locator(
+  //     '[id="type test-Types-multiselectTypeLabel-form-field-input-checkbox-group"]'
+  //   )
+  //   .getByText("Cyprus")
+  //   .click();
+  // await page
+  //   .locator(
+  //     '[id="type test-Types-multiselectTypeLabel-form-field-input-checkbox-group"]'
+  //   )
+  //   .getByText("Cyprus")
+  //   .click();
+  // await page
+  //   .locator(
+  //     '[id="type test-Types-multiselectTypeLabel-form-field-input-checkbox-group"]'
+  //   )
+  //   .getByText("Belgium")
+  //   .click();
+
+  // Ref back
+  await page
+    .locator(
+      '[id="type test-Types-refBackType-form-field-input-radio-group"] > div:nth-child(6) > .text-body-base'
+    )
+    .click();
+
+  // Ontology small
+  await page
+    .locator(
+      '[id="type test-Types-ontologySmallType-form-field-input-ontology"]'
+    )
+    .getByText("Spain")
+    .click();
+
+  // Ontology large
+  await page
+    .locator(
+      '[id="type test-Types-ontologyLargeType-form-field-input-ontology"] > .flex.items-center.justify-between'
+    )
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-ontologyLargeType-form-field-input-ontology"]'
+    )
+    .getByText("Europe")
+    .click();
+
+  // Ontology small array
+
+  await page
+    .locator(
+      '[id="type test-Types-ontologySmallArrayType-form-field-input-ontology"] span'
+    )
+    .filter({ hasText: "Greece" })
+    .click();
+  await page
+    .locator(
+      "li:nth-child(6) > .flex.justify-start > .text-body-base > .w-\\[20px\\] > .fill-input"
+    )
+    .first()
+    .click();
+
+  // Ontology large array
+  await page
+    .locator(
+      '[id="type test-Types-ontologyLargeArrayType-form-field-input-ontology"] > .flex.items-center.justify-between'
+    )
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-ontologyLargeArrayType-form-field-input-ontology"] span'
+    )
+    .filter({ hasText: "Belgium" })
+    .click();
+  await page
+    .locator(
+      ".absolute > .pl-4 > .text-search-filter-group-title > li:nth-child(4) > .flex.justify-start > .text-body-base > .w-\\[20px\\] > .fill-input"
+    )
+    .first()
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-ontologyLargeArrayType-form-field-input-ontology"] label'
+    )
+    .filter({ hasText: "Canada" })
+    .click();
+
+  // Ontology small tree
+  await page
+    .locator(
+      '[id="type test-Types-ontologySmallTreeType-form-field-input-ontology"]'
+    )
+    .getByText("Switzerland")
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-ontologySmallTreeType-form-field-input-ontology"]'
+    )
+    .getByText("Asia")
+    .click();
+
+  // Ontology large tree
+  // await page
+  //   .locator(
+  //     '[id="type test-Types-ontologyLargeTreeType-form-field-input-ontology"] > .flex.items-center.justify-between'
+  //   )
+  //   .click();
+  // await page
+  //   .locator(
+  //     '[id="type test-Types-ontologyLargeTreeType-form-field-input-ontology"]'
+  //   )
+  //   .getByText("Asia")
+  //   .click();
+  // await page
+  //   .locator("div")
+  //   .filter({ hasText: /^Asia$/ })
+  //   .nth(1)
+  //   .click();
+  // await page
+  //   .locator(
+  //     '[id="type test-Types-ontologyLargeTreeType-form-field-input-ontology"]'
+  //   )
+  //   .getByRole("button", { name: "expand EU" })
+  //   .click();
+  // await page
+  //   .locator(
+  //     ".absolute > fieldset > .text-search-filter-group-title.pb-2 > li:nth-child(3) > .text-search-filter-group-title > li:nth-child(2) > .flex.justify-start > .text-body-base > .block"
+  //   )
+  //   .click();
+
+  // Ontology small tree array
+  await page
+    .locator(
+      '[id="type test-Types-ontologySmallTreeArrayType-form-field-input-ontology"]'
+    )
+    .getByText("Uganda")
+    .click();
+  await page
+    .locator(
+      ".text-search-filter-group-title.pb-2.pl-4 > li:nth-child(3) > div:nth-child(2) > .text-body-base > .w-\\[20px\\] > .fill-input"
+    )
+    .first()
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-ontologySmallTreeArrayType-form-field-input-ontology"]'
+    )
+    .getByText("Czech Republic")
+    .click();
+
+  // Ontology large tree array
+  await page
+    .locator(
+      '[id="type test-Types-ontologyLargeTreeArrayType-form-field-input-ontology"] > .flex.items-center.justify-between'
+    )
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-ontologyLargeTreeArrayType-form-field-input-ontology"]'
+    )
+    .getByText("Asia")
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-ontologyLargeTreeArrayType-form-field-input-ontology"]'
+    )
+    .getByRole("button", { name: "expand EU" })
+    .click();
+  await page
+    .locator(
+      '[id="type test-Types-ontologyLargeTreeArrayType-form-field-input-ontology"]'
+    )
+    .getByText("Bulgaria")
+    .click();
+  await page.goto(
+    "http://localhost:3000/form/AddModal.story?schema=type+test&table=Types"
+  );
+  await page.getByRole("button", { name: "Add Types" }).click();
+
+  // Save the form
   await page.getByRole("button", { name: "Save", exact: true }).click();
 });
