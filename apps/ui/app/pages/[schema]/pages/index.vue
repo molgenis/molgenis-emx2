c07d2552b0f24be2bb9d1d69bccc86a358f6b65f
dcruvolo <davidruvolo51@gmail.com>
2026-06-25 16:03:57 +0200

    feat: added 'create page' button

--- apps/ui/app/pages/[schema]/pages/index.vue
+++ apps/ui/app/pages/[schema]/pages/index.vue
@@ -8,8 +8,19 @@ import BreadCrumbs from "../../../../../tailwind-components/app/components/Bread
 import PageHeader from "../../../../../tailwind-components/app/components/PageHeader.vue";
 import BaseIcon from "../../../../../tailwind-components/app/components/BaseIcon.vue";
 import Message from "../../../../../tailwind-components/app/components/Message.vue";
+import Button from "../../../../../tailwind-components/app/components/Button.vue";
+import ButtonDropdown from "../../../../../tailwind-components/app/components/button/Dropdown.vue";
+import EditModal from "../../../../../tailwind-components/app/components/form/EditModal.vue";
+import InputSearch from "../../../../../tailwind-components/app/components/input/Search.vue";
+
 import type { Crumb } from "../../../../../tailwind-components/types/types";
 import type { IContainers } from "../../../../../tailwind-components/types/cms";
+import type {
+  ISchemaMetaData,
+  ITableMetaData,
+} from "../../../../../metadata-utils/src/types.js";
+
+import { getContainersAndMetadata } from "../../../../../tailwind-components/app/gql/cmsPages.js";
 
 interface ICmsPage extends IContainers {
   mg_tableclass: string;
@@ -23,7 +34,7 @@ const schema = Array.isArray(route.params.schema)
 useHead({ title: `Pages - ${schema} - Molgenis` });
 
 interface PagesResponse {
-  data: { Containers: ICmsPage[] };
+  data: { Containers: ICmsPage[]; _schema?: ISchemaMetaData };
   error: Record<string, any>[];
 }
 
@@ -31,12 +42,39 @@ const error = ref<string>("");
 const { data } = await $fetch<PagesResponse>(`/${schema}/graphql`, {
   method: "POST",
   body: {
-    query: `{ Containers (orderby: { name: ASC }) { name mg_tableclass } }`,
+    query: getContainersAndMetadata,
   },
 }).catch((err) => {
   error.value = err;
   throw new Error(err);
 });
+
+const configurablePageTableMetadata = ref<ITableMetaData>();
+const developerPageTableMetadata = ref<ITableMetaData>();
+const formMetadata = ref();
+const showFormModal = ref<boolean>(false);
+const search = ref<string>();
+
+developerPageTableMetadata.value = data._schema?.tables.filter(
+  (row: ITableMetaData) => {
+    return row.id === "DeveloperPages";
+  }
+)[0];
+
+configurablePageTableMetadata.value = data._schema?.tables.filter(
+  (row: ITableMetaData) => {
+    return row.id === "ConfigurablePages";
+  }
+)[0];
+
+function onAddNewPageClick(type: string) {
+  if (type === "ConfigurablePage") {
+    formMetadata.value = configurablePageTableMetadata.value;
+  } else {
+    formMetadata.value = developerPageTableMetadata.value;
+  }
+  showFormModal.value = true;
+}
 
 const crumbs: Crumb[] = [
   { label: schema as string, url: `/${schema}` },
@@ -59,6 +97,31 @@ function setNuxtLink(value: string, page: string): string | undefined {
         <BreadCrumbs :crumbs="crumbs" align="left" />
       </template>
     </PageHeader>
+    <div class="flex pb-7.5 justify-between">
+      <div class="w-3/5 xl:w-2/5 2xl:w-1/5">
+        <label for="page-search" class="sr-only"> Search for pages </label>
+        <InputSearch
+          id="page-search"
+          size="medium"
+          v-model="search"
+          placeholder="Search for pages"
+        />
+      </div>
+      <div class="flex gap-2.5">
+        <ButtonDropdown label="Add new page">
+          <Button
+            type="secondary"
+            class="w-full"
+            @click="onAddNewPageClick('ConfigurablePage')"
+          >
+            Simple page
+          </Button>
+          <Button type="secondary" @click="onAddNewPageClick('DeveloperPage')">
+            developer page
+          </Button>
+        </ButtonDropdown>
+      </div>
+    </div>
     <div
       class="flex flew-wrap justify-start items-center gap-7.5"
       v-if="data && data.Containers"
@@ -93,4 +156,13 @@ function setNuxtLink(value: string, page: string): string | undefined {
       </Message>
     </div>
   </Container>
+  <EditModal
+    v-if="formMetadata"
+    key="edit-modal-configurable-page"
+    :show-button="false"
+    :schema-id="(schema as string)"
+    :metadata="formMetadata"
+    :is-insert="false"
+    v-model:visible="showFormModal"
+  />
 </template>
