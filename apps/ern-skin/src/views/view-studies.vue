7f564ec870b69dda2dec3c48db3d8168f96fe67c
dcruvolo <davidruvolo51@gmail.com>
2026-06-29 17:50:59 +0200

    refactored studies components

--- apps/ern-skin/src/views/view-studies.vue
+++ apps/ern-skin/src/views/view-studies.vue
@@ -5,9 +5,9 @@ import Studies from "../components/Studies.vue";
 </script>
 
 <template>
-  <Page id="page-documents">
+  <Page id="page-studies">
     <PageHeader
-      class="erras-header"
+      id="ernSkinStudies"
       title="ERN-SKIN Registry"
       subtitle="STUDIES"
       titlePositionX="center"
@@ -18,29 +18,32 @@ import Studies from "../components/Studies.vue";
       aria-labelledby="section-documents-title"
       :verticalPadding="1"
     >
-      <h3 id="section-documents-title"></h3>
-      The transparency portal, as defined by the CNIL, allows you to be informed
-      of research projects that utilize information gathered within the
-      framework of ERRAS: the European Registry for RAre Skin diseases. <br />
-      These projects are essential to foster research on rare skin diseases, to
-      improve the lives of patients, the knowledge and the treatment of their
-      diseases. <br />
-      Each project is summarized in accordance with the General Data Protection
-      Regulation (GDPR) as defined in Articles 12 and following:<br />
-      <blockquote id="quote-nav">
+      <p>
+        The transparency portal, as defined by the CNIL, allows you to be
+        informed of research projects that utilize information gathered within
+        the framework of ERRAS: the European Registry for RAre Skin diseases.
+        These projects are essential to foster research on rare skin diseases,
+        to improve the lives of patients, the knowledge and the treatment of
+        their diseases. Each project is summarized in accordance with the
+        General Data Protection Regulation (GDPR) as defined in Articles 12 and
+        following:
+      </p>
+      <blockquote>
         “The controller shall take appropriate measures to provide any
-        information referred to in Articles 13 and 14 and any communication
-        under Articles 15 to 22 and 34 relating to processing to the data
+        information referred to in Articles 13 and 14 and any communication
+        under Articles 15 to 22 and 34 relating to processing to the data
         subject in a concise, transparent, intelligible and easily accessible
         form, using clear and plain language, in particular for any information
-        addressed specifically to a child. The information shall be provided in
+        addressed specifically to a child. The information shall be provided in
         writing, or by other means, including, where appropriate, by electronic
-        means. When requested by the data subject, the information may be
+        means. When requested by the data subject, the information may be
         provided orally, provided that the identity of the data subject is
         proven by other means.”
       </blockquote>
-      Each summary allows you to understand the objectives pursued by the
-      research, the population involved, and the data used.
+      <p>
+        Each summary allows you to understand the objectives pursued by the
+        research, the population involved, and the data used.
+      </p>
       <Accordion id="studies-nav" title="Studies" :isOpenByDefault="true">
         <Studies table="Studies" labelsColumn="title" />
       </Accordion>
@@ -48,17 +51,10 @@ import Studies from "../components/Studies.vue";
   </Page>
 </template>
 
-<style lang="scss">
-#accordion-studies-nav {
-  .accordion-heading {
-    background-color: #0084b4;
-    border-color: #0084b4;
-    color: #ffffff;
-  }
-}
-
-#quote-nav {
-  font-size: 0.8em;
-  margin-left: 40px;
+<style lang="css">
+#accordion-studies-nav .accordion-heading {
+  background-color: #0084b4;
+  border-color: #0084b4;
+  color: #ffffff;
 }
 </style>
