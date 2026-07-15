0ac82f94a519d2a20c247125aa99d92839f1eb3a
Aike <4234850+aikevd@users.noreply.github.com>
2026-07-15 14:47:25 +0200

    feat(apps/molgenis-components): Support cross-schema table-permissions
    The current tablePermissions prop passed to components only knows the current schema's tablePermissions (fetched by MolgenisSession.vue). This poses a problem when components need to know cross-schema access, such as Ref inputs.
    This change adds support for fetching cross-schema table-permissions on demand, with a caching mechanism in client.ts to reduce duplicate/repeated network calls.

--- apps/molgenis-components/src/components/forms/InputRefList.vue
+++ apps/molgenis-components/src/components/forms/InputRefList.vue
@@ -61,7 +61,7 @@
       <div class="m-1">
         <RowButtonAdd
           id="add-entry"
-          v-if="canEdit"
+          v-if="canInsert"
           :label="`Add new ${label}`"
           :tableId="tableId"
           :schemaId="schemaId"
@@ -87,6 +87,7 @@
             :schemaId="schemaId"
             :tableId="tableId"
             :canEdit="canEdit"
+            :canInsert="canInsert"
             :showSelect="true"
             :filter="filter"
             :limit="10"
@@ -111,7 +112,8 @@ import {
 } from "../../../../metadata-utils/src/types";
 import { IRow } from "../../Interfaces/IRow";
 import { INewClient } from "../../client/IClient";
-import Client from "../../client/client";
+import type { ITablePermission } from "../account/Interfaces";
+import Client, { resolveTablePermission } from "../../client/client";
 import FilterWell from "../filters/FilterWell.vue";
 import LayoutModal from "../layout/LayoutModal.vue";
 import Spinner from "../layout/Spinner.vue";
@@ -138,6 +140,7 @@ export default {
       selection: deepClone(this.modelValue),
       count: 0,
       tableMetadata: null as null | ITableMetaData,
+      tablePermission: undefined as undefined | ITablePermission,
       loading: false,
     };
   },
@@ -173,8 +176,16 @@ export default {
       type: Boolean,
       default: () => false,
     },
+    tablePermissions: {
+      type: Array,
+      required: false,
+      default: () => [],
+    },
   },
   computed: {
+    canInsert() {
+      return this.tablePermission?.canInsert ?? this.canEdit;
+    },
     title() {
       const label = this.tableMetadata?.label || this.tableId;
       return "Select " + label;
@@ -263,8 +274,17 @@ export default {
       this.loading = false;
       this.$emit("optionsLoaded", this.data);
     },
+    async loadPermission() {
+      this.tablePermission = await resolveTablePermission(
+        this.schemaId,
+        this.tableId,
+        this.tablePermissions as ITablePermission[]
+      );
+    },
   },
   watch: {
+    schemaId: "loadPermission",
+    tableId: "loadPermission",
     async modelValue() {
       if (!this.modelValue) {
         this.selection = [];
@@ -282,6 +302,7 @@ export default {
     },
   },
   async created() {
+    this.loadPermission();
     this.loading = true;
     this.client = Client.newClient(this.schemaId);
     this.tableMetadata = await this.client.fetchTableMetaData(this.tableId);
