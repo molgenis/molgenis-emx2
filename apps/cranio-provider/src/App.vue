5c6f0efe39509785eba0d99c77b925794bcdb6ac
dcruvolo <davidruvolo51@gmail.com>
2026-07-15 14:38:47 +0100

    chore: replaced footer component with molgenis-viz component

--- apps/cranio-provider/src/App.vue
+++ apps/cranio-provider/src/App.vue
@@ -15,45 +15,44 @@ import {
   Dashboard,
   MessageBox,
   LoadingScreen,
+  AppFooter,
   // @ts-ignore
 } from "molgenis-viz";
 
 // @ts-ignore
 import ProviderSidebar from "./components/ProviderSidebar.vue";
-import AppFooter from "./components/AppFooter.vue";
 
 const session = ref(null);
-
 const loading = ref<boolean>(true);
 const error = ref<string>();
 const cranioSchemas = ref<ICranioSchemas>();
 const currentSchemaName = ref<string>();
 const currentOrganisation = ref<IOrganisations>();
+const publicSchema = ref<string>();
 
-onBeforeMount(async () => {
-  try {
-    cranioSchemas.value = await getCranioSchemaNames();
-    currentSchemaName.value = await getSchemaName();
+async function getAppData() {
+  cranioSchemas.value = await getCranioSchemaNames();
+  currentSchemaName.value = await getSchemaName();
+  currentOrganisation.value = await getOrganisation(
+    `/${cranioSchemas.value?.CRANIO_PUBLIC_SCHEMA}/api/graphql`,
+    currentSchemaName.value
+  );
+}
 
-    currentOrganisation.value = await getOrganisation(
-      `/${cranioSchemas.value?.CRANIO_PUBLIC_SCHEMA}/api/graphql`,
-      currentSchemaName.value
-    );
-
-    if (window && currentOrganisation.value) {
-      window.document.title = `${window.document.title} | ${currentOrganisation.value.name}`;
-    }
-  } catch (err: unknown) {
+getAppData()
+  .then(() => {
+    window.document.title = `${window.document.title} | ${currentOrganisation.value?.name}`;
+    publicSchema.value = cranioSchemas.value?.CRANIO_PUBLIC_SCHEMA;
+  })
+  .catch((err) => {
     if (Object.hasOwn(err as Error, "response")) {
       const message = (err as IMgErrorResponse).response.errors[0].message;
       error.value = message;
     } else {
       error.value = (err as Error).message;
     }
-  } finally {
-    loading.value = false;
-  }
-});
+  })
+  .finally(() => (loading.value = false));
 </script>
 
 <template>
@@ -91,6 +90,46 @@ onBeforeMount(async () => {
         </Dashboard>
       </div>
     </Page>
-    <AppFooter :publicSchema="cranioSchemas?.CRANIO_PUBLIC_SCHEMA" />
+    <AppFooter
+      id="cranio-provider-footer"
+      firstColumnTitle="ERN CRANIO"
+      secondColumnTitle="For Members"
+    >
+      <template v-slot:column-links-1>
+        <li>
+          <a :href="`/${publicSchema}/cranio-public/#/`">Home</a>
+        </li>
+        <li>
+          <a :href="`/${publicSchema}/cranio-public/#/about`">About</a>
+        </li>
+        <li>
+          <a :href="`/${publicSchema}/cranio-public/#/dashboard`">
+            Dashboard
+          </a>
+        </li>
+      </template>
+      <template v-slot:column-links-2>
+        <li>
+          <a :href="`/${publicSchema}/cranio-public/#/Providers`">
+            Providers
+          </a>
+        </li>
+        <li>
+          <a :href="`/${publicSchema}/cranio-public/#/Documents`">
+            Documents
+          </a>
+        </li>
+      </template>
+      <template v-slot:column-logos>
+        <li id="project-logo-link">
+          <a :href="`/${publicSchema}/cranio-public/#/`">
+            <img
+              src="/img/ern-cranio-logo.png"
+              alt="ERN CRANIO: European Reference Network for rare and/or complex craniofacial anomalies and ear, nose and throat (ENT) disorders"
+            />
+          </a>
+        </li>
+      </template>
+    </AppFooter>
   </Molgenis>
 </template>
