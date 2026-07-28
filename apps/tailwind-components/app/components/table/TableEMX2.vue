1c7fa19d9b7df0ff8342be57728cb79ece54a927
Joris de Keijser <chinook25@gmail.com>
2026-07-28 11:46:56 +0200

    feat(tw-explorer): add copy button (#6538)

--- apps/tailwind-components/app/components/table/TableEMX2.vue
+++ apps/tailwind-components/app/components/table/TableEMX2.vue
@@ -212,7 +212,20 @@
                         >
                           {{ row._rowIdString }}
                         </Button>
-
+                        <Button
+                          v-if="isEditable"
+                          :id="`copy-button-${row._rowIdString}`"
+                          :icon-only="true"
+                          type="inline"
+                          icon="copy"
+                          label="copy"
+                          @click="onShowEditModal(row, true)"
+                          :aria-controls="`table-emx2-${schemaId}-${tableId}-modal-copy`"
+                          aria-haspopup="dialog"
+                          :aria-expanded="showEditModal"
+                        >
+                          {{ row._rowIdString }}
+                        </Button>
                         <slot name="additional-row-actions" :row="row" />
                       </div>
                     </template>
@@ -287,7 +300,7 @@
     :schemaId="schemaId"
     :metadata="data.tableMetadata"
     :formValues="rowDataForModal"
-    :isInsert="false"
+    :isInsert="isCopy"
     v-model:visible="showEditModal"
     @update:cancelled="afterClose"
   />
@@ -379,6 +392,7 @@ const showDeleteModal = ref<boolean>(false);
 const showDeleteMultipleModal = ref<boolean>(false);
 const rowDataForModal = ref<IRow>();
 const showModal = ref(false);
+const isCopy = ref(false);
 
 const cellDetailPayload = ref<cellPayload>();
 const columns = ref<IColumn[]>([]);
@@ -730,11 +744,19 @@ function onShowDeleteModal(row: TableRow) {
   showDeleteModal.value = true;
 }
 
-function onShowEditModal(row: TableRow) {
+function onShowEditModal(row: TableRow, isCopyMode = false) {
   const clone: IRow = structuredClone(row);
   delete clone._rowId;
   delete clone._rowIdString;
+  if (isCopyMode) {
+    data.value?.tableMetadata?.columns.forEach((column: IColumn) => {
+      if (column.key === 1) {
+        delete clone[column.id];
+      }
+    });
+  }
   rowDataForModal.value = clone;
+  isCopy.value = isCopyMode;
   showEditModal.value = true;
 }
 
