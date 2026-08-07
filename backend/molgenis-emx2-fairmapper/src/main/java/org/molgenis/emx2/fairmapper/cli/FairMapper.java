f6f3b61b00dc5522ce3a9d1c1859b80105c06db6
jhhaanstra <janhendrikhaanstra@gmail.com>
2026-08-07 09:01:15 +0200

    fix: add harvest command to Fairmapper CLI

--- backend/molgenis-emx2-fairmapper/src/main/java/org/molgenis/emx2/fairmapper/cli/FairMapper.java
+++ backend/molgenis-emx2-fairmapper/src/main/java/org/molgenis/emx2/fairmapper/cli/FairMapper.java
@@ -1,13 +1,14 @@
 package org.molgenis.emx2.fairmapper.cli;
 
 import org.molgenis.emx2.fairmapper.cli.commands.GenerateQuery;
+import org.molgenis.emx2.fairmapper.cli.commands.Harvest;
 import picocli.CommandLine;
 
 @CommandLine.Command(
     name = "fairmapper",
     mixinStandardHelpOptions = true,
     version = "0.1",
-    subcommands = {GenerateQuery.class},
+    subcommands = {GenerateQuery.class, Harvest.class},
     description = "Tool for DCAT harvesting")
 public class FairMapper implements Runnable {
 
