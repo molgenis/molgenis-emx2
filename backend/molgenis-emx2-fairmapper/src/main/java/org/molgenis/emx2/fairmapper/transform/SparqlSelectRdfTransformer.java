c75572fcc16027dbb0d864ced5fc660e0d323965
Jan Hendrik Haanstra <janhendrikhaanstra@gmail.com>
2026-08-10 11:59:56 +0200

    feat: set up harvesting pipeline (#6583)
    * feat: set up harvesting pipeline
    
    * fix: updated harvesting interfaces with InMemoryTableStore and the generic Repository instead of the SailRepository
    
    * ref: use snowflake for harvest id and log this
    
    * ref: remove old output
    
    * fix: use List<String> instead of String[] to fix record equals

--- backend/molgenis-emx2-fairmapper/src/main/java/org/molgenis/emx2/fairmapper/transform/SparqlSelectRdfTransformer.java
+++ backend/molgenis-emx2-fairmapper/src/main/java/org/molgenis/emx2/fairmapper/transform/SparqlSelectRdfTransformer.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.eclipse.rdf4j.query.*;
-import org.eclipse.rdf4j.repository.sail.SailRepository;
-import org.eclipse.rdf4j.repository.sail.SailRepositoryConnection;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.molgenis.emx2.*;
+import org.molgenis.emx2.MolgenisException;
+import org.molgenis.emx2.Row;
+import org.molgenis.emx2.SchemaMetadata;
 import org.molgenis.emx2.io.tablestore.InMemoryTableStore;
-import org.molgenis.emx2.io.tablestore.TableStore;
 import org.molgenis.emx2.rdf.generators.query.ColumnNameSparqlEncoder;
 import org.molgenis.emx2.rdf.generators.query.QueryGenerator;
 import org.molgenis.emx2.rdf.generators.query.TableQueryGenerator;
@@ -47,18 +49,18 @@ public class SparqlSelectRdfTransformer implements RdfTransformer {
   }
 
   @Override
-  public TableStore transform(SailRepository repository) {
+  public InMemoryTableStore transform(Repository repository) {
     InMemoryTableStore tableStore = new InMemoryTableStore();
-    try (SailRepositoryConnection conn = repository.getConnection()) {
+    try (RepositoryConnection conn = repository.getConnection()) {
       tables.forEach(table -> addTableDataToStore(table, conn, tableStore));
     }
     return tableStore;
   }
 
   private void addTableDataToStore(
-      String table, SailRepositoryConnection conn, InMemoryTableStore tableStore) {
+      String table, RepositoryConnection conn, InMemoryTableStore tableStore) {
     TableMetadata tableMetadata = schema.getTableMetadata(table);
-    String query = queryGenerator.generate(tableMetadata);
+    String query = queryGenerator.generate(schema.getTableMetadata(table));
     TupleQuery prepared = conn.prepareTupleQuery(QueryLanguage.SPARQL, query);
 
     try (TupleQueryResult evaluate = prepared.evaluate()) {
