2b79513718cf2fbacbc4aeb02a1b31ac8fc68fa8
Harm Brugge <harmbrugge@gmail.com>
2026-08-11 09:10:56 +0200

    removed redundant code

--- apps/tailwind-components/tests/e2e/components/table/admin.tableEMX2.spec.ts
+++ apps/tailwind-components/tests/e2e/components/table/admin.tableEMX2.spec.ts
@@ -65,10 +65,6 @@ test("the row should be copied and added to the table after copying", async ({
   await page.getByRole("textbox", { name: "name Required" }).fill("copy cat");
   await page.getByRole("button", { name: "Save", exact: true }).click();
   await page.getByRole("button", { name: "Cancel" }).click();
-
-  // check result
-  await page.getByRole("searchbox", { name: "Search Pet" }).fill("copy cat");
-
   await expect(
     page
       .locator("div")
