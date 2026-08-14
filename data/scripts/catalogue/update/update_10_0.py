89cc71d58ef6e57e9f3feb82744da3bb8749151a
BHijmans <b.s.hijmans@umcg.nl>
2026-08-14 09:14:19 +0200

    add migrations for Subpopulations table

--- data/scripts/catalogue/update/update_10_0.py
+++ data/scripts/catalogue/update/update_10_0.py
@@ -75,7 +75,6 @@ class Transform:
         if any(item in ['NetworksStaging','RWEStaging'] for item in self.profile):
             self.collections()
             self.subpopulations()
-            self.collection_events()
 
     def collections(self):
         """ Transform Collections
@@ -86,66 +85,12 @@ class Transform:
                                                           'other exclusion criteria': 'exclusion criteria'})
         df_collections.to_csv(self.path + 'Collections.csv', index=False)
 
-    def catalogues(self):
-        """ Transform Catalogues
+    def subpopulations(self):
+        """ Transform Subpopulations
         """
-        df_catalogues = pd.read_csv(self.path + 'Catalogues.csv', dtype='object', keep_default_na=False)
-        df_catalogues = df_catalogues.rename(columns = {'datasets.resource': 'tables.resource',
-                                                        'datasets.name': 'tables.name'})
-        df_catalogues.to_csv(self.path + 'Catalogues.csv', index=False)
-
-    def datasets(self):
-        """ Transform Datasets and rename
-        """
-        df_datasets = pd.read_csv(self.path + 'Datasets.csv', dtype='object', keep_default_na=False)
-        df_datasets = df_datasets.rename(columns = {'dataset type': 'table type',
-                                                    'mapped to.source dataset': 'mapped to.source table',
-                                                    'mapped to.target dataset': 'mapped to.target table',
-                                                    'mapped from.source dataset': 'mapped from.source table',
-                                                    'mapped from.target dataset': 'mapped from.target table'})
-        df_datasets.to_csv(self.path + 'Tables.csv', index=False)
-
-    def dataset_mappings(self):
-        """ Transform Dataset mappings and rename
-        """
-        df_dataset_mappings = pd.read_csv(self.path + 'Dataset mappings.csv', dtype='object', keep_default_na=False)
-        df_dataset_mappings = df_dataset_mappings.rename(columns = {'source dataset': 'source table',
-                                                                    'target dataset': 'target table'})
-        df_dataset_mappings.to_csv(self.path + 'Table mappings.csv', index=False)
-
-    def variables(self):
-        """ Transform Variables
-        """
-        df_variables = pd.read_csv(self.path + 'Variables.csv', dtype='object', keep_default_na=False)
-        df_variables = df_variables.rename(columns = {'dataset': 'table',
-                                                      'useExternalDefinition.dataset': 'useExternalDefinition.table',
-                                                      'reused in resources.variable.dataset': 'reused in resources.variable.table',
-                                                      'mappings.source dataset': 'mappings.source table',
-                                                      'mappings.target dataset': 'mappings.target table',
-                                                      'permitted values.dataset': 'permitted values.table'})
-        df_variables.to_csv(self.path + 'Variables.csv', index=False)
-
-    def variable_mappings(self):
-        """ Transform Variable mappings
-        """
-        df_variable_mappings = pd.read_csv(self.path + 'Variable mappings.csv', dtype='object', keep_default_na=False)
-        df_variable_mappings = df_variable_mappings.rename(columns = {'source dataset': 'source table',
-                                                                      'source variables other datasets.dataset': 'source variables other tables.table',
-                                                                      'source variables other datasets.name': 'source variables other tables.name',
-                                                                      'target dataset': 'target table'})
-        df_variable_mappings.to_csv(self.path + 'Variable mappings.csv', index=False)
-
-    def variable_values(self):
-        """ Transform Variable Values
-        """
-        df_variable_values = pd.read_csv(self.path + 'Variable values.csv', dtype='object', keep_default_na=False)
-        df_variable_values = df_variable_values.rename(columns={'dataset': 'table'})
-        df_variable_values.to_csv(self.path + 'Variable values.csv', index=False)
-
-    def reused_variables(self):
-        """ Transform Reused Variables
-        """
-        df_reused_variables = pd.read_csv(self.path + 'Reused variables.csv', dtype='object', keep_default_na=False)
-        df_reused_variables = df_reused_variables.rename(columns={'variable.dataset': 'variable.table'})
-        df_reused_variables.to_csv(self.path + 'Reused variables.csv', index=False)
-
+        df_subpopulations = pd.read_csv(self.path + 'Subpopulations.csv', dtype='object', keep_default_na=False)
+        df_subpopulations = df_subpopulations.drop(columns=['inclusion criteria', 'exclusion criteria'], axis=1,
+                                                   inplace=False)
+        df_subpopulations = df_subpopulations.rename(columns={'other inclusion criteria': 'inclusion criteria',
+                                                              'other exclusion criteria': 'exclusion criteria'})
+        df_subpopulations.to_csv(self.path + 'Subpopulations.csv', index=False)
