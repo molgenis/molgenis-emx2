eb3be60ebf8489f2aa22a8341c3507cde6a83bc1
dcruvolo <davidruvolo51@gmail.com>
2026-08-14 18:16:28 +0200

    fix(cms): update stories (#6639)

---
+++ apps/tailwind-components/app/pages/cms/Image.story.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Image from "../../components/cms/Image.vue";
+import DemoImage from "../../assets/img/molgenis-banner.jpg";
+</script>
+
+<template>
+  <Image
+    id="demo-image"
+    :image="{ url: DemoImage }"
+    alt="MOLGENIS: open source software"
+    class="mt-5"
+    width="350px"
+  />
+</template>
