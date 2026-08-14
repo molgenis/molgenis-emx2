798372970f4912d9b3738c143e6a4fb1753a5803
Sander van den Hoek <svandenhoek@users.noreply.github.com>
2026-08-14 16:01:14 +0200

    feat: centralized semantic field mapping & format adjustments (#6305)

--- backend/molgenis-emx2/src/main/java/org/molgenis/emx2/TableMetadata.java
+++ backend/molgenis-emx2/src/main/java/org/molgenis/emx2/TableMetadata.java
@@ -1,5 +1,6 @@
 package org.molgenis.emx2;
 
+import static java.util.Arrays.stream;
 import static org.jooq.impl.DSL.name;
 import static org.molgenis.emx2.Column.column;
 import static org.molgenis.emx2.ColumnType.*;
@@ -7,12 +8,13 @@ import static org.molgenis.emx2.utils.TypeUtils.convertToPascalCase;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import javax.annotation.Nullable;
 import org.jooq.Field;
 import org.jooq.Record;
 import org.jooq.impl.DSL;
 
 public class TableMetadata extends HasLabelsDescriptionsAndSettings<TableMetadata>
-    implements Comparable {
+    implements Comparable, HasSemantics {
 
   public static final String TABLE_NAME_MESSAGE =
       ": Table name must start with a letter, followed by zero or more letters, numbers, spaces or underscores. A space immediately before or after an underscore is not allowed. The character limit is 31.";
@@ -37,17 +39,29 @@ public class TableMetadata extends HasLabelsDescriptionsAndSettings<TableMetadat
   // use to classify the table, influences display, import, export, etc
   private TableType tableType = TableType.DATA;
   // table semantics, typically an ontology URI
-  private String[] semantics = null;
+  private Semantic[] semantics = null;
   // profiles to which this table belongs
   private String[] profiles;
 
-  public String[] getSemantics() {
+  @Nullable
+  @Override
+  public Semantic[] getSemantics() {
     return semantics;
   }
 
-  public TableMetadata setSemantics(String... semantics) {
+  public TableMetadata setSemantics(Semantic[] semantics) {
     this.semantics = semantics;
     return this;
+  }
+
+  public TableMetadata setSemantics(String... semantics) {
+    return setSemantics(
+        semantics == null ? null : stream(semantics).map(Semantic::new).toArray(Semantic[]::new));
+  }
+
+  @Override
+  public SemanticPrefixes getSemanticPrefixes() {
+    return getSchema().getSemanticPrefixes();
   }
 
   public String[] getProfiles() {
