5d5d1f36d8a85a94f19139d0431971adb031dc80
dcruvolo <davidruvolo51@gmail.com>
2026-08-18 14:02:20 +0200

    feat: added test for evaluating page content

--- apps/ui/tests/e2e/cms/gallery-create-landing-page.spec.ts
+++ apps/ui/tests/e2e/cms/gallery-create-landing-page.spec.ts
@@ -5,31 +5,22 @@ const route = playwrightConfig?.use?.baseURL?.startsWith("http://localhost")
   ? playwrightConfig?.use?.baseURL
   : "/apps/ui/";
 
+const newPageName = "playwright-test-page";
+
 test.describe("Create Configurable page (cms):", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto(route + "cms/pages");
     const title = await page.getByRole("heading", { name: "Pages" });
     await title.waitFor();
-
-    if (await page.getByRole("button", { name: "Signin" })) {
-      await page.getByRole("button", { name: "Signin" }).click();
-      await page.getByRole("textbox", { name: "Username" }).click();
-      await page.getByRole("textbox", { name: "Username" }).fill("admin");
-      await page.getByRole("textbox", { name: "Username" }).press("Tab");
-      await page.getByRole("textbox", { name: "Password" }).fill("admin");
-      await page.getByRole("button", { name: "Sign in" }).click();
-    }
-
-    await expect(page.getByRole("button", { name: "Account" })).toBeVisible();
-  });
-
-  test.afterEach(async ({ page }) => {
-    await page.getByRole("button", { name: "Account" }).click();
-    await page.getByRole("button", { name: "Sign out" }).click();
   });
 
   test("Add new landing page", async ({ page }) => {
-    const newPageName = "playwright-test-page";
+    await page.getByRole("button", { name: "Signin" }).click();
+    await page.getByRole("textbox", { name: "Username" }).click();
+    await page.getByRole("textbox", { name: "Username" }).fill("admin");
+    await page.getByRole("textbox", { name: "Username" }).press("Tab");
+    await page.getByRole("textbox", { name: "Password" }).fill("admin");
+    await page.getByRole("button", { name: "Sign in" }).click();
 
     await page.getByRole("button", { name: "Add new page" }).click();
     await page.getByRole("button", { name: "Landing page" }).click();
@@ -43,6 +34,26 @@ test.describe("Create Configurable page (cms):", () => {
 
     await expect(
       page.getByRole("link", { name: newPageName, exact: true })
+    ).toBeVisible();
+  });
+
+  test("new landing page contains initial content", async ({ page }) => {
+    const newPageLink = await page.getByRole("link", {
+      name: newPageName,
+      exact: true,
+    });
+    const href = await newPageLink.getAttribute("href");
+    const url = href?.slice(1, href.length - 1);
+    await page.goto(route + url, { waitUntil: "load" });
+
+    const link = await page.getByText(newPageName, { exact: true });
+    await link.waitFor();
+
+    await expect(
+      page.getByRole("heading", { name: "Title", exact: true })
+    ).toBeVisible();
+    await expect(
+      page.getByRole("heading", { name: "Section Heading", exact: true })
     ).toBeVisible();
   });
 });
