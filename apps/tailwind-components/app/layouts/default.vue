3739d73b61f51b26c3daf2b5cac59038c9a56b50
p237959 <47183404+connoratrug@users.noreply.github.com>
2026-08-20 09:58:57 +0200

    swirtch back to take over mode

--- apps/tailwind-components/app/layouts/default.vue
+++ apps/tailwind-components/app/layouts/default.vue
@@ -35,7 +35,7 @@
       </Header>
 
       <main>
-        <Banner v-if="banner"><div v-html="banner" /></Banner>
+        <Banner v-if="banner" v-html="banner" />
         <slot />
       </main>
 
