1403f68055c9694028eb35c789f83776357a045b
dcruvolo <davidruvolo51@gmail.com>
2026-08-24 16:38:00 +0200

    fix: implemented semantic HTML dropdown component with popper-like events

--- apps/tailwind-components/app/components/button/Dropdown.vue
+++ apps/tailwind-components/app/components/button/Dropdown.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-import { useId } from "vue";
+import { ref, useTemplateRef, useId, onBeforeUnmount, watch } from "vue";
+import { onClickOutside, useFocusWithin } from "@vueuse/core";
 import Button from "../Button.vue";
 
-const ariaId = useId();
+const ariaId: string = useId();
+const isOpen = ref<boolean>(false);
+const dropdownElem = useTemplateRef<HTMLDivElement>("dropdown");
 
 withDefaults(
   defineProps<{
@@ -15,9 +18,50 @@ withDefaults(
     iconPosition: "right",
   }
 );
+
+const { focused } = useFocusWithin(dropdownElem);
+watch(focused, (focused) => {
+  if (!focused && isOpen.value) {
+    isOpen.value = false;
+  }
+});
+
+const stop = onClickOutside(dropdownElem, (event: Event) => {
+  const target = event.target as HTMLElement;
+  if (!dropdownElem.value?.contains(target) && dropdownElem.value !== target) {
+    isOpen.value = false;
+  }
+});
+
+onBeforeUnmount(() => stop());
 </script>
 <template>
-  <VDropdown
+  <div ref="dropdown" class="relative">
+    <Button
+      :id="`dropdown-${ariaId}-toggle`"
+      type="outline"
+      size="medium"
+      :icon="icon"
+      :icon-position="iconPosition"
+      @click="isOpen = !isOpen"
+      :aria-expanded="isOpen"
+      :aria-controls="`dropdown-${ariaId}-content`"
+    >
+      {{ label }}
+    </Button>
+    <div
+      :id="`dropdown-${ariaId}-content`"
+      :aria-hidden="!isOpen"
+      class="absolute bottom mt-0.5 left-0 z-10 bg-dropdown text-dropdown shadow-md rounded-base border transition-opacity ease-in-out motion-safe:duration-150 motion-reduce:duration-0"
+      :class="{
+        'invisible opacity-0': !isOpen,
+        'visible opacity-100': isOpen,
+      }"
+    >
+      <slot></slot>
+    </div>
+  </div>
+  <!-- <VDropdown
     :aria-id="ariaId"
     :distance="2"
     :skidding="4"
@@ -36,7 +80,7 @@ withDefaults(
         <slot />
       </div>
     </template>
-  </VDropdown>
+  </VDropdown> -->
 </template>
 
 <style>
