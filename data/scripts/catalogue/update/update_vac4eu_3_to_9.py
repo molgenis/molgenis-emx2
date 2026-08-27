401db9329b607579121b53964e98e7cd97a6129f
BHijmans <b.s.hijmans@umcg.nl>
2026-08-27 15:59:57 +0200

    add migrations to 8.x

--- data/scripts/catalogue/update/update_vac4eu_3_to_9.py
+++ data/scripts/catalogue/update/update_vac4eu_3_to_9.py
@@ -9,15 +9,15 @@ CATALOGUE_SCHEMA_NAME = config('MG_CATALOGUE_SCHEMA_NAME')
 def get_data_model(profile_path, path_to_write, profile):
     # get data model from profile and write to file
     data_model = pd.DataFrame()
-
     for file_name in os.listdir(profile_path):
         if '.csv' in file_name:
             file_path = Path.joinpath(profile_path, file_name)
             df = pd.read_csv(file_path, keep_default_na=False, dtype='object')
-            df = df.loc[df['profiles'].apply(lambda p: profile in p.split(','))]
+            df['new_profiles'] = df['profiles'].apply(lambda x: x.split(','))
+            df = df[df['new_profiles'].apply(lambda x: any(item in profile for item in x))]
+            df = df.drop('new_profiles', axis=1, inplace=False)
             data_model = pd.concat([data_model, df])
 
-    # data_model = float_to_int(data_model)
     data_model.to_csv(path_to_write + '/molgenis.csv', index=None)
 
 
@@ -40,6 +40,7 @@ class Transform:
         """
         # transformations per table
         self.resources()
+        self.split_resources()
         self.organisations()
         self.publications()
         self.variable_values()
@@ -48,6 +49,9 @@ class Transform:
 
         for table_name in ['Quantitative information', 'Linked resources']:
             self.rename_tables(table_name)
+
+        # remove resources table
+        os.remove(self.path + 'Resources.csv')
 
     # TODO: for vac4eu BPE model is an exception, not part of a network, also other model in VAC4EU
 
@@ -123,6 +127,23 @@ class Transform:
 
         # save to file
         df_resources.to_csv(self.path + 'Resources.csv', index=False)
+
+    def split_resources(self):
+        """ Transform data in Resources: split into separate tables based on type
+        """
+        df_resources = pd.read_csv(self.path + 'Resources.csv', dtype='object')
+
+        # split Resources table
+        df_resources['table_type'] = df_resources['type'].apply(assign_table_type)
+        df_catalogues = df_resources[df_resources['table_type'] == 'Catalogue']
+        df_networks = df_resources[df_resources['table_type'] == 'Network']
+        df_collections = df_resources[df_resources['table_type'] == 'Collection']
+
+        # write tables to file
+        df_catalogues.to_csv(self.path + 'Catalogues.csv', index=False)
+        df_networks.to_csv(self.path + 'Networks.csv', index=False)
+        df_collections.to_csv(self.path + 'Collections.csv', index=False)
+
 
     def organisations(self):
         """ Transform columns in Organisations and alter structure
@@ -233,6 +254,9 @@ class Transform:
                 os.rename(self.path + 'Linked resources.csv', self.path + 'Linkages.csv')
 
 
+
+
+
 def get_resource_type(r_type):
     resource_type = []
     if not pd.isna(r_type):
@@ -335,3 +359,16 @@ def get_publications(df_merged_pubs, df_publications):
     df_resource_pubs = pd.merge(df_merged_pubs, df_publications, on='doi')
 
     return df_resource_pubs
+
+
+def assign_table_type(resource_type):
+    if pd.isna(resource_type):
+        table_type = 'Collection'
+    elif 'Catalogue' in resource_type:
+        table_type = 'Catalogue'
+    elif 'Network' in resource_type:
+        table_type = 'Network'
+    else:
+        table_type = 'Collection'
+
+    return table_type
