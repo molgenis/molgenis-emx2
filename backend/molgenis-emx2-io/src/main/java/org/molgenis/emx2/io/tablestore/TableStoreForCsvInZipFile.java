4476b5bb54402a52ef740272be1aaf9dca03d8fb
Harm Brugge <harmbrugge@gmail.com>
2026-08-27 14:24:24 +0200

    refactor: renamed interface method

--- backend/molgenis-emx2-io/src/main/java/org/molgenis/emx2/io/tablestore/TableStoreForCsvInZipFile.java
+++ backend/molgenis-emx2-io/src/main/java/org/molgenis/emx2/io/tablestore/TableStoreForCsvInZipFile.java
@@ -68,11 +68,11 @@ public class TableStoreForCsvInZipFile implements TableAndFileStore {
 
   @Override
   public void writeTable(String name, List<String> columnNames, Iterable<Row> rows) {
-    writeTableStreaming(name, columnNames, rows::forEach);
+    writeTable(name, columnNames, rows::forEach);
   }
 
   @Override
-  public void writeTableStreaming(String name, List<String> columnNames, RowProducer rows) {
+  public void writeTable(String name, List<String> columnNames, RowProducer rows) {
     if (columnNames.isEmpty()) {
       return;
     }
@@ -82,7 +82,7 @@ public class TableStoreForCsvInZipFile implements TableAndFileStore {
     try (FileSystem zipfs = open()) {
       Path pathInZipfile = zipfs.getPath(File.separator + name + CSV_EXTENSION);
       try (Writer writer = Files.newBufferedWriter(pathInZipfile)) {
-        rows.produce(CsvTableWriter.rowWriter(columnNames, writer, COMMA));
+        rows.forEach(CsvTableWriter.rowWriter(columnNames, writer, COMMA));
       }
     } catch (IOException ioe) {
       throw new MolgenisException("Export failed", ioe);
