dec10a5d30b72ae6a48bec7be292fce09965d7d8
YpeZ <info@ypezijlstra.frl>
2026-08-28 11:52:07 +0200

    fixing line lengths in utils.py

--- tools/pyclient/src/molgenis_emx2_pyclient/utils.py
+++ tools/pyclient/src/molgenis_emx2_pyclient/utils.py
@@ -10,18 +10,19 @@ from io import BytesIO
 
 import math
 import pandas as pd
-import requests
 from requests import Response
 
-from .constants import INT, DECIMAL, BOOL, LONG, STRING, CHECKBOX, MULTISELECT, DATE, DATETIME
-from .exceptions import NoSuchSchemaException, NoSuchColumnException
-from .exceptions import (ServiceUnavailableError, ServerNotFoundError, PyclientException, GraphQLException,
-                         InvalidTokenException,
-                         PermissionDeniedException, NonExistentTemplateException,
-                         ReferenceException)
+from .constants import (INT, DECIMAL, BOOL, LONG, STRING, CHECKBOX, MULTISELECT,
+                        DATE, DATETIME)
+from .exceptions import (NoSuchSchemaException, NoSuchColumnException,
+                         ServiceUnavailableError, ServerNotFoundError,
+                         PyclientException, GraphQLException,
+                         InvalidTokenException, PermissionDeniedException,
+                         NonExistentTemplateException, ReferenceException)
 from .metadata import Table, Schema
 
 log = logging.getLogger("Molgenis EMX2 Pyclient")
+
 
 def read_file(file_path: str | pathlib.Path) -> str:
     """Reads and imports data from a file.
@@ -35,6 +36,7 @@ def read_file(file_path: str | pathlib.Path) -> str:
         data = stream.read()
         stream.close()
     return data
+
 
 def parse_nested_pkeys(pkeys: list) -> str:
     """Converts a list of primary keys and nested primary keys to a string
@@ -54,12 +56,13 @@ def parse_nested_pkeys(pkeys: list) -> str:
                     converted_pkeys.append(parse_nested_pkeys(nested_values).strip())
                 converted_pkeys.append("}")
         else:
-             logging.warning(f"Unexpected data type encountered: {type(pk)!r}.")
+            logging.warning(f"Unexpected data type encountered: {type(pk)!r}.")
 
     return " ".join(converted_pkeys)
 
+
 def convert_dtypes(table_meta: Table) -> dict:
-    """Parses column metadata of a table to a dictionary of column ids to pandas dtypes."""
+    """Converts a table's column types to a dictionary of pandas dtypes"""
 
     type_map = {
         STRING: 'string',
@@ -75,7 +78,10 @@ def convert_dtypes(table_meta: Table) -> dict:
 
     return dtypes
 
-def prepare_filter(expr: str | None, _table: str, schema_meta: Schema) -> dict | None:
+
+def prepare_filter(expr: str | None,
+                   _table: str,
+                   schema_meta: Schema) -> dict | None:
     """Prepares a GraphQL filter based on the expression passed into `get`."""
     if expr in [None, ""]:
         return None
@@ -93,10 +99,12 @@ def prepare_filter(expr: str | None, _table: str, schema_meta: Schema) -> dict |
         elif 'between' in stmt:
             _filter.update(**prepare_between_filter(stmt, _table, schema_meta))
         else:
-            raise ValueError(f"Cannot process statement {stmt!r}, "
-                             f"ensure specifying one of the operators '==', '>', '<', '!=', 'between' "
+            raise ValueError(f"Cannot process statement {stmt!r},"
+                             f" ensure specifying one of the operators"
+                             f" '==', '>', '<', '!=', 'between' "
                              f"in your statement.")
     return _filter
+
 
 def prepare_equals_filter(stmt: str, _table: str, schema_meta: Schema) -> dict:
     """Prepares the filter part if the statement filters on equality."""
@@ -123,6 +131,7 @@ def prepare_equals_filter(stmt: str, _table: str, schema_meta: Schema) -> dict:
 
     return {col.id: {'equals': val}}
 
+
 def prepare_greater_filter(stmt: str, _table: str, schema_meta: Schema) -> dict:
     """Prepares the filter part if the statement filters on greater than."""
     exclusive = '=' not in stmt
@@ -144,9 +153,12 @@ def prepare_greater_filter(stmt: str, _table: str, schema_meta: Schema) -> dict:
         case 'DECIMAL':
             val = float(_val) + 0.0000001 * exclusive
         case _:
-            raise NotImplementedError(f"Cannot perform filter '>' on column with type {col.get('columnType')}.")
+            raise NotImplementedError(
+                f"Cannot perform filter '>' on column with type"
+                f" {col.get('columnType')}.")
 
     return {col.id: {"between": [val, None]}}
+
 
 def prepare_smaller_filter(stmt: str, _table: str, schema_meta: Schema) -> dict:
     """Prepares the filter part if the statement filters on greater than."""
@@ -169,11 +181,16 @@ def prepare_smaller_filter(stmt: str, _table: str, schema_meta: Schema) -> dict:
         case 'DECIMAL':
             val = float(_val) - 0.0000001 * exclusive
         case _:
-            raise NotImplementedError(f"Cannot perform filter '<' on column with type {col.get('columnType')}.")
+            raise NotImplementedError(
+                f"Cannot perform filter '<' on column with type"
+                f" {col.get('columnType')}.")
 
     return {col.id: {"between": [None, val]}}
 
-def prepare_not_equals_filter(stmt: str, _table: str, schema_meta: Schema) -> dict:
+
+def prepare_not_equals_filter(stmt: str,
+                              _table: str,
+                              schema_meta: Schema) -> dict:
     """Prepares the filter part if the statement filters on greater than."""
     _col = stmt.split('!=')[0].strip()
     _val = stmt.split('!=')[1].strip()
@@ -192,7 +209,9 @@ def prepare_not_equals_filter(stmt: str, _table: str, schema_meta: Schema) -> di
             if str(_val).lower() == 'true':
                 val = True
         case 'RADIO' | 'REF' | 'REF_ARRAY' | 'ONTOLOGY' | 'ONTOLOGY_ARRAY':
-            raise NotImplementedError(f"The filter '!=' is not implemented for columns of type {col_type!r}.")
+            raise NotImplementedError(
+                f"The filter '!=' is not implemented for columns of type"
+                f" {col_type!r}.")
         case _:
             try:
                 val = json.loads(''.join(_val.split('`')).replace("'", '"'))
@@ -200,6 +219,7 @@ def prepare_not_equals_filter(stmt: str, _table: str, schema_meta: Schema) -> di
                 val = ''.join(_val.split('`'))
 
     return {col.id: {"not_equals": val}}
+
 
 def prepare_between_filter(stmt: str, _table: str, schema_meta: Schema) -> dict:
     """Prepares the filter part if values between a certain range are requested."""
@@ -210,18 +230,22 @@ def prepare_between_filter(stmt: str, _table: str, schema_meta: Schema) -> dict:
     try:
         val = json.loads(_val)
     except json.decoder.JSONDecodeError:
-        msg = ("To filter on values between a and b, supply them as a list, [a, b]. "
-               "Ensure the values for a and b are numeric.")
+        msg = ("To filter on values between a and b, supply them as a list,"
+               " [a, b]. Ensure the values for a and b are numeric.")
         raise ValueError(msg)
     col_id = ''.join(_col.split('`'))
 
     col = schema_meta.get_table(by='name', value=_table).get_column(by='id', value=col_id)
     if (col_type := col.get('columnType')) not in ['LONG', 'INT', 'DECIMAL']:
-        raise NotImplementedError(f"The filter 'between' is not implemented for columns of type {col_type!r}.")
+        raise NotImplementedError(
+            f"The filter 'between' is not implemented for columns of type"
+            f" {col_type!r}.")
 
     return {col.id: {'between': val}}
 
-def prepare_nested_filter(columns: str, value: str | int | float | list, comparison: str):
+
+def prepare_nested_filter(columns: str, value: str | int | float | list,
+                          comparison: str) -> dict:
     _filter = {}
     current = _filter
     for (i, segment) in enumerate(columns.split('.')[:-1]):
@@ -231,15 +255,18 @@ def prepare_nested_filter(columns: str, value: str | int | float | list, compari
     current[last_segment] = {comparison: prepare_value(value)}
     return _filter
 
+
 def prepare_value(value):
     value = str(value)
     if value.startswith('[') and value.endswith(']'):
         return json.loads(value.replace('\'', '"'))
     return value
 
+
 def format_optional_params(**kwargs):
-    """Parses optional keyword arguments to a format suitable for GraphQL queries."""
-    args = {key: kwargs[key] for key in kwargs.keys() if key not in ('self', None)}
+    """Parses optional keyword arguments to GraphQL query format."""
+    args = {key: kwargs[key] for key in kwargs.keys()
+            if key not in ('self', None)}
     if 'name' in args.keys():
         args['name'] = args.pop('name')
     if 'include_demo_data' in args.keys():
@@ -248,8 +275,12 @@ def format_optional_params(**kwargs):
         args['parentJob'] = args.pop('parent_job')
     return args
 
-def prep_data_or_file(file_path: str | pathlib.Path | None = None, data: list | pd.DataFrame | None = None) -> str | None:
-    """Prepares the data from memory or loaded from disk for addition or deletion action.
+
+def prep_data_or_file(file_path: str | pathlib.Path | None = None,
+                      data: list | pd.DataFrame | None = None) -> str | None:
+    """
+    Prepares the data from memory or loaded from disk for addition or
+    deletion action.
 
     :param file_path: path to the file to be prepared
     :type file_path: str
@@ -270,21 +301,26 @@ def prep_data_or_file(file_path: str | pathlib.Path | None = None, data: list | 
     log.error(message)
     raise FileNotFoundError(message)
 
-def data_to_csv(data: list[dict] | pd.DataFrame, filename: str | pathlib.Path | None = None) -> str | None:
-    """Converts Molgenis-format data (DataFrame or list of dicts) to Molgenis-format CSV
+
+def data_to_csv(data: list[dict] | pd.DataFrame,
+                filename: str | pathlib.Path | None = None) -> str | None:
+    """Converts DataFrame or list of dictionaries to EMX2-format CSV
     
     :param data: input data, in the form of a Molgenis table
-    :param filename: when supplied, output to specified file rather than returning a string
+    :param filename: when supplied, output to specified file rather than
+    returning a string
 
-    :returns: a string containing CSV-formatted content, or nothing when exporting to file
+    :returns: a string containing CSV-formatted content, or nothing when
+    exporting to file
     """
 
     if isinstance(data, pd.DataFrame):
-        data_for_csv = data.copy() # Do not modify the original data
+        data_for_csv = data.copy()  # Do not modify the original data
         object_columns = data_for_csv.select_dtypes(include=['object', 'string']).columns
         data_for_csv[object_columns] = data[object_columns].map(array_to_csv_string)
         if filename:
-            data_for_csv.to_csv(path_or_buf=filename, index=False, quoting=csv.QUOTE_NONNUMERIC)
+            data_for_csv.to_csv(path_or_buf=filename, index=False,
+                                quoting=csv.QUOTE_NONNUMERIC)
             return None
         else:
             return data_for_csv.to_csv(index=False, quoting=csv.QUOTE_NONNUMERIC)
@@ -300,7 +336,8 @@ def data_to_csv(data: list[dict] | pd.DataFrame, filename: str | pathlib.Path | 
             writer.writeheader()
             for row in data:
                 if not isinstance(row, dict):
-                    raise ValueError(f"Cannot prepare row {row!r}. Supply a list of dictionaries.")
+                    raise ValueError(f"Cannot prepare row {row!r}."
+                                     f" Supply a list of dictionaries.")
                 cleaned_row = {}
                 for k, v in row.items():
                     # Replace 'nan' with 'None'
@@ -319,7 +356,10 @@ def data_to_csv(data: list[dict] | pd.DataFrame, filename: str | pathlib.Path | 
                 return target.getvalue()
             return None
 
-def check_schema(schema: str | None, default_schema: str | None, schema_names: list[str]):
+
+def check_schema(schema: str | None,
+                 default_schema: str | None,
+                 schema_names: list[str]):
     """Checks whether the schema used for this action exists."""
     if schema is not None:
         if schema in schema_names:
@@ -327,11 +367,13 @@ def check_schema(schema: str | None, default_schema: str | None, schema_names: l
         else:
             raise NoSuchSchemaException(f"Schema {schema!r} not available.")
     if default_schema is None:
-        raise NoSuchSchemaException(f"Select an existing schema for this operation.")
+        raise NoSuchSchemaException(f"Select an existing schema for"
+                                    f" this operation.")
     return default_schema
 
+
 def csv_string_to_array(csv_string: str) -> list:
-    """Convert EMX2 input of type *_ARRAY, from a string from the CSV API to a list"""
+    """Converts EMX2 array type strings to a list."""
     if pd.notna(csv_string):
         with io.StringIO(csv_string) as in_string:
             reader = csv.reader(in_string, dialect=csv.excel)
@@ -339,8 +381,9 @@ def csv_string_to_array(csv_string: str) -> list:
     else:
         return []
 
+
 def array_to_csv_string(array: list | str) -> str:
-    """Convert a list to a string suitable for output to an EMX2 value of type *_ARRAY, 
+    """Converts a list to a string suitable for output to an EMX2 value of type *_ARRAY,
     through the CSV API
     """
     if isinstance(array, list):
@@ -352,7 +395,8 @@ def array_to_csv_string(array: list | str) -> str:
         return array
 
 
-def validate_graphql_response(response, mutation: str | None = None, fallback_error_message: str | None = None):
+def validate_graphql_response(response, mutation: str | None = None,
+                              fallback_error_message: str | None = None):
     """Validates a GraphQL response and prints the appropriate message.
 
     :param response: a graphql response from the server
@@ -367,14 +411,16 @@ def validate_graphql_response(response, mutation: str | None = None, fallback_er
     """
 
     if response.status_code == 503:
-        raise ServiceUnavailableError(f"Server with url {response.url!r} (temporarily) unavailable.")
+        raise ServiceUnavailableError(
+            f"Server with url {response.url!r} (temporarily) unavailable.")
     if response.status_code == 404:
         raise ServerNotFoundError(f"Server with url {response.url!r} not found.")
     if response.status_code == 400:
         if 'Invalid token or token expired' in response.text:
             raise InvalidTokenException("Invalid token or token expired.")
         if 'permission denied' in response.text:
-            raise PermissionDeniedException(f"Transaction failed: permission denied.")
+            raise PermissionDeniedException(f"Transaction failed:"
+                                            f" permission denied.")
         if 'Graphql API error' in response.text:
             msg = response.json().get("errors", [])[0].get('message')
             log.error(msg)
@@ -394,7 +440,8 @@ def validate_graphql_response(response, mutation: str | None = None, fallback_er
 
         msg = response.json().get("errors", [])[0].get('message', '')
         log.error(msg)
-        raise PyclientException("An unknown error occurred when trying to reach this server.")
+        raise PyclientException("An unknown error occurred when trying to reach"
+                                " this server.")
 
     if response.request.method == 'GET':
         return
@@ -412,10 +459,12 @@ def validate_graphql_response(response, mutation: str | None = None, fallback_er
         message = response_json.get('errors')[0].get('message')
         if 'permission denied' in message:
             log.error("Insufficient permissions for this operations.")
-            raise PermissionDeniedException("Insufficient permissions for this operations.")
+            raise PermissionDeniedException("Insufficient permissions for"
+                                            " this operations.")
         if 'AvailableDataModels' in message:
             log.error("Selected template does not exist.")
-            raise NonExistentTemplateException("Selected template does not exist.")
+            raise NonExistentTemplateException("Selected template does"
+                                               " not exist.")
         log.error(message)
         raise GraphQLException(message)
 
@@ -435,14 +484,20 @@ def response_to_dataframe(response: Response,
     """Parses the response of a CSV query to pandas DataFrame format."""
 
     response_columns = pd.read_csv(BytesIO(response.content)).columns
-    dtypes = {c: t for (c, t) in convert_dtypes(table).items() if c in response_columns}
+    dtypes = {c: t for (c, t) in convert_dtypes(table).items()
+              if c in response_columns}
 
     bool_columns = [c for (c, t) in dtypes.items() if t == 'boolean']
     date_columns = [c.name for c in table.columns
-                    if c.get('columnType') in (DATE, DATETIME) and c.name in response_columns]
-    response_data = pd.read_csv(BytesIO(response.content), keep_default_na=False, na_values=[''], dtype=dtypes,
+                    if c.get('columnType') in (DATE, DATETIME)
+                    and c.name in response_columns]
+    response_data = pd.read_csv(BytesIO(response.content),
+                                keep_default_na=False,
+                                na_values=[''],
+                                dtype=dtypes,
                                 parse_dates=date_columns, dialect=csv.excel())
-    response_data[bool_columns] = response_data[bool_columns].replace({'true': True, 'false': False})
+    response_data[bool_columns] = response_data[bool_columns].replace(
+        {'true': True, 'false': False})
     if parse_arrays:
         array_columns = [c.name for c in table.columns
                          if (c.get('columnType').endswith('_ARRAY') or
@@ -461,7 +516,8 @@ def response_to_dataframe(response: Response,
             elif "not in index" in e.args[0]:
                 msg = f"Columns {e.args[0]}"
             else:
-                msg = f"Columns {e.args[0].split('Index(')[1].split(', dtype')} not in index."
+                msg = (f"Columns {e.args[0].split('Index(')[1].split(', dtype')}"
+                       f" not in index.")
             raise NoSuchColumnException(msg)
         response_data = response_data.drop_duplicates(keep='first').reset_index(drop=True)
 
