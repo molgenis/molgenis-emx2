9974e561f37e2447d8707de1e81d7999bbcf16c7
dcruvolo <davidruvolo51@gmail.com>
2026-09-04 11:25:35 +0200

    feat: finalised container delete functions

--- apps/tailwind-components/app/utils/cms/delete.ts
+++ apps/tailwind-components/app/utils/cms/delete.ts
@@ -1,22 +1,10 @@
-import {
-  getBlocks,
-  getBlockOrders,
-  getComponents,
-  getComponentOrders,
-} from "./get";
+import { cmsFetch, deleteBlock, getPage } from "../cms";
+import type { IConfigurablePages } from "../../../types/cms";
 
-import { updateComponents, updateBlocks } from "./update";
-
-import type {
-  IConfigurablePages,
-  IDeveloperPages,
-  IDependenciesCSS,
-  IDependenciesJS,
-  IBlocks,
-  IComponents,
-} from "../../../types/cms";
-
-import { cmsFetch } from "../cms";
+interface IPageBlocksToRemove {
+  blockId: string;
+  blockOrderId: string;
+}
 
 async function deleteContainer(schema: string, page: string) {
   const query = `mutation deleteContainer($container:[ContainersInput]) {
@@ -29,76 +17,23 @@ async function deleteContainer(schema: string, page: string) {
   return await cmsFetch(schema, query, variables);
 }
 
-async function deletePageComponentRefs(schema: string, page: string) {
-  const pageBlocks = await getBlocks(schema, page);
-  const pageComponents = await getComponents(schema, page);
-
-  if (pageComponents && pageBlocks) {
-    const pageBlockIds = pageBlocks.map((block) => block.id);
-
-    // If a component is used in multiple blocks, remove refs to
-    // blocks on the current page and keep everything else
-    const componentsWithMultipleRefs = pageComponents
-      .filter((component) => {
-        return component.inBlock && component.inBlock.length > 1;
-      })
-      .map((component) => {
-        const newComponent = structuredClone(component);
-        newComponent.inBlock = newComponent.inBlock.filter(
-          (block: IBlocks) => !pageBlockIds.includes(block.id)
-        );
-        return newComponent;
-      });
-
-    if (componentsWithMultipleRefs) {
-      await updateComponents(schema, componentsWithMultipleRefs);
-    }
-  }
-}
-
-async function deletePageComponentOrderRefs(schema: string, page: string) {
-  const pageComponentOrders = await getComponentOrders(schema, page);
-  if (pageComponentOrders) {
-    for (const order of pageComponentOrders) {
-      // await deletePageComponentOrder(schema, id);
-    }
-  }
-}
-
-async function deletePageBlockRefs(schema: string, page: string) {
-  const pageBlocks = await getBlocks(schema, page);
-  if (pageBlocks) {
-    const blocksWithMultipleRefs = pageBlocks
-      .filter((block) => block.inContainer && block.inContainer.length > 1)
-      .map((block) => {
-        const newBlock = structuredClone(block);
-        newBlock.inContainer = newBlock.inContainer.filter((container: any) => {
-          return container.name !== page;
-        });
-        return newBlock;
-      });
-
-    if (blocksWithMultipleRefs) {
-      await updateBlocks(schema, blocksWithMultipleRefs);
-    }
-  }
-}
-
-function deletePageBlockOrderRefs(schema: string, page: string) {}
-
 export async function deleteConfigurablePage(schema: string, page: string) {
-  await deletePageComponentRefs(schema, page);
-  await deletePageBlockRefs(schema, page);
+  // remove blocks and components
+  const pageData = await getPage(schema, page);
+  const currentPage = pageData.page as IConfigurablePages;
+
+  const pageBlocksToRemove = currentPage.blockOrder?.map((block) => {
+    return { blockId: block.block.id, blockOrderId: block.id };
+  }) as IPageBlocksToRemove[];
+
+  for (const block of pageBlocksToRemove) {
+    await deleteBlock(schema, block.blockId, block.blockOrderId, page);
+  }
+
+  // delete page: all refs should be removed
   await deleteContainer(schema, page);
 }
 
 export async function deleteDeveloperPage(schema: string, page: string) {
-  const query = `mutation deleteContainer($container:[ContainersInput]) {
-    delete(Containers:$container) {
-      status
-      message
-    }
-  }`;
-  const variables = { container: { name: page } };
-  return await cmsFetch(schema, query, variables);
+  await deleteContainer(schema, page);
 }
