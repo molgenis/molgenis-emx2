25edc4a37602a273c7a25ead889a7814cff32cd2
connoratrug <c.stroomberg@rug.nl>
2026-09-07 10:03:56 +0200

    fix: enhance table components to support small page size / mobile
    - enhance table component
    - use on index pages

--- apps/tailwind-components/app/components/TableHeadRow.vue
+++ apps/tailwind-components/app/components/TableHeadRow.vue
@@ -1,6 +1,10 @@
 <!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+withDefaults(defineProps<{ stacked?: boolean }>(), { stacked: false });
+</script>
+
 <template>
-  <tr>
+  <tr :class="{ 'hidden sm:table-row': stacked }">
     <slot></slot>
   </tr>
 </template>
