c7f4979ca2ee4f84f9209b9fd142211593d06fa8
Postema Max <max@elynor.com>
2026-09-09 11:37:03 +0200

    review comments

--- apps/tailwind-components/app/components/cms/Section.vue
+++ apps/tailwind-components/app/components/cms/Section.vue
@@ -63,7 +63,7 @@ const showMenu = ref<boolean>(false);
           'w-pg-section': !enableFullScreenWidth,
           'w-full': enableFullScreenWidth,
           grid: columns > 1,
-          'gap-4': columns > 1,
+          'gap-7.5': columns > 1,
           'grid-cols-2': columns === 2,
           'grid-cols-3': columns === 3,
           'grid-cols-4': columns === 4,
