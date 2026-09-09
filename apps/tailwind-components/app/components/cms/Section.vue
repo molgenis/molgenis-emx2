c285fd218b8755d00f5dba4ea365a26a78678242
Postema Max <max@elynor.com>
2026-09-09 14:50:12 +0200

    remove 5th column

--- apps/tailwind-components/app/components/cms/Section.vue
+++ apps/tailwind-components/app/components/cms/Section.vue
@@ -67,7 +67,6 @@ const showMenu = ref<boolean>(false);
           'grid-cols-2': columns === 2,
           'grid-cols-3': columns === 3,
           'grid-cols-4': columns === 4,
-          'grid-cols-5': columns === 5,
         }"
       >
         <slot></slot>
