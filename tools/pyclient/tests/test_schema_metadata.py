78e07f7cc1ec8ea8eae4492da9d394cc1203f9e9
YpeZ <info@ypezijlstra.frl>
2026-09-11 14:59:27 +0200

    fixed tests

--- tools/pyclient/tests/test_schema_metadata.py
+++ tools/pyclient/tests/test_schema_metadata.py
@@ -6,7 +6,6 @@ import os
 
 import pytest
 from dotenv import load_dotenv
-
 from src.molgenis_emx2_pyclient import Client
 from src.molgenis_emx2_pyclient.exceptions import PermissionDeniedException
 
@@ -15,11 +14,13 @@ server_url = os.environ.get("MG_SERVER")
 username = os.environ.get("MG_USERNAME")
 password = os.environ.get("MG_PASSWORD")
 
+
 def test_get_schema_metadata():
     """Tests the `get_schema_metadata` method."""
     with Client(url=server_url) as client:
         schema_metadata = client.get_schema_metadata("pet store")
         assert len(schema_metadata.tables) == 5
+
 
 def test_get_schema_settings():
     """Tests the `get_schema_settings` method."""
@@ -27,6 +28,7 @@ def test_get_schema_settings():
         schema_settings = client.get_schema_settings("pet store")
         assert len(schema_settings) == 1
         assert len(schema_settings[0].values()) == 2
+
 
 def test_get_schema_members():
     """Tests the `get_schema_members` method."""
@@ -39,13 +41,14 @@ def test_get_schema_members():
         client.signin(username, password)
         client.set_schema("pet store")
         schema_members: list[dict] = client.get_schema_members()
-        assert len(schema_members) == 5
+        assert len(schema_members) == 6
         assert schema_members[0].get('email') == "anonymous"
         assert schema_members[0].get('role') == "Viewer"
+
 
 def test_get_schema_roles():
     """Tests the `get_schema_roles` method."""
 
     with Client(url=server_url) as client:
         schema_roles = client.get_schema_roles("pet store")
-        assert len(schema_roles) == 8
+        assert len(schema_roles) == 10
