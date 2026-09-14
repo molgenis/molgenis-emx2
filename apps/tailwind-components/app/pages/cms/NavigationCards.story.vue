78ed888e8909e2a1d1dee13141601af40b4eb970
dcruvolo <davidruvolo51@gmail.com>
2026-09-14 14:50:55 +0200

    fix: updated styles for card layout

--- apps/tailwind-components/app/pages/cms/NavigationCards.story.vue
+++ apps/tailwind-components/app/pages/cms/NavigationCards.story.vue
@@ -34,7 +34,7 @@ const cards: INavigationCards[] = [
 </script>
 
 <template>
-  <div class="my-5 grid grid-cols-3 gap-7.5">
+  <div class="my-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7.5">
     <NavigationCards
       v-for="card in cards"
       :id="card.id"
