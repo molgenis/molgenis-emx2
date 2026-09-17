48531b2bd44a4da25dd9e289b219aed7f796d8d4
jhhaanstra <janhendrikhaanstra@gmail.com>
2026-09-17 14:57:15 +0200

    ref: spotlessApply

--- backend/molgenis-emx2-fairmapper/src/main/java/org/molgenis/emx2/fairmapper/postprocessing/ontologies/OntologyMappingFetcher.java
+++ backend/molgenis-emx2-fairmapper/src/main/java/org/molgenis/emx2/fairmapper/postprocessing/ontologies/OntologyMappingFetcher.java
@@ -7,8 +7,8 @@ public interface OntologyMappingFetcher {
   /**
    * @param schemaName name of schema that contains the ontology table to fetch the mapping of
    * @param tableName name of the ontology table to fetch the mapping of
-   * @return Ontology mapping in the shape of a map where the key is the ontologyTermURI field
-   *     and the value is it's corresponding name.
+   * @return Ontology mapping in the shape of a map where the key is the ontologyTermURI field and
+   *     the value is it's corresponding name.
    */
   Map<String, String> getMapping(String schemaName, String tableName);
 }
