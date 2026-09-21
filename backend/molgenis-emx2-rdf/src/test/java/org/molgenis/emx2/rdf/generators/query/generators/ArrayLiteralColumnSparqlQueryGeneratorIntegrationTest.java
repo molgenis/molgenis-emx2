6dd9ae2d095f4d2a7b18270885426c82237a1135
jhhaanstra <janhendrikhaanstra@gmail.com>
2026-09-21 15:27:17 +0200

    feat: added ArrayLiteralColumnSparqlQueryGeneratorIntegrationTest

---
+++ backend/molgenis-emx2-rdf/src/test/java/org/molgenis/emx2/rdf/generators/query/generators/ArrayLiteralColumnSparqlQueryGeneratorIntegrationTest.java
@@ -0,0 +1,137 @@
+package org.molgenis.emx2.rdf.generators.query.generators;
+
+import static org.molgenis.emx2.rdf.generators.query.generators.SparqlQueryTestUtils.*;
+
+import java.util.List;
+import java.util.Map;
+import org.eclipse.rdf4j.model.vocabulary.FOAF;
+import org.eclipse.rdf4j.query.TupleQueryResult;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.repository.sail.SailRepositoryConnection;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.molgenis.emx2.Column;
+import org.molgenis.emx2.ColumnType;
+import org.molgenis.emx2.SchemaMetadata;
+import org.molgenis.emx2.TableMetadata;
+import org.molgenis.emx2.rdf.generators.query.SparqlVariableUtil;
+import org.molgenis.emx2.rdf.generators.query.TableQueryGenerator;
+
+class ArrayLiteralColumnSparqlQueryGeneratorIntegrationTest {
+
+  private static final String IRI = "https://example.com/person";
+  public static final TableQueryGenerator GENERATOR = new TableQueryGenerator();
+
+  @Test
+  void shouldConcatSelectors() {
+    SailRepository repository =
+        repository(
+            SparqlQueryTestUtils.statement(IRI, FOAF.FIRST_NAME, "Lewis"),
+            SparqlQueryTestUtils.statement(IRI, FOAF.FIRST_NAME, "Robin"),
+            SparqlQueryTestUtils.statement(IRI, FOAF.FIRST_NAME, "Demetrius"));
+
+    TableMetadata table =
+        new SchemaMetadata()
+            .create(
+                TableMetadata.table(
+                    "Person",
+                    Column.column("name", ColumnType.STRING_ARRAY).setSemantics("foaf:firstName")));
+
+    try (SailRepositoryConnection connection = repository.getConnection()) {
+      TupleQueryResult bindingSets = executeQuery(connection, GENERATOR.generate(table));
+      assertHasResults(
+          bindingSets,
+          List.of(Map.of(SparqlVariableUtil.SUBJECT_NAME, IRI, "name", "Lewis|Robin|Demetrius")));
+    }
+  }
+
+  @Test
+  void shouldHandleNoSemantics() {
+    SailRepository repository =
+        repository(
+            SparqlQueryTestUtils.statement(IRI, FOAF.FIRST_NAME, "Lewis"),
+            SparqlQueryTestUtils.statement(IRI, FOAF.FIRST_NAME, "Robin"),
+            SparqlQueryTestUtils.statement(IRI, FOAF.FIRST_NAME, "Demetrius"));
+
+    TableMetadata table =
+        new SchemaMetadata()
+            .create(TableMetadata.table("Person", Column.column("name", ColumnType.STRING_ARRAY)));
+
+    try (SailRepositoryConnection connection = repository.getConnection()) {
+      TupleQueryResult bindingSets = executeQuery(connection, GENERATOR.generate(table));
+      assertHasResults(bindingSets, List.of(Map.of(SparqlVariableUtil.SUBJECT_NAME, IRI)));
+    }
+  }
+
+  @Test
+  void givenMultipleSemantics_thenConcatBindValue() {
+    SailRepository repository =
+        repository(
+            SparqlQueryTestUtils.statement(IRI, FOAF.FIRST_NAME, "Lewis"),
+            SparqlQueryTestUtils.statement(IRI, FOAF.FIRST_NAME, "Robin"),
+            SparqlQueryTestUtils.statement(IRI, FOAF.FIRST_NAME, "Demetrius"));
+
+    TableMetadata table =
+        new SchemaMetadata()
+            .create(
+                TableMetadata.table(
+                    "Person",
+                    Column.column("name", ColumnType.STRING_ARRAY)
+                        .setSemantics("foaf:firstName", "foaf:givenName")));
+
+    try (SailRepositoryConnection connection = repository.getConnection()) {
+      TupleQueryResult bindingSets = executeQuery(connection, GENERATOR.generate(table));
+      assertHasResults(
+          bindingSets,
+          List.of(Map.of(SparqlVariableUtil.SUBJECT_NAME, IRI, "name", "Lewis|Robin|Demetrius")));
+    }
+  }
+
+  @Disabled("This use case is currently a known limitation")
+  @Test
+  void givenMultipleSemantics_whenDifferentSemanticsUsedForSameCollection_thenCombine() {
+    SailRepository repository =
+        repository(
+            SparqlQueryTestUtils.statement(IRI, FOAF.FIRST_NAME, "Lewis"),
+            SparqlQueryTestUtils.statement(IRI, FOAF.GIVEN_NAME, "Robin"),
+            SparqlQueryTestUtils.statement(IRI, FOAF.FIRST_NAME, "Demetrius"));
+
+    TableMetadata table =
+        new SchemaMetadata()
+            .create(
+                TableMetadata.table(
+                    "Person",
+                    Column.column("name", ColumnType.STRING_ARRAY)
+                        .setSemantics("foaf:firstName", "foaf:givenName")));
+
+    try (SailRepositoryConnection connection = repository.getConnection()) {
+      TupleQueryResult bindingSets = executeQuery(connection, GENERATOR.generate(table));
+      assertHasResults(
+          bindingSets,
+          List.of(Map.of(SparqlVariableUtil.SUBJECT_NAME, IRI, "name", "Lewis|Robin|Demetrius")));
+    }
+  }
+
+  @Test
+  void givenCollection_whenValueAppearsMultipleTimes_thenDistinct() {
+    SailRepository repository =
+        repository(
+            SparqlQueryTestUtils.statement(IRI, FOAF.FIRST_NAME, "Lewis"),
+            SparqlQueryTestUtils.statement(IRI, FOAF.FIRST_NAME, "Robin"),
+            SparqlQueryTestUtils.statement(IRI, FOAF.FIRST_NAME, "Robin"));
+
+    TableMetadata table =
+        new SchemaMetadata()
+            .create(
+                TableMetadata.table(
+                    "Person",
+                    Column.column("name", ColumnType.STRING_ARRAY).setSemantics("foaf:firstName")));
+
+    try (SailRepositoryConnection connection = repository.getConnection()) {
+      TupleQueryResult bindingSets = executeQuery(connection, GENERATOR.generate(table));
+      assertHasResults(
+          bindingSets,
+          List.of(Map.of(SparqlVariableUtil.SUBJECT_NAME, IRI, "name", "Lewis|Robin")));
+    }
+  }
+}
