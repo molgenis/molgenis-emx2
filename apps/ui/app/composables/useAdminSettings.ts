d07b9275c7eaa6dac39a21621f1535d85fa540e6
chinook25 <chinook25@gmail.com>
2026-09-22 11:20:07 +0200

    chore: move functionality to composable

--- apps/ui/app/composables/useAdminSettings.ts
+++ apps/ui/app/composables/useAdminSettings.ts
@@ -1,1 +1,46 @@
-export default function useAdminSettings() {}
+import { reactive, ref } from "vue";
+import {
+  createUser,
+  deleteUser,
+  getUsers,
+  type IUser,
+} from "../util/adminUtils";
+
+const LIMIT = 100;
+
+const users = ref<IUser[]>([]);
+const userCount = ref(0);
+
+export default function useAdminSettings() {
+  const totalUserPages = ref(0);
+
+  retrieveUsers();
+
+  async function retrieveUsers(currentPage: number = 1) {
+    const { newUsers, newUserCount } = await getUsers();
+    users.value = newUsers;
+    userCount.value = newUserCount;
+    const divided = userCount.value / LIMIT;
+    totalUserPages.value =
+      userCount.value % LIMIT > 0 ? Math.floor(divided) + 1 : divided;
+  }
+
+  async function addUser(userName: string, password: string) {
+    await createUser(userName, password);
+    retrieveUsers();
+  }
+
+  async function removeUser(user: IUser) {
+    await deleteUser(user);
+    await retrieveUsers();
+  }
+
+  return reactive({
+    users,
+    userCount,
+    totalUserPages,
+    addUser,
+    removeUser,
+    retrieveUsers,
+  });
+}
--- apps/ui/app/pages/admin/users.vue
+++ apps/ui/app/pages/admin/users.vue
@@ -3,7 +3,7 @@
     <ContentBlock
       class="w-full mt-3"
       title="User management"
-      :description="`${userCount} users found`"
+      :description="`${adminSettings.userCount} users found`"
     >
       <Button
         icon="plus"
@@ -26,7 +26,7 @@
           </TableHeadRow>
         </template>
         <template #body>
-          <TableRow v-for="user in users">
+          <TableRow v-for="user in adminSettings.users">
             <TableCell>
               <div class="flex gap-1">
                 <Button
@@ -35,7 +35,7 @@
                   type="secondary"
                   size="small"
                   :label="`Edit user: ${user.email}`"
-                  @click="editUser(user)"
+                  @click="openEditUserModal(user)"
                 />
                 <Button
                   v-if="canDelete(user)"
@@ -81,7 +81,7 @@
                 <Button
                   type="secondary"
                   size="tiny"
-                  @click="manageTokens(user)"
+                  @click="openManageTokensModal(user)"
                 >
                   {{ user.tokens?.length }} tokens
                 </Button>
@@ -94,7 +94,7 @@
         v-if="showNewUserModal"
         v-model:visible="showNewUserModal"
         :usernames="usernames"
-        @addUser="addUser"
+        @addUser="adminSettings.addUser"
       />
 
       <EditUserModal
@@ -103,21 +103,21 @@
         :schemas="schemas"
         :roles="roles"
         :user="selectedUser"
-        @userUpdated="retrieveUsers"
+        @userUpdated="adminSettings.retrieveUsers"
       />
 
       <DeleteUserConfirmation
         v-if="selectedUser && showDeleteUserModal"
         v-model:visible="showDeleteUserModal"
         :user="selectedUser"
-        @deleteUser="removeUser(selectedUser)"
+        @deleteUser="adminSettings.removeUser(selectedUser)"
       />
 
       <TokenManagement
         v-if="selectedUser && showTokenModal"
         v-model:visible="showTokenModal"
         :user="selectedUser"
-        @userUpdated="retrieveUsers"
+        @userUpdated="adminSettings.retrieveUsers"
       />
     </ContentBlock>
   </Container>
@@ -126,29 +126,27 @@
 <script setup lang="ts">
 import { definePageMeta } from "#imports";
 import { computed, ref } from "vue";
+import BaseIcon from "../../../../tailwind-components/app/components/BaseIcon.vue";
+import Button from "../../../../tailwind-components/app/components/Button.vue";
+import Container from "../../../../tailwind-components/app/components/Container.vue";
+import ContentBlock from "../../../../tailwind-components/app/components/content/ContentBlock.vue";
+import ShowMore from "../../../../tailwind-components/app/components/ShowMore.vue";
+import Table from "../../../../tailwind-components/app/components/Table.vue";
+import TableCell from "../../../../tailwind-components/app/components/TableCell.vue";
+import TableHead from "../../../../tailwind-components/app/components/TableHead.vue";
+import TableHeadRow from "../../../../tailwind-components/app/components/TableHeadRow.vue";
+import TableRow from "../../../../tailwind-components/app/components/TableRow.vue";
+import DeleteUserConfirmation from "../../components/DeleteUserConfirmation.vue";
+import EditUserModal from "../../components/EditUserModal.vue";
+import NewUserModal from "../../components/NewUserModal.vue";
+import TokenManagement from "../../components/TokenManagement.vue";
+import useAdminSettings from "../../composables/useAdminSettings.ts";
 import {
-  createUser,
-  deleteUser,
   getRoles,
   getSchemas,
-  getUsers,
   type ISchemaInfo,
   type IUser,
-} from "~/util/adminUtils";
-import ContentBlock from "../../../../tailwind-components/app/components/content/ContentBlock.vue";
-import Table from "../../../../tailwind-components/app/components/Table.vue";
-import TableHead from "../../../../tailwind-components/app/components/TableHead.vue";
-import TableRow from "../../../../tailwind-components/app/components/TableRow.vue";
-import BaseIcon from "../../../../tailwind-components/app/components/BaseIcon.vue";
-import ShowMore from "../../../../tailwind-components/app/components/ShowMore.vue";
-import Container from "../../../../tailwind-components/app/components/Container.vue";
-import TableCell from "../../../../tailwind-components/app/components/TableCell.vue";
-import TableHeadRow from "../../../../tailwind-components/app/components/TableHeadRow.vue";
-import Button from "../../../../tailwind-components/app/components/Button.vue";
-import NewUserModal from "../../components/NewUserModal.vue";
-import EditUserModal from "../../components/EditUserModal.vue";
-import DeleteUserConfirmation from "../../components/DeleteUserConfirmation.vue";
-import TokenManagement from "../../components/TokenManagement.vue";
+} from "../../util/adminUtils";
 
 /**
  * Todo:
@@ -163,7 +161,6 @@ definePageMeta({
   middleware: "admin-only",
 });
 
-const LIMIT = 100;
 const showEditUserModal = ref(false);
 const showNewUserModal = ref(false);
 const showTokenModal = ref(false);
@@ -171,48 +168,27 @@ const showDeleteUserModal = ref(false);
 const selectedUser = ref<IUser | null>(null);
 
 const currentPage = ref(1);
-const users = ref<IUser[]>([]);
-const userCount = ref(0);
-const totalPages = ref(0);
 const schemas = ref<ISchemaInfo[]>([]);
 const roles = ref<string[]>([]);
 const schema = ref<string>("");
 
-retrieveUsers();
+const adminSettings = useAdminSettings();
+
 schemas.value = await getSchemas();
 // @ts-expect-error
 schema.value = schemas.value.length ? schemas.value[0].id : "";
 roles.value = await getRoles(schemas.value);
 
 const usernames = computed(() => {
-  return users.value.map((user) => user.email);
+  return adminSettings.users.map((user) => user.email);
 });
 
-async function addUser(userName: string, password: string) {
-  await createUser(userName, password);
-  retrieveUsers();
-}
-
-function updateCurrentPage(newPage: number) {
+async function updateCurrentPage(newPage: number) {
   currentPage.value = newPage;
-  retrieveUsers();
+  await adminSettings.retrieveUsers(newPage);
 }
 
-async function retrieveUsers() {
-  const { newUsers, newUserCount } = await getUsers();
-  users.value = newUsers;
-  userCount.value = newUserCount;
-  const divided = userCount.value / LIMIT;
-  totalPages.value =
-    userCount.value % LIMIT > 0 ? Math.floor(divided) + 1 : divided;
-}
-
-async function removeUser(user: IUser) {
-  await deleteUser(user);
-  await retrieveUsers();
-}
-
-function editUser(user: IUser) {
+function openEditUserModal(user: IUser) {
   selectedUser.value = user;
   showEditUserModal.value = true;
 }
@@ -222,7 +198,7 @@ function showDeleteUserConfirmation(user: IUser) {
   showDeleteUserModal.value = true;
 }
 
-function manageTokens(user: IUser) {
+function openManageTokensModal(user: IUser) {
   selectedUser.value = user;
   showTokenModal.value = true;
 }
