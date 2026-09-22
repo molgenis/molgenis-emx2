bde50c2493e8e36f372ccb1a3c781320584524b6
dcruvolo <davidruvolo51@gmail.com>
2026-09-22 10:25:09 +0200

    fix: updated cms loader test

--- backend/molgenis-emx2-datamodels/src/test/java/org/molgenis/emx2/datamodels/PagesTest.java
+++ backend/molgenis-emx2-datamodels/src/test/java/org/molgenis/emx2/datamodels/PagesTest.java
@@ -7,6 +7,6 @@ import org.junit.jupiter.api.Test;
 public class PagesTest extends TestLoaders {
   @Test
   public void pagesDemoTestLoader() {
-    assertEquals(25, pagesSchema.getTableNames().size());
+    assertEquals(27, pagesSchema.getTableNames().size());
   }
 }
