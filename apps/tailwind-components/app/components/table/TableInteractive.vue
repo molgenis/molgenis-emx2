cda2a7daa862c156c32c29513164ba3550c6469d
chinook25 <chinook25@gmail.com>
2026-09-23 10:56:34 +0200

    add pagination and search to table

--- apps/tailwind-components/app/components/table/TableInteractive.vue
+++ apps/tailwind-components/app/components/table/TableInteractive.vue
@@ -3,10 +3,44 @@ import Table from "../Table.vue";
 const props = defineProps<{
   columns: { id: string; label: string }[];
   rows: Record<string, unknown>[];
+  settings: {
+    search: string | undefined;
+    page: number;
+    pageSize: number;
+    totalRowCount: number;
+  };
 }>();
+
+const emit = defineEmits<{
+  (event: "update:settings", value: typeof props.settings): void;
+}>();
+
+function handlePagingRequest(page: number) {
+  emit("update:settings", { ...props.settings, page });
+}
+
+function handlePageSizeChange(pageSize: string) {
+  emit("update:settings", {
+    ...props.settings,
+    pageSize: Number.parseInt(pageSize),
+    page: 1,
+  });
+}
+
+function handleSearchChange(search?: string) {
+  emit("update:settings", { ...props.settings, search, page: 1 });
+}
 </script>
 
 <template>
+  <InputSearch
+    class="w-3/5 xl:w-2/5 2xl:w-1/5"
+    size="medium"
+    modelValue="settings.search"
+    @update:model-value="handleSearchChange($event)"
+    placeholder="Search roles"
+    id="search-input"
+  />
   <Table>
     <template #head>
       <TableHeadRow>
@@ -24,4 +58,14 @@ const props = defineProps<{
     </template>
     <template #foot> </template>
   </Table>
+  <Pagination
+    class="pt-0 pb-[30px]"
+    :current-page="settings.page"
+    :totalPages="Math.ceil(settings.totalRowCount / settings.pageSize)"
+    :jumpToEdge="true"
+    :pageSize="settings.pageSize"
+    :showPageSizeSelector="true"
+    @update="handlePagingRequest($event)"
+    @update:pageSize="handlePageSizeChange($event)"
+  />
 </template>
