4beca743e7dd3aed9a730fb58908f936b16b23ae
chinook25 <chinook25@gmail.com>
2026-09-23 10:09:47 +0200

    feat: add component

---
+++ apps/tailwind-components/app/components/table/TableInteractive.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Table from "../Table.vue";
+const props = defineProps<{
+  columns: { id: string; label: string }[];
+  rows: Record<string, unknown>[];
+}>();
+</script>
+
+<template>
+  <Table>
+    <template #head>
+      <TableHeadRow>
+        <TableHead v-for="column in columns" :key="column.id">
+          {{ column.label }}
+        </TableHead>
+      </TableHeadRow>
+    </template>
+    <template #body>
+      <TableRow v-for="row in rows" :key="`${row.schemaId}/${row.roleName}`">
+        <TableCell v-for="column in columns" :key="column.id">
+          {{ row[column.id] }}
+        </TableCell>
+      </TableRow>
+    </template>
+    <template #foot> </template>
+  </Table>
+</template>
