03dbf4e1a3e6a470c203006409890a3bfd7fb0ed
chinook25 <chinook25@gmail.com>
2026-09-24 12:37:45 +0200

    add table interactive story

---
+++ apps/tailwind-components/app/pages/table/Interactive.story.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import TableInteractive from "~/components/table/TableInteractive.vue";
+import type { ITableSettings } from "~~/types/types";
+
+const columns = [
+  { id: "name", label: "Name" },
+  { id: "email", label: "Email" },
+  { id: "role", label: "Role" },
+];
+const rows = [
+  { name: "John Doe", email: "john.doe@example.com", role: "User" },
+  { name: "Jane Smith", email: "jane.smith@example.com", role: "Admin" },
+];
+const settings: ITableSettings = {
+  page: 1,
+  pageSize: 10,
+  orderby: {
+    column: "name",
+    direction: "ASC",
+  },
+  orderedColumnsIds: [],
+};
+</script>
+
+<template>
+  <TableInteractive
+    :columns="columns"
+    :rows="rows"
+    :count="rows.length"
+    :settings="settings"
+    @update:settings="(value) => (settings = value)"
+  />
+</template>
